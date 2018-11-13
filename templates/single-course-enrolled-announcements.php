af092caf8f7c9a7e80e26c9726d19ae5a2fc7ae9
Delowar Hossain <delowar@Delowars-Mac-mini.local>
2018-11-13 10:42:43 +0600

    single page layout updated

--- templates/single-course-enrolled-announcements.php
+++ templates/single-course-enrolled-announcements.php
@@ -17,12 +17,20 @@ do_action('tutor_course/single/enrolled/before/wrap');
 <div <?php tutor_post_class('tutor-single-anouncement-wrap'); ?>>
     <div class="tutor-container">
         <div class="tutor-row">
-            <div class="tutor-col">
+            <div class="tutor-col-8">
                 <?php do_action('tutor_course/single/enrolled/before/wrap'); ?>
                 <?php tutor_course_enrolled_lead_info(); ?>
                 <?php tutor_course_enrolled_nav(); ?>
                 <?php tutor_course_announcements(); ?>
                 <?php do_action('tutor_course/single/enrolled/after/wrap'); ?>
+            </div>
+            <div class="tutor-col-4">
+                <div class="tutor-single-course-sidebar">
+                    <?php tutor_course_video(); ?>
+                    <?php tutor_course_requirements_html(); ?>
+                    <?php tutor_course_material_includes_html(); ?>
+                    <?php tutor_course_target_audience_html(); ?>
+                </div>
             </div>
         </div>
     </div>
--- templates/single-course-enrolled-overview.php
+++ templates/single-course-enrolled-overview.php
@@ -16,10 +16,18 @@ do_action('tutor_course/single/enrolled/before/wrap');
     <div <?php tutor_post_class('tutor-single-overview-wrap'); ?>>
         <div class="tutor-container">
             <div class="tutor-row">
-                <div class="tutor-col">
+                <div class="tutor-col-8">
                     <?php tutor_course_enrolled_lead_info(); ?>
                     <?php tutor_course_enrolled_nav(); ?>
-                    <?php get_tutor_posts_attachments(); ?>
+                    <?php tutor_course_content(); ?>
+                </div>
+                <div class="tutor-col-4">
+                    <div class="tutor-single-course-sidebar">
+                        <?php tutor_course_video(); ?>
+                        <?php tutor_course_requirements_html(); ?>
+                        <?php tutor_course_material_includes_html(); ?>
+                        <?php tutor_course_target_audience_html(); ?>
+                    </div>
                 </div>
             </div>
         </div>
