19143a4d9caac51f668b125f9054c07f32a700ae
MhShohel <mhshohel89@gmail.com>
2018-11-15 14:50:04 +0600

    fixing the enrolled content

--- templates/single/course/course-enrolled.php
+++ templates/single/course/course-enrolled.php
@@ -12,21 +12,36 @@ if ( ! defined( 'ABSPATH' ) )
 	exit;
 ?>
 
-<div class="tutor-single-course-segment  tutor-course-enrolled-wrap">
-    <h><?php _e('Enrolled', 'tutor'); ?></h>
-
-    <p>
-        <?php
-        $enrolled = tutor_utils()->is_enrolled();
-        _e(sprintf("Enrolled at : %s", date(get_option('date_format'), strtotime($enrolled->post_date)) ), 'tutor');
-        ?>
-    </p>
-
-	<?php
-	$lesson_url = tutor_utils()->get_course_first_lesson();
-	if ($lesson_url){
+<div class="tutor-price-preview-box">
+    <div class="tutor-price-box-thumbnail">
+		<?php
+		if(tutor_utils()->has_video_in_single()){
+			tutor_course_video();
+		} else{
+			get_tutor_course_thumbnail();
+		}
 		?>
-        <a href="<?php echo $lesson_url; ?>" class="tutor-button"><?php _e('Start Course', 'tutor'); ?></a>
-	<?php } ?>
+    </div>
 
-</div>
+	<?php tutor_course_material_includes_html(); ?>
+
+
+    <div class="tutor-single-course-segment  tutor-course-enrolled-wrap">
+        <h><?php _e('Enrolled', 'tutor'); ?></h>
+        <p>
+			<?php
+			$enrolled = tutor_utils()->is_enrolled();
+			_e(sprintf("Enrolled at : %s", date(get_option('date_format'), strtotime($enrolled->post_date)) ), 'tutor');
+			?>
+        </p>
+		<?php
+		$lesson_url = tutor_utils()->get_course_first_lesson();
+		if ($lesson_url){
+			?>
+            <a href="<?php echo $lesson_url; ?>" class="tutor-button"><?php _e('Start Course', 'tutor'); ?></a>
+		<?php } ?>
+
+    </div>
+
+</div> <!-- tutor-price-preview-box -->
+
