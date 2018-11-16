edd00d3eca989a91e7b0bc6381d81c41db54c025
MhShohel <mhshohel89@gmail.com>
2018-11-16 18:41:46 +0600

    Fixed an auncaught fatal error at profile enrolled courses

--- templates/profile/enrolled_course.php
+++ templates/profile/enrolled_course.php
@@ -3,19 +3,14 @@ $user_name = sanitize_text_field(get_query_var('tutor_student_username'));
 $get_user = tutor_utils()->get_user_by_login($user_name);
 $user_id = $get_user->ID;
 
-
 $my_courses = tutor_utils()->get_enrolled_courses_by_user($user_id);
 ?>
 
 <div class="tutor-courses <?php tutor_container_classes() ?>">
-
-
 	<?php
-	if ($my_courses->have_posts()):
-
+	if ($my_courses && $my_courses->have_posts()):
 		while ($my_courses->have_posts()):
 			$my_courses->the_post();
-
 			/**
 			 * @hook tutor_course/archive/before_loop_course
 			 * @type action
@@ -33,13 +28,8 @@ $my_courses = tutor_utils()->get_enrolled_courses_by_user($user_id);
 			do_action('tutor_course/archive/after_loop_course');
 
 		endwhile;
-
 		wp_reset_postdata();
-
 	endif;
-
 	?>
-
-
 </div>
 
