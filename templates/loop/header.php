b0fbf964f0d53a145238d72c7e78fb7c56d9b6b0
Delowar Hossain <delowar@Delowars-Mac-mini.local>
2018-11-21 17:13:22 +0600

    Changing/modifiying some css changes

--- templates/loop/header.php
+++ templates/loop/header.php
@@ -1,5 +1,10 @@
 <div class="tutor-course-header">
 	<?php
-	tutor_course_loop_thumbnail();
+	    tutor_course_loop_thumbnail();
 	?>
+    <div class="tutor-course-loop-header-meta">
+        <?php
+            echo '<span class="tutor-course-loop-level">'.get_tutor_course_level().'</span>';
+        ?>
+    </div>
 </div>
