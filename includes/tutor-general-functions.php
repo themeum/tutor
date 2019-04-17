f298aefec742fc9b280bcda9577e45f2a9e0b83f
MhShohel <mhshohel89@gmail.com>
2019-04-17 18:51:15 +0600

    Adding withdraw, myprofile, add withdra method

--- includes/tutor-general-functions.php
+++ includes/tutor-general-functions.php
@@ -1,4 +1,8 @@
 <?php
+
+if ( ! defined( 'ABSPATH' ) )
+	exit;
+
 /**
  * Tutor general Functions
  */
--- templates/dashboard/withdraw-method-fields/text.php
+++ templates/dashboard/withdraw-method-fields/text.php
@@ -1,2 +1,2 @@
 
-<input type="text" name="withdraw_method_field[<?php echo $method_id ?>][<?php echo $field_name ?>]">
+<input type="text" name="withdraw_method_field[<?php echo $method_id ?>][<?php echo $field_name ?>]" value="<?php echo $old_value; ?>" >
