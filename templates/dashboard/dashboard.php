02988c93427d3f229627706f26c0968b2c7fe91f
Delowar Hossain <delowar@Delowars-Mac-mini.local>
2019-04-26 15:50:54 +0600

    dashboard page updated

--- templates/dashboard/dashboard.php
+++ templates/dashboard/dashboard.php
@@ -1,22 +1,64 @@
-<h1>Dashboard</h1>
+<h1><?php _e('Dashboard', 'tutor') ?></h1>
 
 <div class="tutor-dashboard-content-inner">
-    <div class='tutor-mycourse-wrap'>
-        <?php
+
+    <?php
+        $enrolled_course = tutor_utils()->get_enrolled_courses_by_user();
         $completed_courses = tutor_utils()->get_completed_courses_ids_by_user();
-        $my_courses = tutor_utils()->get_enrolled_courses_by_user();
+        $total_students = tutor_utils()->get_total_students_by_instructor(get_current_user_id());
+        $my_courses = tutor_utils()->get_courses_by_instructor(get_current_user_id(), 'any');
+        $earning_sum = tutor_utils()->get_earning_sum();
 
-        $completed_courses_count = count($completed_courses);
-        $my_courses_count = $my_courses? $my_courses->post_count : 0;
-        $active_course_count = ($my_courses_count - $completed_courses_count);
+        $enrolled_course_count = $enrolled_course->post_count;
+        $completed_course_count = count($completed_courses);
+        $active_course_count = $enrolled_course_count - $completed_course_count;
 
+    ?>
 
-        echo __('My Course : ', 'tutor') . $my_courses_count . "<br />";
-        echo __('Active Course : ', 'tutor') . $active_course_count . "<br />";
-        echo __('Complete Course : ', 'tutor') . $completed_courses_count . "<br />";
+    <ul class="tutor-dashboard-info-cards">
+        <li>
+            <p>
+                <?php _e('Enrolled Course', 'tutor'); ?>
+                <span><?php echo esc_html($enrolled_course_count); ?></span>
+            </p>
+        </li>
+        <li>
+            <p>
+                <?php _e('Active Course', 'tutor'); ?>
+                <span><?php echo esc_html($active_course_count); ?></span>
+            </p>
+        </li>
+        <li>
+            <p>
+                <?php _e('Completed Course', 'tutor'); ?>
+                <span><?php echo esc_html($completed_course_count); ?></span>
+            </p>
+        </li>
 
-
+        <?php
+            if(current_user_can(tutor()->instructor_role)) :
         ?>
 
-    </div>
+        <li>
+            <p>
+                <?php _e('Total Students', 'tutor'); ?>
+                <span><?php echo esc_html($total_students); ?></span>
+            </p>
+        </li>
+        <li>
+            <p>
+                <?php _e('Total Courses', 'tutor'); ?>
+                <span><?php echo esc_html(count($my_courses)); ?></span>
+            </p>
+        </li>
+        <li>
+            <p>
+                <?php _e('Total Earning', 'tutor'); ?>
+                <span><?php echo tutor_utils()->tutor_price($earning_sum->instructor_amount); ?></span>
+            </p>
+        </li>
+        <?php
+            endif;
+        ?>
+    </ul>
 </div>
