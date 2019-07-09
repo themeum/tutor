32ca450f4e13a4edea5fb22e4c30db5511f02c83
MhShohel <mhshohel89@gmail.com>
2019-07-09 17:26:43 +0600

    Added Course builder

--- classes/Dashboard.php
+++ classes/Dashboard.php
@@ -8,9 +8,7 @@
  * @since v.1.3.4
  */
 
-
 namespace TUTOR;
-
 
 if ( ! defined( 'ABSPATH' ) )
 	exit;
@@ -39,7 +37,6 @@ class Dashboard {
 			/**
 			 * Get course which currently in edit, or insert new course
 			 */
-
 			$course_ID = (int) sanitize_text_field(tutor_utils()->array_get('course_ID', $_GET));
 
 			if ($course_ID){
@@ -52,7 +49,6 @@ class Dashboard {
 			$post = get_post( $post_id );
 			setup_postdata( $post );
 		}
-
 	}
 
 	public function tutor_load_template_after(){
@@ -62,7 +58,6 @@ class Dashboard {
 		if ($tutor_dashboard_page === 'create-course'){
 			wp_reset_query();
 		}
-
 	}
 
 	/**
@@ -82,9 +77,85 @@ class Dashboard {
 		$post = get_post($post_ID);
 		$update = true;
 
+		/**
+		 * Update the post
+		 */
+
+		$content = wp_kses_post(tutor_utils()->array_get('content', $_POST));
+		$title = sanitize_text_field(tutor_utils()->array_get('title', $_POST));
+		$tax_input = tutor_utils()->array_get('tax_input', $_POST);
+
+		$postData = array(
+			'ID'            => $post_ID,
+			'post_title'    => $title,
+			'post_name'     => sanitize_title($title),
+			'post_content'  => $content,
+		);
+
+		//Publish or Pending...
+		if (tutor_utils()->array_get('course_submit_btn', $_POST) === 'save_course_as_draft'){
+			$postData['post_status'] = 'draft';
+		}else{
+			$can_publish_course = (bool) tutor_utils()->get_option('instructor_can_publish_course');
+			if ($can_publish_course){
+				$postData['post_status'] = 'publish';
+			}else{
+				$postData['post_status'] = 'pending';
+			}
+		}
+
+		wp_update_post($postData);
+
+		/**
+		 * Setting Thumbnail
+		 */
+		$_thumbnail_id = (int) sanitize_text_field(tutor_utils()->array_get('tutor_course_thumbnail_id', $_POST));
+		if ($_thumbnail_id){
+			update_post_meta($post_ID, '_thumbnail_id', $_thumbnail_id);
+		}else{
+			delete_post_meta($post_ID, '_thumbnail_id');
+		}
+
+		/**
+		 * Adding taxonomy
+		 */
+		if ( tutor_utils()->count($tax_input) ) {
+			foreach ( $tax_input as $taxonomy => $tags ) {
+				$taxonomy_obj = get_taxonomy($taxonomy);
+				if ( ! $taxonomy_obj ) {
+					/* translators: %s: taxonomy name */
+					_doing_it_wrong( __FUNCTION__, sprintf( __( 'Invalid taxonomy: %s.' ), $taxonomy ), '4.4.0' );
+					continue;
+				}
+
+				// array = hierarchical, string = non-hierarchical.
+				if ( is_array( $tags ) ) {
+					$tags = array_filter($tags);
+				}
+				wp_set_post_terms( $post_ID, $tags, $taxonomy );
+			}
+		}
+
+		/**
+		 * Adding support for do_action();
+		 */
 		do_action( "save_post_{$course_post_type}", $post_ID, $post, $update );
 		do_action( 'save_post', $post_ID, $post, $update );
+		do_action( 'save_tutor_course', $post_ID, $postData);
 
+		/**
+		 * If update request not comes from edit page, redirect it to edit page
+		 */
+		$edit_mode = (int) sanitize_text_field(tutor_utils()->array_get('course_ID', $_GET));
+		if ( ! $edit_mode){
+			$edit_page_url = add_query_arg(array('course_ID' => $post_ID));
+			wp_redirect($edit_page_url);
+			die();
+		}
+
+		/**
+		 * Finally redirect it to previous page to avoid multiple post request
+		 */
 		wp_redirect(tutor_utils()->referer());
 		die();
 	}
--- templates/dashboard/create-course.php
+++ templates/dashboard/create-course.php
@@ -9,94 +9,136 @@ $course_id = get_the_ID();
 
 <?php do_action('tutor/dashboard_course_builder_before'); ?>
 
-<form action="" method="post" enctype="multipart/form-data">
+    <form action="" method="post" enctype="multipart/form-data">
 
-	<?php wp_nonce_field( tutor()->nonce_action, tutor()->nonce ); ?>
-    <input type="hidden" value="tutor_add_course_builder" name="tutor_action"/>
-    <input type="hidden" name="course_ID" id="course_ID" value="<?php echo get_the_ID(); ?>">
-    <input type="hidden" name="post_ID" id="post_ID" value="<?php echo get_the_ID(); ?>">
-
-
-    <div class="tutor-dashboard-course-builder-wrap">
-
-	    <?php do_action('tutor/dashboard_course_builder_form_field_before'); ?>
-
-        <div class="tutor-course-builder-section tutor-course-builder-info">
+		<?php wp_nonce_field( tutor()->nonce_action, tutor()->nonce ); ?>
+        <input type="hidden" value="tutor_add_course_builder" name="tutor_action"/>
+        <input type="hidden" name="course_ID" id="course_ID" value="<?php echo get_the_ID(); ?>">
+        <input type="hidden" name="post_ID" id="post_ID" value="<?php echo get_the_ID(); ?>">
 
 
-            <div class="tutor-form-row">
-                <div class="tutor-form-col-12">
-                    <div class="tutor-form-group">
-                        <label>
-							<?php _e('Course Title', 'tutor'); ?>
-                        </label>
+        <div class="tutor-dashboard-course-builder-wrap">
 
-                        <input type="text" name="title" value="<?php echo get_the_title(); ?>" placeholder="<?php _e('ex. Learn photoshop CS6 from scratch', 'tutor'); ?>">
+			<?php do_action('tutor/dashboard_course_builder_form_field_before'); ?>
+
+            <div class="tutor-course-builder-section tutor-course-builder-info">
+
+
+                <div class="tutor-form-row">
+                    <div class="tutor-form-col-12">
+                        <div class="tutor-form-group">
+                            <label>
+								<?php _e('Course Title', 'tutor'); ?>
+                            </label>
+
+                            <input type="text" name="title" value="<?php echo get_the_title(); ?>" placeholder="<?php _e('ex. Learn photoshop CS6 from scratch', 'tutor'); ?>">
+                        </div>
                     </div>
+
                 </div>
 
-            </div>
+                <div class="tutor-form-row">
+                    <div class="tutor-form-col-12">
+                        <div class="tutor-form-group">
+                            <label>
+								<?php _e('Description', 'tutor'); ?>
+                            </label>
 
-            <div class="tutor-form-row">
-                <div class="tutor-form-col-12">
-                    <div class="tutor-form-group">
-                        <label>
-							<?php _e('Description', 'tutor'); ?>
-                        </label>
-
-						<?php
-						$editor_settings = array(
-							'media_buttons' => false,
-							'quicktags'     => false,
-							'editor_height' => 150,
-							'textarea_name' => 'content'
-						);
-						wp_editor($post->post_content, 'course_description', $editor_settings);
-						?>
-                    </div>
-                </div>
-            </div>
-
-            <div class="tutor-form-row">
-                <div class="tutor-form-col-12">
-                    <div class="tutor-form-group">
-                        <label>
-							<?php _e('Choose a category', 'tutor'); ?>
-                        </label>
-                        <div class="tutor-form-field-course-categories">
-							<?php echo tutor_course_categories_checkbox(); ?>
+							<?php
+							$editor_settings = array(
+								'media_buttons' => false,
+								'quicktags'     => false,
+								'editor_height' => 150,
+								'textarea_name' => 'content'
+							);
+							wp_editor($post->post_content, 'course_description', $editor_settings);
+							?>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="tutor-form-row">
-                <div class="tutor-form-col-12">
-                    <div class="tutor-form-group">
-                        <label>
-							<?php _e('Course Thumbnail', 'tutor'); ?>
-                        </label>
+                <div class="tutor-form-row">
+                    <div class="tutor-form-col-12">
+                        <div class="tutor-form-group">
+                            <label>
+								<?php _e('Choose a category', 'tutor'); ?>
+                            </label>
+                            <div class="tutor-form-field-course-categories">
+								<?php echo tutor_course_categories_checkbox($course_id); has_category(); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-                        <div class="tutor-form-field tutor-form-field-course-thumbnail tutor-thumbnail-wrap">
+                <div class="tutor-form-row">
+                    <div class="tutor-form-col-12">
+                        <div class="tutor-form-group">
+                            <label>
+								<?php _e('Course Thumbnail', 'tutor'); ?>
+                            </label>
 
-                            <div class="tutor-row">
-                                <div class="tutor-col-4">
-                                    <div class="builder-course-thumbnail-img-src">
-										<?php $builder_course_img_src = tutor_placeholder_img_src(); ?>
-                                        <img src="<?php echo $builder_course_img_src; ?>" class="thumbnail-img" data-placeholder-src="<?php echo $builder_course_img_src; ?>">
+                            <div class="tutor-form-field tutor-form-field-course-thumbnail tutor-thumbnail-wrap">
+
+                                <div class="tutor-row">
+                                    <div class="tutor-col-4">
+                                        <div class="builder-course-thumbnail-img-src">
+											<?php
+
+											$builder_course_img_src = tutor_placeholder_img_src();
+											$_thumbnail_url = get_the_post_thumbnail_url($course_id);
+											$post_thumbnail_id = get_post_thumbnail_id( $course_id );
+
+											if ( ! $_thumbnail_url){
+												$_thumbnail_url = $builder_course_img_src;
+											}
+											?>
+                                            <img src="<?php echo $_thumbnail_url; ?>" class="thumbnail-img" data-placeholder-src="<?php echo $builder_course_img_src; ?>">
+                                        </div>
+                                    </div>
+
+                                    <div class="tutor-col-8">
+                                        <div class="builder-course-thumbnail-upload-wrap">
+                                            <a href="javascript:;" class="tutor-course-thumbnail-delete-btn"><i class="tutor-icon-garbage"></i> </a>
+                                            <input type="hidden" id="tutor_course_thumbnail_id" name="tutor_course_thumbnail_id" value="<?php echo $post_thumbnail_id; ?>">
+                                            <a href="javascript:;" class="tutor-course-thumbnail-upload-btn"><?php _e('Upload Image', 'tutor'); ?></a>
+                                        </div>
                                     </div>
                                 </div>
 
-                                <div class="tutor-col-8">
-                                    <div class="builder-course-thumbnail-upload-wrap">
-                                        <a href="javascript:;" class="tutor-course-thumbnail-delete-btn"><i class="tutor-icon-garbage"></i> </a>
-                                        <input type="hidden" id="tutor_course_thumbnail_id" name="tutor_course_thumbnail_id" value="">
-                                        <a href="javascript:;" class="tutor-course-thumbnail-upload-btn"><?php _e('Upload Image', 'tutor'); ?></a>
-                                    </div>
-                                </div>
                             </div>
 
                         </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+			<?php do_action('tutor/dashboard_course_builder_form_field_after'); ?>
+
+            <div class="tutor-form-row">
+                <div class="tutor-form-col-12">
+                    <div class="tutor-form-group">
+                        <div class="tutor-form-field">
+
+                            <button type="submit" name="course_submit_btn" value="save_course_as_draft"><?php _e('Save course as draft', 'tutor');
+                            ?></button>
+
+							<?php
+							$can_publish_course = (bool) tutor_utils()->get_option('instructor_can_publish_course');
+							if ($can_publish_course){
+								?>
+                                <button type="submit" name="course_submit_btn" value="publish_course"><?php _e('Publish Course', 'tutor'); ?></button>
+								<?php
+							}else{
+								?>
+                                <button type="submit" name="course_submit_btn" value="submit_for_review"><?php _e('Submit for Review', 'tutor');
+                                ?></button>
+								<?php
+							}
+							?>
+
+                        </div>
 
                     </div>
                 </div>
@@ -104,22 +146,6 @@ $course_id = get_the_ID();
 
         </div>
 
-
-        <?php do_action('tutor/dashboard_course_builder_form_field_after'); ?>
-
-        <div class="tutor-form-row">
-            <div class="tutor-form-col-12">
-                <div class="tutor-form-group">
-                    <div class="tutor-form-field">
-                        <button type="submit"><?php _e('Save Course', 'tutor'); ?></button>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-
-    </div>
-
-</form>
+    </form>
 
 <?php do_action('tutor/dashboard_course_builder_after'); ?>
