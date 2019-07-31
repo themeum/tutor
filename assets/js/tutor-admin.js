36ce61955a0a8e9e0d01d8720b607eef960aaf24
Delowar Hossain <delowar@Delowars-Mac-mini.local>
2019-07-31 12:09:20 +0600

    profile photo upload on backend issue

--- assets/js/tutor-admin.js
+++ assets/js/tutor-admin.js
@@ -1738,6 +1738,43 @@ jQuery(document).ready(function($){
         $(this).closest('.tutor-individual-attachment-file').remove();
     });
 
+    /**
+     * Used for backend profile photo upload.
+     */
+
+    //tutor_video_poster_upload_btn
+    $(document).on( 'click', '.tutor_video_poster_upload_btn',  function( event ){
+        event.preventDefault();
+
+        var $that = $(this);
+        var frame;
+        // If the media frame already exists, reopen it.
+        if ( frame ) {
+            frame.open();
+            return;
+        }
+
+        // Create a new media frame
+        frame = wp.media({
+            title: 'Select or Upload Media Of Your Chosen Persuasion',
+            button: {
+                text: 'Use this media'
+            },
+            multiple: false  // Set to true to allow multiple files to be selected
+        });
+
+        // When an image is selected in the media frame...
+        frame.on( 'select', function() {
+            // Get media attachment details from the frame state
+            var attachment = frame.state().get('selection').first().toJSON();
+            $that.closest('.tutor-video-poster-wrap').find('.video-poster-img').html('<img src="'+attachment.url+'" alt="" />');
+            $that.closest('.tutor-video-poster-wrap').find('input').val(attachment.id);
+        });
+        // Finally, open the modal on click
+        frame.open();
+    });
+
+
 
 
 });
