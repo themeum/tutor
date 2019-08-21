c25a707d4eb76f8bfa08a47277285d46a4573e6a
Delowar Hosain <delowar6494@gmail.com>
2019-08-21 12:30:13 +0600

    quiz spotlight mode issue fixed

--- templates/single-quiz.php
+++ templates/single-quiz.php
@@ -8,7 +8,7 @@
  * @url https://themeum.com
  */
 
-get_header();
+get_tutor_header();
 
 $course = tutor_utils()->get_course_by_quiz(get_the_ID());
 ?>
@@ -21,16 +21,13 @@ $course = tutor_utils()->get_course_by_quiz(get_the_ID());
         <div class="tutor-lesson-sidebar">
 		    <?php tutor_lessons_sidebar(); ?>
         </div>
-
-
         <div id="tutor-single-entry-content" class="tutor-quiz-single-entry-wrap tutor-single-entry-content">
             <input type="hidden" name="tutor_quiz_id" id="tutor_quiz_id" value="<?php the_ID(); ?>">
-
             <div class="tutor-single-page-top-bar">
                 <div class="tutor-topbar-item tutor-hide-sidebar-bar">
                     <a href="javascript:;" class="tutor-lesson-sidebar-hide-bar"><i class="tutor-icon-menu-2"></i> </a>
-                    <a href="<?php echo get_the_permalink($course->ID); ?>">
-                        <i class="tutor-icon-next-2"></i> <?php echo sprintf(__('Go to %s Course Home %s', 'tutor'), '<strong>', '</strong>') ; ?>
+                    <a href="<?php echo get_the_permalink($course->ID); ?>"  class="tutor-topbar-home-btn">
+                        <i class="tutor-icon-next-2"></i> <?php echo __('Go to Course Home', 'tutor') ; ?>
                     </a>
                 </div>
                 <div class="tutor-topbar-item tutor-topbar-content-title-wrap">
@@ -41,9 +38,6 @@ $course = tutor_utils()->get_course_by_quiz(get_the_ID());
 
                 <div class="tutor-topbar-item tutor-topbar-mark-to-done" style="width: 150px;"></div>
             </div>
-
-
-
             <div class="tutor-quiz-single-wrap ">
                 <input type="hidden" name="tutor_quiz_id" id="tutor_quiz_id" value="<?php the_ID(); ?>">
 
@@ -62,4 +56,4 @@ $course = tutor_utils()->get_course_by_quiz(get_the_ID());
 
 <?php
 
-get_footer();
+get_tutor_footer();
--- templates/single/quiz/single_quiz_contents.php
+++ templates/single/quiz/single_quiz_contents.php
@@ -28,8 +28,8 @@ $course = tutor_utils()->get_course_by_quiz(get_the_ID());
 <div class="tutor-single-page-top-bar">
     <div class="tutor-topbar-item tutor-hide-sidebar-bar">
         <a href="javascript:;" class="tutor-lesson-sidebar-hide-bar"><i class="tutor-icon-menu-2"></i> </a>
-        <a href="<?php echo get_the_permalink($course->ID); ?>">
-            <i class="tutor-icon-next-2"></i> <?php echo sprintf(__('Go to %s Course Home %s', 'tutor'), '<strong>', '</strong>') ; ?>
+        <a href="<?php echo get_the_permalink($course->ID); ?>"  class="tutor-topbar-home-btn">
+            <i class="tutor-icon-next-2"></i> <?php echo __('Go to Course Home', 'tutor') ; ?>
         </a>
     </div>
     <div class="tutor-topbar-item tutor-topbar-content-title-wrap">
