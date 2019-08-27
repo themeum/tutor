21d63eefb551bd3643328bbc5f80b44105dae2d6
MhShohel <mhshohel89@gmail.com>
2019-08-27 13:00:50 +0600

    Fixing htmls space

--- templates/dashboard/dashboard.php
+++ templates/dashboard/dashboard.php
@@ -2,17 +2,17 @@
 
 <div class="tutor-dashboard-content-inner">
 
-    <?php
-        $enrolled_course = tutor_utils()->get_enrolled_courses_by_user();
-        $completed_courses = tutor_utils()->get_completed_courses_ids_by_user();
-        $total_students = tutor_utils()->get_total_students_by_instructor(get_current_user_id());
-        $my_courses = tutor_utils()->get_courses_by_instructor(get_current_user_id(), 'any');
-        $earning_sum = tutor_utils()->get_earning_sum();
+	<?php
+	$enrolled_course = tutor_utils()->get_enrolled_courses_by_user();
+	$completed_courses = tutor_utils()->get_completed_courses_ids_by_user();
+	$total_students = tutor_utils()->get_total_students_by_instructor(get_current_user_id());
+	$my_courses = tutor_utils()->get_courses_by_instructor(get_current_user_id(), 'any');
+	$earning_sum = tutor_utils()->get_earning_sum();
 
-        $enrolled_course_count = $enrolled_course ? $enrolled_course->post_count : 0;
-        $completed_course_count = count($completed_courses);
-        $active_course_count = $enrolled_course_count - $completed_course_count;
-    ?>
+	$enrolled_course_count = $enrolled_course ? $enrolled_course->post_count : 0;
+	$completed_course_count = count($completed_courses);
+	$active_course_count = $enrolled_course_count - $completed_course_count;
+	?>
 
     <div class="tutor-dashboard-info-cards">
         <div class="tutor-dashboard-info-card">
@@ -34,66 +34,63 @@
             </p>
         </div>
 
-        <?php
-            if(current_user_can(tutor()->instructor_role)) :
-        ?>
-
-        <div class="tutor-dashboard-info-card">
-            <p>
-                <span><?php _e('Total Students', 'tutor'); ?></span>
-                <span class="tutor-dashboard-info-val"><?php echo esc_html($total_students); ?></span>
-            </p>
-        </div>
-        <div class="tutor-dashboard-info-card">
-            <p>
-                <span><?php _e('Total Courses', 'tutor'); ?></span>
-                <span class="tutor-dashboard-info-val"><?php echo esc_html(count($my_courses)); ?></span>
-            </p>
-        </div>
-        <div class="tutor-dashboard-info-card">
-            <p>
-                <span><?php _e('Total Earning', 'tutor'); ?></span>
-                <span class="tutor-dashboard-info-val"><?php echo tutor_utils()->tutor_price($earning_sum->instructor_amount); ?></span>
-            </p>
-        </div>
-        <?php
-            endif;
-        ?>
+		<?php
+		if(current_user_can(tutor()->instructor_role)) :
+			?>
+            <div class="tutor-dashboard-info-card">
+                <p>
+                    <span><?php _e('Total Students', 'tutor'); ?></span>
+                    <span class="tutor-dashboard-info-val"><?php echo esc_html($total_students); ?></span>
+                </p>
+            </div>
+            <div class="tutor-dashboard-info-card">
+                <p>
+                    <span><?php _e('Total Courses', 'tutor'); ?></span>
+                    <span class="tutor-dashboard-info-val"><?php echo esc_html(count($my_courses)); ?></span>
+                </p>
+            </div>
+            <div class="tutor-dashboard-info-card">
+                <p>
+                    <span><?php _e('Total Earning', 'tutor'); ?></span>
+                    <span class="tutor-dashboard-info-val"><?php echo tutor_utils()->tutor_price($earning_sum->instructor_amount); ?></span>
+                </p>
+            </div>
+		<?php
+		endif;
+		?>
     </div>
 
-    <?php
-        $instructor_course = tutor_utils()->get_courses_for_instructors(get_current_user_id());
-        if(count($instructor_course)) {
-    ?>
-
-    <div class="tutor-dashboard-info-table-wrap">
-        <h3><?php _e('Most Popular Courses', 'tutor'); ?></h3>
-        <table class="tutor-dashboard-info-table">
-            <thead>
-            <tr>
-                <td><?php _e('Course Name', 'tutor'); ?></td>
-                <td><?php _e('Enrolled', 'tutor'); ?></td>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            $instructor_course = tutor_utils()->get_courses_for_instructors(get_current_user_id());
-            foreach ($instructor_course as $course){
-	            $enrolled = tutor_utils()->count_enrolled_users_by_course($course->ID);?>
-
+	<?php
+	$instructor_course = tutor_utils()->get_courses_for_instructors(get_current_user_id());
+	if(count($instructor_course)) {
+		?>
+        <div class="tutor-dashboard-info-table-wrap">
+            <h3><?php _e('Most Popular Courses', 'tutor'); ?></h3>
+            <table class="tutor-dashboard-info-table">
+                <thead>
                 <tr>
-                    <td>
-			            <a href="<?php echo get_the_permalink($course->ID); ?>" target="_blank"><?php echo $course->post_title; ?></a> <br />
-                        <small class="label-course-status label-course-<?php echo $course->post_status; ?>"> <?php echo $course->post_status; ?></small>
-                    </td>
-                    <td><?php echo $enrolled; ?></td>
+                    <td><?php _e('Course Name', 'tutor'); ?></td>
+                    <td><?php _e('Enrolled', 'tutor'); ?></td>
                 </tr>
-	            <?php
-            }
-            ?>
-            </tbody>
-        </table>
-    </div>
-    <?php } ?>
+                </thead>
+                <tbody>
+				<?php
+				$instructor_course = tutor_utils()->get_courses_for_instructors(get_current_user_id());
+				foreach ($instructor_course as $course){
+					$enrolled = tutor_utils()->count_enrolled_users_by_course($course->ID);?>
+                    <tr>
+                        <td>
+                            <a href="<?php echo get_the_permalink($course->ID); ?>" target="_blank"><?php echo $course->post_title; ?></a> <br />
+                            <small class="label-course-status label-course-<?php echo $course->post_status; ?>"> <?php echo $course->post_status; ?></small>
+                        </td>
+                        <td><?php echo $enrolled; ?></td>
+                    </tr>
+					<?php
+				}
+				?>
+                </tbody>
+            </table>
+        </div>
+	<?php } ?>
 
 </div>
