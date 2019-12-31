29b529da2666a8fa58e5157678e3b86515db67ba
MhShohel <mhshohel89@gmail.com>
2019-12-31 12:31:36 +0600

    Adding courses api

--- classes/RestAPI.php
+++ classes/RestAPI.php
@@ -47,82 +47,66 @@ class RestAPI {
 			'count'     => '10',
 		), $_GET);
 
-/*
+		$limit = (int) $a['count'];
+		$exclude_ids_query = '';
+		$in_ids_query = '';
+		$tax_join = '';
+		$tax_where = '';
 
-		$wpdb->query("SELECT SQL_CALC_FOUND_ROWS  wp_posts.ID 
-FROM wp_posts  
-LEFT JOIN wp_term_relationships ON (wp_posts.ID = wp_term_relationships.object_id) WHERE 1=1  AND ( 
-  wp_term_relationships.term_taxonomy_id IN (19,31,32)
-) AND wp_posts.post_type = 'courses' AND ((wp_posts.post_status = 'publish')) GROUP BY wp_posts.ID ORDER BY wp_posts.ID DESC LIMIT 0, 10");
-*/
-
-
-		$tax = new \WP_Tax_Query(array(array(
-			'taxonomy' => 'course-category',
-			'field'    => 'term_id',
-			'terms'    => 19,
-			'operator' => 'IN',
-		)));
-
-		print_r($tax->get_sql($wpdb->posts, 'ID'));
-
-		//die(print_r($a));
+		/**
+		 * Exclude Course IDS
+		 */
+		if ( ! empty($a['exclude_ids'])){
+			$exclude_ids = (array) explode(',', sanitize_text_field($a['exclude_ids']));
+			if (tutils()->count($exclude_ids)){
+				$exclude_ids_query = "AND ID NOT IN('$exclude_ids')";
+			}
+		}
 
 		if ( ! empty($a['id'])){
 			$ids = (array) explode(',', $a['id']);
-			$a['post__in'] = $ids;
+			if (tutils()->count($ids)){
+				$in_ids_query = "AND ID IN('$ids')";
+			}
 		}
 
-		if ( ! empty($a['exclude_ids'])){
-			$exclude_ids = (array) explode(',', $a['exclude_ids']);
-			$a['post__not_in'] = $exclude_ids;
-		}
 		if ( ! empty($a['category'])){
 			$category = (array) explode(',', $a['category']);
-
-			$a['tax_query'] = array(
+			$tax = new \WP_Tax_Query(
 				array(
-					'taxonomy' => 'course-category',
-					'field'    => 'term_id',
-					'terms'    => $category,
-					'operator' => 'IN',
-				),
+					array(
+						'taxonomy' => 'course-category',
+						'field'    => 'term_id',
+						'terms'    => $category,
+						'operator' => 'IN',
+					)
+				)
 			);
-		}
-		$a['posts_per_page'] = (int) $a['count'];
 
-
-		$query = new \WP_Query($a);
-
-		die();
-
-
-
-		if ($query->have_posts()){
-
-			$courses = array();
-			while ($query->have_posts()){
-				$query->the_post();
-				global $post;
-
-				$post = (array) $post;
-				$post = wp_array_slice_assoc($post, array('ID', 'post_author', 'post_date', 'post_date_gmt', 'post_content', 'post_title', 'post_excerpt', 'post_status', 'post_type' ));
-
-				//print_r($post);
-
-				//tutils()->get_courses()
-
-
-				$courses[] = $post;
-			}
-			wp_send_json_success($courses);
-
-		}else{
-			wp_send_json_error();
+			$tax_sql = $tax->get_sql($wpdb->posts, 'ID');
+			$tax_join = tutils()->array_get('join', $tax_sql);
+			$tax_where = tutils()->array_get('where', $tax_sql);
 		}
 
 
+		$course_post_type = tutor()->course_post_type;
+		$query = $wpdb->get_results("SELECT ID, post_author, post_title, post_name,post_status, menu_order 
+			from {$wpdb->posts} 
+			
+			{$tax_join}
+			
+			WHERE 1=1 AND post_status = 'publish'
+			{$exclude_ids_query}
+			{$in_ids_query}
+			{$tax_where}
+			AND post_type = '{$course_post_type}' LIMIT {$limit} ", ARRAY_A);
 
+
+		if (tutils()->count($query)){
+			$results = apply_filters('tutor/api/get_courses', $query);
+			wp_send_json_success($results);
+		}
+		wp_send_json_error();
 
 
 	}
