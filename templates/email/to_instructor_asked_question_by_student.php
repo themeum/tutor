c2f6cec5944bad250546b4ffed8cfaa36f918027
Sk Saju <sksaaju@gmail.com>
2020-06-08 12:57:43 +0600

    fixed qa notification issue

--- templates/email/to_instructor_asked_question_by_student.php
+++ templates/email/to_instructor_asked_question_by_student.php
@@ -9,7 +9,7 @@
 <p> Dear {instructor_username}, </p>
 
 <p>
-	Mr. {student_username} asked a question on <strong>{course_name}</strong> at <strong>{enroll_time}</strong>. The course URL is: <strong>{course_url}</strong>. You will find the question below.
+	{student_username} asked a question on <strong>{course_name}</strong> at <strong>{enroll_time}</strong>. The reply URL is: <strong>{course_url}</strong>. You will find the question below.
 </p>
 
 <br />
