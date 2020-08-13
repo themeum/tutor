8f6f04d15d43fb4367bb13e35a46f893f5ae0f19
sksaju <sksaaju@gmail.com>
2020-08-13 19:00:27 +0600

    add new instructor signup email template

---
+++ templates/email/to_admin_new_instructor_signup.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package TutorLMS/Templates
+ * @since 1.6.9
+ */
+
+?>
+
+<p> Dear Admin, </p>
+
+<p>
+	<strong>{instructor_username}</strong> has recently signup at <strong>{signup_time}</strong>.
+</p>
