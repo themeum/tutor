73803b8f4c166cc3b5653858cd0dc13156269bb1
JK <jayedulk@gmail.com>
2020-09-30 12:12:18 +0600

    Disabled course complete button for imported Google Class.

--- templates/single/course/complete_form.php
+++ templates/single/course/complete_form.php
@@ -17,7 +17,9 @@ if ( ! defined( 'ABSPATH' ) )
 do_action('tutor_course/single/before/complete_form');
 
 $is_completed_course = tutor_utils()->is_completed_course();
-if ( ! $is_completed_course) {
+$enable_state = apply_filters('is_tutor_course_complete_button_enabled', array('course_id'=>get_the_ID(), 'state'=>true));
+
+if ( ! $is_completed_course && $enable_state['state']) {
 	?>
     <div class="tutor-course-compelte-form-wrap">
 
