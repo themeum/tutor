05133a575b680098cccac1de325985dcda8bb66e
shewa <shewa12kpi@gmail.com>
2020-09-30 21:40:34 +0600

    new folder created for restapi

--- classes/RestAPI.php
+++ classes/RestAPI.php
@@ -69,7 +69,7 @@ class RestAPI {
 				$className
 			);
 
-			$className = str_replace('TUTOR'.DIRECTORY_SEPARATOR, 'classes'.DIRECTORY_SEPARATOR, $className);
+			$className = str_replace('TUTOR'.DIRECTORY_SEPARATOR, 'restapi'.DIRECTORY_SEPARATOR, $className);
 			$file_name = $this->path.$className.'.php';
 
 			if (file_exists($file_name) ) {
@@ -299,84 +299,4 @@ class RestAPI {
 
 	} 
 
-
-	public function courses_api() {
-		global $wpdb;
-
-		$a = array_merge(array(
-			'post_type'     => $this->course_post_type,
-			'post_status'   => 'publish',
-			
-			'id'            => '',
-			'exclude_ids'   => '',
-			'category'      => '',
-
-			'orderby'       => 'ID',
-			'order'         => 'DESC',
-			'count'         => '10',
-		), $_GET);
-
-		$limit = (int) $a['count'];
-		$exclude_ids_query = '';
-		$in_ids_query = '';
-		$tax_join = '';
-		$tax_where = '';
-
-		$orderby = sanitize_text_field($a['orderby']);
-		$order = sanitize_text_field($a['order']);
-
-		/**
-		 * Exclude Course IDS
-		 */
-		if (!empty($a['exclude_ids'])) {
-			$exclude_ids = (array) explode(',', sanitize_text_field($a['exclude_ids']));
-			if (tutils()->count($exclude_ids)) {
-				$exclude_ids_query = "AND ID NOT IN('$exclude_ids')";
-			}
-		}
-
-		if (!empty($a['id'])) {
-			$ids = (array) explode(',', $a['id']);
-			if (tutils()->count($ids)) {
-				$in_ids_query = "AND ID IN('$ids')";
-			}
-		}
-
-		if (!empty($a['category'])) {
-			$category = (array) explode(',', $a['category']);
-			$tax = new \WP_Tax_Query(
-				array(
-					array(
-						'taxonomy' => 'course-category',
-						'field'    => 'term_id',
-						'terms'    => $category,
-						'operator' => 'IN',
-					)
-				)
-			);
-
-			$tax_sql = $tax->get_sql($wpdb->posts, 'ID');
-			$tax_join = tutils()->array_get('join', $tax_sql);
-			$tax_where = tutils()->array_get('where', $tax_sql);
-		}
-
-		$course_post_type = tutor()->course_post_type;
-		$query = $wpdb->get_results("SELECT ID, post_author, post_title 
-			from {$wpdb->posts} 
-			
-			{$tax_join}
-			
-			WHERE 1=1 AND post_status = 'publish'
-			{$exclude_ids_query}
-			{$in_ids_query}
-			{$tax_where}
-			AND post_type = '{$course_post_type}' ORDER BY {$orderby} {$order} LIMIT {$limit} ", ARRAY_A);
-
-
-		if (tutils()->count($query)) {
-			$results = apply_filters('tutor/api/get_courses', $query);
-			wp_send_json_success($results);
-		}
-		wp_send_json_error();
-	}
 }
