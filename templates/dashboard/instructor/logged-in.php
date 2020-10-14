11b9537fe21076de3b3d13cf2fefc1760fb99e8b
shewa <shewa12kpi@gmail.com>
2020-10-14 19:07:43 +0600

    instructor status pending page done

--- templates/dashboard/instructor/logged-in.php
+++ templates/dashboard/instructor/logged-in.php
@@ -7,24 +7,62 @@
 $is_instructor = tutor_utils()->is_instructor();
 if ($is_instructor){
     ?>
+<style type="text/css">
+    .tutor-instructor-pending-content img {
+        margin-bottom:69px;
+        border-radius: 10px;        
+    }
+    .tutor-instructor-thankyou-wrapper{
+        text-align: center;
+    }
 
-    <div class="tutor-alert-warning tutor-instructor-alert">
-        <h2><?php _e("You are an instructor", 'tutor'); ?></h2>
-
-        <p>
-            <?php
-            echo sprintf(__("Registered at : %s %s", 'tutor'), date_i18n(get_option('date_format'), $is_instructor), date_i18n(get_option('time_format'),
-                $is_instructor) );
-            ?>
-        </p>
-
-        <p>
-            <?php
-            echo sprintf(__('Status : %s', 'tutor'), tutor_utils()->instructor_status());
-            ?>
-        </p>
-
+    .tutor-instructor-thankyou-text {
+        height: 48px;
+        line-height: 48px;
+        font-size: 40px;
+        font-style: normal;
+        font-weight: 500;
+        letter-spacing: 0px;
+        color: #161616;
+        margin: 0 auto 26px auto;
+    }    
+    .tutor-instructor-extra-text {
+        height: 54px;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 400;
+        letter-spacing: 0px;
+        color: #525252;
+        line-height: 30px;
+        margin: 0 auto 26px auto;
+   
+    }
+</style>
+<div class="tutor-instructor-pending-wrapper">
+    <div class="tutor-alert-info tutor-alert">
+        <?php _e('With MySpace becoming more popular every day, there is the constant need to be different.','tutor');?>
     </div>
+    
+    <div class="tutor-instructor-pending-content">
+        <img src="<?php echo esc_url(tutor()->url . 'assets/images/new-user.png')?>" alt="<?php _e('New User','tutor')?>">
+        <div class="tutor-instructor-thankyou-wrapper">
+            <div class="tutor-instructor-thankyou-text">
+                <p>
+                    <?php _e('Thank you for registering as an instructor!','tutor');?>
+                </p>                
+            </div>
+            <div class="tutor-instructor-extra-text">
+                <p>
+                    <?php _e('We\'ve received your application, and we will review it soon. Please hang tight!','tutor');?>
+                </p>                
+            </div>
+
+            <a class="tutor-button" href="<?= esc_url(tutor_utils()->tutor_dashboard_url())?>">
+                <?php _e('Go to Dashboard','tutor');?>
+            </a>
+        </div>
+    </div>
+</div>
 
 <?php }
 else{
