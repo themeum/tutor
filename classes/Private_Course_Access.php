3875d96f04c268a2dc58d3870656dd54af678c4d
sksaju <sksaaju@gmail.com>
2020-10-19 17:48:31 +0600

    remove deprecated function

--- classes/Private_Course_Access.php
+++ classes/Private_Course_Access.php
@@ -1,9 +1,9 @@
 <?php
+
 namespace TUTOR;
 
-if ( ! defined( 'ABSPATH' ) )
-	exit;
-
+if (!defined('ABSPATH'))
+    exit;
 
 class Private_Course_Access {
 
@@ -20,16 +20,14 @@ class Private_Course_Access {
         add_action('pre_get_posts', array($this, 'enable_private_access'));
     }
 
-    public function enable_private_access($query=null){
+    public function enable_private_access($query = null) {
 
-        if(!is_admin()){
-
+        if (!is_admin()) {
             global $wpdb;
             $p_type = implode(',', $this->necessary_child_types);
-            $p_name = isset($query->query['name']) ? $query->query['name'] : '';
-            $p_name = $wpdb->escape($p_name);
+            $p_name = isset($query->query['name']) ? esc_sql($query->query['name']) : '';
 
-            if(empty($p_name)){
+            if (empty($p_name)) {
                 $query->set('post_status', array('private', 'publish'));
                 return;
             }
@@ -37,9 +35,9 @@ class Private_Course_Access {
             // Get using raw query to speed up
             $private_query = "SELECT ID, post_parent FROM {$wpdb->posts} WHERE post_type IN ({$p_type}) AND post_name='{$p_name}' AND post_status='private'";
             $private_result = $wpdb->get_results($private_query);
-            $private_course_id = count($private_result)>0;
-            
-            if(count($private_result)>0){
+            $private_course_id = count($private_result) > 0;
+
+            if (count($private_result) > 0) {
                 add_action('pre_get_posts', array($this, 'enable_private_access'));
                 $query->set('post_status', array('private', 'publish'));
             }
