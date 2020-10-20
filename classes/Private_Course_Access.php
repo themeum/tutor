dc1d5055f56b7c0515c3d474151e7e5033d3c92c
JK <jayedulk@gmail.com>
2020-10-20 13:28:54 +0600

    Private course issue fix restored

--- classes/Private_Course_Access.php
+++ classes/Private_Course_Access.php
@@ -1,44 +1,38 @@
 <?php
-
 namespace TUTOR;
 
-if (!defined('ABSPATH'))
-    exit;
+if ( ! defined( 'ABSPATH' ) )
+	exit;
+
 
 class Private_Course_Access {
-
-    private $necessary_child_types = [
-        '\'courses\'',
-        '\'topics\'',
-        '\'lesson\'',
-        '\'tutor_quiz\'',
-        '\'tutor_announcements\'',
-        '\'tutor_assignments\''
-    ];
+    
+    private $allow_empty=false;
 
     public function __construct() {
         add_action('pre_get_posts', array($this, 'enable_private_access'));
     }
 
-    public function enable_private_access($query = null) {
+    public function enable_private_access($query=null){
 
-        if (!is_admin()) {
+        if(!is_admin() && is_user_logged_in() ){
+
             global $wpdb;
-            $p_type = implode(',', $this->necessary_child_types);
-            $p_name = isset($query->query['name']) ? esc_sql($query->query['name']) : '';
+            $p_name = isset($query->query['name']) ? $query->query['name'] : '';
+            $p_name = esc_sql($p_name);
 
-            if (empty($p_name)) {
+            if($this->allow_empty && empty($p_name)){
                 $query->set('post_status', array('private', 'publish'));
                 return;
             }
 
             // Get using raw query to speed up
-            $private_query = "SELECT ID, post_parent FROM {$wpdb->posts} WHERE post_type IN ({$p_type}) AND post_name='{$p_name}' AND post_status='private'";
-            $private_result = $wpdb->get_results($private_query);
-            $private_course_id = count($private_result) > 0;
-
-            if (count($private_result) > 0) {
-                add_action('pre_get_posts', array($this, 'enable_private_access'));
+            $private_query = "SELECT ID, post_parent FROM {$wpdb->posts} WHERE post_type='courses' AND post_name='{$p_name}' AND post_status='private'";
+            $result = $wpdb->get_results($private_query);
+            $private_course_id = (is_array($result) && isset($result[0])) ? $result[0]->ID : 0;
+            
+            if(tutils()->is_enrolled($private_course_id)){
+                $this->allow_empty = true;
                 $query->set('post_status', array('private', 'publish'));
             }
         }
