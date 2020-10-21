edf875b48c86a26cad1ff6f8d21657799d75e88a
JK <jayedulk@gmail.com>
2020-10-21 13:45:42 +0600

    Mail text changed.

--- templates/email/to_instructor_quiz_completed.php
+++ templates/email/to_instructor_quiz_completed.php
@@ -9,5 +9,5 @@
 <p> Hi {instructor_username}, </p>
 
 <p>
-    <strong>{username}</strong> just submitted answers for <strong>{quiz_name}</strong> in course <strong>{course_name}</strong> at <strong>{submission_time}</strong>. You can review it from: <strong>{quiz_review_url}</strong>.
+    <strong>{username}</strong> just submitted answers for <strong>{quiz_name}</strong> in course <strong>{course_name}</strong> at <strong>{submission_time}</strong>. You can review it from: <strong>{quiz_review_url}</strong>. 
 </p>
