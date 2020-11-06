9be21236ff3f5d8657c3785ddb23ed35a4a86adb
JK <jayedulk@gmail.com>
2020-11-06 20:33:48 +0600

    Shortcode course archive attribute

--- includes/tutor-template-functions.php
+++ includes/tutor-template-functions.php
@@ -314,7 +314,9 @@ if( ! function_exists('tutor_course_loop_wrap_classes')) {
 if( ! function_exists('tutor_course_loop_col_classes')) {
     function tutor_course_loop_col_classes( $echo = true ) {
         $course_filter  = (bool) tutor_utils()->get_option('course_archive_filter', false);
-        $course_cols    = /* ($course_filter) ? 2 :  */ tutor_utils()->get_option('courses_col_per_row', 4);
+        // $course_cols    = ($course_filter) ? 2 :  tutor_utils()->get_option('courses_col_per_row', 4);
+        $shortcode_arg  = isset($GLOBALS['tutor_shortcode_arg']) ? $GLOBALS['tutor_shortcode_arg']['column_per_row'] : null;
+        $course_cols    = $shortcode_arg===null ? tutor_utils()->get_option('courses_col_per_row', 4) : $shortcode_arg;
         $classes        = apply_filters( 'tutor_course_loop_col_classes', array(
             'tutor-course-col-' . $course_cols,
         ) );
