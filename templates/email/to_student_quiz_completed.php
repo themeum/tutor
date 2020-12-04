342909b2ac14bf40650d2b36cf45db5710298533
JoomShaper <joomshaper@JoomShapers-iMac.local>
2020-12-04 11:37:02 +0600

    Minor text fix

--- templates/email/to_student_quiz_completed.php
+++ templates/email/to_student_quiz_completed.php
@@ -9,5 +9,5 @@
 <p><?php printf(__('Dear %s,', 'tutor'), '{username}'); ?> </p>
 
 <p>
-	<?php printf(__('Thank you for submitting your answers for %s in course %s at %. This message is to confirm that we have received your answers. You can access this quiz on: %s.', 'tutor'), '<strong>{quiz_name}</strong>', '<strong>{course_name}</strong>', '<strong>{submission_time}</strong>', '<strong>{quiz_url}</strong>'); ?>
+	<?php printf(__('Thank you for submitting your answers for %s in course %s at %s. This message is to confirm that we have received your answers. You can access this quiz on: %s.', 'tutor'), '<strong>{quiz_name}</strong>', '<strong>{course_name}</strong>', '<strong>{submission_time}</strong>', '<strong>{quiz_url}</strong>'); ?>
 </p>
