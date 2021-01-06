6299a65dab20afa5735e5926514cd7f656b58aef
Shewa <shewa@Shewas-iMac.local>
2021-01-06 16:00:23 +0600

    new mail template added

--- templates/email/to_admin_lesson_updated.php
+++ templates/email/to_admin_lesson_updated.php
@@ -9,9 +9,9 @@
 <p><?php _e('Hi,', 'tutor'); ?></p>
 
 <p> 
-<?php _e('Instructor <strong>{instructor_name}</strong> has updated {course_name} on <strong>{site_name}</strong>', 'tutor')?>
+<?php _e('Instructor <strong>{instructor_name}</strong> has updated a lesson from {course_name} on <strong>{site_name}</strong>', 'tutor')?>
 </p>
 
 <p> <?php _e("Course Link: {course_url}",'tutor')?> </p>
-
+<p> <?php _e("Lesson Name: {post_name}",'tutor')?> </p>
 <p><?php _e('Reply to this email to communicate with the instructor.', 'tutor'); ?></p>
