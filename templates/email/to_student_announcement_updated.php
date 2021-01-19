0c485322a3df0aa97051093e190da922a28251c0
sksaju <sksaaju@gmail.com>
2021-01-19 17:18:19 +0600

    add announcement update email template, set notify default value

---
+++ templates/email/to_student_announcement_updated.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package TutorLMS/Templates
+ * @since 1.7.9
+ */
+
+?>
+
+<p><?php _e('Hi,', 'tutor'); ?></p>
+<p>
+    <?php _e('The instructor updated announcement on course - <strong>{course_name}</strong>', 'tutor'); ?>
+    <br />
+    {announcement}
+</p>
+
