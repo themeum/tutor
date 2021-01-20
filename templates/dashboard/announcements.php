02552ad39b2d70546f457e4094b1be11f53f71ee
sksaju <sksaaju@gmail.com>
2021-01-20 13:44:05 +0600

    add textdomain, update code indent

--- templates/dashboard/announcements.php
+++ templates/dashboard/announcements.php
@@ -1,6 +1,6 @@
 <?php
-if ( ! defined( 'ABSPATH' ) )
-exit;
+if (!defined('ABSPATH'))
+    exit;
 /**
  * Template for displaying Announcements
  *
@@ -13,13 +13,13 @@ exit;
  * @version 1.7.9
  */
 $per_page           = 10;
-$paged              = isset( $_GET['paged'] ) ? $_GET['paged'] : 1;
+$paged              = isset($_GET['paged']) ? $_GET['paged'] : 1;
 
 $order_filter       = isset($_GET['order']) ? $_GET['order'] : 'DESC';
 $search_filter      = isset($_GET['search']) ? $_GET['search'] : '';
 //announcement's parent
-$course_id          = isset($_GET['course-id']) ? $_GET['course-id'] : ''; 
-$date_filter        = isset($_GET['date']) ? $_GET['date'] : ''; 
+$course_id          = isset($_GET['course-id']) ? $_GET['course-id'] : '';
+$date_filter        = isset($_GET['date']) ? $_GET['date'] : '';
 
 $year               = date('Y', strtotime($date_filter));
 $month              = date('m', strtotime($date_filter));
@@ -36,7 +36,7 @@ $args = array(
     'order'             => sanitize_text_field($order_filter),
 
 );
-if(!empty($date_filter)){
+if (!empty($date_filter)) {
     $args['date_query'] = array(
         array(
             'year'      => $year,
@@ -57,26 +57,25 @@ $notify_checked = tutils()->get_option('email_to_students.new_announcement_poste
 ?>
 
 <div class="tutor-dashboard-content-inner tutor-frontend-dashboard-withdrawal">
-<h4><?php echo __('Announcement', 'tutor'); ?></h4>
+    <h4><?php echo __('Announcement', 'tutor'); ?></h4>
     <!--notice-->
     <div class="withdraw-page-current-balance new-announcement-wrap">
         <div class="balance-info new-announcement-content">
             <div class="tutor-announcement-big-icon">
                 <i class="tutor-icon-speaker"></i>
-            </div> 
-            
+            </div>
             <div>
-                <small><?php esc_html_e('Create Announcement', 'tutor'); ?></small>
+                <small><?php _e('Create Announcement', 'tutor'); ?></small>
                 <p>
                     <strong>
-                        <?php esc_html_e('Notify all students of your course','tutor');?>
+                        <?php _e('Notify all students of your course', 'tutor'); ?>
                     </strong>
                 </p>
             </div>
         </div>
         <div class="new-announcement-button">
             <button type="button" class="tutor-btn tutor-announcement-add-new">
-                <?php esc_html_e('Add New Announcement','tutor');?>
+                <?php _e('Add New Announcement', 'tutor'); ?>
             </button>
         </div>
     </div>
@@ -85,131 +84,121 @@ $notify_checked = tutils()->get_option('email_to_students.new_announcement_poste
 </div>
 <!--sorting-->
 <div class="tutor-dashboard-announcement-sorting-wrap">
-<div class="tutor-form-group">
+    <div class="tutor-form-group">
         <label for="">
             <?php _e('Courses', 'tutor'); ?>
         </label>
-
-            <select class="tutor-report-category tutor-announcement-course-sorting ignore-nice-select">
-                <?php if(empty($course_id)):?>
-                        <option value="">Select course</option>
-                <?php endif;?>
-                <?php if($courses):?>
-                <?php foreach($courses as $course):?>
-
-                    <option value="<?php echo esc_attr($course->ID)?>" <?php selected($course_id,$course->ID,'selected')?>>
+        <select class="tutor-report-category tutor-announcement-course-sorting ignore-nice-select">
+            <?php if (empty($course_id)) : ?>
+                <option value=""><?php _e('Select course', 'tutor'); ?></option>
+            <?php endif; ?>
+            <?php if ($courses) : ?>
+                <?php foreach ($courses as $course) : ?>
+                    <option value="<?php echo esc_attr($course->ID) ?>" <?php selected($course_id, $course->ID, 'selected') ?>>
                         <?php echo $course->post_title; ?>
                     </option>
-                <?php endforeach;?>
-                <?php else:?>
-                <option value="">No course found</option>
-                <?php endif;?>
-            </select>
-
+                <?php endforeach; ?>
+            <?php else : ?>
+                <option value=""><?php _e('No course found', 'tutor'); ?></option>
+            <?php endif; ?>
+        </select>
     </div>
 
     <div class="tutor-form-group">
         <label><?php _e('Sort By', 'tutor'); ?></label>
         <select class="tutor-announcement-order-sorting ignore-nice-select">
-            <option <?php selected( $order_filter, 'ASC' ); ?>>ASC</option>
-            <option <?php selected( $order_filter, 'DESC' ); ?>>DESC</option>
+            <option <?php selected($order_filter, 'ASC'); ?>><?php _e('ASC', 'tutor'); ?></option>
+            <option <?php selected($order_filter, 'DESC'); ?>><?php _e('DESC', 'tutor'); ?></option>
         </select>
-  
     </div>
 
     <div class="tutor-form-group tutor-announcement-datepicker">
         <label><?php _e('Date', 'tutor'); ?></label>
-            <input type="text" class="tutor-announcement-date-sorting" id="tutor-announcement-datepicker" value="<?php echo $date_filter; ?>" autocomplete="off"/>
-            <i class="tutor-icon-calendar"></i>
+        <input type="text" class="tutor-announcement-date-sorting" id="tutor-announcement-datepicker" value="<?php echo $date_filter; ?>" autocomplete="off" />
+        <i class="tutor-icon-calendar"></i>
     </div>
 </div>
 <!--sorting end-->
 <div class="tutor-announcement-table-wrap">
-
-   
-        <table class="tutor-dashboard-announcement-table">
-            <thead>
-                <tr>
-                    <th style="width:20%"><?php _e('Date', 'tutor'); ?></th>
-                    <th style="text-align:left"><?php _e('Announcements', 'tutor'); ?></th>
-
-                </tr>
-            </thead>
-            <tbody>
-                    <?php if($the_query->have_posts()):?>
-                    <?php foreach($the_query->posts as $post):?>
+    <table class="tutor-dashboard-announcement-table">
+        <thead>
+            <tr>
+                <th style="width:20%"><?php _e('Date', 'tutor'); ?></th>
+                <th style="text-align:left"><?php _e('Announcements', 'tutor'); ?></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if ($the_query->have_posts()) : ?>
+                <?php foreach ($the_query->posts as $post) : ?>
                     <?php
-                        $course = get_post($post->post_parent);
-                        $dateObj = date_create($post->post_date);
-                        $date_format = date_format($dateObj,'F j, Y, g:i a');
+                    $course = get_post($post->post_parent);
+                    $dateObj = date_create($post->post_date);
+                    $date_format = date_format($dateObj, 'F j, Y, g:i a');
                     ?>
-                        <tr>
-                            <td class="tutor-announcement-date"><?php echo esc_html($date_format);?></td>
-                            <td class="tutor-announcement-content-wrap">
-                                <div class="tutor-announcement-content">
-                                    <span>
-                                        <?php echo esc_html($post->post_title); ?>
-                                    </span>
-                                    <p>
-                                        <?php echo $course? $course->post_title : '';?>
-                                    </p>
-                                </div>
-                                <div class="tutor-announcement-buttons">
-                                    <li>
-                                        <a type="button" course-name="<?php echo esc_attr($course->post_title)?>" announcement-date="<?php echo esc_attr($date_format)?>" announcement-title="<?php echo esc_attr($post->post_title);?>" announcement-summary="<?php echo esc_attr($post->post_content);?>" course-id="<?php echo esc_attr($post->post_parent);?>" announcement-id="<?php echo esc_attr($post->ID);?>" class="tutor-btn bordered-btn tutor-announcement-details">
-                                            <?php _e('Details','tutor');?>
-                                        </a>
-                                    </li>
-                                    
-                                    <li class="tutor-dropdown ">
-                                       <i class="tutor-icon-action"></i>
-                                        <div class="arrow-up"></div>
-                                        <ul  class="tutor-dropdown-menu">
-                                        <li  announcement-title="<?php echo $post->post_title;?>" announcement-summary="<?php echo $post->post_content;?>" course-id="<?php echo $post->post_parent;?>" announcement-id="<?php echo $post->ID;?>" class="tutor-announcement-edit">
-                                            <i class="tutor-icon-classic-editor"></i>
-                                            <?php _e('Edit','tutor');?>
-                                        </li>
-                                        <li class="tutor-announcement-delete" announcement-id="<?php echo $post->ID;?>">
-                                            <i class="tutor-icon-cross"></i>
-                                            <?php _e('Delete','tutor');?>
-                                        </li>
-                                        </ul>
-                                                                            
-                                    </li>
-                                    
-                                </div>
-                            </td>
-                        </tr>
-                    <?php endforeach;?>
-                    <?php else:?>
                     <tr>
-                        <td>
-                            <?php _e('Announcements not found','tutor');?>
+                        <td class="tutor-announcement-date"><?php echo esc_html($date_format); ?></td>
+                        <td class="tutor-announcement-content-wrap">
+                            <div class="tutor-announcement-content">
+                                <span>
+                                    <?php echo esc_html($post->post_title); ?>
+                                </span>
+                                <p>
+                                    <?php echo $course ? $course->post_title : ''; ?>
+                                </p>
+                            </div>
+                            <div class="tutor-announcement-buttons">
+                                <li>
+                                    <a type="button" course-name="<?php echo esc_attr($course->post_title) ?>" announcement-date="<?php echo esc_attr($date_format) ?>" announcement-title="<?php echo esc_attr($post->post_title); ?>" announcement-summary="<?php echo esc_attr($post->post_content); ?>" course-id="<?php echo esc_attr($post->post_parent); ?>" announcement-id="<?php echo esc_attr($post->ID); ?>" class="tutor-btn bordered-btn tutor-announcement-details">
+                                        <?php _e('Details', 'tutor'); ?>
+                                    </a>
+                                </li>
+                                <li class="tutor-dropdown ">
+                                    <i class="tutor-icon-action"></i>
+                                    <div class="arrow-up"></div>
+                                    <ul class="tutor-dropdown-menu">
+                                        <li announcement-title="<?php echo $post->post_title; ?>" announcement-summary="<?php echo $post->post_content; ?>" course-id="<?php echo $post->post_parent; ?>" announcement-id="<?php echo $post->ID; ?>" class="tutor-announcement-edit">
+                                            <i class="tutor-icon-classic-editor"></i>
+                                            <?php _e('Edit', 'tutor'); ?>
+                                        </li>
+                                        <li class="tutor-announcement-delete" announcement-id="<?php echo $post->ID; ?>">
+                                            <i class="tutor-icon-cross"></i>
+                                            <?php _e('Delete', 'tutor'); ?>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </div>
                         </td>
                     </tr>
-                    <?php endif;?>
-            </tbody>
-        </table>
-  
+                <?php endforeach; ?>
+            <?php else : ?>
+                <tr>
+                    <td>
+                        <?php _e('Announcements not found', 'tutor'); ?>
+                    </td>
+                </tr>
+            <?php endif; ?>
+        </tbody>
+    </table>
+
 </div>
 
 <!--pagination-->
 <div class="tutor-pagination">
     <?php
-        $big = 999999999; // need an unlikely integer
-        
-        echo paginate_links( array(
-            'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-            'format'    => '?paged=%#%',
-            'current'   => $paged,
-            'total'     => $the_query->max_num_pages
-        ) );
+    $big = 999999999; // need an unlikely integer
+
+    echo paginate_links(array(
+        'base'      => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+        'format'    => '?paged=%#%',
+        'current'   => $paged,
+        'total'     => $the_query->max_num_pages
+    ));
     ?>
 </div>
 <!--pagination end-->
 
 <?php
-    include 'announcements/create.php';
-    include 'announcements/update.php';
-    include 'announcements/details.php';
+include 'announcements/create.php';
+include 'announcements/update.php';
+include 'announcements/details.php';
 ?>
