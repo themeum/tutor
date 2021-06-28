e202b552572ea7c6072a631cdc6249c5d256f288
Hasan Tareq <hsntareq@gmail.com>
2021-06-28 18:30:21 +0600

    Settings page options listing

--- views/options/option_field.php
+++ views/options/option_field.php
@@ -1,22 +1,16 @@
 <div class="tutor-option-field-row">
-	<?php
-	if (isset($field['label'])){
-		?>
-        <div class="tutor-option-field-label">
-            <label for=""><?php echo $field['label']; ?></label>
-        </div>
-		<?php
-	}
-	?>
-    <div class="tutor-option-field">
-		<?php
-        echo $this->field_type($field);
-
-		if (isset($field['desc'])){
-			echo "<p class='desc'>{$field['desc']}</p>";
-		}
-
-		do_action( 'tutor_options_after_field_'.$field['field_key'] );
-		?>
-    </div>
+	<div class="tutor-option-field-label">
+		<label>Dashboard Page</label>
+		<p class="desc">
+			This page will be used for student and instructor dashboard
+		</p>
+	</div>
+	<div class="tutor-option-field-input">
+		<select class="tutor-form-select">
+			<option selected="">Dashboard Page</option>
+			<option value="1">One</option>
+			<option value="2">Two</option>
+			<option value="3">Three</option>
+		</select>
+	</div>
 </div>
