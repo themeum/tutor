840467b3ab083e6888b20f68be6fa8335d97c424
Hasan Tareq <hsntareq@gmail.com>
2021-07-16 10:53:49 +0600

    Template and navigation fixed

--- tutor.php
+++ tutor.php
@@ -4,7 +4,7 @@ Plugin Name: Tutor LMS
 Plugin URI: https://www.themeum.com/product/tutor-lms/
 Description: Tutor is a complete solution for creating a Learning Management System in WordPress way. It can help you to create small to large scale online education site very conveniently. Power features like report, certificate, course preview, private file sharing make Tutor a robust plugin for any educational institutes.
 Author: Themeum
-Version: 1.9.5
+Version: 1.9.4
 Author URI: https://themeum.com
 Requires at least: 4.5
 Tested up to: 5.7
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) )
 /**
  * Defined the tutor main file
  */
-define( 'TUTOR_VERSION', '1.9.5' );
+define( 'TUTOR_VERSION', '1.9.4' );
 define( 'TUTOR_FILE', __FILE__ );
 
 /**
@@ -41,6 +41,7 @@ if ( ! function_exists('tutor') ) {
 		$info = array(
 			'path'                  => $path,
 			'url'                   => plugin_dir_url( TUTOR_FILE ),
+			'icon_dir'              => plugin_dir_url( TUTOR_FILE ) . 'assets/images/images-v2/icons/',
 			'basename'              => plugin_basename( TUTOR_FILE ),
 			'version'               => TUTOR_VERSION,
 			'nonce_action'          => 'tutor_nonce_action',
---
+++ views/options/template/certificate.php
@@ -0,0 +1,230 @@
+<!-- .tutor-option-main-title -->
+<div class="tutor-option-main-title">
+    <h2>Certificate</h2>
+    <a href="#">
+        <i class="las la-undo-alt"></i>
+        Reset to Default
+    </a>
+</div>
+<!-- end /.tutor-option-main-title -->
+
+<!-- .tutor-option-single-item  (Certificate) -->
+<div class="tutor-option-single-item create-certificate-steps">
+    <div class="item-wrapper">
+        <h4>
+            Create Your Certificate <br />
+            In 3 Steps
+        </h4>
+        <ul>
+            <li>Select your favorite design</li>
+            <li>Type in your text & upload your signature</li>
+            <li><strong>Press Save,</strong>Your certificate Ready</li>
+        </ul>
+        <div class="create-certificate-btn">
+            <button class="tutor-btn tutor-is-sm">
+                <!-- <span class="tutor-btn-icon las la-file-signature"></span> -->
+                <span class="tutor-btn-icon tutor-v2-icon-test icon-certificate-filled"></span>
+                <span>Create Certificate</span>
+            </button>
+        </div>
+    </div>
+</div>
+<!-- end /.tutor-option-single-item  (Certificate) -->
+
+<!-- .tutor-option-single-item  (Certificate) -->
+
+<div class="tutor-option-single-item all-certificate">
+    <h4>All Certificate</h4>
+    <div class="item-wrapper">
+        <div class="tutor-option-field-row">
+            <div class="certificate-thumb">
+
+                <img src="<?php echo tutor()->url . 'assets/images/images-v2' ?>/certificate-thumb/cetificate-thumb-1.jpg" alt="" />
+            </div>
+            <div class="tutor-option-field-label">
+                <label>Write PHP Like a Pro: Build a PHP MVC Framework From Scratch </label>
+                <p class="desc">Categorey: Design, Illustration</p>
+            </div>
+            <div class="tutor-option-field-input d-flex has-btn-after">
+                <label class="tutor-form-toggle">
+                    <input type="checkbox" class="tutor-form-toggle-input" checked />
+                    <span class="tutor-form-toggle-control"></span>
+                </label>
+                <button class="tutor-btn tutor-is-outline tutor-is-default tutor-is-xs">Edit</button>
+                <!-- <span class="delete-btn" style="width: 18px; height: 20px"></span> -->
+                <span class="delete-btn tutor-v2-icon-test icon-delete-stroke-filled"></span>
+            </div>
+        </div>
+        <div class="tutor-option-field-row">
+            <div class="certificate-thumb">
+                <img src="<?php echo tutor()->url . 'assets/images/images-v2' ?>/certificate-thumb/cetificate-thumb-2.jpg" alt="" />
+            </div>
+            <div class="tutor-option-field-label">
+                <label>Machine Learning A-Z: Hands-On Python & R In Data Science </label>
+                <p class="desc">Categorey: Design, Illustration</p>
+            </div>
+            <div class="tutor-option-field-input d-flex has-btn-after">
+                <label class="tutor-form-toggle">
+                    <input type="checkbox" class="tutor-form-toggle-input" checked />
+                    <span class="tutor-form-toggle-control"></span>
+                </label>
+                <button class="tutor-btn tutor-is-outline tutor-is-default tutor-is-xs">Edit</button>
+                <!-- <span class="delete-btn" style="width: 18px; height: 20px"></span> -->
+                <span class="delete-btn tutor-v2-icon-test icon-delete-stroke-filled"></span>
+            </div>
+        </div>
+        <div class="tutor-option-field-row">
+            <div class="certificate-thumb">
+                <img src="<?php echo tutor()->url . 'assets/images/images-v2' ?>/certificate-thumb/cetificate-thumb-3.jpg" alt="" />
+            </div>
+            <div class="tutor-option-field-label">
+                <label>The Ultimate Drawing Course - Beginner to Advanced </label>
+                <p class="desc">Categorey: Design, Illustration</p>
+            </div>
+            <div class="tutor-option-field-input d-flex has-btn-after">
+                <label class="tutor-form-toggle">
+                    <input type="checkbox" class="tutor-form-toggle-input" checked />
+                    <span class="tutor-form-toggle-control"></span>
+                </label>
+                <button class="tutor-btn tutor-is-outline tutor-is-default tutor-is-xs">Edit</button>
+                <!-- <span class="delete-btn" style="width: 18px; height: 20px"></span> -->
+                <span class="delete-btn tutor-v2-icon-test icon-delete-stroke-filled"></span>
+            </div>
+        </div>
+        <div class="tutor-option-field-row">
+            <div class="certificate-thumb">
+                <img src="<?php echo tutor()->url . 'assets/images/images-v2' ?>/certificate-thumb/cetificate-thumb-4.jpg" alt="" />
+            </div>
+            <div class="tutor-option-field-label">
+                <label>Master Procedural Maze & Dungeon Generation </label>
+                <p class="desc">Categorey: Design, Illustration</p>
+            </div>
+            <div class="tutor-option-field-input d-flex has-btn-after">
+                <label class="tutor-form-toggle">
+                    <input type="checkbox" class="tutor-form-toggle-input" checked />
+                    <span class="tutor-form-toggle-control"></span>
+                </label>
+                <button class="tutor-btn tutor-is-outline tutor-is-default tutor-is-xs">Edit</button>
+                <!-- <span class="delete-btn" style="width: 18px; height: 20px"></span> -->
+                <span class="delete-btn tutor-v2-icon-test icon-delete-stroke-filled"></span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end /.tutor-option-single-item  (Certificate) -->
+
+<!-- .tutor-option-single-item  (Certificate) -->
+<div class="tutor-option-single-item no-certificate">
+    <h4>No Certificate</h4>
+    <div class="item-wrapper">
+        <div class="certificate-thumb">
+            <img src="<?php echo tutor()->url . 'assets/images/images-v2' ?>/certificate-thumb/no-certificate-thumb.svg" alt="" />
+            <p>No certificate generate yet</p>
+        </div>
+    </div>
+</div>
+<!-- end /.tutor-option-single-item  (Certificate) -->
+
+<!-- .tutor-option-single-item  (Certificate) -->
+<div class="tutor-option-single-item certificate-template">
+    <h4>Select Certificate Template</h4>
+    <div class="item-wrapper">
+        <div class="tutor-option-field-row d-block">
+            <div class="tutor-option-field-label">
+                <label>Horizontal Template</label>
+            </div>
+            <div class="tutor-option-field-input">
+                <div class="radio-thumbnail horizontal">
+                    <label for="certificate-template-horizontal-1">
+                        <input type="radio" name="certificate-template" id="certificate-template-horizontal-1" />
+                        <span class="icon-wrapper">
+                            <img src="<?php echo tutor()->url . 'assets/images/images-v2' ?>/certificate-template/certificate-horizontal-1.jpg" alt="" />
+                        </span>
+                    </label>
+                    <label for="certificate-template-horizontal-2">
+                        <input type="radio" name="certificate-template" id="certificate-template-horizontal-2" />
+                        <span class="icon-wrapper">
+                            <img src="<?php echo tutor()->url . 'assets/images/images-v2' ?>/certificate-template/certificate-horizontal-2.jpg" alt="" />
+                        </span>
+                    </label>
+                    <label for="certificate-template-horizontal-3">
+                        <input type="radio" name="certificate-template" id="certificate-template-horizontal-3" />
+                        <span class="icon-wrapper">
+                            <img src="<?php echo tutor()->url . 'assets/images/images-v2' ?>/certificate-template/certificate-horizontal-3.jpg" alt="" />
+                        </span>
+                    </label>
+                    <label for="certificate-template-horizontal-4">
+                        <input type="radio" name="certificate-template" id="certificate-template-horizontal-4" />
+                        <span class="icon-wrapper">
+                            <img src="<?php echo tutor()->url . 'assets/images/images-v2' ?>/certificate-template/certificate-horizontal-4.jpg" alt="" />
+                        </span>
+                    </label>
+                    <label for="certificate-template-horizontal-5">
+                        <input type="radio" name="certificate-template" id="certificate-template-horizontal-5" />
+                        <span class="icon-wrapper">
+                            <img src="<?php echo tutor()->url . 'assets/images/images-v2' ?>/certificate-template/certificate-horizontal-5.jpg" alt="" />
+                        </span>
+                    </label>
+                    <label for="certificate-template-horizontal-6">
+                        <input type="radio" name="certificate-template" id="certificate-template-horizontal-6" />
+                        <span class="icon-wrapper">
+                            <img src="<?php echo tutor()->url . 'assets/images/images-v2' ?>/certificate-template/certificate-horizontal-6.jpg" alt="" />
+                        </span>
+                    </label>
+                    <label for="certificate-template-horizontal-7">
+                        <input type="radio" name="certificate-template" id="certificate-template-horizontal-7" checked />
+                        <span class="icon-wrapper">
+                            <img src="<?php echo tutor()->url . 'assets/images/images-v2' ?>/certificate-template/certificate-horizontal-7.jpg" alt="" />
+                        </span>
+                    </label>
+                </div>
+            </div>
+        </div>
+        <div class="tutor-option-field-row d-block">
+            <div class="tutor-option-field-label">
+                <label>Vertical Template</label>
+            </div>
+            <div class="tutor-option-field-input">
+                <div class="radio-thumbnail vertical">
+                    <label for="certificate-template-vertical-1">
+                        <input type="radio" name="certificate-template" id="certificate-template-vertical-1" />
+                        <span class="icon-wrapper">
+                            <img src="<?php echo tutor()->url . 'assets/images/images-v2' ?>/certificate-template/certificate-vertical-1.jpg" alt="" />
+                        </span>
+                    </label>
+                    <label for="certificate-template-vertical-2">
+                        <input type="radio" name="certificate-template" id="certificate-template-vertical-2" />
+                        <span class="icon-wrapper">
+                            <img src="<?php echo tutor()->url . 'assets/images/images-v2' ?>/certificate-template/certificate-vertical-2.jpg" alt="" />
+                        </span>
+                    </label>
+                    <label for="certificate-template-vertical-3">
+                        <input type="radio" name="certificate-template" id="certificate-template-vertical-3" />
+                        <span class="icon-wrapper">
+                            <img src="<?php echo tutor()->url . 'assets/images/images-v2' ?>/certificate-template/certificate-vertical-3.jpg" alt="" />
+                        </span>
+                    </label>
+                    <label for="certificate-template-vertical-4">
+                        <input type="radio" name="certificate-template" id="certificate-template-vertical-4" />
+                        <span class="icon-wrapper">
+                            <img src="<?php echo tutor()->url . 'assets/images/images-v2' ?>/certificate-template/certificate-vertical-4.jpg" alt="" />
+                        </span>
+                    </label>
+                    <label for="certificate-template-vertical-5">
+                        <input type="radio" name="certificate-template" id="certificate-template-vertical-5" />
+                        <span class="icon-wrapper">
+                            <img src="<?php echo tutor()->url . 'assets/images/images-v2' ?>/certificate-template/certificate-vertical-5.jpg" alt="" />
+                        </span>
+                    </label>
+                    <label for="certificate-template-vertical-6">
+                        <input type="radio" name="certificate-template" id="certificate-template-vertical-6" />
+                        <span class="icon-wrapper">
+                            <img src="<?php echo tutor()->url . 'assets/images/images-v2' ?>/certificate-template/certificate-vertical-6.jpg" alt="" />
+                        </span>
+                    </label>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end /.tutor-option-single-item  (Certificate) -->
