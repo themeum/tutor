d9e70573fd508140077789b301443fe3b7e246c5
Hasan Tareq <hsntareq@gmail.com>
2021-08-24 12:48:45 +0600

    Toggle switch done

--- views/options/field-types/checkbox_horizontal.php
+++ views/options/field-types/checkbox_horizontal.php
@@ -1,18 +1,15 @@
 <?php
 if (!empty($field['options'])) {
-    $default = $field['default'] ?? '';
-    $option_value = $this->get($field['key'], $default);
 ?>
     <div class="tutor-option-field-row d-block">
-        <div class="tutor-option-field-label">
-            <label><?php echo $field['label']; ?></label>
-            <p class="desc"><?php echo $field['desc'] ?></p>
-        </div>
+        <?php include tutor()->path . "views/options/template/field_heading.php"; ?>
+
         <div class="tutor-option-field-input">
             <div class="type-check d-flex">
                 <?php foreach ($field['options'] as $optionKey => $option) : ?>
                     <div class="tutor-form-check">
-                        <input type="checkbox" id="check_<?php echo $optionKey ?>" value="1" class="tutor-form-check-input" name="tutor_option[<?php echo $field['key'] ?>][<?php echo $optionKey ?>]" <?php checked($this->get($field['key'] . '.' . $optionKey), '1') ?>>
+                        <input type="checkbox" id="check_<?php echo $optionKey ?>" name="tutor_option[<?php echo $field['key'] ?>][<?php echo $optionKey ?>]" value="1" <?php checked($this->get($field['key'] . '.' . $optionKey), '1') ?> class="tutor-form-check-input" />
+
                         <label for="check_<?php echo $optionKey ?>"> <?php echo $option; ?> </label>
                     </div>
                 <?php endforeach; ?>
