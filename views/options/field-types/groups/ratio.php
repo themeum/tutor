9b206ca5d29925238fc7ca66053540bae917303b
Hasan Tareq <hsntareq@gmail.com>
2021-08-31 16:55:57 +0600

    design profile fixing

---
+++ views/options/field-types/groups/ratio.php
@@ -0,0 +1,4 @@
+<label for="revenue-instructor" class="revenue-percentage">
+    <span><?php echo $label ?></span>
+    <input type="number" class="tutor-form-control" placeholder="0 %" name="<?php echo $input_name ?>" id="<?php echo $group_field['id'] ?>" min="0" max="100" value="<?php echo $input_value ?>">
+</label>
