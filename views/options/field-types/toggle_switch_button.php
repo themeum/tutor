f5deffb2afde071dd51d583397332a1d8641b086
Hasan Tareq <hsntareq@gmail.com>
2021-09-01 18:40:54 +0600

    fixing email editing

--- views/options/field-types/toggle_switch_button.php
+++ views/options/field-types/toggle_switch_button.php
@@ -1,4 +1,6 @@
 <?php
+$default = isset($field['default']) ? $field['default'] : '';
+$option_value = $this->get($field['key'], $default);
 $field_id = 'field_' . $field['key'];
 $email_edit_url = add_query_arg(
     array('page' => 'tutor_settings', 'tab_page' => 'email', 'edit' => $field['key']),
@@ -7,14 +9,15 @@ $email_edit_url = add_query_arg(
 ?>
 <div class="tutor-option-field-row" id="<?php echo $field_id; ?>">
     <div class="tutor-option-field-label has-tooltip">
-        <label>Course Enrolled</label>
+        <label><?php echo $field['label'] ?></label>
         <div class="tooltip-wrap tooltip-icon">
-            <span class="tooltip-txt tooltip-right">I'm your tiny tooltips text</span>
+            <span class="tooltip-txt tooltip-right"><?php echo $field['desc'] ?></span>
         </div>
     </div>
     <div class="tutor-option-field-input d-flex has-btn-after">
         <label class="tutor-form-toggle">
-            <input type="checkbox" class="tutor-form-toggle-input" checked="">
+            <input type="hidden" name="tutor_option[<?php echo $field['key']; ?>]" value="off">
+            <input type="checkbox" name="tutor_option[<?php echo $field['key']; ?>]" value="on" <?php $option_value ? checked($option_value[1], 'on') : '' ?> class="tutor-form-toggle-input">
             <span class="tutor-form-toggle-control"></span>
         </label>
         <a class="tutor-btn tutor-is-outline tutor-is-default tutor-is-xs" href="<?php echo $email_edit_url ?>">Edit</a>
