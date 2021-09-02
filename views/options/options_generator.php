6010e350f89441dfb687a871dc5e0ccead78d0d1
Hasan Tareq <hsntareq@gmail.com>
2021-09-02 12:31:13 +0600

    email edit template

--- views/options/options_generator.php
+++ views/options/options_generator.php
@@ -8,9 +8,9 @@
 $url_page = isset($_GET['tab_page']) ? $_GET['tab_page'] : null;
 
 if (isset($_GET['tab_page']) && $_GET['tab_page'] == 'email' && isset($_GET['edit'])) {
-    include tutor()->path . 'views/options/email_edit.php';
+    include tutor()->path . 'views/options/email-edit.php';
 } else {
-    include tutor()->path . 'views/options/settings_form.php';
+    include tutor()->path . 'views/options/settings-form.php';
 } ?>
 <style>
     .tutor-notification {
