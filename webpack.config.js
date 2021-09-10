3567076c769681cd54f8eedae7aeb6d9c4dbc609
JK <jayedulk@gmail.com>
2021-09-10 12:40:33 +0600

    Redundant code removed

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,17 @@
 const path = require( 'path' );
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 const TerserPlugin = require( 'terser-webpack-plugin' );
 const CssMinimizerPlugin = require( 'css-minimizer-webpack-plugin' );
-const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
-
-// JS Directory path.
-const JS_DIR = path.resolve( __dirname, 'assets/react' );
-const BUILD_DIR = path.resolve( __dirname, 'assets/js' );
-
-const reactRoot = path.resolve( __dirname, 'assets/react' );
-const nodeModules = path.resolve( __dirname, 'node_modules' );
 
 module.exports = ( env, options ) => {
 
 	const mode = options.mode || 'development';
-	const extPrefix = ( 'production' === mode ) ? '.min' : '';
-
+	
 	const config = {
 		mode,
 		module: {
 			rules: [
 				{
 					test: /\.js$/,
-					include: [ JS_DIR ],
 					exclude: /node_modules/,
 					use: 'babel-loader',
 				}
@@ -58,11 +47,8 @@ module.exports = ( env, options ) => {
 			'tutor-course-builder'	: './assets/react/course-builder/index.js',
 			'tutor-setup'		    : './assets/react/admin/tutor-setup.js',
         },
-        resolve: {
-            modules: [ reactRoot, nodeModules ],
-        },
 		output: {
-			path: path.resolve( __dirname, BUILD_DIR ),
+			path: path.resolve( __dirname, path.resolve( __dirname, 'assets/js' ) ),
 			filename: `[name].js`,
 		},
 	});
