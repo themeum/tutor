7f4c2ef4139c4bcd66e2555535924934c3f970b3
Hasan Tareq <hsntareq@gmail.com>
2021-09-14 18:42:07 +0600

    export done

--- views/options/template/import_export.php
+++ views/options/template/import_export.php
@@ -1,155 +1,162 @@
 <div class="tutor-option-main-title">
-    <h2>Import/Export</h2>
+	<h2>Import/Export</h2>
 </div>
 <div class="tutor-backend-alert alert-warning">
-    <span class="alert-icon tutor-v2-icon-test icon-warning-filled"></span>
-    <span class="alert-content">Warning: Importing, Restoring, or Resetting will overwrite ALL existing settings. Please proceed with caution.</span>
+	<span class="alert-icon tutor-v2-icon-test icon-warning-filled"></span>
+	<span class="alert-content">Warning: Importing, Restoring, or Resetting will overwrite ALL existing settings. Please proceed with caution.</span>
 </div>
+<?php
+		$option = (array) maybe_unserialize( get_option( 'tutor_option' ) );
+
+echo '<pre>';
+print_r( json_encode( $option ) );
+echo '</pre>';
+?>
 <div class="tutor-option-single-item">
-    <h4>Export</h4>
-    <div class="item-wrapper">
-        <div class="tutor-option-field-row">
-            <div class="tutor-option-field-label">
-                <p class="text-regular-h6">Current Settings</p>
-                <p class="text-medium-small">
-                    <span style="font-weight: 500">Last Update: </span>20 July, 2020, 12:47 pm
-                </p>
-            </div>
-            <div class="tutor-option-field-input">
-                <button class="tutor-btn tutor-is-outline tutor-is-sm">Export Settings</button>
-            </div>
-        </div>
-    </div>
+	<h4>Export</h4>
+	<div class="item-wrapper">
+		<div class="tutor-option-field-row">
+			<div class="tutor-option-field-label">
+				<p class="text-regular-h6">Current Settings</p>
+				<p class="text-medium-small">
+					<span style="font-weight: 500">Last Update: </span>20 July, 2020, 12:47 pm
+				</p>
+			</div>
+			<div class="tutor-option-field-input">
+				<button class="tutor-btn tutor-is-outline tutor-is-sm" id="export_settings">Export Settings</button>
+			</div>
+		</div>
+	</div>
 </div>
 
 <div class="tutor-option-single-item item-variation-table settings-history">
-    <h4>Settings History</h4>
-    <div class="item-wrapper">
-        <div class="tutor-option-field-row">
-            <div class="tutor-option-field-label">
-                <p>Date</p>
-            </div>
-        </div>
-        <div class="tutor-option-field-row">
-            <div class="tutor-option-field-label">
-                <p class="text-medium-small">20 July, 2020, 12:47 pm</p>
-            </div>
-            <div class="tutor-option-field-input">
-                <button class="tutor-btn tutor-is-outline tutor-is-default tutor-is-xs">Apply</button>
-                <div class="popup-opener">
-                    <button type="button" class="popup-btn">
-                        <span class="toggle-icon"></span>
-                    </button>
-                    <ul class="popup-menu">
-                        <li>
-                            <a href="#">
-                                <span class="icon tutor-v2-icon-test icon-msg-archive-filled"></span>
-                                <span>Download</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="icon tutor-v2-icon-test icon-delete-fill-filled"></span>
-                                <span>Delete</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="tutor-option-field-row">
-            <div class="tutor-option-field-label">
-                <p class="text-medium-small">20 July, 2020, 12:47 pm</p>
-            </div>
-            <div class="tutor-option-field-input">
-                <button class="tutor-btn tutor-is-outline tutor-is-default tutor-is-xs">Apply</button>
-                <div class="popup-opener">
-                    <button type="button" class="popup-btn">
-                        <span class="toggle-icon"></span>
-                    </button>
-                    <ul class="popup-menu">
-                        <li>
-                            <a href="#">
-                                <span class="icon tutor-v2-icon-test icon-msg-archive-filled"></span>
-                                <span>Download</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="icon tutor-v2-icon-test icon-delete-fill-filled"></span>
-                                <span>Delete</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="tutor-option-field-row">
-            <div class="tutor-option-field-label">
-                <p class="text-medium-small">20 July, 2020, 12:47 pm</p>
-            </div>
-            <div class="tutor-option-field-input">
-                <button class="tutor-btn tutor-is-outline tutor-is-default tutor-is-xs">Apply</button>
-                <div class="popup-opener">
-                    <button type="button" class="popup-btn">
-                        <span class="toggle-icon"></span>
-                    </button>
-                    <ul class="popup-menu">
-                        <li>
-                            <a href="#">
-                                <span class="icon tutor-v2-icon-test icon-msg-archive-filled"></span>
-                                <span>Download</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="icon tutor-v2-icon-test icon-delete-fill-filled"></span>
-                                <span>Delete</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
+	<h4>Settings History</h4>
+	<div class="item-wrapper">
+		<div class="tutor-option-field-row">
+			<div class="tutor-option-field-label">
+				<p>Date</p>
+			</div>
+		</div>
+		<div class="tutor-option-field-row">
+			<div class="tutor-option-field-label">
+				<p class="text-medium-small">20 July, 2020, 12:47 pm</p>
+			</div>
+			<div class="tutor-option-field-input">
+				<button class="tutor-btn tutor-is-outline tutor-is-default tutor-is-xs">Apply</button>
+				<div class="popup-opener">
+					<button type="button" class="popup-btn">
+						<span class="toggle-icon"></span>
+					</button>
+					<ul class="popup-menu">
+						<li>
+							<a href="#">
+								<span class="icon tutor-v2-icon-test icon-msg-archive-filled"></span>
+								<span>Download</span>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<span class="icon tutor-v2-icon-test icon-delete-fill-filled"></span>
+								<span>Delete</span>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="tutor-option-field-row">
+			<div class="tutor-option-field-label">
+				<p class="text-medium-small">20 July, 2020, 12:47 pm</p>
+			</div>
+			<div class="tutor-option-field-input">
+				<button class="tutor-btn tutor-is-outline tutor-is-default tutor-is-xs">Apply</button>
+				<div class="popup-opener">
+					<button type="button" class="popup-btn">
+						<span class="toggle-icon"></span>
+					</button>
+					<ul class="popup-menu">
+						<li>
+							<a href="#">
+								<span class="icon tutor-v2-icon-test icon-msg-archive-filled"></span>
+								<span>Download</span>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<span class="icon tutor-v2-icon-test icon-delete-fill-filled"></span>
+								<span>Delete</span>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="tutor-option-field-row">
+			<div class="tutor-option-field-label">
+				<p class="text-medium-small">20 July, 2020, 12:47 pm</p>
+			</div>
+			<div class="tutor-option-field-input">
+				<button class="tutor-btn tutor-is-outline tutor-is-default tutor-is-xs">Apply</button>
+				<div class="popup-opener">
+					<button type="button" class="popup-btn">
+						<span class="toggle-icon"></span>
+					</button>
+					<ul class="popup-menu">
+						<li>
+							<a href="#">
+								<span class="icon tutor-v2-icon-test icon-msg-archive-filled"></span>
+								<span>Download</span>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<span class="icon tutor-v2-icon-test icon-delete-fill-filled"></span>
+								<span>Delete</span>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <div class="tutor-option-single-item item-variation-dragndrop import-setting">
-    <h4>Import Settings</h4>
-    <div class="item-wrapper">
-        <div class="tutor-option-field-row d-block">
-            <div class="tutor-option-field-label">
-                <div class="drag-drop-zone ">
-                    <span class="icon tutor-v2-icon-test icon-upload-icon-line"></span>
-                    <div class="title">Drag &amp; Drop your JSON File here </div>
-                    <div class="subtitle"><span>File Format:</span> .json <br> Or</div>
-                    <label for="drag-drop-input" class="tutor-btn tutor-is-sm">
-                        <input type="file" name="drag-drop-input" id="drag-drop-input" accept="">
-                        <span>Browse File</span>
-                    </label>
-                    <span class="file-info"></span>
-                </div>
-            </div>
-            <div class="tutor-option-field-input">
-                <button class="tutor-btn tutor-is-sm">Update Settings</button>
-            </div>
-        </div>
-    </div>
+	<h4>Import Settings</h4>
+	<div class="item-wrapper">
+		<div class="tutor-option-field-row d-block">
+			<div class="tutor-option-field-label">
+				<div class="drag-drop-zone ">
+					<span class="icon tutor-v2-icon-test icon-upload-icon-line"></span>
+					<div class="title">Drag &amp; Drop your JSON File here </div>
+					<div class="subtitle"><span>File Format:</span> .json <br> Or</div>
+					<label for="drag-drop-input" class="tutor-btn tutor-is-sm">
+						<input type="file" name="drag-drop-input" id="drag-drop-input" accept="">
+						<span>Browse File</span>
+					</label>
+					<span class="file-info"></span>
+				</div>
+			</div>
+			<div class="tutor-option-field-input">
+				<button class="tutor-btn tutor-is-sm">Update Settings</button>
+			</div>
+		</div>
+	</div>
 </div>
 
 <div class="tutor-option-single-item">
-    <h4>Reset Settings</h4>
-    <div class="item-wrapper">
-        <div class="tutor-option-field-row">
-            <div class="tutor-option-field-label">
-                <p class="text-regular-h6"> Reset Everything to Default</p>
-                <p class="text-medium-small">
-                    <span style="font-weight: 500">Reset subtitle
-                </span></p>
-            </div>
-            <div class="tutor-option-field-input">
-                <button class="tutor-btn tutor-is-outline tutor-is-sm">Reset All Settings</button>
-            </div>
-        </div>
-    </div>
+	<h4>Reset Settings</h4>
+	<div class="item-wrapper">
+		<div class="tutor-option-field-row">
+			<div class="tutor-option-field-label">
+				<p class="text-regular-h6"> Reset Everything to Default</p>
+				<p class="text-medium-small">
+					<span style="font-weight: 500">Reset subtitle
+				</span></p>
+			</div>
+			<div class="tutor-option-field-input">
+				<button class="tutor-btn tutor-is-outline tutor-is-sm">Reset All Settings</button>
+			</div>
+		</div>
+	</div>
 </div>
