9bd2d7f6d4bc6cc70c57b65bf159132e8cbad088
iamsaief <saiefalemon@gmail.com>
2021-09-25 16:25:49 +0600

    added notification - tab, elements

--- v2-library/_src/js/tutorNotificationTab.js
+++ v2-library/_src/js/tutorNotificationTab.js
@@ -4,31 +4,24 @@
 (function tutorNotificationTab() {
 	document.addEventListener('click', (e) => {
 		const attr = 'data-tutor-notification-tab-target';
-		const tabItems = document.querySelectorAll('.tab-header-item, .tab-body-item');
-
 		const activeItems = document.querySelectorAll('.tab-header-item.is-active, .tab-body-item.is-active');
 
-		// Opening Offcanvas
 		if (e.target.hasAttribute(attr)) {
 			e.preventDefault();
 			const id = e.target.hasAttribute(attr)
 				? e.target.getAttribute(attr)
 				: e.target.closest(`[${attr}]`).getAttribute(attr);
 
-			activeItems.forEach((m) => {
-				m.classList.remove('is-active');
-			});
-
 			const tabBodyItem = document.getElementById(id);
-			if (tabBodyItem) {
+
+			if (e.target.hasAttribute(attr) && tabBodyItem) {
 				activeItems.forEach((m) => {
 					m.classList.remove('is-active');
 				});
+
+				e.target.classList.add('is-active');
 				tabBodyItem.classList.add('is-active');
 			}
-
-			console.log(tabHeaderItem);
-			console.log('getA', e.target.getAttribute(attr), 'clo', e.target.closest(`[${attr}]`).getAttribute(attr));
 		}
 	});
 })();
