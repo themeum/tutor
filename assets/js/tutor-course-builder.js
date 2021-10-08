ab55df11d688accff1f16e90acd200445dff9566
JK <jayedulk@gmail.com>
2021-10-08 09:54:15 +0600

    Ceertificate tab switch

--- assets/js/tutor-course-builder.js
+++ assets/js/tutor-course-builder.js
@@ -3,6 +3,12 @@ var __webpack_exports__ = {};
 /*!**********************************************!*\
   !*** ./assets/react/course-builder/index.js ***!
   \**********************************************/
-
+window.jQuery(document).ready(function ($) {
+  $('.tutor-certificate-template-tab [data-tutor-tab-target]').click(function () {
+    $(this).addClass('is-active').siblings().removeClass('is-active');
+    $('#' + $(this).data('tutor-tab-target')).show().siblings().hide();
+  });
+});
 /******/ })()
 ;
+//# sourceMappingURL=tutor-course-builder.js.map
