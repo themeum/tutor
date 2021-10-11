344425b9369964aae9f84f6e24fcf0e9db448b4e
jayedul <jayedulk@gmail.com>
2021-10-11 12:21:40 +0600

    Dev JS commit

--- assets/js/tutor-course-builder.js
+++ assets/js/tutor-course-builder.js
@@ -1,17 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************************************!*\
-  !*** ./assets/react/course-builder/index.js ***!
-  \**********************************************/
-window.jQuery(document).ready(function ($) {
-  $('.tutor-certificate-template-tab [data-tutor-tab-target]').click(function () {
-    $(this).addClass('is-active').siblings().removeClass('is-active');
-    $('#' + $(this).data('tutor-tab-target')).show().siblings().hide();
-  });
-  $('.').click(function () {
-    $(this).siblings().filter('tutor-certificate-collapsible');
-  });
-});
-/******/ })()
-;
-//# sourceMappingURL=tutor-course-builder.js.map
+window.jQuery(document).ready(function(t){t(".tutor-certificate-template-tab [data-tutor-tab-target]").click(function(){t(this).addClass("is-active").siblings().removeClass("is-active"),t("#"+t(this).data("tutor-tab-target")).show().siblings().hide()}),t(".").click(function(){t(this).siblings().filter("tutor-certificate-collapsible")})});
