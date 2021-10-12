456eb479ee450704806bb9ac41aea3d21c72b170
jayedul <jayedulk@gmail.com>
2021-10-12 17:30:35 +0600

    Remove css loader setting

--- classes/Tutor.php
+++ classes/Tutor.php
@@ -546,8 +546,6 @@ final class Tutor{
 	public static function default_options(){
 		$options = array (
 			'pagination_per_page'               => '20',
-			'load_tutor_css'                    => '1',
-			'load_tutor_js'                     => '1',
 			'course_allow_upload_private_files' => '1',
 			'display_course_instructors'        => '1',
 			'enable_q_and_a_on_course'          => '1',
