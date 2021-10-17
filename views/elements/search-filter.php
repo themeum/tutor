69b658fc2a22b51f797fb593a766bdd377946eb9
shewa12 <shewa12kpi@gmail.com>
2021-10-17 16:47:02 +0600

    backend admin page reuseable elements added

---
+++ views/elements/search-filter.php
@@ -0,0 +1,43 @@
+<?php if ( isset( $data ) ) : ?>
+
+	<div class="tutor-admin-page-filters" style="display: flex; justify-content: space-between">
+		<?php if ( $data['bulk_action'] ) : ?>
+			<div class="tutor-admin-bulk-action-wrapper">
+				<form action="" method="post">
+					<div class="tutor-bulk-action-group">
+						<select name="bulk-action" id="tutor-backend-bulk-action">
+							<?php foreach( $data['bulk_actions'] as $k => $v) : ?>
+								<option value="<?php esc_attr_e( $v['value'] ); ?>">
+									<?php esc_html_e( $v['option'] ); ?>
+								</option>
+							<?php endforeach; ?>
+						</select>
+						<button type="button" class="tutor-btn">
+							<?php esc_html_e( 'Apply', 'tutor' ); ?>
+						</button>
+					</div>
+				</form>
+			</div>
+		<?php endif; ?>
+		<?php if ( isset( $data['search_filter'] ) && true === $data['search_filter'] ) : ?>
+			<div class="tutor-admin-page-search-filter-wrapper">
+				<form>
+					<div class="tutor-form-group select-with-input" style="display: flex; align-items: center; column-gap: 15px;">
+						<select name="tutor-admin-search-by" id="tutor-admin-search-by">
+							<option value="course">
+								<?php esc_html_e( 'Course', 'tutor' ); ?>
+							</option>
+							<option value="student">
+								<?php esc_html_e( 'Student', 'tutor' ); ?>
+							</option>
+						</select>
+						<input type="text" name="tutor-admin-search-by" id="tutor-admin-search-by">
+						<button type="button" class="tutor-btn">
+							<?php esc_html_e( 'Apply', 'tutor' ); ?>
+						</button>
+					</div>
+				</form>
+			</div>
+		<?php endif; ?>
+	</div>
+<?php endif; ?>
