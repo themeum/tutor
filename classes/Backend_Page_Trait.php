957337d3b93fb8b85225e2292bc4c0a8293392b5
shewa12 <shewa12kpi@gmail.com>
2021-10-17 17:14:11 +0600

    trait file added for backend admin page

---
+++ classes/Backend_Page_Trait.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Backend Page Trait to use with existing class
+ * contains backend page related reuseable code snippet
+ *
+ * @package Enrollment List
+ * @since v2.0.0
+ */
+
+namespace TUTOR;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+/**
+ * Trait for backend pages
+ * Reuse able methods implemented can be override from child class
+ */
+trait Backend_Page_Trait {
+	/**
+	 * Bulk action abstract property.
+	 *
+	 * @var $bulk_action
+	 */
+	public $bulk_action = true;
+
+	/**
+	 * Bulk action default option
+	 *
+	 * @return array
+	 * @since v2.0.0
+	 */
+	public function bulk_action_default(): array {
+		return array(
+			'value'  => 'bulk action',
+			'option' => __( 'Bulk Action', 'tutor-pro' ),
+		);
+	}
+
+	/**
+	 * Bulk action complete
+	 *
+	 * @return array
+	 * @since v2.0.0
+	 */
+	public function bulk_action_complete(): array {
+		return array(
+			'value'  => 'complete',
+			'option' => __( 'Complete', 'tutor-pro' ),
+		);
+	}
+
+	/**
+	 * Bulk action on hold
+	 *
+	 * @return array
+	 * @since v2.0.0
+	 */
+	public function bulk_action_on_hold(): array {
+		return array(
+			'value'  => 'on-hold',
+			'option' => __( 'On Hold', 'tutor-pro' ),
+		);
+	}
+
+	/**
+	 * Bulk action pending
+	 *
+	 * @return array
+	 * @since v2.0.0
+	 */
+	public function bulk_action_pending(): array {
+		return array(
+			'value'  => 'pending',
+			'option' => __( 'Pending', 'tutor-pro' ),
+		);
+	}
+
+	/**
+	 * Bulk action processing
+	 *
+	 * @return array
+	 * @since v2.0.0
+	 */
+	public function bulk_action_processing(): array {
+		return array(
+			'value'  => 'processing',
+			'option' => __( 'Processing', 'tutor-pro' ),
+		);
+	}
+
+	/**
+	 * Bulk action delete
+	 *
+	 * @return array
+	 * @since v2.0.0
+	 */
+	public function bulk_action_delete(): array {
+		return array(
+			'value'  => 'delete',
+			'option' => __( 'Delete', 'tutor-pro' ),
+		);
+	}
+
+	/**
+	 * Bulk action cancel
+	 *
+	 * @return array
+	 * @since v2.0.0
+	 */
+	public function bulk_action_cancel(): array {
+		return array(
+			'value'  => 'cancel',
+			'option' => __( 'Cancel', 'tutor-pro' ),
+		);
+	}
+}
