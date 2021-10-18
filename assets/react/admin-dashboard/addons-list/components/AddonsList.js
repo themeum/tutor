1c8c692b98f8f99b91b11703f947228f350043ed
iamsaief <saiefalemon@gmail.com>
2021-10-19 02:51:09 +0600

    added empty-state, loading spinner, responsive fix

--- assets/react/admin-dashboard/addons-list/components/AddonsList.js
+++ assets/react/admin-dashboard/addons-list/components/AddonsList.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAddons } from '../context/AddonsContext';
 import AddonCard from './AddonCard';
 
+import emptyStateImg from '../../../../images/empty-state.svg';
+
 const AddonsList = () => {
 	const { allAddons, loading } = useAddons();
+
 	return (
-		<div className="tutor-addons-list-items tutor-mt-30">
+		<div className={`tutor-addons-list-items tutor-mt-30 ${allAddons.length < 3 ? 'is-less-items' : ''}`}>
 			{allAddons.length ? (
 				allAddons.map((addon, index) => {
 					return <AddonCard addon={addon} key={index} />;
 				})
 			) : loading ? (
-				<p>Loading...</p>
+				<div className="tutor-addons-loading">{/* Loading... */}</div>
 			) : (
-				<p>Empty</p>
+				<div className="tutor-addons-card empty-state tutor-py-20">
+					<div className="card-body">
+						<div className="text-medium-caption tutor-mb-20">Nothing Found!</div>
+						<img src={emptyStateImg} alt="empty state illustration" />
+					</div>
+				</div>
 			)}
 		</div>
 	);
