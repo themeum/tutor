1f94b52c083c6ed829d18ffd0fc67ed46d577b54
jayedul <jayedulk@gmail.com>
2021-10-18 17:48:29 +0600

    Assignment design update v2

--- views/fragments/announcement-list.php
+++ views/fragments/announcement-list.php
@@ -194,7 +194,7 @@ $courses = (current_user_can('administrator')) ? tutils()->get_courses() : tutil
                     </td>
                     <td data-th="<?php _e('Action', 'tutor'); ?>">
                         <div class="tutor-bs-d-flex tutor-bs-align-items-center">
-                            <button class="tutor-btn tutor-is-default tutor-is-xs tutor-mr-10 tutor-announcement-details"  data-tutor-modal-target="<?php echo $details_modal_id; ?>">
+                            <button class="tutor-btn tutor-is-default tutor-is-xs tutor-mr-10"  data-tutor-modal-target="<?php echo $details_modal_id; ?>">
                                 <?php _e('Details', 'tutor'); ?>
                             </button>
                             
