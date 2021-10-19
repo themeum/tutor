1d32592a00b02e4710aabb6407cb7d176a0cfd5c
iamsaief <saiefalemon@gmail.com>
2021-10-19 11:48:41 +0600

    fixed minor changes

--- assets/react/admin-dashboard/addons-list/components/AddonsList.js
+++ assets/react/admin-dashboard/addons-list/components/AddonsList.js
@@ -6,15 +6,19 @@ const emptyStateImg = `${_tutorobject.tutor_url}assets/images/empty-state.svg`;
 
 const AddonsList = () => {
 	const { allAddons, loading } = useAddons();
-	
+
 	return (
-		<div className={`tutor-addons-list-items tutor-mt-30 ${allAddons.length < 3 ? 'is-less-items' : ''}`}>
+		<div
+			className={`tutor-addons-list-items tutor-mt-30 ${allAddons.length < 3 ? 'is-less-items' : ''} ${
+				allAddons.length ? 'is-active' : ''
+			}`}
+		>
 			{allAddons.length ? (
 				allAddons.map((addon, index) => {
-					return <AddonCard addon={addon} key={index} />;
+					return <AddonCard addon={addon} key={index} addonId={index} />;
 				})
 			) : loading ? (
-				<div className="tutor-addons-loading">{/* Loading... */}</div>
+				<div className="tutor-addons-loading"></div>
 			) : (
 				<div className="tutor-addons-card empty-state tutor-py-20">
 					<div className="card-body">
