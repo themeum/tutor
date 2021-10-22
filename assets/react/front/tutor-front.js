510fa18e1b38dbb3df9a5977710cd3102fab74c2
jayedul <jayedulk@gmail.com>
2021-10-22 15:18:23 +0600

    Frontend dashboard review page update v2

--- assets/react/front/tutor-front.js
+++ assets/react/front/tutor-front.js
@@ -1337,40 +1337,6 @@ jQuery(document).ready(function ($) {
     });
 
     /**
-     * Open Tutor Modal to edit review
-     * @since v.1.4.0
-     */
-    $(document).on('click', '.open-tutor-edit-review-modal', function (e) {
-        e.preventDefault();
-
-        var $that = $(this);
-        var review_id = $that.attr('data-review-id');
-
-        var nonce_key = _tutorobject.nonce_key;
-
-        var json_data = { review_id: review_id, action: 'tutor_load_edit_review_modal' };
-        json_data[nonce_key] = _tutorobject[nonce_key];
-
-        $.ajax({
-            url: _tutorobject.ajaxurl,
-            type: 'POST',
-            data: json_data,
-            beforeSend: function () {
-                $that.addClass('tutor-updating-message');
-            },
-            success: function (data) {
-                if (typeof data.data !== 'undefined') {
-                    $('.tutor-edit-review-modal-wrap .modal-container').html(data.data.output);
-                    $('.tutor-edit-review-modal-wrap').attr('data-review-id', review_id).addClass('show');
-                }
-            },
-            complete: function () {
-                $that.removeClass('tutor-updating-message');
-            }
-        });
-    });
-
-    /**
      * Update the rating
      * @since v.1.4.0
      */
