4b43ec8a7a6d112e0dbbbca1b3721e807528c010
Md Rashed Hossain <wprashed@icloud.com>
2021-10-25 10:24:00 +0600

    quiz-attempts-list

--- classes/Quiz_Attempts_List.php
+++ classes/Quiz_Attempts_List.php
@@ -12,15 +12,190 @@ class Quiz_Attempts_List extends \Tutor_List_Table {
 
 	const QUIZ_ATTEMPT_PAGE = 'tutor_quiz_attempts';
 
-	function __construct(){
-		global $status, $page;
+	/**
+	 * Trait for utilities
+	 *
+	 * @var $page_title
+	 */
 
-		//Set parent defaults
-		parent::__construct( array(
-			'singular'  => 'attempt',     //singular name of the listed records
-			'plural'    => 'attempts',    //plural name of the listed records
-			'ajax'      => false        //does this table support ajax?
-		) );
+	use Backend_Page_Trait;
+	/**
+	 * Page Title
+	 *
+	 * @var $page_title
+	 */
+	public $page_title;
+
+	/**
+	 * Bulk Action
+	 *
+	 * @var $bulk_action
+	 */
+	public $bulk_action = true;
+
+	/**
+	 * Handle dependencies
+	 */
+	public function __construct() {
+		$this->page_title = __( 'Quiz Attempts', 'tutor' );
+		/**
+		 * Handle bulk action
+		 *
+		 * @since v2.0.0
+		 */
+		add_action( 'wp_ajax_tutor_quiz_attempts_bulk_action', array( $this, 'quiz_attempts_bulk_action' ) );
+	}
+
+	/**
+	 * Available tabs that will visible on the right side of page navbar
+	 *
+	 * @param string $user_id selected quiz_attempts id | optional.
+	 * @param string $date selected date | optional.
+	 * @param string $search search by user name or email | optional.
+	 * @return array
+	 * @since v2.0.0
+	 */
+	public function tabs_key_value( $user_id, $course_id, $date, $search ): array {
+		$url       = get_pagenum_link();
+		$pass  = self::get_quiz_attempt_number( 'approved', $user_id, $course_id, $date, $search );
+		$fail = self::get_quiz_attempt_number( 'pending', $user_id, $course_id, $date, $search );
+		$pending = self::get_quiz_attempt_number( 'blocked', $user_id, $course_id, $date, $search );
+		$tabs      = array(
+			array(
+				'key'   => 'all',
+				'title' => __( 'All', 'tutor-pro' ),
+				'value' => $pass + $fail + $pending,
+				'url'   => $url . '&data=all',
+			),
+			array(
+				'key'   => 'pass',
+				'title' => __( 'Pass', 'tutor-pro' ),
+				'value' => $pass,
+				'url'   => $url . '&data=pass',
+			),
+			array(
+				'key'   => 'fail',
+				'title' => __( 'Fail', 'tutor-pro' ),
+				'value' => $fail,
+				'url'   => $url . '&data=fail',
+			),
+			array(
+				'key'   => 'pending',
+				'title' => __( 'Pending', 'tutor-pro' ),
+				'value' => $pending,
+				'url'   => $url . '&data=pending',
+			),
+		);
+		return $tabs;
+	}
+
+	/**
+	 * Prepare bulk actions that will show on dropdown options
+	 *
+	 * @return array
+	 * @since v2.0.0
+	 */
+	public function prpare_bulk_actions(): array {
+		$actions = array(
+			$this->bulk_action_default(),
+			$this->bulk_action_delete(),
+		);
+		return $actions;
+	}
+
+	/**
+	 * Count enrolled number by status & filters
+	 * Count all enrollment | approved | cancelled
+	 *
+	 * @param string $status | required.
+	 * @param string $user_id selected user id | optional.
+	 * @param string $date selected date | optional.
+	 * @param string $search_term search by user name or email | optional.
+	 * @return int
+	 * @since v2.0.0
+	 */
+	protected static function get_instructor_number( $status = '', $user_id = '', $course_id = '', $attempt_id = '', $date = '', $search_term = ''  ): int {
+		global $wpdb;
+		$status      = sanitize_text_field( $status );
+		$course_id   = sanitize_text_field( $course_id );
+		$user_id   = sanitize_text_field( $user_id );
+		$attempt_id   = sanitize_text_field( $attempt_id );
+		$date        = sanitize_text_field( $date );
+		$search_term = sanitize_text_field( $search_term );
+
+		$search_term = '%' . $wpdb->esc_like( $search_term ) . '%';
+
+		// add user id in where clause.
+		$user_query = '';
+		if ( '' !== $user_id ) {
+			$user_query = "AND user.ID = $user_id";
+		}
+
+		// add quiz id in where clause.
+		$quiz_query = '';
+		if ( '' !== $quiz_id ) {
+			$quiz_query = "AND quiz.ID = $user_id";
+		}
+
+		$count = $wpdb->get_var(
+			$wpdb->prepare(
+				"SELECT COUNT(attempt_id)
+				FROM 	{$wpdb->prefix}tutor_quiz_attempts quiz_attempts
+					   INNER JOIN {$wpdb->posts} quiz
+							   ON quiz_attempts.quiz_id = quiz.ID
+					   INNER JOIN {$wpdb->users}
+							   ON quiz_attempts.user_id = {$wpdb->users}.ID
+			   WHERE 	attempt_status != %s
+					   AND ( user_email LIKE %s OR display_name LIKE %s OR post_title LIKE %s )
+			   ",
+			   'attempt_started',
+				$status,
+				$search_term,
+				$search_term,
+				$search_term,
+				$search_term
+			)
+		);
+		return $count ? $count : 0;
+	}
+
+	/**
+	 * Handle bulk action for instructor delete
+	 *
+	 * @return string JSON response.
+	 * @since v2.0.0
+	 */
+	public function quiz_attempts_bulk_action() {
+		// check nonce.
+		tutor_utils()->checking_nonce();
+		$status   = isset( $_POST['bulk-action'] ) ? sanitize_text_field( $_POST['bulk-action'] ) : '';
+		$bulk_ids = isset( $_POST['bulk-ids'] ) ? sanitize_text_field( $_POST['bulk-ids'] ) : array();
+		$update   = self::update_quiz_attempts( $status, $bulk_ids );
+		return true === $update ? wp_send_json_success() : wp_send_json_error();
+		exit;
+	}
+
+	/**
+	 * Execute bulk action for enrollments ex: complete | cancel
+	 *
+	 * @param string $status hold status for updating.
+	 * @param string $users_ids ids that need to update.
+	 * @return bool
+	 * @since v2.0.0
+	 */
+	public static function update_quiz_attempts( $status, $user_ids ): bool {
+		global $wpdb;
+		$quiz_attempts_table = $wpdb->tutor_quiz_attempts;
+		$update     = $wpdb->query(
+			$wpdb->prepare(
+				" UPDATE {$quiz_attempts_table}
+				SET 	attempt_status = %s 
+				WHERE ID IN ($user_ids)
+			",
+				$status
+			)
+		);
+		return false === $update ? false : true;
 	}
 
 	function column_default($item, $column_name){
@@ -33,6 +208,7 @@ class Quiz_Attempts_List extends \Tutor_List_Table {
 	}
 
 	function column_student($item){
+		/*
 		$actions = array();
 
 		$actions['answer'] = sprintf('<a href="?page=%s&sub_page=%s&attempt_id=%s">'.__('Review', 'tutor').'</a>',$_REQUEST['page'],'view_attempt',$item->attempt_id);
@@ -52,8 +228,15 @@ class Quiz_Attempts_List extends \Tutor_List_Table {
 			$quiz_title,
 			$item->attempt_id,
 			$this->row_actions($actions)
-		);
+		);*/
+		return $item->attempt_ended_at;
+		
 	}
+
+	function column_student_info($item){
+		return $item->display_name;
+	}
+
 
 	function column_quiz($item){
 		return $item->post_title;
@@ -80,9 +263,13 @@ class Quiz_Attempts_List extends \Tutor_List_Table {
 		echo $item->total_questions;
 	}
 
+	function column_total_correct_answer($item) {
+		echo $item->total_correct_answer;
+	}
+
 	function column_earned_marks($item){
 
-	    if ($item->attempt_status === 'review_required'){
+	    /*if ($item->attempt_status === 'review_required'){
             $output = '<span class="result-review-required">' . __('Under Review', 'tutor') . '</span>';
         }else {
 
@@ -98,7 +285,9 @@ class Quiz_Attempts_List extends \Tutor_List_Table {
                 $output .= '<span class="result-fail">' . __('Fail', 'tutor') . '</span>';
             }
         }
-		return $output;
+		return $output;*/
+
+		return $item->total_marks;
 	}
 
 	function column_attempt_status($item){
