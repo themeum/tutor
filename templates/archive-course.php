56458897ac83a5cc2c2b29debf2f4fc58fd17ef2
Md Rashed Hossain <wprashed@icloud.com>
2021-10-29 12:01:25 +0600

    course-filter

--- templates/archive-course.php
+++ templates/archive-course.php
@@ -19,15 +19,15 @@ $supported_filters = tutor_utils()->get_option('supported_course_filters', array
 
 if ($course_filter && count($supported_filters)) {
 ?>
-	<div class="tutor-course-filter-wrapper">
-		<div class="tutor-course-filter-container">
-			<?php tutor_load_template('course-filter.filters'); ?>
+	<div class="tutor-course-listing-filter tutor-filter-course-grid-2 tutor-filter-course-grid-3-">
+		<div class="tutor-course-filter">
+    		<div class="tutor-course-filter-widget">
+				<?php tutor_load_template('course-filter.filters'); ?>
+			</div>
 		</div>
-		<div>
-			<div id="tutor-course-filter-loop-container" class="<?php tutor_container_classes(); ?> tutor-course-filter-loop-container" data-column_per_row="<?php echo tutor_utils()->get_option( 'courses_col_per_row', 4 ); ?>">
-				<?php tutor_load_template('archive-course-init'); ?>
-			</div><!-- .wrap -->
-		</div>
+		<div id="tutor-course-filter-loop-container" class="<?php tutor_container_classes(); ?> tutor-course-filter-loop-container" data-column_per_row="<?php echo tutor_utils()->get_option( 'courses_col_per_row', 4 ); ?>">
+			<?php tutor_load_template('archive-course-init'); ?>
+		</div><!-- .wrap -->
 	</div>
 <?php
 } else {
