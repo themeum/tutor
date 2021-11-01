78406abba0ad46bf749c383bd46b58144900d6ca
Md Rashed Hossain <wprashed@icloud.com>
2021-11-01 17:01:58 +0600

    filter

--- templates/global/course-archive-filter-bar.php
+++ templates/global/course-archive-filter-bar.php
@@ -22,11 +22,11 @@
     <div class="tutor-course-archive-filters-wrap">
         <form class="tutor-course-filter-form" method="get">
             <select name="tutor_course_filter">
-                <option value="newest_first" <?php selected("newest_first", $sort_by); ?> ><?php _e("Release Date (newest first)", "tutor");
+                <option value="newest_first" <?php selected("newest_first", $sort_by); ?> ><?php esc_html_e("Release Date (newest first)", "tutor");
 					?></option>
-                <option value="oldest_first" <?php selected("oldest_first", $sort_by); ?>><?php _e("Release Date (oldest first)", "tutor"); ?></option>
-                <option value="course_title_az" <?php selected("course_title_az", $sort_by); ?>><?php _e("Course Title (a-z)", "tutor"); ?></option>
-                <option value="course_title_za" <?php selected("course_title_za", $sort_by); ?>><?php _e("Course Title (z-a)", "tutor"); ?></option>
+                <option value="oldest_first" <?php selected("oldest_first", $sort_by); ?>><?php esc_html_e("Release Date (oldest first)", "tutor"); ?></option>
+                <option value="course_title_az" <?php selected("course_title_az", $sort_by); ?>><?php esc_html_e("Course Title (a-z)", "tutor"); ?></option>
+                <option value="course_title_za" <?php selected("course_title_za", $sort_by); ?>><?php esc_html_e("Course Title (z-a)", "tutor"); ?></option>
             </select>
         </form>
     </div>
