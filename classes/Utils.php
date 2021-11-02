3aca85efa4956bf38a043895bf81b853ade136a6
Zaman <personalzaman@gmail.com>
2021-11-02 10:19:51 +0600

    Modified empty state function

--- classes/Utils.php
+++ classes/Utils.php
@@ -7934,10 +7934,11 @@ class Utils {
 	 * 
 	 * @return mixed|html
 	 */
-	public function tutor_empty_state( string $title ) { ?>
+	public function tutor_empty_state( string $title = '' ) { 
+		$page_title = $title ? $title : ''; ?>
 		<div class="tutor-bs-d-flex tutor-bs-d-md-flex tutor-bs-flex-column tutor-bs-justify-content-center tutor-bs-align-items-center">
-			<img src="<?php echo esc_url( tutor()->url . 'assets/images/emptystate.svg' ); ?>" alt="<?php esc_attr_e( $title ); ?>" />
-			<p><?php echo sprintf( esc_html_x( '%s', $title, 'tutor' ), $title ); ?></p>
+			<img src="<?php echo esc_url( tutor()->url . 'assets/images/emptystate.svg' ); ?>" alt="<?php esc_attr_e( $page_title ); ?>" />
+			<p><?php echo sprintf( esc_html_x( '%s', $page_title, 'tutor' ), $page_title ); ?></p>
 		</div>
 	<?php }
 }
