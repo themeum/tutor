8b1ec9ad03064330fd068bec52fbc5e8d084f46e
shewa12 <shewa12kpi@gmail.com>
2021-11-08 13:36:11 +0600

    date-picker read to mount

--- assets/react/v2/tutor-date-picker.js
+++ assets/react/v2/tutor-date-picker.js
@@ -3,10 +3,12 @@ import ReactDom from 'react-dom';
 
 window.addEventListener('DOMContentLoaded', function() {
     function DatePicker() {
-        const element = `<h3>Hello</h3>`;
-        const wrapper =  document.getElementById('tutor-v2-date-picker');
-        if (wrapper) {
-            ReactDom.render(element, document.getElementById('tutor-v2-date-picker'));
+        const element = <h3>Hello</h3>;
+        const wrappers =  document.querySelectorAll('.tutor-v2-date-picker');
+        for(let wrapper of wrappers) {
+            if (wrapper) {
+                ReactDom.render(element, wrapper);
+            }
         }
     }
     DatePicker();
