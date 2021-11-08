a7a7f7d8abc8965962bc6f1fb9a2476b3243eb16
Md Rashed Hossain <wprashed@icloud.com>
2021-11-08 13:10:20 +0600

    header-part

--- views/quiz/header-context/backend-dashboard-students-attempts.php
+++ views/quiz/header-context/backend-dashboard-students-attempts.php
@@ -1,15 +1,13 @@
-<header class="tutor-wp-dashboard-header d-flex justify-content-between align-items-center tutor-px-30 tutor-py-20 tutor-mb-22 tutor-pt-15 tutor-pb-15" style="margin-left:-20px">
-    <div class="header-title text-medium-h5 color-text-primary">
-        <p>
+<header class="tutor-wp-dashboard-header justify-content-between align-items-center tutor-px-30 tutor-py-20 tutor-mb-22 tutor-pt-15 tutor-pb-15" style="margin-left:-20px">
+    <div class="color-text-primary back">
             <a href="<?php echo $back_url; ?>">
-                <strong>Back</strong>
+            <span class="ttr-previous-line"></span> <span class="text"><?php _e('Back', 'tutor'); ?></span>
             </a>
-        </p>
-        <div>
-            <?php echo __('Course', 'tutor').': '.$course_title; ?>
-        </div>
-		<span class="text-primary-h5">
+    </div>
+    <div class="text-regular-small color-text-subsued tutor-mt-30">
+        <?php echo __('Course', 'tutor').': '.$course_title; ?>
+    </div>
+    <div class="header-title text-medium-h5 color-text-primary tutor-mt-10">
             <?php echo __('Quiz', 'tutor') . ': ' . $quiz_title; ?>
-        </span>
     </div>
 </header>
