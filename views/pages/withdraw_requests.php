5cea64478df6a257d06bf9bc8cb17ea6fc76706e
shewa12 <shewa12kpi@gmail.com>
2021-11-09 16:28:39 +0600

    account info masking for withdraw info

--- views/pages/withdraw_requests.php
+++ views/pages/withdraw_requests.php
@@ -178,10 +178,10 @@ $filters = array(
 											</span>
 											<div class="tooltip-wrap">
 												<span class="text-medium-small color-text-primary">
-													<?php echo esc_html( $details['account_number']['value'] ); ?>
+													<?php echo esc_html( tutor_utils()->asterisks_center_text( $details['account_number']['value'] ) ); ?>
 												</span>
-												<div class="tutor-tooltip-wrap-area text-regular-small tooltip-txt tooltip-top">
-													<div class="withdraw-tutor-tooltip-content text-regular-small flex-center">
+												<div class="tutor-tooltip-wrap-area text-regular-small tooltip-txt tooltip-top tutor-bs-d-flex">
+													<div class="withdraw-tutor-tooltip-content text-regular-small flex-center tutor-mr-10">
 														<?php echo esc_html( $details['account_number']['value'] ); ?>
 													</div>
 													<div data-text-copy="00246502343048234045" class="withdraw-tutor-copy-to-clipboard text-regular-small flex-center">
@@ -205,10 +205,10 @@ $filters = array(
 											</span>
 											<div class="tooltip-wrap">
 												<span class="text-medium-small color-text-primary dotedtext">
-													<?php echo esc_html( $details['iban']['value'] ); ?>
+													<?php echo esc_html( tutor_utils()->asterisks_center_text( $details['iban']['value'] ) ); ?>
 												</span>
-												<div class="tutor-tooltip-wrap-area text-regular-small tooltip-txt tooltip-top">
-													<div class="withdraw-tutor-tooltip-content text-regular-small flex-center">
+												<div class="tutor-tooltip-wrap-area text-regular-small tooltip-txt tooltip-top  tutor-bs-d-flex">
+													<div class="withdraw-tutor-tooltip-content text-regular-small flex-center tutor-mr-10">
 														<?php echo esc_html( $details['iban']['value'] ); ?>
 													</div>
 													<div data-text-copy="IBAN0000000065" class="withdraw-tutor-copy-to-clipboard text-regular-small flex-center">
@@ -234,10 +234,10 @@ $filters = array(
 								<?php elseif ( 'paypal_withdraw' === $details['withdraw_method_key'] ) : ?>
 									<div class="tooltip-wrap">
 										<span class="dotedtext">
-											<?php echo esc_html( $details['paypal_email']['value'] ); ?>
+											<?php echo esc_html( '' !== $details['paypal_email']['value'] ? tutor_utils()->asterisks_email( $details['paypal_email']['value'] ) : '' ); ?>
 										</span>
-										<div class="tutor-tooltip-wrap-area text-regular-small tooltip-txt tooltip-top">
-											<div class="withdraw-tutor-tooltip-content text-regular-small flex-center">
+										<div class="tutor-tooltip-wrap-area text-regular-small tooltip-txt tooltip-top  tutor-bs-d-flex me-auto">
+											<div class="withdraw-tutor-tooltip-content text-regular-small flex-center tutor-mr-10">
 												<?php echo esc_html( $details['paypal_email']['value'] ); ?>
 											</div>
 											<div data-text-copy="dehsnmmail@mail.com" class="withdraw-tutor-copy-to-clipboard text-regular-small flex-center">
@@ -289,11 +289,8 @@ $filters = array(
 											</span>
 											<div class="tutor-tooltip-wrap-area text-regular-small tooltip-txt tooltip-left">
 												<div class="withdraw-tutor-tooltip-content text-regular-small flex-center">
-													<strong>
-														<?php echo esc_html( $details['rejects']['reject_type'] ); ?>
-													</strong><br>
 													<span>
-														<?php echo esc_html( $details['rejects']['reject_comment'] ); ?>
+														<?php echo esc_html( '' !== $details['rejects']['reject_comment'] ? $details['rejects']['reject_comment'] : $details['rejects']['reject_type'] ); ?>
 													</span>
 												</div>
 											</div>
