66de23776e6c885859d1853072895f6b280d87dc
shewa12 <shewa12kpi@gmail.com>
2021-11-09 18:14:12 +0600

    react-date-picker added

--- assets/react/v2/tutor-date-picker.js
+++ assets/react/v2/tutor-date-picker.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import TutorDatepicker from '../../../v2-library/src/components/datapicker/TutorDatepicker';
 
 window.addEventListener('DOMContentLoaded', function() {
     function DatePicker() {
-        const element = <h3>Hello</h3>;
+        const element = <TutorDatepicker></TutorDatepicker>;
         const wrappers =  document.querySelectorAll('.tutor-v2-date-picker');
         for(let wrapper of wrappers) {
             if (wrapper) {
