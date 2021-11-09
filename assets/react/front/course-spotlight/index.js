829e4391ee0b9f09258be8ace53b8aed5182d1b8
Md Rashed Hossain <wprashed@icloud.com>
2021-11-09 17:42:40 +0600

    Spotlight-quiz-true-false

--- assets/react/front/course-spotlight/index.js
+++ assets/react/front/course-spotlight/index.js
@@ -1,1 +1,30 @@
-console.log('Course spotlight JS loaded');
+document.addEventListener('DOMContentLoaded', (event) => {
+    const topBar = document.querySelector('.tutor-single-page-top-bar');
+    const sideBar = document.querySelector('.tutor-lesson-sidebar');
+    sideBar.style.top = topBar.clientHeight + 'px';
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const sideBarTabs = document.querySelectorAll('.tutor-sidebar-tab-item');
+    sideBarTabs.forEach((tab) => {
+        tab.addEventListener('click', (event) => {
+            console.log('tab activate');
+            clearActiveClass();
+            event.currentTarget.classList.add('active');
+            let id = event.currentTarget.getAttribute('data-sidebar-tab');
+            document.getElementById(id).classList.add('active');
+        });
+    });
+
+    const clearActiveClass = function() {
+        for (let i = 0; i < sideBarTabs.length; i++) {
+            sideBarTabs[i].classList.remove('active');
+        }
+        let sidebarTabItems = document.querySelectorAll(
+            '.tutor-lesson-sidebar-tab-item'
+        );
+        for (let i = 0; i < sidebarTabItems.length; i++) {
+            sidebarTabItems[i].classList.remove('active');
+        }
+    };
+});
--- templates/single-quiz.php
+++ templates/single-quiz.php
@@ -21,29 +21,57 @@ $enable_spotlight_mode = tutor_utils()->get_option('enable_spotlight_mode');
 <?php do_action('tutor_quiz/single/before/wrap'); ?>
 
 
-    <div class="tutor-single-lesson-wrap <?php echo $enable_spotlight_mode ? "tutor-spotlight-mode" : ""; ?>">
+    <div class="tutor-single-lesson-wraper <?php echo $enable_spotlight_mode ? "tutor-spotlight-mode" : ""; ?>">
 
         <div class="tutor-lesson-sidebar">
 		    <?php tutor_lessons_sidebar(); ?>
         </div>
-        <div id="tutor-single-entry-content" class="tutor-quiz-single-entry-wrap tutor-single-entry-content">
+        <div id="tutor-single-entry-content" class="tutor-quiz-single-entry-wrap tutor-single-entry-content sidebar-hidden">
             <input type="hidden" name="tutor_quiz_id" id="tutor_quiz_id" value="<?php the_ID(); ?>">
-            <div class="tutor-single-page-top-bar">
-                <div class="tutor-topbar-item tutor-hide-sidebar-bar">
-                    <a href="javascript:;" class="tutor-lesson-sidebar-hide-bar"><i class="tutor-icon-angle-left"></i> </a>
-                    <a href="<?php echo get_the_permalink($course->ID); ?>"  class="tutor-topbar-home-btn">
-                        <i class="tutor-icon-home"></i> <?php echo __('Go to Course Home', 'tutor') ; ?>
+            <div class="tutor-single-page-top-bar d-flex justify-content-between">
+                <div class="tutor-topbar-item tutor-topbar-sidebar-toggle tutor-hide-sidebar-bar flex-center">
+                    <a href="javascript:;" class="tutor-lesson-sidebar-hide-bar">
+                        <span class="ttr-icon-light-left-line color-text-white flex-center"></span>
                     </a>
                 </div>
-                <div class="tutor-topbar-item tutor-topbar-content-title-wrap">
+                <div class="tutor-topbar-item tutor-topbar-content-title-wrap flex-center">
                     <?php
-                    tutor_utils()->get_lesson_type_icon(get_the_ID(), true, true);
-                    the_title(); ?>
+
+                    if ($post->post_type === 'tutor_quiz') {
+                        echo wp_kses_post( '<span class="ttr-quiz-filled color-text-white tutor-mr-5"></span>' );
+                        echo wp_kses_post( '<span class="text-regular-caption color-design-white">' );
+                        esc_html_e( 'Quiz: ', 'tutor' );
+                        the_title(); 
+                        echo wp_kses_post( '</span>' );
+                    } elseif ($post->post_type === 'tutor_assignments'){
+                        echo wp_kses_post( '<span class="ttr-assignment-filled color-text-white tutor-mr-5"></span>' );
+                        echo wp_kses_post( '<span class="text-regular-caption color-design-white">' );
+                        esc_html_e( 'Assignment: ', 'tutor' );
+                        the_title(); 
+                        echo wp_kses_post( '</span>' );
+                    } elseif ($post->post_type === 'tutor_zoom_meeting'){
+                        echo wp_kses_post( '<span class="ttr-zoom-brand color-text-white tutor-mr-5"></span>' );
+                        echo wp_kses_post( '<span class="text-regular-caption color-design-white">' );
+                        esc_html_e( 'Zoom Meeting: ', 'tutor' );
+                        the_title(); 
+                        echo wp_kses_post( '</span>' );
+                    } else{
+                        echo wp_kses_post( '<span class="ttr-youtube-brand color-text-white tutor-mr-5"></span>' );
+                        echo wp_kses_post( '<span class="text-regular-caption color-design-white">' );
+                        esc_html_e( 'Lesson: ', 'tutor' );
+                        the_title(); 
+                        echo wp_kses_post( '</span>' );
+                    }
+
+                    ?>
                 </div>
 
-                <div class="tutor-topbar-item tutor-topbar-mark-to-done" style="width: 150px;"></div>
+                <div class="tutor-topbar-cross-icon flex-center">
+                    <span class="ttr-line-cross-line color-text-white flex-center"></span>
+                </div>
+
             </div>
-            <div class="tutor-quiz-single-wrap ">
+            <div class="tutor-quiz-wrapper tutor-quiz-wrapper d-flex justify-content-center tutor-mt-100 tutor-pb-100">
                 <input type="hidden" name="tutor_quiz_id" id="tutor_quiz_id" value="<?php the_ID(); ?>">
 
 		        <?php
