070c8a6806f14df1f491190169896feaaf19a457
iamsaief <saiefalemon@gmail.com>
2021-11-11 13:37:56 +0600

    header btn disable styles

--- views/options/settings.php
+++ views/options/settings.php
@@ -29,7 +29,7 @@
 			</div>
 		</div>
 		<div class="save-button">
-			<button id="save_tutor_option" class="tutor-btn tutor-text-nowrap tutor-btn-disable  tutor-no-hover">
+			<button id="save_tutor_option" class="tutor-btn tutor-text-nowrap" disabled>
 				<?php _e( 'Save Changes', 'tutor' ); ?>
 			</button>
 		</div>
