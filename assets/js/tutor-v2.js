366f5f631131268b460d6c307ed016e051bf7494
Hasan Tareq <hsntareq@gmail.com>
2021-11-14 23:02:47 +0600

    Media upload adding js

--- assets/js/tutor-v2.js
+++ assets/js/tutor-v2.js
@@ -3150,11 +3150,38 @@ window.jQuery(document).ready(function ($) {
   var __ = window.wp.i18n.__;
   /**
    * upload thumbnail
-   */
-
+   * /
   $(document).on('click', '.tutor-thumbnail-uploader .tutor-thumbnail-upload-button', function (event) {
+      event.preventDefault();
+       var wrapper = $(this).closest('.tutor-thumbnail-uploader');
+      var frame;
+      if (frame) {
+          frame.open();
+          return;
+      }
+      frame = wp.media({
+          title: wrapper.data('media-heading'),
+          button: {
+              text: wrapper.data('button-text')
+          },
+          library: { type: "image" },
+          multiple: false, // Set to true to allow multiple files to be selected
+      });
+      frame.on('select', function () {
+          var attachment = frame.state().get('selection').first().toJSON();
+           wrapper.find('img').attr('src', attachment.url);
+          wrapper.find('input[type="hidden"].tutor-tumbnail-id-input').val(attachment.id);
+          wrapper.find('.delete-btn').show();
+      });
+      frame.open();
+  });
+  */
+
+  var uploaderButton = document.querySelector('.tutor-thumbnail-uploader .tutor-thumbnail-upload-button');
+
+  uploaderButton.onclick = function (event) {
     event.preventDefault();
-    var wrapper = $(this).closest('.tutor-thumbnail-uploader');
+    var wrapper = uploaderButton.closest('.tutor-thumbnail-uploader');
     var frame;
 
     if (frame) {
@@ -3163,9 +3190,9 @@ window.jQuery(document).ready(function ($) {
     }
 
     frame = wp.media({
-      title: wrapper.data('media-heading'),
+      title: wrapper.dataset.mediaHeading,
       button: {
-        text: wrapper.data('button-text')
+        text: wrapper.dataset.buttonText
       },
       library: {
         type: "image"
@@ -3173,18 +3200,21 @@ window.jQuery(document).ready(function ($) {
       multiple: false // Set to true to allow multiple files to be selected
 
     });
+    console.log(wrapper);
     frame.on('select', function () {
       var attachment = frame.state().get('selection').first().toJSON();
-      wrapper.find('img').attr('src', attachment.url);
-      wrapper.find('input[type="hidden"].tutor-tumbnail-id-input').val(attachment.id);
-      wrapper.find('.delete-btn').show();
+      wrapper.querySelector('img').setAttribute('src', attachment.url);
+      wrapper.querySelector('input[type="hidden"].tutor-tumbnail-id-input').value = attachment.id;
+      wrapper.querySelector('.delete-btn').style.display = "block";
+      document.getElementById('save_tutor_option').disabled = false;
     });
     frame.open();
-  });
+  };
   /**
    * Thumbnail Delete
    * @since v.1.5.6
    */
+
 
   $(document).on('click', '.tutor-thumbnail-uploader .delete-btn', function (e) {
     e.preventDefault();
--- assets/react/lib/media-chooser.js
+++ assets/react/lib/media-chooser.js
@@ -1,16 +1,16 @@
-window.jQuery(document).ready(function($){
+window.jQuery(document).ready(function ($) {
 
-    const {__} = window.wp.i18n;
+    const { __ } = window.wp.i18n;
 
     /**
      * upload thumbnail
-     */
-     $(document).on( 'click', '.tutor-thumbnail-uploader .tutor-thumbnail-upload-button',  function( event ){
+     * /
+    $(document).on('click', '.tutor-thumbnail-uploader .tutor-thumbnail-upload-button', function (event) {
         event.preventDefault();
 
         var wrapper = $(this).closest('.tutor-thumbnail-uploader');
         var frame;
-        if ( frame ) {
+        if (frame) {
             frame.open();
             return;
         }
@@ -22,7 +22,7 @@ window.jQuery(document).ready(function($){
             library: { type: "image" },
             multiple: false, // Set to true to allow multiple files to be selected
         });
-        frame.on( 'select', function() {
+        frame.on('select', function () {
             var attachment = frame.state().get('selection').first().toJSON();
 
             wrapper.find('img').attr('src', attachment.url);
@@ -31,12 +31,45 @@ window.jQuery(document).ready(function($){
         });
         frame.open();
     });
+*/
 
+    const uploaderButton = document.querySelector('.tutor-thumbnail-uploader .tutor-thumbnail-upload-button');
+    uploaderButton.onclick = (event) => {
+        event.preventDefault();
+        var wrapper = uploaderButton.closest('.tutor-thumbnail-uploader');
+        var frame;
+        if (frame) {
+            frame.open();
+            return;
+        }
+
+
+        frame = wp.media({
+            title: wrapper.dataset.mediaHeading,
+            button: {
+                text: wrapper.dataset.buttonText
+            },
+            library: { type: "image" },
+            multiple: false, // Set to true to allow multiple files to be selected
+        });
+        console.log(wrapper);
+
+        frame.on('select', () => {
+            var attachment = frame.state().get('selection').first().toJSON();
+            wrapper.querySelector('img').setAttribute('src', attachment.url);
+            wrapper.querySelector('input[type="hidden"].tutor-tumbnail-id-input').value = attachment.id;
+            wrapper.querySelector('.delete-btn').style.display = "block";
+            document.getElementById('save_tutor_option').disabled = false;
+        })
+
+        frame.open();
+
+    }
     /**
      * Thumbnail Delete
      * @since v.1.5.6
      */
-     $(document).on('click', '.tutor-thumbnail-uploader .delete-btn', function(e){
+    $(document).on('click', '.tutor-thumbnail-uploader .delete-btn', function (e) {
         e.preventDefault();
 
         var $that = $(this);
@@ -44,7 +77,7 @@ window.jQuery(document).ready(function($){
 
         wrapper.find('input[type="hidden"].tutor-tumbnail-id-input').val('');
         wrapper.find('img').attr('src', '');
-        
+
         $that.hide();
     });
 });
