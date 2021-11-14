872777bfa862b9fa0dbf10ba8fa265c13a956f22
Md Rashed Hossain <wprashed@icloud.com>
2021-11-14 12:35:59 +0600

    quiz-image-multiple-choice & single choice

--- templates/single/quiz/parts/ordering.php
+++ templates/single/quiz/parts/ordering.php
@@ -1,4 +1,4 @@
-<div id="quiz-ordering-ans-area" class="quiz-question-ans-choice-area tutor-mt-70 question-type-<?php echo $question_type; ?> <?php echo $answer_required? 'quiz-answer-required':''; ?> ">
+<div id="quiz-ordering-ans-area" class="quiz-question-ans-choice-area quiz-image-ordering-ans-area tutor-mt-70 question-type-<?php echo $question_type; ?> <?php echo $answer_required? 'quiz-answer-required':''; ?> ">
     <?php
         if ( is_array($answers) && count($answers) ) {
             $answer_i = 0;
@@ -17,18 +17,16 @@
                 echo $answer_i.'.'; 
             ?>
         </div>
-        <div class="quiz-image-ordering-ans-item tutor-ml-20">
-            <div class="tutor-quiz-image-ordering-box d-flex">
-               <div class="tutor-quiz-image-ordering-icon d-flex align-items-center justify-content-center">
-                  <span class="ttr-humnurger-filled color-black-fill"></span>
-               </div>
-               <div class="tutor-quiz-image-ordering-item d-flex align-items-center tutor-p-10">
-                  <img src="<?php echo wp_get_attachment_image_url($answer->image_id, 'full') ?>" />
-                  <span class="text-regular-body color-text-primary tutor-ml-16">
-                    <?php echo $answer_title; ?>
-                  </span>
-                  <input type="hidden" name="attempt[<?php echo $is_started_quiz->attempt_id; ?>][quiz_question][<?php echo $question->question_id; ?>][answers][]" value="<?php echo $answer->answer_id; ?>" >
-               </div>
+        <div class="quiz-image-ordering-ans-item d-flex tutor-ml-20">
+            <div class="tutor-quiz-image-ordering-icon d-flex align-items-center justify-content-center">
+                <span class="ttr-humnurger-filled color-black-fill"></span>
+            </div>
+            <div class="tutor-quiz-image-ordering-item d-flex align-items-center tutor-p-10">
+                <img src="<?php echo wp_get_attachment_image_url($answer->image_id, 'full') ?>" />
+                <span class="text-regular-body color-text-primary tutor-ml-16">
+                <?php echo $answer_title; ?>
+                </span>
+                <input type="hidden" name="attempt[<?php echo $is_started_quiz->attempt_id; ?>][quiz_question][<?php echo $question->question_id; ?>][answers][]" value="<?php echo $answer->answer_id; ?>" >
             </div>
         </div>
     </div>
--- templates/single/quiz/parts/true-false-image.php
+++ templates/single/quiz/parts/true-false-image.php
@@ -1,27 +1,30 @@
 <?php if ($answer->answer_view_format === 'image' || $answer->answer_view_format === 'text_image'){ ?>
-<div id="quiz-image-matching-ans-area" class="quiz-question-ans-choice-area tutor-mt-70 question-type-<?php echo $question_type; ?> <?php echo $answer_required? 'quiz-answer-required':''; ?> "">
-    <div class="quiz-image-matching-ans d-flex align-items-start">
-        <?php
-            if ( is_array($answers) && count($answers) ) {
-                foreach ($answers as $answer){
-                    $answer_title = stripslashes($answer->answer_title);
-                    $answer->is_correct ? $quiz_answers[] = $answer->answer_id : 0; ?>
-       
-        <div class="quiz-image-box">
-            <label for="<?php echo $answer->answer_id; ?>">
-                
-                <div class="quiz-image d-flex tutor-mb-15">
-                    <img src="<?php echo wp_get_attachment_image_url($answer->image_id, 'full') ?>"  width="162" height="162" />
-                </div>
-                <input class="tutor-form-check-input" id="<?php echo $answer->answer_id; ?>" name="attempt[<?php echo $is_started_quiz->attempt_id; ?>][quiz_question][<?php echo $question->question_id; ?>]" type="radio" value="<?php echo $answer->answer_id; ?>">
-                <span class="text-regular-body color-text-primary">
+<div class="quiz-question-ans-choice-area tutor-mt-70 question-type-<?php echo $question_type; ?> <?php echo $answer_required? 'quiz-answer-required':''; ?> "">
+    <div id="tutor-quiz-image-multiple-choice" class="tutor-quiz-wrap">
+        <div class="tutor-image-checkbox">
+            <?php
+                if ( is_array($answers) && count($answers) ) {
+                    foreach ($answers as $answer){
+                        $answer_title = stripslashes($answer->answer_title);
+                        $answer->is_correct ? $quiz_answers[] = $answer->answer_id : 0; ?>
+            <label for="<?php echo $answer->answer_id; ?>" class="tutor-form-check-input">
+                <input type="radio" class="tutor-form-check-input" id="<?php echo $answer->answer_id; ?>" name="attempt[<?php echo $is_started_quiz->attempt_id; ?>][quiz_question][<?php echo $question->question_id; ?>]" value="<?php echo $answer->answer_id; ?>" />
+                <div class="tutor-multiple-checkbox">
+                    <img src="<?php echo wp_get_attachment_image_url($answer->image_id, 'full') ?>" />
                     <?php
-                        if ($answer->answer_view_format !== 'image'){ echo $answer_title;}
+                            if ($answer->answer_view_format == 'text_image') {
                     ?>
-                </span>
+                    <div class="text-regular-body color-text-primary tutor-px-15 tutor-py-10 ">
+                        <?php
+                             echo $answer_title;
+                        ?>
+                    </div>
+                    <?php } ?>
+                    <span class="tutor-icon-checkbox"></span>
+                </div>
             </label>
+            <?php } } ?>
         </div>
-        <?php } }  ?>
     </div>
 </div>
 <?php } ?>
