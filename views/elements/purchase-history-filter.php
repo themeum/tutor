b1a82607303fd5981cb89b9c993540266ac118ce
shewa12 <shewa12kpi@gmail.com>
2021-11-15 13:29:16 +0600

    react-date-picker width updated

--- views/elements/purchase-history-filter.php
+++ views/elements/purchase-history-filter.php
@@ -20,40 +20,6 @@
     <?php endif; ?>
     
     <?php if ( $data['filter_calendar'] ) : ?>
-        
-        <form action="" type="get">
-            <?php if ( isset( $_GET['course_id']) && '' !== $_GET['course_id'] ): ?>
-                <input type="hidden" name="course_id" value="<?php esc_html_e( $_GET['course_id']); ?>">
-            <?php endif; ?>    
-            <div class="calendar-filter tutor-bs-d-lg-flex tutor-bs-align-items-center tutor-bs-justify-content-between">
-                <div class="tutor-input-group tutor-form-control-sm tutor-mr-5">
-                    <input
-                    type="date"
-                    id="tutor-backend-filter-date"
-                    name="start_date"
-                    class="tutor-form-control color-text-subsued"
-                    placeholder="<?php echo esc_attr( get_option( 'date_format' ) ); ?>"
-                    value="<?php esc_attr_e( $start_date ); ?>"
-                    required
-                    />
-                </div>
-                <span class="tutor-mr-5">-</span>
-                <div class="tutor-input-group tutor-form-control-sm tutor-mr-10">
-                    <input
-                    type="date"
-                    id="tutor-backend-filter-date"
-                    name="end_date"
-                    class="tutor-form-control color-text-subsued"
-                    placeholder="<?php echo esc_attr( get_option( 'date_format' ) ); ?>"
-                    value="<?php esc_attr_e( $end_date ); ?>"
-                    required
-                    />
-                </div>  
-                <button type="submit" class="tutor-border tutor-radius-6 tutor-p-4 tutor-bg-white tutor-bs-mt-3 tutor-bs-mt-lg-0">
-                    <span style="vertical-align:middle" class="ttr-search-filled color-primary-main text-regular-h4"></span>
-                </button>
-            </div>
-            <div class="tutor-v2-date-range-picker"></div>
-        </form>
+        <div class="tutor-v2-date-range-picker tutor-bs-d-lg-flex"></div>
     <?php endif; ?>
 </div>
