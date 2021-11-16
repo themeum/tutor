76f264d3eee1359035709baa3dc284853d134212
Md Rashed Hossain <wprashed@icloud.com>
2021-11-16 11:58:11 +0600

    lesson-video-player

--- templates/single/video/vimeo.php
+++ templates/single/video/vimeo.php
@@ -29,18 +29,13 @@ do_action('tutor_lesson/single/before/video/vimeo');
     <div class="tutor-single-lesson-segment tutor-lesson-video-wrap">
 
 		<?php
-		if ($disable_default_player_vimeo){
-			?>
-            <div class="tutor-video-embeded-wrap">
-                <iframe src="https://player.vimeo.com/video/<?php echo $video_id; ?>" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-            </div>
-			<?php
-		}else{
-			?>
-            <div class="plyr__video-embed" id="tutorPlayer">
-                <iframe src="https://player.vimeo.com/video/<?php echo $video_id; ?>?loop=false&amp;byline=false&amp;portrait=false&amp;title=false&amp;speed=true&amp;transparent=0&amp;gesture=media" allowfullscreen allowtransparency allow="autoplay"></iframe>
-            </div>
+		    if ($disable_default_player_vimeo){
+		?>
+            <iframe src="https://player.vimeo.com/video/<?php echo $video_id; ?>" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+        <?php
+            } else {
+        ?>
+            <iframe src="https://player.vimeo.com/video/<?php echo $video_id; ?>?loop=false&amp;byline=false&amp;portrait=false&amp;title=false&amp;speed=true&amp;transparent=0&amp;gesture=media" allowfullscreen allowtransparency allow="autoplay"></iframe>
 		<?php } ?>
-    </div>
 <?php
 do_action('tutor_lesson/single/after/video/vimeo'); ?>
