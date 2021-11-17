9a6cc7600d6482d01d4a58cbd7e8a4bd792ffb04
Md Rashed Hossain <wprashed@icloud.com>
2021-11-17 16:15:36 +0600

    quiz-update

--- templates/single/quiz/previous-attempts.php
+++ templates/single/quiz/previous-attempts.php
@@ -4,6 +4,11 @@
  * @version 1.6.4
  */
 
+$previous_attempts = tutor_utils()->quiz_attempts();
+$attempted_count = is_array($previous_attempts) ? count($previous_attempts) : 0;
+$attempts_allowed = tutor_utils()->get_quiz_option($quiz_id, 'attempts_allowed', 0);
+$attempt_remaining = $attempts_allowed - $attempted_count;
+
 if(isset($_GET['view_quiz_attempt_id'])) {
     // Load single attempt details if ID provided
     $attempt_id = (int) sanitize_text_field(tutils()->array_get('view_quiz_attempt_id', $_GET));
@@ -25,4 +30,20 @@ tutor_load_template_from_custom_path(tutor()->path . '/views/quiz/attempt-table.
     'attempt_list' => $previous_attempts,
     'context' => 'course-single-previous-attempts'
 ));
+
+if ($attempt_remaining > 0 || $attempts_allowed == 0 && $previous_attempts) {
+    do_action('tuotr_quiz/start_form/before', $quiz_id);
 ?>
+	<div class="tutor-quiz-btn-grp tutor-mt-30">
+		<form id="tutor-start-quiz" method="post">
+			<?php wp_nonce_field( tutor()->nonce_action, tutor()->nonce ); ?>
+
+			<input type="hidden" value="<?php echo $quiz_id; ?>" name="quiz_id"/>
+			<input type="hidden" value="tutor_start_quiz" name="tutor_action"/>
+
+			<button type="submit" class="tutor-btn tutor-btn-primary tutor-btn-md start-quiz-btn" name="start_quiz_btn" value="start_quiz">
+				<?php _e( 'Start Quiz', 'tutor' ); ?>
+			</button>
+		</form>
+	</div>
+<?php } ?>
