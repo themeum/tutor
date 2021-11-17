11a5571d10a883ea8ae31323b696cdcbd38806f7
Md Rashed Hossain <wprashed@icloud.com>
2021-11-17 17:52:46 +0600

    Update meta.php

--- templates/single/quiz/parts/meta.php
+++ templates/single/quiz/parts/meta.php
@@ -53,3 +53,49 @@
     </div>
     <?php } ?>
 </div>
+
+<div class="quiz-flash-message">
+    <?php if ($remaining_time_context < 0) { ?>
+    <div class="tutor-quiz-warning-box time-remaining-warning d-flex align-items-center justify-content-between">
+        <div class="flash-info d-flex align-items-center">
+            <span class="ttr-warning-outline-circle-filled color-design-warning tutor-mr-7"></span>
+            <span class="text-regular-caption color-text-title">
+                <?php
+                    _e('Your time limit for this quiz has expired, please reattempt the quiz. Attempts
+                    remaining:', 'tutor');
+                ?>
+                 <?php
+                    if($attempts_allowed != 0){
+                        if($attempted_count){
+                            echo $attempted_count . '/';
+                        }
+                    }
+                    echo $attempts_allowed == 0 ? __('No limit', 'tutor') : $attempts_allowed;
+                ?>
+            </span>
+        </div>
+        <div class="flash-action">
+            <form id="tutor-start-quiz" method="post">
+			    <?php wp_nonce_field( tutor()->nonce_action, tutor()->nonce ); ?>
+			    <input type="hidden" value="<?php echo $quiz_id; ?>" name="quiz_id"/>
+			    <input type="hidden" value="tutor_start_quiz" name="tutor_action"/>
+                <button type="submit" class="tutor-btn tutor-btn-md reattempt-btn" name="start_quiz_btn">
+                    <?php _e('Reattempt', 'tutor'); ?>
+                </button>
+            </form>
+        </div>
+    </div>
+    <?php 
+        }
+        if ($remaining_time_context < 0 && $attempts_allowed == $attempted_count) {
+    ?>
+    <div class="tutor-quiz-warning-box time-over d-flex align-items-center justify-content-between">
+        <div class="flash-info d-flex align-items-center">
+            <span class="ttr-cross-circle-outline-filled color-design-danger tutor-mr-7"></span>
+            <span class="text-regular-caption color-text-title">
+                <?php _e('Unfortunately, you are out of time and quiz attempts.', 'tutor'); ?>
+            </span>
+        </div>
+    </div>
+    <?php } ?>
+</div>
