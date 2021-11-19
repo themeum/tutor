a87d53fd40ef439f383294f5c2370a63f0cc4a3e
shewa12 <shewa12kpi@gmail.com>
2021-11-19 15:27:40 +0600

    instructor listing and profile style updated

--- templates/student-public-profile.php
+++ templates/student-public-profile.php
@@ -76,8 +76,8 @@ foreach ( $tutor_user_social_icons as $key => $social_icon ) {
 ?>
 
 <?php do_action( 'tutor_student/before/wrap' ); ?>
-
-	<div <?php tutor_post_class( 'tutor-full-width-student-profile tutor-page-wrap tutor-user-public-profile tutor-user-public-profile-' . $profile_layout ); ?>>
+<?php $user_identifier = $is_instructor ? 'tutor-instructor' : 'tutor-student'; ?>
+	<div <?php tutor_post_class( 'tutor-full-width-student-profile tutor-page-wrap tutor-user-public-profile tutor-user-public-profile-' . $profile_layout . ' ' . $user_identifier ); ?> >
 		<div class="tutor-bs-container photo-area">
 			<div class="cover-area">
 				<div style="background-image:url(<?php echo tutor_utils()->get_cover_photo_url( $user_id ); ?>)"></div>
