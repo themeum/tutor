ecbcb15de7e1320443e1813922d5accb509a425a
iamsaief <saiefalemon@gmail.com>
2021-11-22 01:07:17 +0600

    popover menu - copy to clipboard

--- v2-library/_src/js/tutorPopupMenu.js
+++ v2-library/_src/js/tutorPopupMenu.js
@@ -50,3 +50,31 @@
 		}
 	});
 })();
+
+/**
+ * Popupover - Copy to clipboard
+ */
+document.addEventListener('click', async (e) => {
+	const btnTargetAttr = 'data-tutor-copy-target';
+	if (e.target.hasAttribute(btnTargetAttr)) {
+		const id = e.target.getAttribute(btnTargetAttr);
+
+		/* Get the text field */
+		const content = document.getElementById(id).textContent;
+
+		/* Copy the text inside the text field */
+		await navigator.clipboard.writeText(content);
+		const copiedTxt = await navigator.clipboard.readText();
+
+		showToolTip(e.target);
+	}
+});
+// Showing tooltip
+const showToolTip = (targetEl) => {
+	const toolTip = `<span class="tooltip">Copied!</span>`;
+	targetEl.insertAdjacentHTML('beforebegin', toolTip);
+
+	setTimeout(() => {
+		document.querySelector('.tooltip').remove();
+	}, 500);
+};
