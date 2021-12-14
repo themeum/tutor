2eb7244dd338e7d9b9b8dd39fb9a8a1fb522907c
shewa12 <shewa12kpi@gmail.com>
2021-12-14 16:51:06 +0600

    coding errors fixed

--- templates/dashboard/quiz-attempts.php
+++ templates/dashboard/quiz-attempts.php
@@ -12,41 +12,49 @@
  */
 
 
-if(isset($_GET['view_quiz_attempt_id'])) {
-    // Load single attempt details if ID provided
-    include __DIR__ . '/quiz-attempts/quiz-reviews.php';
-    return;
+if ( isset( $_GET['view_quiz_attempt_id'] ) ) {
+	// Load single attempt details if ID provided
+	include __DIR__ . '/quiz-attempts/quiz-reviews.php';
+	return;
 }
 
-$per_page = 20;
-$current_page = max( 1, tutor_utils()->array_get('current_page', $_GET) );
-$offset = ($current_page-1)*$per_page;
+$item_per_page = 20;
+$current_page  = max( 1, tutor_utils()->array_get( 'current_page', $_GET ) );
+$offset        = ( $current_page - 1 ) * $item_per_page;
 ?>
 
-<h3><?php _e('Quiz Attempts', 'tutor'); ?></h3>
+<h3><?php esc_html_e( 'Quiz Attempts', 'tutor' ); ?></h3>
 <?php
-$course_id = tutor_utils()->get_assigned_courses_ids_by_instructors();
-$quiz_attempts = tutor_utils()->get_quiz_attempts_by_course_ids($offset, $per_page, $course_id);
-$quiz_attempts_count = tutor_utils()->get_total_quiz_attempts_by_course_ids($course_id);
+$course_id           = tutor_utils()->get_assigned_courses_ids_by_instructors();
+$quiz_attempts       = tutor_utils()->get_quiz_attempts_by_course_ids( $offset, $item_per_page, $course_id );
+$quiz_attempts_count = tutor_utils()->get_total_quiz_attempts_by_course_ids( $course_id );
 
-add_action( 'tutor_quiz/table/after/course_title', function($attempt) {
-    ?>
-    <span><?php _e('Student', 'tutor'); ?>: </span> <strong title="<?php echo $attempt->user_email; ?>"><?php echo $attempt->display_name; ?></strong>
-    <?php
-} );
+add_action(
+	'tutor_quiz/table/after/course_title',
+	function( $attempt ) {
+		?>
+	<span><?php esc_html_e( 'Student', 'tutor' ); ?>: </span> <strong title="<?php echo esc_attr( $attempt->user_email ); ?>"><?php echo esc_attr( $attempt->display_name ); ?></strong>
+		<?php
+	}
+);
 
-tutor_load_template_from_custom_path(tutor()->path . '/views/quiz/attempt-table.php', array(
-    'attempt_list' => $quiz_attempts,
-    'context' => 'frontend-dashboard-students-attempts'
-));
+tutor_load_template_from_custom_path(
+	tutor()->path . '/views/quiz/attempt-table.php',
+	array(
+		'attempt_list' => $quiz_attempts,
+		'context'      => 'frontend-dashboard-students-attempts',
+	)
+);
 
 ?>
 <div class="tutor-pagination">
-    <?php
-    echo paginate_links( array(
-        'format' => '?current_page=%#%',
-        'current' => $current_page,
-        'total' => ceil($quiz_attempts_count/$per_page)
-    ) );
-    ?>
+	<?php
+	echo paginate_links(
+		array(
+			'format'  => '?current_page=%#%',
+			'current' => $current_page,
+			'total'   => ceil( $quiz_attempts_count / $item_per_page ),
+		)
+	);
+	?>
 </div>
