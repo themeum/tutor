318aae9282eb24fc4101fd7a0e24fb107ee06b28
iamsaief <saiefalemon@gmail.com>
2021-12-24 15:59:04 +0600

    header responsive, wishlist row removed, thumbnail bg image

--- templates/dashboard/wishlist.php
+++ templates/dashboard/wishlist.php
@@ -11,32 +11,30 @@ $wishlists = tutor_utils()->get_wishlist();
 <div class="tutor-text-medium-h5 tutor-color-text-primary tutor-mb-25"><?php _e('Wishlist', 'tutor'); ?></div>
 <div class="tutor-dashboard-content-inner my-wishlist">
 	<?php if (is_array($wishlists) && count($wishlists)): ?>
-		<div class="tutor-bs-row">
-			<div class="tutor-course-listing-grid tutor-course-listing-grid-3">
-				<?php
-					foreach ($wishlists as $post) {
-						setup_postdata($post);
+		<div class="tutor-course-listing-grid tutor-course-listing-grid-3">
+			<?php
+				foreach ($wishlists as $post) {
+					setup_postdata($post);
 
-						/**
-						 * @hook tutor_course/archive/before_loop_course
-						 * @type action
-						 * Usage Idea, you may keep a loop within a wrap, such as bootstrap col
-						 */
-						do_action('tutor_course/archive/before_loop_course');
+					/**
+					 * @hook tutor_course/archive/before_loop_course
+					 * @type action
+					 * Usage Idea, you may keep a loop within a wrap, such as bootstrap col
+					 */
+					do_action('tutor_course/archive/before_loop_course');
 
-						tutor_load_template('loop.course');
+					tutor_load_template('loop.course');
 
-						/**
-						 * @hook tutor_course/archive/after_loop_course
-						 * @type action
-						 * Usage Idea, If you start any div before course loop, you can end it here, such as </div>
-						 */
-						do_action('tutor_course/archive/after_loop_course');
-					}
-					wp_reset_postdata();
-				?>
-			</div>
-    	</div>
+					/**
+					 * @hook tutor_course/archive/after_loop_course
+					 * @type action
+					 * Usage Idea, If you start any div before course loop, you can end it here, such as </div>
+					 */
+					do_action('tutor_course/archive/after_loop_course');
+				}
+				wp_reset_postdata();
+			?>
+		</div>
 	<?php else: ?>
 		<?php tutor_utils()->tutor_empty_state( __('You do not have any course on the wishlist yet.', 'tutor')); ?>
 	<?php endif; ?>
--- templates/loop/thumbnail.php
+++ templates/loop/thumbnail.php
@@ -16,6 +16,6 @@ $placeholder_img = tutor()->url . 'assets/images/placeholder.jpg';
 ?>
 
 <a href="<?php the_permalink(); ?>" class="tutor-course-listing-thumb-permalink"> 
-    <!-- <img src="<?php echo esc_url($tutor_course_img); ?>" alt="Course Thumbnail __________"> -->
+    <!-- <img src="<?php echo esc_url($tutor_course_img); ?>" alt="Course Thumbnail"> -->
     <div class="tutor-course-listing-thumbnail" style="background-image:url(<?php echo empty(esc_url($tutor_course_img)) ? $placeholder_img : esc_url($tutor_course_img) ?>)"></div>
 </a>
