f38f23e2f2440ded78cec7cc52ea4f49f2e94761
shewa12 <shewa12kpi@gmail.com>
2021-12-27 15:21:36 +0600

    spacing & indentation added

--- templates/dashboard/enrolled-courses.php
+++ templates/dashboard/enrolled-courses.php
@@ -1,94 +1,95 @@
 <?php
-    /**
-     * @package TutorLMS/Templates
-     * @version 1.4.3
-     */
+	/**
+	 * @package TutorLMS/Templates
+	 * @version 1.4.3
+	 */
 
-    $page_tabs = array(
-        'enrolled-courses' => __('All Courses', 'tutor'),
-        'enrolled-courses/active-courses' => __('Active Courses', 'tutor'),
-        'enrolled-courses/completed-courses' => __('Completed Courses', 'tutor')
-    );
+	$page_tabs = array(
+		'enrolled-courses'                   => __( 'All Courses', 'tutor' ),
+		'enrolled-courses/active-courses'    => __( 'Active Courses', 'tutor' ),
+		'enrolled-courses/completed-courses' => __( 'Completed Courses', 'tutor' ),
+	);
 
-    // Default tab set
-    (!isset($active_tab, $page_tabs[$active_tab])) ? $active_tab='enrolled-courses' : 0;
+	// Default tab set
+	( ! isset( $active_tab, $page_tabs[ $active_tab ] ) ) ? $active_tab = 'enrolled-courses' : 0;
 
-    // Get course list
-    $courses_list_array = array(
-        'enrolled-courses' => tutor_utils()->get_enrolled_courses_by_user( get_current_user_id(), array( 'private', 'publish' ) ),
-        'enrolled-courses/active-courses' =>  tutor_utils()->get_active_courses_by_user(),
-        'enrolled-courses/completed-courses' => tutor_utils()->get_courses_by_user(),
-    );
+	// Get course list
+	$courses_list_array = array(
+		'enrolled-courses'                   => tutor_utils()->get_enrolled_courses_by_user( get_current_user_id(), array( 'private', 'publish' ) ),
+		'enrolled-courses/active-courses'    => tutor_utils()->get_active_courses_by_user(),
+		'enrolled-courses/completed-courses' => tutor_utils()->get_courses_by_user(),
+	);
 
-    // Prepare course list based on page tab
-    $courses_list = $courses_list_array[$active_tab];
-?>
+	// Prepare course list based on page tab
+	$courses_list = $courses_list_array[ $active_tab ];
+	?>
 
-<h3><?php esc_html_e($page_tabs[$active_tab]); ?></h3>
+<h3><?php esc_html_e( $page_tabs[ $active_tab ] ); ?></h3>
 <div class="tutor-dashboard-content-inner enrolled-courses">
-    <div class="tutor-dashboard-inline-links">
-        <ul>
-            <?php 
-                foreach($page_tabs as $slug => $tab) {
-                    ?>
-                    <li class="<?php echo $slug==$active_tab ? 'active' : '' ?>">
-                        <a href="<?php echo esc_url( tutor_utils()->get_tutor_dashboard_page_permalink( $slug ) ); ?>"> 
-                            <?php 
-                                echo esc_html($tab); 
+	<div class="tutor-dashboard-inline-links">
+		<ul>
+			<?php
+			foreach ( $page_tabs as $slug => $tab ) {
+				?>
+					<li class="<?php echo $slug == $active_tab ? 'active' : ''; ?>">
+						<a href="<?php echo esc_url( tutor_utils()->get_tutor_dashboard_page_permalink( $slug ) ); ?>"> 
+						<?php
+							echo esc_html( $tab );
 
-                                $course_count = ($courses_list_array[$slug] && $courses_list_array[$slug]->have_posts()) ? count($courses_list_array[$slug]->posts) : 0;
-                                if($course_count) {
-                                    echo esc_html('&nbsp;('.$course_count.')');
-                                }
-                            ?>
-                        </a> 
-                    </li>
-                    <?php
-                }
-            ?>
-        </ul>
-    </div>
+							$course_count = ( $courses_list_array[ $slug ] && $courses_list_array[ $slug ]->have_posts() ) ? count( $courses_list_array[ $slug ]->posts ) : 0;
+						if ( $course_count ) {
+							echo esc_html( '&nbsp;(' . $course_count . ')' );
+						}
+						?>
+						</a> 
+					</li>
+					<?php
+			}
+			?>
+		</ul>
+	</div>
 
-    <?php if ( $courses_list && $courses_list->have_posts() ): ?>
-        <div class="tutor-course-listing-grid tutor-course-listing-grid-3">
-            <?php
-                while ( $courses_list->have_posts() ) {
+	<?php if ( $courses_list && $courses_list->have_posts() ) : ?>
+		<div class="tutor-course-listing-grid tutor-course-listing-grid-3">
+			<?php
+			while ( $courses_list->have_posts() ) {
 
-                    $courses_list->the_post();
-                    $avg_rating = tutor_utils()->get_course_rating()->rating_avg;
-                    $tutor_course_img = get_tutor_course_thumbnail_src();
-                    
-                    /**
-                     * wp 5.7.1 showing plain permalink for private post
-                     * since tutor do not work with plain permalink
-                     * url is set to post_type/slug (courses/course-slug)
-                     * @since 1.8.10
-                    */
-                        
-                    $post = $courses_list->post;
-                    $custom_url = home_url( $post->post_type.'/'.$post->post_name );
-                    
-                    /**
-                     * @hook tutor_course/archive/before_loop_course
-                     * @type action
-                     * Usage Idea, you may keep a loop within a wrap, such as bootstrap col
-                     */
-                    do_action( 'tutor_course/archive/before_loop_course' );
-        
-                    tutor_load_template( 'loop.course' );
-        
-                    /**
-                     * @hook tutor_course/archive/after_loop_course
-                     * @type action
-                     * Usage Idea, If you start any div before course loop, you can end it here, such as </div>
-                     */
-                    do_action( 'tutor_course/archive/after_loop_course' );
-                }
-                
-                wp_reset_postdata();
-	        ?>
-        </div>
-    <?php else: ?>
-        <?php tutor_utils()->tutor_empty_state(); ?>
-    <?php endif; ?>
+				$courses_list->the_post();
+				$avg_rating       = tutor_utils()->get_course_rating()->rating_avg;
+				$tutor_course_img = get_tutor_course_thumbnail_src();
+
+				/**
+				 * wp 5.7.1 showing plain permalink for private post
+				 * since tutor do not work with plain permalink
+				 * url is set to post_type/slug (courses/course-slug)
+					 *
+				 * @since 1.8.10
+				*/
+
+				$post       = $courses_list->post;
+				$custom_url = home_url( $post->post_type . '/' . $post->post_name );
+
+				/**
+				 * @hook tutor_course/archive/before_loop_course
+				 * @type action
+				 * Usage Idea, you may keep a loop within a wrap, such as bootstrap col
+				 */
+				do_action( 'tutor_course/archive/before_loop_course' );
+
+				tutor_load_template( 'loop.course' );
+
+				/**
+				 * @hook tutor_course/archive/after_loop_course
+				 * @type action
+				 * Usage Idea, If you start any div before course loop, you can end it here, such as </div>
+				 */
+				do_action( 'tutor_course/archive/after_loop_course' );
+			}
+
+				wp_reset_postdata();
+			?>
+		</div>
+	<?php else : ?>
+		<?php tutor_utils()->tutor_empty_state(); ?>
+	<?php endif; ?>
 </div>
