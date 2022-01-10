d823b835508280d176e18600b3c8f4db2aa7130a
shewa12 <shewa12kpi@gmail.com>
2022-01-10 17:41:17 +0600

    update: tutor_text_editor wrapper added

--- templates/global/tutor-text-editor.php
+++ templates/global/tutor-text-editor.php
@@ -11,21 +11,10 @@
  $content = isset( $data['content'] ) ? $data['content'] : '';
  $args    = isset( $data['args'] ) ? $data['args'] : array();
 ?>
-<div id="<?php echo esc_attr( isset( $data['div_id'] ) ? $data['div_id'] : '' ); ?>"></div>
-<?php
-// var_dump( get_post_meta( $attempt_id, 'instructor_feedback', true ) );
-	// // esc_textarea( get_post_meta( $attempt_id, 'instructor_feedback', true ) );.
-	// $editor_config = array(
-	// 'wpautop'          => true,
-	// 'media_buttons'    => false,
-	// 'default_editor'   => 'TinyMCE',
-	// 'drag_drop_upload' => false,
-	// 'editor_css'       => '<style>#wp-tutor-instructor-quiz-feedback-editor-container { margin-right: 1px;}</style>',
-	// 'textarea_name'    => 'tutor-instructor-feedback-content',
-	// 'tinymce'          => true,
-	// 'quicktags'        => false,
-	// 'textarea_rows'    => 3,
-	// );
-	// wp_editor( get_post_meta( $attempt_id, 'instructor_feedback', true ), 'tutor-instructor-quiz-feedback', $editor_config );
-	tutor_utils()->render_text_editor( $content, $div_id, $args );
-?>
+<div class="tutor-text-editor-wrapper">
+	<div id="<?php echo esc_attr( isset( $data['div_id'] ) ? $data['div_id'] : '' ); ?>"></div>
+	<?php
+		tutor_utils()->render_text_editor( $content, $div_id, $args );
+	?>
+
+</div>
