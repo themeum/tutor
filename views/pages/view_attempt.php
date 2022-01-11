73462b4ce77b449afdfb0bc427efb1ed52b8464d
shewa12 <shewa12kpi@gmail.com>
2022-01-11 15:24:31 +0600

    new: instructor feedback on the frontend added

--- views/pages/view_attempt.php
+++ views/pages/view_attempt.php
@@ -35,31 +35,15 @@ $user    = get_userdata( $user_id );
 				'context'      => 'backend-dashboard-students-attempts',
 			)
 		);
+		/**
+		 * Load Instructor Feedback template
+		 * pass quiz id
+		 *
+		 * @since v2.0.0
+		 */
+		tutor_load_template_from_custom_path(
+			tutor()->path . 'views/quiz/instructor-feedback.php',
+			array( 'quiz_id' => $quiz_id )
+		);
 		?>
 </div>
-
-<div class="wrap">
-	<div class="quiz-attempt-answers-wrap">
-		<div class="attempt-answers-header tutor-mb-10">
-			<div class="attempt-header-quiz">
-				<h3><?php esc_html_e( 'Instructor Feedback', 'tutor' ); ?></h3>
-			</div>
-		</div>
-		<div class="tutor-instructor-feedback-wrap tutor-mb-15">
-			<div id="tutor-instructor-feedback-editor"></div>
-			<?php
-				$content 	= get_post_meta( $quiz_id, 'instructor_feedback', true );
-				$editor_args          = array(
-					'content' => $content,
-					'args'    => array(),
-				);
-				$text_editor_template = tutor()->path . 'templates/global/tutor-text-editor.php';
-				tutor_load_template_from_custom_path( $text_editor_template, $editor_args );
-				?>
-		</div>
-		<button class="tutor-btn <?php echo is_admin() ? 'tutor-btn-wordpress' : ''; ?> tutor-instructor-feedback tutor-mt-5" data-quiz-id="<?php echo esc_attr( $quiz_id ); ?>" data-toast_success_message="<?php esc_html_e( 'Updated', 'tutor' ); ?>">
-				<?php esc_html_e( 'Update', 'tutor' ); ?>
-		</button>
-	</div>
-</div>
-
