e5b1abc0e56706fca8cf6bb5c613bfa2d68efc37
Hasan Tareq <hsntareq@gmail.com>
2022-01-14 18:06:22 +0600

    Fixed multiple issues

--- templates/dashboard/dashboard.php
+++ templates/dashboard/dashboard.php
@@ -225,10 +225,10 @@ $courses_in_progress = tutor_utils()->get_active_courses_by_user( get_current_us
 
 <?php if ( ! tutor_utils()->is_instructor() ) : ?>
 	<div class="tutor-frontend-dashboard-course-porgress">
-		<div class="tutor-text-medium-h5 tutor-color-text-primary tutor-capitalize-text tutor-mb-25">
-			<?php esc_html_e( 'In Progress Course', 'tutor' ); ?>
-		</div>
-		<?php if ( is_array( $courses_in_progress ) && $courses_in_progress->have_posts() ) : ?>
+		<?php if ( $courses_in_progress->have_posts() ) : ?>
+			<div class="tutor-text-medium-h5 tutor-color-text-primary tutor-capitalize-text tutor-mb-25">
+				<?php esc_html_e( 'In Progress Course', 'tutor' ); ?>
+			</div>
 			<?php
 			while ( $courses_in_progress->have_posts() ) :
 				$courses_in_progress->the_post();
