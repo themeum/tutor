4d080673cf980d86cbb0e1ec7b80a18e39954961
nur alam <nuralam@nurs-iMac.local>
2022-01-26 18:57:20 +0600

    my quiz attempt tab done

--- views/quiz/header-context/frontend-dashboard-my-attempts.php
+++ views/quiz/header-context/frontend-dashboard-my-attempts.php
@@ -1,26 +1,27 @@
 <?php if(!empty($back_url)): ?>
     <div class="tutor-color-text-primary back">
-        <a href="<?php echo $back_url; ?>">
-            <span class="ttr-previous-line"></span> <span class="text"><?php _e('Back', 'tutor'); ?></span>
+        <a class="tutor-back-btn" href="<?php echo $back_url; ?>">
+            <span class="ttr-previous-line tutor-color-design-dark"></span>
+            <span class="text tutro-text-regular-caption tutor-color-text-primary"><?php _e('Back', 'tutor'); ?></span>
         </a>
     </div>
 <?php endif; ?>
 
-<div class="text-regular-small tutor-color-text-subsued tutor-mt-30">
+<div class="text-regular-small tutor-color-text-subsued tutor-mt-26">
     <?php _e('Course', 'tutor'); ?>: <?php echo $course_title; ?>
 </div>
 
-<div class="header-title tutor-text-medium-h5 tutor-color-text-primary tutor-mt-10 tutor-mb-20">
+<div class="header-title tutor-text-medium-h5 tutor-color-text-primary tutor-mt-8 tutor-mb-10">
     <?php echo $quiz_title; ?>
 </div>
 
 <div class="tutor-mb-30 tutor-text-regular-small tutor-color-text-subsued">
     <div class="tutor-bs-d-flex">
-        <div class="tutor-mr-15">
-            <?php _e('Quiz Time', 'tutor'); ?>: <span class="tutor-color-text-primary"><strong><?php echo $quiz_time; ?></strong></span>
+        <div class="tutor-mr-15 tutor-color-text-title">
+            <?php _e('Quiz Time', 'tutor'); ?>: <span class="tutor-fweight-600"><?php echo $quiz_time; ?></span>
         </div>
-        <div>
-            <?php _e('Attempt Time', 'tutor'); ?>: <span class="tutor-color-text-primary"><strong><?php echo $attempt_time; ?></strong></span>
+        <div class="tutor-color-text-title">
+            <?php _e('Attempt Time', 'tutor'); ?>: <span class="tutor-fweight-600"><?php echo $attempt_time; ?></span>
         </div>
     </div>
 </div>
