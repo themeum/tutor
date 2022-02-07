acf478b7218f1059269bed4b71dd713ab2929ea0
nur alam <nuralam@nurs-iMac.local>
2022-02-07 12:14:37 +0600

    sidebar add qna text area toggle added

--- views/qna/qna-new.php
+++ views/qna/qna-new.php
@@ -4,11 +4,14 @@
 <div class="tutor-qa-new tutor-quesanswer" data-course_id="<?php echo $course_id; ?>" data-question_id="0" data-context="<?php echo $context; ?>">
     <div class="tutor-quesanswer-askquestion">
         <textarea placeholder="You Have any question?" class="tutor-form-control"></textarea>
-        <div class="ask-new-qna-text-area-show tutor-bs-d-flex tutor-bs-justify-content-end">
-            <button class="tutor-ask-new-qna-btn tutor-btn tutor-btn-primary tutor-btn-md">
+        <div class="tutor-bs-d-flex tutor-bs-justify-content-end">
+            <button class="tutor-btn tutor-btn-primary tutor-btn-md">
                 <?php _e('Ask a New Question', 'tutor'); ?>
                 <!-- <?php _e('Submit My Question', 'tutor'); ?> -->
             </button>
+            <a class="sidebar-ask-new-qna-btn tutor-btn tutor-btn-primary tutor-btn-md">
+                <?php _e('Submit My Question', 'tutor'); ?>
+            </a>
         </div>
     </div>
 </div>
