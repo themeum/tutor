6cc0083c172b32e324869236638a4b1411c0a598
nur alam <nuralam@nurs-iMac.local>
2022-02-07 19:37:09 +0600

    sidebar qna tab scrollbar fixes

--- views/qna/qna-new.php
+++ views/qna/qna-new.php
@@ -6,11 +6,11 @@
         <textarea placeholder="You Have any question?" class="tutor-form-control"></textarea>
         <div class="tutor-bs-d-flex tutor-bs-justify-content-end">
             <button class="tutor-btn tutor-btn-primary tutor-btn-md">
-                <?php _e('Ask a New Question', 'tutor'); ?>
-                <!-- <?php _e('Submit My Question', 'tutor'); ?> -->
+                <!-- <?php _e('Ask a New Question', 'tutor'); ?> -->
+                <?php _e('Submit My Question', 'tutor'); ?>
             </button>
             <a class="sidebar-ask-new-qna-btn tutor-btn tutor-btn-primary tutor-btn-md">
-                <?php _e('Submit My Question', 'tutor'); ?>
+                <?php _e('Ask a New Question', 'tutor'); ?>
             </a>
         </div>
     </div>
