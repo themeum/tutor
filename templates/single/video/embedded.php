037224eaf4de746576138e0a2628f0da5a13ef91
jayedul <jayedulk@gmail.com>
2022-02-09 16:22:54 +0600

    Course content previous next feature implemented

--- templates/single/video/embedded.php
+++ templates/single/video/embedded.php
@@ -13,13 +13,7 @@
 
 if ( ! defined( 'ABSPATH' ) )
 	exit;
-
-global $previous_id;
-global $next_id;
-$content_id = tutor_utils()->get_post_id($course_content_id??null);
-$contents = tutor_utils()->get_course_prev_next_contents_by_id($content_id);
-$previous_id = $contents->previous_id;
-$next_id = $contents->next_id;
+    
 $video_info = tutor_utils()->get_video_info();
 
 do_action( 'tutor_lesson/single/before/video/embedded' );
@@ -29,22 +23,6 @@ do_action( 'tutor_lesson/single/before/video/embedded' );
         <input type="hidden" id="tutor_video_tracking_information" value="<?php echo esc_attr(json_encode($jsonData??null)); ?>">
 
         <?php echo tutor_utils()->array_get('source_embedded', $video_info); ?>
-
-        <?php if($previous_id): ?>
-            <div class="tutor-lesson-prev flex-center">
-                <a href="<?php echo get_the_permalink($previous_id); ?>">
-                    <span class="tutor-icon-angle-left-filled"></span>
-                </a>
-            </div>
-        <?php endif; ?>
-
-        <?php if($next_id): ?>
-            <div class="tutor-lesson-next flex-center">
-                <a href="<?php echo get_the_permalink($next_id); ?>">
-                    <span class="tutor-icon-angle-right-filled"></span>
-                </a>
-            </div>
-        <?php endif; ?>
     </div>
 <?php endif; ?>
 <?php do_action('tutor_lesson/single/after/video/embedded'); ?>
