6fa1cee198853edeb92f463ca82a044e97f2797a
Hasan Tareq <hsntareq@gmail.com>
2022-02-28 14:02:21 +0600

    TUTOR2-514: fixed video loading

--- templates/single/video/embedded.php
+++ templates/single/video/embedded.php
@@ -13,13 +13,14 @@
 
 if ( ! defined( 'ABSPATH' ) )
 	exit;
-    
+
 $video_info = tutor_utils()->get_video_info();
 
 do_action( 'tutor_lesson/single/before/video/embedded' );
 ?>
 <?php if($video_info ): ?>
     <div class="course-players">
+        <div class="loading-spinner"></div>
         <input type="hidden" id="tutor_video_tracking_information" value="<?php echo esc_attr(json_encode($jsonData??null)); ?>">
 
         <?php echo tutor_utils()->array_get('source_embedded', $video_info); ?>
