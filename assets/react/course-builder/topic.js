665f1a28bc67bc71cd8c4295c08b7dfd5633b2c3
nur-alam <nuralam862@gmail.com>
2022-03-03 17:42:55 +0600

    quize timing js updated && topic loading js updated

--- assets/react/course-builder/topic.js
+++ assets/react/course-builder/topic.js
@@ -65,7 +65,7 @@ window.jQuery(document).ready(function($) {
 	/**
 	 * Confirmation for deleting Topic
 	 */
-	$(document).on('click', '.tutor-topics-wrap .topic-delete-btn', function(e) {
+	$(document).on('click', '.tutor-topics-wrap .topic-delete-btn i', function(e) {
 		var $that = $(this);
 		var container = $(this).closest('.tutor-topics-wrap');
 		var topic_id = container.attr('data-topic-id');
