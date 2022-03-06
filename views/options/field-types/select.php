c458fb52299916cbc19604a537e8560ed371103e
sifat009 <bhacker150@gmail.com>
2022-03-06 17:06:00 +0600

    fixed settings page not savig issue

--- views/options/field-types/select.php
+++ views/options/field-types/select.php
@@ -7,7 +7,8 @@
  * @since 2.0
  */
 $field_key = sanitize_key($field['key']);
-$selected_option_key = $this->get($field_key, (isset($field['default']) ? esc_attr($field['default']) : null));
+$dashboard_page_id = (int) tutor_utils()->get_option('tutor_dashboard_page_id');
+// $selected_option_key = $this->get($field_key, (isset($field['default']) ? esc_attr($field['default']) : null));
 $field_id  = sanitize_key('field_' . $field_key); ?>
 
 <div class="tutor-option-field-row" id="<?php echo esc_attr($field_id); ?>">
@@ -18,10 +19,10 @@ $field_id  = sanitize_key('field_' . $field_key); ?>
 			if (!isset($field['options']) && $field['options'] !== false) {
 				echo '<option value="-1">' . __('Select Option', 'tutor') . '</option>';
 			}
-			if (!empty($field['options'])) {
+			if (!empty($field['options'])) { 
 				foreach ($field['options'] as $option_key => $option) {
 			?>
-					<option value="<?php echo esc_attr($option_key); ?>" <?php selected($selected_option_key, $option_key); ?>><?php echo esc_attr($option); ?></option>
+					<option value="<?php echo esc_attr($option_key); ?>" <?php echo $dashboard_page_id === $option_key ? " selected='selected'" : "" ?>><?php echo esc_attr($option); ?></option>
 			<?php
 				}
 			}
