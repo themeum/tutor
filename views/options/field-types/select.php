d286cb06bfe74c8d5ecc93062a6a00247e97e037
sifat009 <bhacker150@gmail.com>
2022-03-07 13:30:48 +0600

    fixed page not save issue

--- views/options/field-types/select.php
+++ views/options/field-types/select.php
@@ -6,6 +6,7 @@
  * @since 2.0
  */
 $field_key = sanitize_key( $field['key'] );
+$dashboard_page_id = (int) tutor_utils()->get_option('tutor_dashboard_page_id');
 $field_id  = sanitize_key( 'field_' . $field_key );?>
 <div class="tutor-option-field-row" id="<?php echo esc_attr( $field_id ); ?>">
 	<?php require tutor()->path . 'views/options/template/common/field_heading.php'; ?>
@@ -18,7 +19,7 @@ $field_id  = sanitize_key( 'field_' . $field_key );?>
 			if ( ! empty( $field['options'] ) ) {
 				foreach ( $field['options'] as $option_key => $option ) {
 					?>
-					<option value="<?php echo esc_attr( $option_key ); ?>" <?php selected( $this->get( $field['key'], ( isset( $field['default'] ) ? esc_attr( $field['default'] ) : null ) ), $option_key ); ?>><?php echo esc_attr( $option ); ?></option>
+					<option value="<?php echo esc_attr( $option_key ); ?>" <?php echo $dashboard_page_id === $option_key ? " selected='selected'" : ""; ?>><?php echo esc_attr( $option ); ?></option>
 					<?php
 				}
 			}
