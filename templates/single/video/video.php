6b8f8c09a5548825635858e59158a068950df2e5
jayedul <jayedulk@gmail.com>
2022-03-08 15:24:02 +0600

    Lesson navigation fix

--- templates/single/video/video.php
+++ templates/single/video/video.php
@@ -16,7 +16,11 @@ if ( ! defined( 'ABSPATH' ) )
 $video_info = tutor_utils()->get_video_info();
 
 do_action('tutor_lesson/single/before/video');
-if ($video_info){
+
+$source_key = is_object($video_info) ? 'source_'.$video_info->source : null;
+$has_source = $source_key ? $video_info->$source_key : null;
+
+if ($has_source){
     tutor_load_template('single.video.'.$video_info->source);
 } else {
     $feature_image = get_post_meta( get_the_ID(  ), '_thumbnail_id', true );
