5d0da8a218a6c18fe1b564eb2c9effe2c63dcc14
shewa12 <shewa12kpi@gmail.com>
2022-03-14 23:45:43 +0600

    update: welcome image style

--- views/pages/welcome.php
+++ views/pages/welcome.php
@@ -10,7 +10,12 @@
 ?>
 
 <div class="wrap">
-	<div class="tutor-welcome-page-wrapper">
-		<img src="<?php echo esc_url( tutor()->url . 'assets/images/whats-new.png' ); ?>" alt="<?php echo esc_attr_e( 'What\'s new on Tutor', 'tutor' ); ?>">
+	<div class="tutor-welcome-page-wrapper" style="min-height: 100vh;
+	background-position: center;
+	background-repeat: no-repeat;
+	height: auto;
+	background-size: cover;
+	width: 100%;
+	height: 100%; background-image: url( <?php echo esc_url( tutor()->url . 'assets/images/whats-new.png' ); ?> );">
 	</div>
 </div>
