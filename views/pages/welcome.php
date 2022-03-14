f0ba5336a9db9ab50a5f0478ae39e2ef16b2fcd7
shewa12 <shewa12kpi@gmail.com>
2022-03-14 23:37:27 +0600

    new: welcome message files

---
+++ views/pages/welcome.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Welcome page template
+ *
+ * @package Tutor\Welcome
+ *
+ * @since v2.0.0
+ */
+
+?>
+
+<div class="wrap">
+	<div class="tutor-welcome-page-wrapper">
+		<img src="<?php echo esc_url( tutor()->url . 'assets/images/whats-new.png' ); ?>" alt="<?php echo esc_attr_e( 'What\'s new on Tutor', 'tutor' ); ?>">
+	</div>
+</div>
