f75991f29cd713ef4695c5625601c15852625804
Shewa <shewa@Shewas-MacBook-Pro.local>
2022-03-15 12:40:03 +0600

    fix: edd price template issue fixed

--- templates/loop/course-price-edd.php
+++ templates/loop/course-price-edd.php
@@ -20,8 +20,8 @@
             $enroll_btn = tutor_course_loop_add_to_cart(false);
 
             $product_id = tutor_utils()->get_course_product_id($course_id);
-            $product    = wc_get_product( $product_id );
-            
+            $edd_price = edd_price( $product_id );
+         
             $total_enrolled = tutor_utils()->count_enrolled_users_by_course($course_id);
             $maximum_students = tutor_utils()->get_course_settings($course_id, 'maximum_students');
 
@@ -32,7 +32,7 @@
                 $price_html = '<div class="list-item-price-with-booking tutor-bs-d-flex tutor-bs-align-items-center tutor-bs-justify-content-between">
                                 <div class="list-item-price tutor-bs-d-flex tutor-bs-align-items-center"> 
                                     <span class="price tutor-text-bold-h6 tutor-color-text-primary">'.
-                                        $product->get_price_html() . 
+                                        $edd_price . 
                                     ' </span>
                                 </div>
                             </div>';
@@ -60,14 +60,14 @@
             }
 
             if ( $maximum_students == $total_enrolled && $maximum_students != 0) {
-                $price_html = '<div class="tutor-bs-d-flex tutor-bs-align-items-center tutor-bs-justify-content-between"><div class="list-item-price tutor-bs-d-flex tutor-bs-align-items-center"> <span class="price tutor-text-bold-h6 tutor-color-text-primary">'.$product->get_price_html() . ' </span></div>';
+                $price_html = '<div class="tutor-bs-d-flex tutor-bs-align-items-center tutor-bs-justify-content-between"><div class="list-item-price tutor-bs-d-flex tutor-bs-align-items-center"> <span class="price tutor-text-bold-h6 tutor-color-text-primary">'.$edd_price . ' </span></div>';
                 $restrict = '<div class="list-item-booking booking-full tutor-bs-d-flex tutor-bs-align-items-center"><div class="booking-progress tutor-bs-d-flex"><span class="btn-icon tutor-color-design-warning tutor-icon-circle-outline-info-filled"></span></div><div class="tutor-text-medium-caption tutor-color-text-primary">'. __('Fully Booked', 'tutor') .'</div></div></div>';
                 echo wp_kses_post($price_html);
                 echo wp_kses_post($restrict);
             }
 
             if ( $maximum_students == 0) {
-                $price_html = '<div class="tutor-bs-d-flex tutor-bs-align-items-center tutor-bs-justify-content-between"><div class="list-item-price tutor-bs-d-flex tutor-bs-align-items-center"> <span class="price tutor-text-bold-h6 tutor-color-text-primary">'.$product->get_price_html() . ' </span></div>';
+                $price_html = '<div class="tutor-bs-d-flex tutor-bs-align-items-center tutor-bs-justify-content-between"><div class="list-item-price tutor-bs-d-flex tutor-bs-align-items-center"> <span class="price tutor-text-bold-h6 tutor-color-text-primary">'.$edd_price . ' </span></div>';
                 $cart_html = '<div class="list-item-button"> '.apply_filters( 'tutor_course_restrict_new_entry', $enroll_btn ) . ' </div></div>';
                 echo wp_kses_post($price_html);
                 echo wp_kses_post($cart_html);
