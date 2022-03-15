f8a3d3531c5ecd2316980fa4beb534f9941aaaf3
Shewa <shewa@Shewas-MacBook-Pro.local>
2022-03-15 11:59:02 +0600

    update: welcome page markup updated

--- views/pages/welcome.php
+++ views/pages/welcome.php
@@ -10,12 +10,7 @@
 ?>
 
 <div class="wrap">
-	<div class="tutor-welcome-page-wrapper" style="min-height: 100vh;
-	background-position: center;
-	background-repeat: no-repeat;
-	height: auto;
-	background-size: cover;
-	width: 100%;
-	height: 100%; background-image: url( <?php echo esc_url( tutor()->url . 'assets/images/whats-new.png' ); ?> );">
+	<div class="tutor-welcome-page-wrapper">
+		<img src="<?php echo esc_url( tutor()->url . 'assets/images/whats-new.png' ); ?>" alt="<?php echo esc_attr_e( 'Welcome image', 'tutor' ); ?>" style="width: 100%;">
 	</div>
 </div>
