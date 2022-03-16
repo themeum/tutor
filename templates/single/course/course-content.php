862e46d9f4d58b2feadbe0673f2833f616aa0d10
Shewa <shewa@Shewas-MacBook-Pro.local>
2022-03-16 18:09:49 +0600

    fix: gutenberg block not working on about content and redundant code removes

--- templates/single/course/course-content.php
+++ templates/single/course/course-content.php
@@ -14,106 +14,18 @@
 
 global $post;
 
-function truncate($text, $length = 100, $ending = '...', $exact = true, $considerHtml = true) 
-{
-    if (is_array($ending)) {
-        extract($ending);
-    }
-    if ($considerHtml) {
-        if (mb_strlen(preg_replace('/<.*?>/', '', $text)) <= $length) {
-            return $text;
-        }
-        $totalLength = mb_strlen($ending);
-        $openTags = array();
-        $truncate = '';
-        preg_match_all('/(<\/?([\w+]+)[^>]*>)?([^<>]*)/', $text, $tags, PREG_SET_ORDER);
-        foreach ($tags as $tag) {
-            if (!preg_match('/img|br|input|hr|area|base|basefont|col|frame|isindex|link|meta|param/s', $tag[2])) {
-                if (preg_match('/<[\w]+[^>]*>/s', $tag[0])) {
-                    array_unshift($openTags, $tag[2]);
-                } else if (preg_match('/<\/([\w]+)[^>]*>/s', $tag[0], $closeTag)) {
-                    $pos = array_search($closeTag[1], $openTags);
-                    if ($pos !== false) {
-                        array_splice($openTags, $pos, 1);
-                    }
-                }
-            }
-            $truncate .= $tag[1];
-
-            $contentLength = mb_strlen(preg_replace('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/i', ' ', $tag[3]));
-            if ($contentLength + $totalLength > $length) {
-                $left = $length - $totalLength;
-                $entitiesLength = 0;
-                if (preg_match_all('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/i', $tag[3], $entities, PREG_OFFSET_CAPTURE)) {
-                    foreach ($entities[0] as $entity) {
-                        if ($entity[1] + 1 - $entitiesLength <= $left) {
-                            $left--;
-                            $entitiesLength += mb_strlen($entity[0]);
-                        } else {
-                            break;
-                        }
-                    }
-                }
-
-                $truncate .= mb_substr($tag[3], 0 , $left + $entitiesLength);
-                break;
-            } else {
-                $truncate .= $tag[3];
-                $totalLength += $contentLength;
-            }
-            if ($totalLength >= $length) {
-                break;
-            }
-        }
-
-    } else {
-        if (mb_strlen($text) <= $length) {
-            return $text;
-        } else {
-            $truncate = mb_substr($text, 0, $length - strlen($ending));
-        }
-    }
-    if (!$exact) {
-        $spacepos = mb_strrpos($truncate, ' ');
-        if (isset($spacepos)) {
-            if ($considerHtml) {
-                $bits = mb_substr($truncate, $spacepos);
-                preg_match_all('/<\/([a-z]+)>/', $bits, $droppedTags, PREG_SET_ORDER);
-                if (!empty($droppedTags)) {
-                    foreach ($droppedTags as $closingTag) {
-                        if (!in_array($closingTag[1], $openTags)) {
-                            array_unshift($openTags, $closingTag[1]);
-                        }
-                    }
-                }
-            }
-            $truncate = mb_substr($truncate, 0, $spacepos);
-        }
-    }
-
-    $truncate .= $ending;
-
-    if ($considerHtml) {
-        foreach ($openTags as $tag) {
-            $truncate .= '</'.$tag.'>';
-        }
-    }
-
-    return $truncate;
-}
-
-
-
 do_action('tutor_course/single/before/content');
 
 if (tutor_utils()->get_option('enable_course_about', true, true)) {
-	$string = get_the_content();
-	$limit = 500;
-	$has_readmore = false;
-	if (strlen($string) > $limit) {
+    $string             = get_the_content();
+    $post_size_in_words = sizeof( explode(" ", $string) );
+	$word_limit         = 100;
+	$has_readmore       = false;
+
+	if ( $post_size_in_words > $word_limit ) {
 		$has_readmore = true;
 		// truncate string
-		$first_part = truncate($string, $limit);
+		$first_part =  force_balance_tags( html_entity_decode( wp_trim_words( htmlentities( wpautop( $string ) ), $word_limit ) ) );
 	}
 ?>
 	<div class='tab-item-content <?php echo $has_readmore ? 'tutor-has-showmore' : '' ?>'>
@@ -124,10 +36,16 @@ if (tutor_utils()->get_option('enable_course_about', true, true)) {
 			<div class="text-regular-body tutor-color-black-60 tutor-mt-12">
 				<?php
                     if ($has_readmore) {
-                        echo "<div class='showmore-short-text'>{$first_part}</div>";
-                        echo "<div class='showmore-text'>{$string}</div>";
+                        ?>
+                        <div class='showmore-short-text'>
+                            <?php echo wp_kses_post( $first_part ); ?>
+                        </div>
+                        <div class='showmore-text'>
+                            <?php echo wp_kses_post( $string ); ?>
+                        </div>
+                        <?php
                     } else {
-                        echo $string;
+                        echo wp_kses_post( $string );
                     }
 				?>
 			</div>
