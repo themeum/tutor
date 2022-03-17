18ec7eaa0bf8e64709185136ebc1c66ff05bbabd
Hasan Tareq <hsntareq@gmail.com>
2022-03-17 12:58:18 +0600

    Backend Setting will keep previous data from now

--- views/options/field-types/checkbox_horizontal.php
+++ views/options/field-types/checkbox_horizontal.php
@@ -12,18 +12,19 @@ if ( ! empty( $field['options'] ) ) {
 	$saved_data                             = $this->get( esc_attr( $field_key ), array() );
 	! is_array( $saved_data ) ? $saved_data = array() : 0;
 	?>
-	<div class="tutor-option-field-row d-block" id="<?php echo esc_attr( $field_id ); ?>">
+	<div class="tutor-option-field-row tutor-d-block" id="<?php echo esc_attr( $field_id ); ?>">
 		<?php include tutor()->path . 'views/options/template/common/field_heading.php'; ?>
-		<div class="tutor-option-field-input d-flex">
-			<div class="type-check d-flex">
+		<div class="tutor-option-field-input tutor-d-flex">
+			<div class="type-check tutor-d-flex">
 				<?php
+				// pr($saved_data);
 				foreach ( $field['options'] as $option_key => $option ) :
-						$input_id = 'tutor_check_' . $option_key;
-						$_checked = in_array( $option_key, $saved_data ) ? 'checked="checked"' : '';
+					// echo $saved_data[$option_key];
+						$_checked = 1 == $saved_data[$option_key] ? ' checked="checked"' : '';
 					?>
 					<div class="tutor-form-check">
-						<input type="checkbox" id="<?php echo esc_attr( $input_id ); ?>" name="tutor_option[<?php echo esc_attr( $field_key ); ?>][<?php echo $option_key; ?>]" value="<?php echo esc_attr( $option_key ); ?>" <?php echo wp_kses_post( $_checked ); ?> class="tutor-form-check-input" />
-						<label for="<?php echo esc_attr( $input_id ); ?>"> <?php echo esc_attr( $option ); ?> </label>
+						<input type="checkbox" id="<?php echo esc_attr( $option_key ); ?>" name="tutor_option[<?php echo esc_attr( $field_key ); ?>][<?php echo $option_key; ?>]" value="<?php echo esc_attr( $option_key ); ?>" <?php echo wp_kses_post( $_checked ); ?> class="tutor-form-check-input" />
+						<label for="<?php echo esc_attr( $option_key ); ?>"> <?php echo esc_attr( $option ); ?> </label>
 					</div>
 				<?php endforeach; ?>
 			</div>
