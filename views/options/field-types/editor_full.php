072d8d48f748e4a0c6a95486ab2616dcba5bc1fa
kawshar <kawshar@joomshaper.com>
2022-03-23 04:14:22 +0600

    design fixes

--- views/options/field-types/editor_full.php
+++ views/options/field-types/editor_full.php
@@ -8,7 +8,7 @@
 
 $field_key  = isset( $field['key'] ) ? esc_attr( $field['key'] ) : null;
 $field_id   = esc_attr( 'field_' . $field_key );
-echo $saved_data = $this->get( $field_key, array() );
+$saved_data = $this->get( $field_key, array() );
 ?>
 <div class="tutor-option-field-row tutor-d-block" id="<?php echo esc_attr( $field_id ); ?>">
 	<?php require tutor()->path . 'views/options/template/common/field_heading.php'; ?>
@@ -17,22 +17,18 @@ echo $saved_data = $this->get( $field_key, array() );
 		<div class="tutor-wp-editor" style="position: relative;">
 			<div class="loading-spinner"></div>
 			<?php
-			$editor_id    = 'editor_' . $field_id;
-			// $content = empty( $saved_data ) ? $content_demo : wp_unslash( $saved_data );
-			// $content = empty( $saved_data ) ? $field['default'] : wp_unslash( $saved_data );
+			$editor_id = 'editor_' . $field_id;
 			$content = empty( $saved_data ) ? $field['default'] : wp_unslash( json_decode( $saved_data ) );
 			$pattern = array( '/\<[\/]{0,1}div[^\>]*\>/i', '/<p>(?:\s|&nbsp;)*?<\/p>/i' );
 			$content = preg_replace( $pattern, '', $content );
 			$content = html_entity_decode($content);
-			$args    = array(
-				'textarea_name' => "tutor_option[{$field_key}]",
+			$args = array(
+				'textarea_name' => 'tutor_option[' .  $field_key. ']',
 				'tinymce'       => array(
-					'toolbar1' => 'bold,italic,underline,forecolor,fontselect,fontsizeselect,formatselect,alignleft,aligncenter,alignright,bullist,numlist,link,unlink,removeformat',
-					'toolbar2' => '',
-					'toolbar3' => '',
+					'toolbar1' => 'bold, italic, underline, forecolor, fontselect, fontsizeselect, formatselect, alignleft, aligncenter, alignright, bullist, numlist, link, unlink, removeformat',
 				),
 				'media_buttons' => false,
-				'quicktags'     => false,
+				'quicktags'     => true,
 				'elementpath'   => false,
 				'wpautop'       => false,
 				'statusbar'     => false,
