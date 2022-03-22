9c82f566bd4a60057ffa84d56d3934a159ec4d77
kawshar <kawshar@joomshaper.com>
2022-03-22 17:49:40 +0600

    cleaned unnecessary code

--- views/quiz/header-context/course-single-previous-attempts.php
+++ views/quiz/header-context/course-single-previous-attempts.php
@@ -2,7 +2,7 @@
     $passing_grade = tutor_utils()->get_quiz_option(get_the_ID(), 'passing_grade', 0);
 ?>
 <?php if(!empty($back_url)): ?>
-    <div class="tutor-color-black back tutor-mb-24">
+    <div class="tutor-color-black tutor-mb-24">
         <a class="tutor-back-btn" href="<?php echo $back_url; ?>">
             <span class="tutor-icon-previous-line tutor-color-design-dark"></span>
             <span class="text text tutro-tutor-fs-7 tutor-color-black"><?php _e('Back', 'tutor'); ?></span>
