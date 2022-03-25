c2aab5d899a902d4c79f8ee927300a05320ace86
iamsaief <saiefalemon@gmail.com>
2022-03-25 18:08:11 +0600

    frontend dashboard zoom set api responsive issues, filter menu responsive, offcanvas popup menu padding issue

--- templates/dashboard/settings/social-profile.php
+++ templates/dashboard/settings/social-profile.php
@@ -17,7 +17,7 @@ $user = wp_get_current_user();
 		<?php
 			tutor_load_template( 'dashboard.settings.nav-bar', array( 'active_setting_nav' => 'social-profile' ) );
 		?>
-		<div class="tutor-fs-6 tutor-fw-medium tutor-color-black tutor-mt-32"><?php esc_html_e( 'Social Profile Link', 'tutor' ) ?></div>
+		<div class="tutor-fs-5 tutor-fw-medium tutor-color-black tutor-mt-32"><?php esc_html_e( 'Social Profile Link', 'tutor' ) ?></div>
 	</div>
 
 	<form id="user_social_form" action="" method="post" enctype="multipart/form-data">
@@ -30,16 +30,16 @@ $user = wp_get_current_user();
 			$tutor_user_social_icons = tutor_utils()->tutor_user_social_icons();
 		foreach ( $tutor_user_social_icons as $key => $social_icon ) {
 			?>
-						<div class="tutor-row tutor-align-items-center tutor-mb-32 tutor-social-field">
-							<div class="tutor-col-12 tutor-col-sm-4 tutor-col-md-12 tutor-col-lg-3">
-								<i class="<?php echo esc_html( $social_icon['icon_classes'] ); ?>"></i>
-						<?php echo esc_html( $social_icon['label'] ); ?>
-							</div>
-							<div class="tutor-col-12 tutor-col-sm-8 tutor-col-md-12 tutor-col-lg-6">
-								<input class="tutor-form-control" type="url" name="<?php echo esc_attr( $key ); ?>" value="<?php echo esc_url( get_user_meta( $user->ID, $key, true ) ); ?>" placeholder="<?php echo esc_html( $social_icon['placeholder'] ); ?>">
-							</div>
-						</div>
-				<?php
+				<div class="tutor-row tutor-align-items-center tutor-mb-32 tutor-social-field">
+					<div class="tutor-col-12 tutor-col-sm-4 tutor-col-md-12 tutor-col-lg-3 tutor-d-flex tutor-align-items-center">
+						<i class="<?php echo esc_html( $social_icon['icon_classes'] ); ?> tutor-icon-20 tutor-mr-4"></i>
+				<?php echo esc_html( $social_icon['label'] ); ?>
+					</div>
+					<div class="tutor-col-12 tutor-col-sm-8 tutor-col-md-12 tutor-col-lg-6">
+						<input class="tutor-form-control" type="url" name="<?php echo esc_attr( $key ); ?>" value="<?php echo esc_url( get_user_meta( $user->ID, $key, true ) ); ?>" placeholder="<?php echo esc_html( $social_icon['placeholder'] ); ?>">
+					</div>
+				</div>
+			<?php
 		}
 		?>
 
