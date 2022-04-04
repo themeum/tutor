0865a08a0415fce1e1ab5c5f1105255de06c3b48
kawshar <kawshar@joomshaper.com>
2022-04-04 14:10:08 +0600

    ui improvements

--- views/modal/topic-form.php
+++ views/modal/topic-form.php
@@ -6,6 +6,7 @@
                 <div class="tutor-modal-title">
                     <?php echo $data['modal_title']; ?>
                 </div>
+                
                 <button class="tutor-iconic-btn tutor-modal-close" data-tutor-modal-close>
                     <span class="tutor-icon-times" area-hidden="true"></span>
                 </button>
@@ -13,26 +14,22 @@
 
             <div class="tutor-modal-body">
                 <div class="tutor-mb-32">
-                    <label class="tutor-fs-7 tutor-fw-medium tutor-color-black-70 tutor-mb-4 d-block"><?php _e('Topic Name', 'tutor'); ?></label>
-                    <div class="tutor-input-group tutor-mb-16">
-                        <input type="text" name="topic_title" class="tutor-form-control tutor-mb-12" value="<?php echo !empty($data['title']) ? $data['title'] : ''; ?>"/>
-                        <p class="tutor-input-feedback tutor-has-icon">
-                            <i class="tutor-icon-circle-info-o tutor-input-feedback-icon"></i>
-                            <?php _e('Topic titles are displayed publicly wherever required. Each topic may contain one or more lessons, quiz and assignments.', 'tutor'); ?>
-                        </p>
+                    <label class="tutor-form-label"><?php _e('Topic Name', 'tutor'); ?></label>
+                    <input type="text" name="topic_title" class="tutor-form-control" value="<?php echo !empty($data['title']) ? $data['title'] : ''; ?>"/>
+                    <div class="tutor-input-feedback tutor-has-icon">
+                        <i class="tutor-icon-circle-info-o tutor-input-feedback-icon"></i>
+                        <?php _e('Topic titles are displayed publicly wherever required. Each topic may contain one or more lessons, quiz and assignments.', 'tutor'); ?>
                     </div>
                 </div>
-                
+
                 <div>
-                    <label class="tutor-fs-7 tutor-fw-medium tutor-color-black-70 tutor-mb-4 d-block"><?php _e('Topic Summary', 'tutor'); ?></label>
-                    <div class="tutor-input-group tutor-mb-16">
-                        <textarea name="topic_summery" class="tutor-form-control tutor-mb-12"><?php echo !empty($data['summary']) ? $data['summary'] : ''; ?></textarea>
-                        <p class="tutor-input-feedback tutor-has-icon">
-                            <i class="tutor-icon-circle-info-o tutor-input-feedback-icon"></i>
-                            <?php _e('Add a summary of short text to prepare students for the activities for the topic. The text is shown on the course page beside the tooltip beside the topic name.', 'tutor'); ?>
-                        </p>
-                        <input type="hidden" name="topic_course_id" value="<?php echo $data['course_id']; ?>">
-                        <input type="hidden" name="topic_id" value="<?php echo $data['topic_id']; ?>">
+                    <label class="tutor-form-label"><?php _e('Topic Summary', 'tutor'); ?></label>
+                    <textarea name="topic_summery" class="tutor-form-control tutor-mb-12"><?php echo !empty($data['summary']) ? $data['summary'] : ''; ?></textarea>
+                    <input type="hidden" name="topic_course_id" value="<?php echo $data['course_id']; ?>">
+                    <input type="hidden" name="topic_id" value="<?php echo $data['topic_id']; ?>">
+                    <div class="tutor-input-feedback tutor-has-icon">
+                        <i class="tutor-icon-circle-info-o tutor-input-feedback-icon"></i>
+                        <?php _e('Add a summary of short text to prepare students for the activities for the topic. The text is shown on the course page beside the tooltip beside the topic name.', 'tutor'); ?>
                     </div>
                 </div>
             </div>
