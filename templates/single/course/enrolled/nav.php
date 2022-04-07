143817dc630579abe00dc2478f3f04df16ed5210
kawshar <kawshar@joomshaper.com>
2022-04-08 02:06:42 +0600

    ui fixes

--- templates/single/course/enrolled/nav.php
+++ templates/single/course/enrolled/nav.php
@@ -27,14 +27,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 			 */
 			$default_active_key = apply_filters( 'tutor_default_topics_active_tab', 'info' );
 			?>
-				<li class="tutor-nav-item">
-					<a class="<?php echo $nav_key == $default_active_key ? 'is-active' : ''; ?>" href="#" data-tutor-nav-target="tutor-course-details-tab-<?php echo $nav_key; ?>"><?php echo $nav_item['title']; ?></a>
+				<li>
+					<a class="tutor-nav-item<?php echo $nav_key == $default_active_key ? ' is-active' : ''; ?>" href="#" data-tutor-nav-target="tutor-course-details-tab-<?php echo $nav_key; ?>"><?php echo $nav_item['title']; ?></a>
 				</li>
 			<?php
 		}
 	?>
-	<li class="tutor-nav-item tutor-nav-more tutor-d-none">
-		<a class="tutor-nav-more-item" href="#"><span class="tutor-mr-4">More</span> <span class="icon-seemore tutor-icon-times"></span></a>
+	<li class="tutor-nav-more tutor-d-none">
+		<a class="tutor-nav-item tutor-nav-more-item" href="#"><span class="tutor-mr-4">More</span> <span class="icon-seemore tutor-icon-times"></span></a>
 		<ul class="tutor-nav-more-list tutor-dropdown"></ul>
 	</li>
 </ul>
