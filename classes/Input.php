ec142a617a0b11b2fff05d8d8ae96ecfba14c716
Md Harun Ur Rashid <harun@Mds-MacBook-Air-2.local>
2022-04-11 14:03:49 +0600

    seperate method for get and post request

--- classes/Input.php
+++ classes/Input.php
@@ -22,16 +22,15 @@ class Input {
 	const TYPE_TEXTAREA = 'textarea';
 
 	/**
-	 * Get input value
+	 * common data sanitize method
 	 *
-	 * @param string  $key request key.
-     * @param mixed   $default default value if input key is not exit.
-	 * @param string  $type input type. Default is string.
-	 * @param boolean $trim remove blank splace from start and end.
+	 * @param string	$value		input value.
+	 * @param string 	$default 	default value if input key is not exit
+	 * @param string 	$type 		Default is Input::TYPE_STRING
+	 * @param boolean 	$trim		remove blank splace from start and end.
 	 * @return mixed
 	 */
-	public static function get( $key, $default = null, $type = self::TYPE_STRING, $trim = true ) {
-		$value = isset( $_REQUEST[ $key ] ) ? $_REQUEST[ $key ] : $default;
+	private static function sanitize( $value, $default = null, $type = self::TYPE_STRING, $trim = true ) {
 		if( $value === $default ) return $default;
 
 		if ( $trim ) {
@@ -43,6 +42,34 @@ class Input {
 		if( self::TYPE_TEXTAREA === $type) return sanitize_textarea_field( wp_unslash( $value ) );
 
 		return sanitize_text_field( wp_unslash( $value ) );
+	}
+
+	/**
+	 * Get input value from GET request
+	 *
+	 * @param string  $key 		$_GET request key.
+     * @param mixed   $default 	default value if input key is not exit.
+	 * @param string  $type 	input type. Default is Input::TYPE_STRING
+	 * @param boolean $trim 	remove blank splace from start and end.
+	 * @return mixed
+	 */
+	public static function get( $key, $default = null, $type = self::TYPE_STRING, $trim = true ) {
+		$value = isset( $_GET[ $key ] ) ? $_GET[ $key ] : $default;
+		return self::sanitize( $value, $default, $type, $trim );
+	}
+
+	/**
+	 * Get input value from POST request
+	 *
+	 * @param string  $key 		$_POST request key.
+     * @param mixed   $default 	default value if input key is not exit.
+	 * @param string  $type 	input type. Default is Input::TYPE_STRING
+	 * @param boolean $trim 	remove blank splace from start and end.
+	 * @return mixed
+	 */
+	public static function post( $key, $default = null, $type = self::TYPE_STRING, $trim = true ) {
+		$value = isset( $_POST[ $key ] ) ? $_POST[ $key ] : $default;
+		return self::sanitize( $value, $default, $type, $trim );
 	}
 
 	/**
