0701116ca3f436dbbe19eb080bbc2a2ee0f3b27a
Md Harun Ur Rashid <harun@Mds-MacBook-Air-2.local>
2022-04-13 11:39:50 +0600

    WordPress phpcs fix for Input class

--- classes/Input.php
+++ classes/Input.php
@@ -5,7 +5,7 @@
  * @author: themeum
  * @author_uri: https://themeum.com
  * @package Tutor
- * @since 2.0.0
+ * @since 2.0.2
  */
 
 namespace TUTOR;
@@ -13,33 +13,45 @@ namespace TUTOR;
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
+/**
+ * Input class for handling GET and POST request
+ *
+ * @since 2.0.2
+ */
 class Input {
 
-	const TYPE_RAW = 'raw';
-	const TYPE_STRING = 'string';
-	const TYPE_INT = 'int';
+	const TYPE_RAW      = 'raw';
+	const TYPE_STRING   = 'string';
+	const TYPE_INT      = 'int';
 	const TYPE_TEXTAREA = 'textarea';
 
 	/**
-	 * common data sanitize method
+	 * Common data sanitize method
 	 *
-	 * @param string	$value		input value.
-	 * @param string 	$default 	default value if input key is not exit
-	 * @param string 	$type 		Default is Input::TYPE_STRING
-	 * @param boolean 	$trim		remove blank splace from start and end.
+	 * @param string  $value      input value.
+	 * @param string  $default    default value if input key is not exit.
+	 * @param string  $type       Default is Input::TYPE_STRING.
+	 * @param boolean $trim       remove blank splace from start and end.
 	 * @return mixed
 	 */
 	private static function sanitize( $value, $default = null, $type = self::TYPE_STRING, $trim = true ) {
-		if( $value === $default ) return $default;
+		if ( $value === $default ) {
+			return $default;
+		}
 
 		if ( $trim ) {
 			$value = trim( $value );
 		}
 
-		if( self::TYPE_RAW === $type ) return $value;
-		if( self::TYPE_INT === $type ) return (int) sanitize_text_field( wp_unslash( $value ) );
-		if( self::TYPE_TEXTAREA === $type) return sanitize_textarea_field( wp_unslash( $value ) );
+		if ( self::TYPE_RAW === $type ) {
+			return $value;
+		}
+		if ( self::TYPE_INT === $type ) {
+			return (int) sanitize_text_field( wp_unslash( $value ) );
+		}
+		if ( self::TYPE_TEXTAREA === $type ) {
+			return sanitize_textarea_field( wp_unslash( $value ) );
+		}
 
 		return sanitize_text_field( wp_unslash( $value ) );
 	}
@@ -47,13 +59,14 @@ class Input {
 	/**
 	 * Get input value from GET request
 	 *
-	 * @param string  $key 		$_GET request key.
-     * @param mixed   $default 	default value if input key is not exit.
-	 * @param string  $type 	input type. Default is Input::TYPE_STRING
-	 * @param boolean $trim 	remove blank splace from start and end.
+	 * @param string  $key      $_GET request key.
+	 * @param mixed   $default  default value if input key is not exit.
+	 * @param string  $type     input type. Default is Input::TYPE_STRING.
+	 * @param boolean $trim     remove blank splace from start and end.
 	 * @return mixed
 	 */
 	public static function get( $key, $default = null, $type = self::TYPE_STRING, $trim = true ) {
+		//phpcs:ignore
 		$value = isset( $_GET[ $key ] ) ? $_GET[ $key ] : $default;
 		return self::sanitize( $value, $default, $type, $trim );
 	}
@@ -61,13 +74,14 @@ class Input {
 	/**
 	 * Get input value from POST request
 	 *
-	 * @param string  $key 		$_POST request key.
-     * @param mixed   $default 	default value if input key is not exit.
-	 * @param string  $type 	input type. Default is Input::TYPE_STRING
-	 * @param boolean $trim 	remove blank splace from start and end.
+	 * @param string  $key      $_POST request key.
+	 * @param mixed   $default  default value if input key is not exit.
+	 * @param string  $type     input type. Default is Input::TYPE_STRING.
+	 * @param boolean $trim     remove blank splace from start and end.
 	 * @return mixed
 	 */
 	public static function post( $key, $default = null, $type = self::TYPE_STRING, $trim = true ) {
+		//phpcs:ignore
 		$value = isset( $_POST[ $key ] ) ? $_POST[ $key ] : $default;
 		return self::sanitize( $value, $default, $type, $trim );
 	}
@@ -79,6 +93,7 @@ class Input {
 	 * @return boolean
 	 */
 	public static function has( $key ) {
+		//phpcs:ignore
 		return isset( $_REQUEST[ $key ] );
 	}
 }
