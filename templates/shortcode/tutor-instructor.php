6b2b980b29b9adfa3d229cd8f09cc1464595c8eb
kawshar <kawshar@joomshaper.com>
2022-04-13 14:20:12 +0600

    migrated to new grid system

--- templates/shortcode/tutor-instructor.php
+++ templates/shortcode/tutor-instructor.php
@@ -1,17 +1,15 @@
 <div class="tutor-pagination-wrapper-replacable tutor-instructor-list-wrapper">
 	<?php if ( count( $instructors ) ) : ?>
 		<div class="tutor-instructor-list">
-			<div class="tutor-row tutor-gx-xl-4">
+			<div class="tutor-grid tutor-grid-<?php echo esc_attr( $column_count ); ?>">
 				<?php foreach ( $instructors as $instructor ) : ?>
-					<div class="tutor-col-lg-<?php echo round(12 / $column_count); ?> tutor-mb-32">
-						<?php
-							$instructor->course_count = tutor_utils()->get_course_count_by_instructor( $instructor->ID );
-							$instructor->ratings = tutor_utils()->get_instructor_ratings( $instructor->ID );
-							tutor_load_template( 'instructor.' . $layout, array(
-								'instructor' => $instructor
-							));
-						?>
-					</div>
+					<?php
+						$instructor->course_count = tutor_utils()->get_course_count_by_instructor( $instructor->ID );
+						$instructor->ratings = tutor_utils()->get_instructor_ratings( $instructor->ID );
+						tutor_load_template( 'instructor.' . $layout, array(
+							'instructor' => $instructor
+						));
+					?>
 				<?php endforeach; ?>
 			</div>
 		</div>
@@ -21,7 +19,7 @@
 
 	<?php
 		// @todo: convert to pagination
-		if ($current_page>1 || $instructors_count>$limit) {
+		if ($current_page > 1 || $instructors_count>$limit) {
 
 			$pagination_data = array(
 				'total_page'  => $instructors_count,
