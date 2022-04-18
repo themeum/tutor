765d0770a1783201ad954571ca181f04e8dadabc
kawshar <kawshar@joomshaper.com>
2022-04-18 14:15:40 +0600

    ui fixes for quiz

--- templates/single/quiz/parts/choice-box.php
+++ templates/single/quiz/parts/choice-box.php
@@ -32,8 +32,6 @@
 	}
 ?>
 
-<!-- <div id="tutor-quiz-image-multiple-choice" class="tutor-quiz-multiple-variation tutor-quiz-wrap"> -->
-
 <div class="quiz-question-ans-choice-area tutor-mt-40 question-type-<?php echo $question_type; ?> <?php echo $answer_required ? 'quiz-answer-required' : ''; ?>">
 	<div id="<?php echo $id; ?>" class="<?php echo $class; ?> tutor-quiz-wrap">
 		<div class="tutor-row">
@@ -45,18 +43,9 @@
 					?>
 
 					<?php if ( $answer->answer_view_format !== 'image' && $answer->answer_view_format !== 'text_image' ) : ?>
-						<!-- <div class="quiz-question-ans-choice tutor-mb-16">
-							<label for="<?php echo $answer->answer_id; ?>">
-								<input class="tutor-form-check-input" id="<?php echo $answer->answer_id; ?>" name="attempt[<?php echo $is_started_quiz->attempt_id; ?>][quiz_question][<?php echo $question->question_id; ?>]<?php echo esc_html( 'multiple_choice' === $question_type ? '[]' : '' ); ?>" type="<?php echo $choice_type; ?>" value="<?php echo $answer->answer_id; ?>">
-								<span class="tutor-fs-6 tutor-color-black">
-									<?php echo 'True' === $answer_title || 'False' === $answer_title ? tutor_utils()->translate_dynamic_text( strtolower(  $answer_title ) ) : $answer_title; ?>
-								</span>
-							</label>
-						</div> -->
-
-						<div class="tutor-col-6 tutor-col-lg-3 tutor-mb-16">
+						<div class="tutor-col-6 tutor-col-lg-6 tutor-mb-16">
 							<label for="<?php echo $answer->answer_id; ?>" class="tutor-quiz-question-item">
-								<div class="tutor-card tutor-py-8 tutor-px-16">
+								<div class="tutor-card tutor-px-16 tutor-py-12">
 									<div class="tutor-d-flex tutor-align-items-center">
 										<input class="tutor-form-check-input" id="<?php echo $answer->answer_id; ?>" name="attempt[<?php echo $is_started_quiz->attempt_id; ?>][quiz_question][<?php echo $question->question_id; ?>]<?php echo esc_html( 'multiple_choice' === $question_type ? '[]' : '' ); ?>" type="<?php echo $choice_type; ?>" value="<?php echo $answer->answer_id; ?>">
 										<span class="tutor-fs-6 tutor-color-black tutor-ml-8">
@@ -67,7 +56,7 @@
 							</label>
 						</div>
 					<?php else: ?>
-						<div class="tutor-col-6 tutor-col-lg-3 tutor-mb-16">
+						<div class="tutor-col-6 tutor-col-lg-6 tutor-mb-16">
 							<label for="<?php echo $answer->answer_id; ?>" class="tutor-quiz-question-item tutor-quiz-question-item-has-media">
 								<input type="<?php echo $choice_type; ?>" class="tutor-form-check-input" id="<?php echo $answer->answer_id; ?>" name="attempt[<?php echo $is_started_quiz->attempt_id; ?>][quiz_question][<?php echo $question->question_id; ?>]" value="<?php echo $answer->answer_id; ?>" />
 								<div class="tutor-card">
