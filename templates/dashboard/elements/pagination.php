bf0309d38bb93438257e47b7f87cb236bb932ce5
jayedul <jayedulk@gmail.com>
2022-04-20 15:43:01 +0600

    Pagination fix, report stat count fix

--- templates/dashboard/elements/pagination.php
+++ templates/dashboard/elements/pagination.php
@@ -11,13 +11,14 @@ $paged    = $data['paged'];
 $per_page = $data['per_page'];
 $big      = 999999999;
 $total_page = isset( $data['total_page'] ) ? $data['total_page'] : ceil( $data['total_items'] / $per_page );
+$pagination_enabled_class = wp_doing_ajax() ? ' is-ajax-pagination-enabled ' : '';
 
 // @todo: conditions are incorrect
 
 if(isset($data['layout']) && $data['layout']['type']=='load_more') {
 	$current_url = tutor()->current_url;
 	
-	echo '<nav '.(isset($data['ajax']) ? ' data-tutor_pagination_ajax="'.esc_attr( json_encode($data['ajax']) ).'" ' : '').' data-tutor_pagination_layout="'.esc_attr( json_encode( $data['layout'] ) ).'">';
+	echo '<nav '.(isset($data['ajax']) ? ' data-tutor_pagination_ajax="'.esc_attr( json_encode($data['ajax']) ).'" ' : '').' data-tutor_pagination_layout="'.esc_attr( json_encode( $data['layout'] ) ).'" class="'.$pagination_enabled_class.'">';
 		
 		if ( $paged < $total_page ) {
 			echo '<a class="tutor-btn tutor-btn-outline-primary page-numbers tutor-mr-16" href="'.add_query_arg( array('current_page' => $paged+1), $current_url ).'">'.
@@ -31,7 +32,7 @@ if(isset($data['layout']) && $data['layout']['type']=='load_more') {
 }
 
 if ( (isset( $data['total_page'] ) && $data['total_page']) || (isset( $data['total_items'] ) && $data['total_items']) ) : ?>
-	<nav class="tutor-ui-pagination tutor-mt-40" <?php echo isset($data['ajax']) ? ' data-tutor_pagination_ajax="'.esc_attr( json_encode($data['ajax']) ).'" ' : ''; ?>>
+	<nav class="tutor-ui-pagination tutor-mt-40 <?php echo $pagination_enabled_class; ?>" <?php echo isset($data['ajax']) ? ' data-tutor_pagination_ajax="'.esc_attr( json_encode($data['ajax']) ).'" ' : ''; ?>>
 		<div class="tutor-pagination-hints">
 			<div class="tutor-fs-7 tutor-color-black-60">
 				<?php esc_html_e( 'Page', 'tutor' ); ?> 
