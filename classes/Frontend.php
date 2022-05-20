7b2f13daec68438514f200f84fa746612b58512d
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2022-05-20 11:22:59 +0600

    Restrict wp admin for instructors when setting is enabled

--- classes/Frontend.php
+++ classes/Frontend.php
@@ -21,10 +21,13 @@ class Frontend {
 		add_action( 'after_setup_theme', array( $this, 'remove_admin_bar' ) );
 		add_filter( 'nav_menu_link_attributes', array( $this, 'add_menu_atts' ), 10, 3 );
 		// add_action('pre_get_posts', array($this, 'tutor_offset_courses'));
+		add_action( 'admin_init', array( $this, 'restrict_wp_admin_area' ) );
 	}
+
 	function tutor_offset_courses($query){
 		if (!is_admin() && $query->is_main_query() && is_archive(tutor()->course_post_type)) $query->set('offset', 0);
 	}
+
 	/**
 	 * Remove admin bar based on option
 	 */
@@ -32,6 +35,20 @@ class Frontend {
 		$hide_admin_bar_for_users = (bool) get_tutor_option( 'hide_admin_bar_for_users' );
 		if ( ! current_user_can( 'administrator' ) && ! is_admin() && $hide_admin_bar_for_users ) {
 			show_admin_bar( false );
+		}
+	}
+
+	/**
+	 * Restrict the WP admin area for non-admin users like student, instructor
+	 *
+	 * @return void
+	 */
+	public function restrict_wp_admin_area() {
+		$hide_admin_bar_for_users	= (bool) get_tutor_option( 'hide_admin_bar_for_users' );
+		$is_administrator			= current_user_can( 'administrator' );
+		
+		if ( $hide_admin_bar_for_users && ! $is_administrator && ! wp_doing_ajax() ) {
+			wp_die( __( 'Access Denied!', 'tutor' ) );
 		}
 	}
 
