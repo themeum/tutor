29478e3b989c11d392fdb26f172cc6347a6c452d
sifat009 <bhacker150@gmail.com>
2022-05-25 19:01:23 +0600

    Update: accordion

--- v2-library/_src/js/accordion.js
+++ v2-library/_src/js/accordion.js
@@ -1,20 +1,23 @@
 /**
  * Tutor accrodion
  */
-(window.tutorAccordion = ($) => {
-
-	const accordionItemHeaders = document.querySelectorAll('.tutor-accordion-item-header');
-	if (accordionItemHeaders.length) {	
-		accordionItemHeaders.forEach((accordionItemHeader) => {
-			accordionItemHeader.addEventListener('click', () => {
-				accordionItemHeader.classList.toggle('is-active');
-				const accordionItemBody = accordionItemHeader.nextElementSibling;
-				if (accordionItemHeader.classList.contains('is-active')) {
-					$(accordionItemBody).slideDown();
-				} else {
-					$(accordionItemBody).slideUp();
-				}
+(window.tutorAccordion = () => {
+	(function($) {
+		const accordionItemHeaders = document.querySelectorAll(
+			'.tutor-accordion-item-header',
+		);
+		if (accordionItemHeaders.length) {
+			accordionItemHeaders.forEach((accordionItemHeader) => {
+				accordionItemHeader.addEventListener('click', () => {
+					accordionItemHeader.classList.toggle('is-active');
+					const accordionItemBody = accordionItemHeader.nextElementSibling;
+					if (accordionItemHeader.classList.contains('is-active')) {
+						$(accordionItemBody).slideDown();
+					} else {
+						$(accordionItemBody).slideUp();
+					}
+				});
 			});
-		});
-	}
-})(jQuery);
+		}
+	})(jQuery);
+})();
