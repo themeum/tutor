d36e2da8082e5f75d6bcee6b0aee37b19e43dd61
shewa12 <shewa12kpi@gmail.com>
2022-06-08 17:26:02 +0600

    update: default quiz attemt count cache time updated

--- cache/QuizAttempts.php
+++ cache/QuizAttempts.php
@@ -45,7 +45,7 @@ class QuizAttempts extends AbstractCache {
 	 *
 	 * @since v2.0.6
 	 */
-	private const HOUR_IN_SECONDS = 3600;
+	private const HOUR_IN_SECONDS = 1800;
 
 	/**
 	 * Data for caching
