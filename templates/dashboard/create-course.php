bb6a5432df2a0cbab96deb2bedc3ce71b5094e0e
shewa12 <shewa12kpi@gmail.com>
2022-06-30 13:45:57 +0600

    fix: wp post context fixed

--- templates/dashboard/create-course.php
+++ templates/dashboard/create-course.php
@@ -9,9 +9,17 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
+use TUTOR\Input;
+
 get_tutor_header( true );
 
 do_action( 'tutor_load_template_before', 'dashboard.create-course', null );
+
+$course_id = Input::get( 'course_ID', 0, Input::TYPE_INT );
+$post      = get_post( $course_id );
+
+setup_postdata( $post );
 
 do_action( 'tutor_frontend_course_builder' );
 
