79ecd8d9b8682b14c8ea47d79ae5aae7a66ae45f
shewa12 <shewa12kpi@gmail.com>
2022-07-28 14:00:28 +0600

    new: tinymce init script added

---
+++ assets/react/lib/tinymce.js
@@ -0,0 +1,24 @@
+
+/**
+ * Initialize min version of tinyMCE
+ *
+ * @param {*} selector   css select to init editor 
+ * @param {*} plugins    supported plugins (For ex: codesample code ) separated by space
+ * @param {*} toolbar    default supported tools:
+ * bold italic underline alignleft aligncenter alignright bullist numlist link unlink
+ * 
+ * To add more tools pass as third arguments
+ * @param {*} setContent default content
+ */
+function initTinyMCE(selector, plugins = '', tools = '') {
+    let defaultTools = `bold italic underline alignleft aligncenter alignright bullist numlist link unlink ${tools}`;
+    tinymce.init({
+        selector: selector,
+        height: 250,
+        plugins: plugins,
+        toolbar: defaultTools,
+        menu: {},
+        menubar: {}
+    });
+}
+export default initTinyMCE;
