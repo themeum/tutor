42a0c37a1f42edbcb4d839eeb0b29fabca12f1ff
MD.HARUN-UR-RASHID <harun.cox@gmail.com>
2022-08-22 21:02:08 +0600

    esc_attr added

--- views/options/field-types/number.php
+++ views/options/field-types/number.php
@@ -13,13 +13,13 @@ if ( isset( $field['default'] ) && trim( $value ) === '' ) {
 
 $field_key	= isset( $field['key'] ) ? esc_attr( $field['key'] ) : null;
 $field_id	= esc_attr( 'field_' . $field_key );
-$min		= isset( $field['min'] ) ? esc_attr( $field['min'] ) : 0;
+$min		= isset( $field['min'] ) ? $field['min'] : 0;
 
 ?>
 <div class="tutor-option-field-row" id="<?php echo esc_attr( $field_id ); ?>">
 <?php require tutor()->path . 'views/options/template/common/field_heading.php'; ?>
 
 	<div class="tutor-option-field-input">
-		<input class="tutor-form-control" type="number" name="tutor_option[<?php echo esc_attr( $field_key ); ?>]" value="<?php echo esc_attr( $value ); ?>" placeholder="<?php echo $min; ?>" min="<?php echo $min; ?>">
+		<input class="tutor-form-control" type="number" name="tutor_option[<?php echo esc_attr( $field_key ); ?>]" value="<?php echo esc_attr( $value ); ?>" placeholder="<?php echo esc_attr( $min ); ?>" min="<?php echo esc_attr( $min ); ?>">
 	</div>
 </div>
