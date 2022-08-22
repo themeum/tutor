113608919db43887f7455f2a0a53a05ae88e776e
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2022-08-22 16:00:44 +0600

    Unit Test: get_user_name added

--- tests/phpunit/UtilsTest.php
+++ tests/phpunit/UtilsTest.php
@@ -14,4 +14,26 @@ class UtilsTest extends \WP_UnitTestCase {
         $result = $utils->get_total_review();
 		$this->assertIsNumeric( $result );
 	}
+
+	public function test_get_user_name() {
+		$username           = 'admin';
+		$user               = new \WP_User();
+		$user->user_login   = $username;
+		$user->display_name = $username;
+
+		// If someone not set first and last name
+		$this->assertEquals( 'admin', tutor_utils()->get_user_name( $user ) );
+
+		// If someone only set first name
+		$user->first_name = 'Jhon';
+		$this->assertEquals( 'Jhon', tutor_utils()->get_user_name( $user ) );
+
+		// If someone set first and last name both
+		$user->last_name = 'Kou';
+		$this->assertEquals( 'Jhon Kou', tutor_utils()->get_user_name( $user ) );
+
+		// If someone only set last name
+		unset( $user->first_name );
+		$this->assertEquals( $username, tutor_utils()->get_user_name( $user ) );
+	}
 }
