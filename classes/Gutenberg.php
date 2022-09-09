294bc576c66a55c7391e8e376d0b34f6b7646de8
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2022-09-09 16:15:04 +0600

    Fix - gutenberg main author not change

--- classes/Gutenberg.php
+++ classes/Gutenberg.php
@@ -25,8 +25,39 @@ class Gutenberg {
 		add_action( 'init', array($this, 'register_blocks') );
 		add_filter('block_categories_all', array($this, 'registering_new_block_category'), 10, 2);
 		add_action('wp_ajax_render_block_tutor', array($this, 'render_block_tutor'));
+		add_filter( 'rest_user_query', array($this, 'author_list_dropdown'), 10, 2 );
 	}
 	
+	/**
+	 * Bind author list on gutenberg editor mode
+	 *
+	 * @param array $prepared_args
+	 * @param WP_REST_Request $request
+	 * @return void
+	 * 
+	 * @since 2.1.0
+	 */
+	public function author_list_dropdown( $prepared_args, $request ) {
+		$url	= $request->get_header('referer');
+		$parts	= parse_url( $url );
+		parse_str( $parts['query'], $query );
+		$post_id = (int) $query['post'] ?? 0;
+
+		if ( ! $post_id ) {
+			return $prepared_args;
+		}
+		
+		$post = get_post( $post_id );
+		
+		if ( $post->post_type === tutor()->course_post_type && tutor_utils()->get_option( 'enable_gutenberg_course_edit' ) === true ){
+			// Modify the wp/v2/users endpoint request from gutenberg editor
+			unset( $prepared_args['who'] );
+			$prepared_args['role__in'] = array( 'administrator', 'tutor_instructor' );
+		}
+
+		return $prepared_args;
+	}
+
 	function register_blocks() {
 		global $pagenow;
 		if ( 'widgets.php' !== $pagenow ) {
