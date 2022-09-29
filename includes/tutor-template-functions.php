399b67a3da6624e636c32a38f2daca15fb621f49
shewa12 <shewa12kpi@gmail.com>
2022-09-29 14:08:22 +0600

    update: if course is not purchaseable then don't use wc or edd enroll btn template

--- includes/tutor-template-functions.php
+++ includes/tutor-template-functions.php
@@ -492,13 +492,12 @@ if ( ! function_exists( 'tutor_course_loop_price' ) ) {
 		} else {
 			$tutor_course_sell_by = apply_filters( 'tutor_course_sell_by', null );
 
-			if ( $tutor_course_sell_by ) {
+			if ( $tutor_course_sell_by && tutor_utils()->is_course_purchasable( $course_id ) ) {
 				tutor_load_template( 'loop.course-price-' . $tutor_course_sell_by );
 			} else {
 				tutor_load_template( 'loop.course-price' );
 			}
 		}
-
 		echo apply_filters( 'tutor_course_loop_price', ob_get_clean() );
 	}
 }
