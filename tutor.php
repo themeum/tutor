f68d95f4c5cb9c0f13707541b431dd7f04a012a0
shewa12 <shewa12kpi@gmail.com>
2022-10-17 12:05:50 +0600

    update: change log updated

--- tutor.php
+++ tutor.php
@@ -7,7 +7,7 @@
  * Version: 2.1.0
  * Author URI: https://themeum.com
  * Requires at least: 5.3
- * Tested up to: 6.0.1
+ * Tested up to: 6.0.2
  * License: GPLv2 or later
  * Text Domain: tutor
  */
