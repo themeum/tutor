cbecc9ed91a14f6517d80aa696061bfd07acc1f7
shewa12 <shewa12kpi@gmail.com>
2022-11-04 16:28:50 +0600

    fix: wpcs warning & db prepare

--- classes/Private_Course_Access.php
+++ classes/Private_Course_Access.php
@@ -1,41 +1,85 @@
 <?php
+/**
+ * Private Course Access
+ *
+ * @since 1.0.0
+ * @package Tutor\Course
+ */
+
 namespace TUTOR;
 
-if ( ! defined( 'ABSPATH' ) )
+if ( ! defined( 'ABSPATH' ) ) {
 	exit;
+}
 
-
+/**
+ * Register Tutor's post types
+ *
+ * @version  1.0.0
+ * @package  Tutor\Course
+ * @category Private_Course_Access
+ * @author   Themeum <support@themeum.com>
+ */
 class Private_Course_Access {
-    
-    private $allow_empty=false;
 
-    public function __construct() {
-        add_action('pre_get_posts', array($this, 'enable_private_access'));
-    }
+	/**
+	 * Allow empty
+	 *
+	 * @var boolean
+	 */
+	private $allow_empty = false;
 
-    public function enable_private_access($query=null){
+	/**
+	 * Register hooks
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+		add_action( 'pre_get_posts', array( $this, 'enable_private_access' ) );
+	}
 
-        if(!is_admin() && is_user_logged_in() ){
+	/**
+	 * Enable private course access
+	 *
+	 * @param mixed $query query.
+	 *
+	 * @return void
+	 */
+	public function enable_private_access( $query = null ) {
 
-            global $wpdb;
-            $p_name = isset($query->query['name']) ? $query->query['name'] : '';
-            $p_name = esc_sql($p_name);
+		if ( ! is_admin() && is_user_logged_in() ) {
 
-            if($this->allow_empty && empty($p_name)){
-                $query->set('post_status', array('private', 'publish'));
-                return;
-            }
+			global $wpdb;
+			$p_name = isset( $query->query['name'] ) ? $query->query['name'] : '';
+			$p_name = esc_sql( $p_name );
 
-            // Get using raw query to speed up
-            $course_post_type = tutor()->course_post_type;
-            $private_query = "SELECT ID, post_parent FROM {$wpdb->posts} WHERE post_type='{$course_post_type}' AND post_name='{$p_name}' AND post_status='private'";
-            $result = $wpdb->get_results($private_query);
-            $private_course_id = (is_array($result) && isset($result[0])) ? $result[0]->ID : 0;
-            
-            if($private_course_id>0 && tutor_utils()->is_enrolled($private_course_id)){
-                $this->allow_empty = true;
-                $query->set('post_status', array('private', 'publish'));
-            }
-        }
-    }
+			if ( $this->allow_empty && empty( $p_name ) ) {
+				$query->set( 'post_status', array( 'private', 'publish' ) );
+				return;
+			}
+
+			// Get using raw query to speed up.
+			$course_post_type = tutor()->course_post_type;
+
+			$private_query = $wpdb->prepare(
+				"SELECT ID, post_parent
+                    FROM {$wpdb->posts}
+                    WHERE post_type = %s
+                        AND post_name = %s
+                        AND post_status = %s
+                ",
+				$course_post_type,
+				$p_name,
+				'private'
+			);
+
+			$result = $wpdb->get_results( $private_query ); //phpcs:ignore
+			$private_course_id = ( is_array( $result ) && isset( $result[0] ) ) ? $result[0]->ID : 0;
+
+			if ( $private_course_id > 0 && tutor_utils()->is_enrolled( $private_course_id ) ) {
+				$this->allow_empty = true;
+				$query->set( 'post_status', array( 'private', 'publish' ) );
+			}
+		}
+	}
 }
