4d8c04d4c876d2ea849d09e57fa54291afba695e
shewa12 <shewa12kpi@gmail.com>
2022-11-07 14:46:13 +0600

    fix: question_answers_list class's wpcs warnings fixed

--- classes/Question_Answers_List.php
+++ classes/Question_Answers_List.php
@@ -1,64 +1,106 @@
 <?php
+/**
+ * Question answer list management
+ *
+ * @package Tutor\QuestionAnswer
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 1.0.0
+ */
+
 namespace TUTOR;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Question_Answers_List
+ *
+ * @since 1.0.0
+ */
 class Question_Answers_List {
 
+	/**
+	 * Backend Page trait
+	 */
 	use Backend_Page_Trait;
 
-	const Question_Answer_PAGE = 'question_answer';
+	/**
+	 * Page slug
+	 *
+	 * @var string
+	 */
+	const QUESTION_ANSWER_PAGE = 'question_answer';
 
-	function __construct($register_hook=true) {
-		if(!$register_hook) {
+	/**
+	 * Register hooks & dependencies
+	 *
+	 * @param boolean $register_hook weather to register hook or not.
+	 *
+	 * @return void
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct( $register_hook = true ) {
+		if ( ! $register_hook ) {
 			return;
 		}
-
 	}
 
-
-	function get_items($args=array()) {
+	/**
+	 * Get question answers
+	 *
+	 * @param array $args query args.
+	 *
+	 * @return array
+	 *
+	 * @since 1.0.0
+	 */
+	public function get_items( $args = array() ) {
 		$per_page = tutor_utils()->get_option( 'pagination_per_page' );
 
 		$search_term = '';
-		if (isset($args['search'])){
-			$search_term = sanitize_text_field($args['search']);
+		if ( isset( $args['search'] ) ) {
+			$search_term = sanitize_text_field( $args['search'] );
 		}
 
-		$current_page 	= isset( $_REQUEST['paged'] ) ? absint( $_REQUEST['paged'] ) : 1;
-
-		$question_status= !empty($args['tab']) ? $args['tab'] : null;
-		$items 			= tutor_utils()->get_qa_questions(($current_page-1)*$per_page, $per_page, $search_term, null, null, null, $question_status, false, $args);
-		$total_items 	= tutor_utils()->get_qa_questions(($current_page-1)*$per_page, $per_page, $search_term, null, null, null, $question_status, true, $args);
+		$current_page    = absint( Input::sanitize_request_data( 'paged', 1 ) );
+		$question_status = ! empty( $args['tab'] ) ? $args['tab'] : null;
+		$items           = tutor_utils()->get_qa_questions( ( $current_page - 1 ) * $per_page, $per_page, $search_term, null, null, null, $question_status, false, $args );
+		$total_items     = tutor_utils()->get_qa_questions( ( $current_page - 1 ) * $per_page, $per_page, $search_term, null, null, null, $question_status, true, $args );
 
 		return array(
-			'items' => $items,
+			'items'      => $items,
 			'pagination' => array(
 				'total_items' => $total_items,
 				'per_page'    => $per_page,
-				'paged'		  => $current_page
-			)
+				'paged'       => $current_page,
+			),
 		);
 	}
 
+	/**
+	 * Get bulk action as an array
+	 *
+	 * @return array
+	 */
 	public function get_bulk_actions() {
 		return array(
 			$this->bulk_action_default(),
-			$this->bulk_action_delete()
+			$this->bulk_action_delete(),
 		);
 	}
 
 	/**
 	 * Get answer list by question id & type
 	 *
-	 * @since v2.0.2
-	 *
-	 * @param integer $question_id
-	 * @param string $question_type
+	 * @param integer $question_id question id.
+	 * @param string  $question_type question type.
 	 *
 	 * @return array list of answer
+	 *
+	 * @since 2.0.2
 	 */
 	public static function answer_list_by_question( int $question_id, string $question_type ): array {
 		global $wpdb;
@@ -67,8 +109,8 @@ class Question_Answers_List {
 				"SELECT * FROM {$wpdb->prefix}tutor_quiz_question_answers 
 				where belongs_question_id = %d 
 					AND belongs_question_type = %s 
-				order by answer_order asc ;", 
-				$question_id, 
+				order by answer_order asc ;",
+				$question_id,
 				$question_type
 			)
 		);
