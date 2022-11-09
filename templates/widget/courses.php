19ba9de171a05e4743c578159168916c73dc318c
shewa12 <shewa12kpi@gmail.com>
2022-11-09 13:23:18 +0600

    fix: tutor course widget design broken issue

--- templates/widget/courses.php
+++ templates/widget/courses.php
@@ -4,15 +4,17 @@
  *
  * @package Tutor/Tempaltes
  * @version 1.3.1
- *
  */
 
 if ( have_posts() ) :
-	while ( have_posts() ) : the_post();
+	while ( have_posts() ) :
+		the_post();
 		?>
 		<div class="<?php echo esc_attr( tutor_widget_course_loop_classes() ); ?>">
-			<?php tutor_load_template('loop.course'); ?>
+			<div class="tutor-card tutor-course-card tutor-mb-12">
+				<?php tutor_load_template( 'loop.course' ); ?>
+			</div>
 		</div>
-	<?php
+		<?php
 	endwhile;
 endif;
