6215fcd4242a6a54c84f3a6094ac719188146e5c
shewa12 <shewa12kpi@gmail.com>
2022-12-06 17:26:11 +0600

    Fix: WPCS warnings, esc issues

--- templates/loop/start_content_wrap.php
+++ templates/loop/start_content_wrap.php
@@ -1,8 +1,14 @@
 <?php
 /**
- * @package TutorLMS/Templates
- * @version 1.4.3
+ * Start content wrapper
+ *
+ * @package Tutor\Templates
+ * @subpackage CourseLoopPart
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 1.4.3
  */
+
 ?>
 
 <div class="tutor-card-body">
