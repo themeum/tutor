ac588635af601c2bac81130a3c9b29312342b628
shewa12 <shewa12kpi@gmail.com>
2022-12-06 15:55:01 +0600

    Fix: doc comments updated

--- templates/loop/footer.php
+++ templates/loop/footer.php
@@ -1,10 +1,14 @@
 <?php
 /**
- * @package TutorLMS/Templates
- * @version 1.4.3
+ * @package Tutor\Templates
+ * @subpackage CourseLoopPart
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 1.4.3
  */
+
 ?>
 
 <div class="tutor-card-footer">
-    <?php tutor_course_loop_price(); ?>
+	<?php tutor_course_loop_price(); ?>
 </div>
--- templates/loop/loop-before-content.php
+++ templates/loop/loop-before-content.php
@@ -2,8 +2,11 @@
 /**
  * Course Loop End
  *
- * @package TutorLMS/Templates
- * @version 1.4.3
+ * @package Tutor\Templates
+ * @subpackage CourseLoopPart
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 1.4.3
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
