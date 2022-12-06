a0680d1bd3139c304ca1507282219c13da103d39
shewa12 <shewa12kpi@gmail.com>
2022-12-06 13:11:08 +0600

    Fix: inline spacing, indentation fixed course-continue template

--- templates/loop/course-continue.php
+++ templates/loop/course-continue.php
@@ -29,12 +29,12 @@
 if ( $lesson_url && ! $is_completed_course ) {
 	ob_start();
 	?>
-		<a href="<?php echo esc_url( $lesson_url ); ?>" class="<?php echo $button_class; ?>" data-course_id="<?php echo get_the_ID(); ?>">
+		<a href="<?php echo esc_url( $lesson_url ); ?>" class="<?php echo esc_attr( $button_class ); ?>" data-course_id="<?php echo get_the_ID(); ?>">
 		<?php
 		if ( ! $is_completed_course && $completed_percent != 0 ) {
 			esc_html_e( 'Continue Learning', 'tutor' );
 		}
-		if ( $completed_percent == 0 && ! $is_completed_course ) {
+		if ( 0 == $completed_percent && ! $is_completed_course ) {
 			esc_html_e( 'Start Learning', 'tutor' );
 		}
 		?>
@@ -43,6 +43,7 @@ if ( $lesson_url && ! $is_completed_course ) {
 		$enroll_btn = ob_get_clean();
 }
 
+    //phpcs:ignore --printing safe data.
 	echo apply_filters( 'tutor_course/loop/start/button', $enroll_btn, get_the_ID() );
 ?>
 </div>
