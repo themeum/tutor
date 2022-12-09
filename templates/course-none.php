717626dff11dae173acc2be376d07c20de5bef16
shewa12 <shewa12kpi@gmail.com>
2022-12-09 12:19:31 +0600

    Update: doc comment updated on course-none template

--- templates/course-none.php
+++ templates/course-none.php
@@ -1,13 +1,15 @@
 <?php
 /**
  * TUTOR Course None
- * 
- * @author Themeum
+ *
+ * @package Tutor\Templates
+ * @subpackage CourseEmptyState
+ * @author Themeum <support@themeum.com>
  * @link https://themeum.com
- * @package TutorLMS/Templates
- * @since 1.0.0
- * @version 1.4.3
+ * @since 1.4.3
  */
+
 ?>
 
 <?php tutor_utils()->tutor_empty_state( 'No Course Found', 'tutor' ); ?>
+
