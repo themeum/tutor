2a2da6f422954d7fd389c131c80049b54c6fc778
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2022-12-12 16:18:38 +0600

    wpcs fix templates/single-content-loader.php

--- templates/single-content-loader.php
+++ templates/single-content-loader.php
@@ -2,14 +2,14 @@
 /**
  * Template for displaying single lesson, assignment, quiz etc.
  *
- * @author Themeum
+ * @package Tutor\Templates
+ * @author Themeum <support@themeum.com>
  * @link https://themeum.com
- * @package TutorLMS/Templates
  * @since 1.0.0
- * @version 1.4.3
  */
 
 global $post;
+//phpcs:ignore WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
 $currentPost = $post;
 
 $method_map = array(
@@ -24,15 +24,25 @@ $previous_id = $contents->previous_id;
 $next_id     = $contents->next_id;
 
 $enable_spotlight_mode = tutor_utils()->get_option( 'enable_spotlight_mode' );
-extract( $data ); // $context, $html_content
+//phpcs:ignore WordPress.PHP.DontExtract.extract_extract
+extract( $data ); // $data variable consist $context, $html_content.
 
+/**
+ * Single course sidebar content
+ *
+ * @param boolean $echo echo the content or not.
+ * @param string  $context device context (mobile/desktop).
+ * @return string HTML output string.
+ */
 function tutor_course_single_sidebar( $echo = true, $context = 'desktop' ) {
 	ob_start();
 	tutor_load_template( 'single.lesson.lesson_sidebar', array( 'context' => $context ) );
 	$output = apply_filters( 'tutor_lesson/single/lesson_sidebar', ob_get_clean() );
 
 	if ( $echo ) {
-		echo tutor_kses_html( $output );
+		add_filter( 'wp_kses_allowed_html', 'tutor_kses_allowed_html', 10, 2 );
+		echo wp_kses_post( $output );
+		remove_filter( 'wp_kses_allowed_html', 'tutor_kses_allowed_html' );
 	}
 
 	return $output;
@@ -46,25 +56,31 @@ get_tutor_header();
 
 <?php do_action( 'tutor_' . $context . '/single/before/wrap' ); ?>
 <div class="tutor-course-single-content-wrapper<?php echo $enable_spotlight_mode ? ' tutor-spotlight-mode' : ''; ?>">
-	<div class="tutor-course-single-sidebar-wrapper tutor-<?php echo $context; ?>-sidebar">
+	<div class="tutor-course-single-sidebar-wrapper tutor-<?php echo esc_attr( $context ); ?>-sidebar">
 		<?php tutor_course_single_sidebar(); ?>
 	</div>
 	<div id="tutor-single-entry-content" class="tutor-quiz-single-entry-wrap">
 		<?php ( isset( $method_map[ $context ] ) && is_callable( $method_map[ $context ] ) ) ? $method_map[ $context ]() : 0; ?>
-		<?php echo isset( $html_content ) ? $html_content : ''; ?>
+		<?php
+			/**
+			 * Note: $html_content comes from extracted $data variable
+			 * $html_content consist dynamic HTML content which is loaded by tutor_load_template_from_custom_path
+			 */
+			echo isset( $html_content ) ? $html_content : ''; //phpcs:ignore 
+		?>
 	</div>
 </div>
 
 <!-- Course Progressbar on sm/mobile  -->
 <?php
-	// Get the ID of this content and the corresponding course
+	// Get the ID of this content and the corresponding course.
 	$course_content_id = get_the_ID();
 	$course_id         = tutor_utils()->get_course_id_by_subcontent( $course_content_id );
 
-	// Get total content count
+	// Get total content count.
 	$course_stats = tutor_utils()->get_course_completed_percent( $course_id, 0, true );
 
-	// Is Lesstion Complete
+	// Is Lesstion Complete.
 	$is_completed_lesson = tutor_utils()->is_completed_lesson();
 ?>
 
@@ -73,15 +89,15 @@ get_tutor_header();
 		<div class="tutor-row tutor-align-center">
 			<div class="tutor-spotlight-mobile-progress-left <?php echo ! $is_completed_lesson ? 'tutor-col-sm-8 tutor-col-6' : 'tutor-col-12'; ?>">
 				<div class="tutor-fs-7 tutor-color-muted">
-					<?php echo $course_stats['completed_percent'] . '% '; ?><span><?php echo __( 'Complete', 'tutor' ); ?></span>
+					<?php echo esc_html( $course_stats['completed_percent'] ) . '% '; ?><span><?php esc_html_e( 'Complete', 'tutor' ); ?></span>
 				</div>
 				<div class="list-item-progress tutor-my-16">
-					<div class="tutor-progress-bar tutor-mt-12" style="--tutor-progress-value:<?php echo $course_stats['completed_percent']; ?>%;">
+					<div class="tutor-progress-bar tutor-mt-12" style="--tutor-progress-value:<?php echo esc_attr( $course_stats['completed_percent'] ); ?>%;">
 						<span class="tutor-progress-value" area-hidden="true"></span>
 					</div>
 				</div>
 			</div>
-			
+
 			<?php if ( ! $is_completed_lesson ) : ?>
 				<div class="tutor-spotlight-mobile-progress-right tutor-col-sm-4 tutor-col-6">
 					<?php tutor_lesson_mark_complete_html(); ?>
