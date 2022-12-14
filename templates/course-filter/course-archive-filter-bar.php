ef20f2be82129296555481072d46c752a25e1e32
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2022-12-14 12:12:34 +0600

    WPCS Fix - course-archive-filter-bar.php

--- templates/course-filter/course-archive-filter-bar.php
+++ templates/course-filter/course-archive-filter-bar.php
@@ -1,7 +1,12 @@
 <?php
 /**
- * @package TutorLMS/Templates
- * @version 1.4.3
+ * Template for course archive filter
+ *
+ * @package Tutor\Templates
+ * @subpackage Course_Filter
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 1.4.3
  */
 
 if ( ! tutor_utils()->get_option( 'course_archive_filter_sorting', true, true, true ) ) {
@@ -12,7 +17,7 @@ $sort_by = \TUTOR\Input::get( 'course_order', '' );
 ?>
 
 <!--
-	Note Do not remove tutor-course-filter attr. It required by _archive.js for filter function.
+	Note: Do not remove tutor-course-filter attr. It required by _archive.js for filter function.
 !-->
 <div style="text-align: right;" class="tutor-course-filter" tutor-course-filter>
 	<form style="display: inline-block;">
