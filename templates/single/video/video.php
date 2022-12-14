da170459a386141edd693baaa8809952854093ef
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2022-12-14 16:21:09 +0600

    WPCS Fix - single/video/video.php

--- templates/single/video/video.php
+++ templates/single/video/video.php
@@ -2,34 +2,42 @@
 /**
  * Display Video
  *
- * @since v.1.0.0
- * @author themeum
- * @url https://themeum.com
- *
- * @package TutorLMS/Templates
- * @version 1.4.3
+ * @package Tutor\Templates
+ * @subpackage Single\Video
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 1.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) )
+if ( ! defined( 'ABSPATH' ) ) {
 	exit;
+}
 
 $video_info = tutor_utils()->get_video_info();
 
-do_action('tutor_lesson/single/before/video');
+do_action( 'tutor_lesson/single/before/video' );
 
-$source_key = is_object($video_info) && 'html5' !== $video_info->source ? 'source_'.$video_info->source : null;
+$source_key = is_object( $video_info ) && 'html5' !== $video_info->source ? 'source_' . $video_info->source : null;
 
-$has_source = (is_object($video_info) && $video_info->source_video_id) || (isset($source_key) ? $video_info->$source_key : null);
+$has_source = ( is_object( $video_info ) && $video_info->source_video_id ) || ( isset( $source_key ) ? $video_info->$source_key : null );
 
-if ($has_source) {
-    tutor_load_template('single.video.'.$video_info->source);
+if ( $has_source ) {
+	tutor_load_template( 'single.video.' . $video_info->source );
 } else {
-    $feature_image = get_post_meta( get_the_ID(  ), '_thumbnail_id', true );
-    $url = $feature_image ? wp_get_attachment_url( $feature_image ) : null;
-    if($url) {
-        echo '<div class="tutor-lesson-feature-image">
-                <img src="'.$url.'" />
+	$feature_image = get_post_meta( get_the_ID(), '_thumbnail_id', true );
+	$url           = $feature_image ? wp_get_attachment_url( $feature_image ) : null;
+	if ( $url ) {
+		$html_markup = '<div class="tutor-lesson-feature-image">
+                <img src="' . $url . '" />
             </div>';
-    }
+		echo wp_kses(
+			$html_markup,
+			array(
+				'div' => array( 'class' => true ),
+				'img' => array( 'src' => true ),
+			)
+		);
+	}
 }
-do_action('tutor_lesson/single/after/video'); ?>
+
+do_action( 'tutor_lesson/single/after/video' );
