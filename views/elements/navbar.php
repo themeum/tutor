65b025d556ec1a3912206245d4df71ad25074b07
shewa12 <shewa12kpi@gmail.com>
2022-12-14 12:58:27 +0600

    Doc Comment: Doc comment updated navbar.php file

--- views/elements/navbar.php
+++ views/elements/navbar.php
@@ -2,25 +2,29 @@
 /**
  * Navbar Component.
  *
- * @package Navbar component
+ * @package Tutor\Views
+ * @subpackage Tutor\ViewElements
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 2.0.0
  */
+
 if ( isset( $data ) && count( $data ) ) : ?>
 	<div class="tutor-wp-dashboard-header tutor-px-24 tutor-mb-24">
 		<div class="tutor-row tutor-align-lg-center">
 			<div class="tutor-col-lg">
 				<div class="tutor-d-lg-flex tutor-align-lg-center tutor-px-12 tutor-py-16">
-					<span class="tutor-fs-5 tutor-fw-medium <?php echo !isset($data['sub_page_title']) ? 'tutor-mr-16' : ''; ?>">
+					<span class="tutor-fs-5 tutor-fw-medium <?php echo ! isset( $data['sub_page_title'] ) ? 'tutor-mr-16' : ''; ?>">
 						<?php echo esc_html( $data['page_title'] ); ?>
 					</span>
 
-					<?php if(isset($data['sub_page_title'])): ?>
+					<?php if ( isset( $data['sub_page_title'] ) ) : ?>
 						<span class="tutor-mx-8" area-hidden="true">/</span>
-						
 						<span class="tutor-fs-7 tutor-color-muted">
 							<?php echo esc_html( $data['sub_page_title'] ); ?>
 						</span>
 					<?php endif; ?>
-					
+
 					<?php
 					// If modal target set then button will be set as modal button otherwise url button.
 					if ( isset( $data['modal_target'] ) && '' !== $data['modal_target'] ) :
