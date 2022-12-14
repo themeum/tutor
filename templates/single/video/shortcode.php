c64b4ea3c23d9a4c2fb449b2518463309fe6eada
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2022-12-14 16:20:03 +0600

    WPCS Fix - single/video/shortcode.php

--- templates/single/video/shortcode.php
+++ templates/single/video/shortcode.php
@@ -1,14 +1,12 @@
 <?php
-
 /**
- * Display Video HTML5
+ * Display shortcode
  *
- * @since v.1.0.0
- * @author themeum
- * @url https://themeum.com
- *
- * @package TutorLMS/Templates
- * @version 1.4.3
+ * @package Tutor\Templates
+ * @subpackage Single\Video
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 1.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -20,10 +18,10 @@ $video_info = tutor_utils()->get_video_info();
 do_action( 'tutor_lesson/single/before/video/shortcode' );
 ?>
 	<div class="tutor-video-player">
-        <div class="loading-spinner" area-hidden="true"></div>
-        <div class="tutor-ratio tutor-ratio-16x9">
-            <?php echo do_shortcode( tutor_utils()->array_get( 'source_shortcode', $video_info ) ) ; ?>
-        </div>
-    </div>
+		<div class="loading-spinner" area-hidden="true"></div>
+		<div class="tutor-ratio tutor-ratio-16x9">
+			<?php echo do_shortcode( tutor_utils()->array_get( 'source_shortcode', $video_info ) ); ?>
+		</div>
+	</div>
 <?php
 do_action( 'tutor_lesson/single/after/video/shortcode' ); ?>
