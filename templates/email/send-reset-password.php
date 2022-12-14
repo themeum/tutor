507860be5240ec88d1b656ac95ce9eba1d1034ba
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2022-12-14 19:10:15 +0600

    WPCS fix - email/send-reset-password.php

--- templates/email/send-reset-password.php
+++ templates/email/send-reset-password.php
@@ -1,19 +1,30 @@
 <?php
 /**
- * @package TutorLMS/Templates
- * @version 1.4.3
+ * Send reset password template
+ *
+ * @package Tutor\Templates
+ * @subpackage Email
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 1.4.3
  */
 
 defined( 'ABSPATH' ) || exit;
 ?>
 
 
-<p><?php printf( esc_html__( 'Hi %s,', 'tutor' ), esc_html( $user_login ) ); ?>
+<p>
+	<?php printf( esc_html__( 'Hi %s,', 'tutor' ), esc_html( $user_login ) ); ?>
+</p>
 
-<p><?php printf( esc_html__( 'Someone has requested a new password for the following account on %s:', 'tutor' ), esc_html( wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES ) ) ); ?></p>
+<p>
+	<?php printf( esc_html__( 'Someone has requested a new password for the following account on %s:', 'tutor' ), esc_html( wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES ) ) ); ?>
+</p>
 
 <p><?php printf( esc_html__( 'Username: %s', 'tutor' ), esc_html( $user_login ) ); ?></p>
-<p><?php esc_html_e( 'If you didn\'t make this request, just ignore this email. If you\'d like to proceed:', 'tutor' ); ?></p>
+<p>
+	<?php esc_html_e( 'If you didn\'t make this request, just ignore this email. If you\'d like to proceed:', 'tutor' ); ?>
+</p>
 <p>
 	<a class="link" href="<?php echo add_query_arg( array( 'reset_key' => $reset_key, 'user_id' => $user_id ), tutor_utils()->tutor_dashboard_url('retrieve-password') ); ?>"><?php // phpcs:ignore ?>
 		<?php esc_html_e( 'Click here to reset your password', 'tutor' ); ?>
