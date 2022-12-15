6ffd1f5962cad1f6640c0850fb80b8122fe41ec6
shewa12 <shewa12kpi@gmail.com>
2022-12-15 11:25:13 +0600

    Doc Comment: View file doc comment updated on metabox/course-add-edd-product-metabox.php

--- views/metabox/course-add-edd-product-metabox.php
+++ views/metabox/course-add-edd-product-metabox.php
@@ -2,16 +2,17 @@
 /**
  * Add product metabox
  *
- * @author Themeum
- * @url https://themeum.com
- *
- * @since v.1.0.0
+ * @package Tutor\Views
+ * @subpackage Tutor\MetaBox
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 1.0.0
  */
 
-$products = tutor_utils()->get_edd_products();
+$products   = tutor_utils()->get_edd_products();
 $product_id = tutor_utils()->get_course_product_id();
-$info_text = __('Sell your product, process by EDD', 'tutor');
+$info_text  = __( 'Sell your product, process by EDD', 'tutor' );
 $label_info = __( 'When selling the course', 'tutor' );
 
 require __DIR__ . '/product-selection.php';
-?>
+
