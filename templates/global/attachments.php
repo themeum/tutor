e070a8975df1c1a61279997af3439bb6ddad543f
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2022-12-15 10:50:44 +0600

    wpcs fix - global/attachments.php

--- templates/global/attachments.php
+++ templates/global/attachments.php
@@ -2,12 +2,11 @@
 /**
  * Display attachments
  *
- * @since v.1.0.0
- * @author themeum
- * @url https://themeum.com
- *
- * @package TutorLMS/Templates
- * @version 1.4.3
+ * @package Tutor\Templates
+ * @subpackage Global
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 1.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -20,30 +19,30 @@ $open_mode_view = apply_filters( 'tutor_pro_attachment_open_mode', null ) == 'vi
 do_action( 'tutor_global/before/attachments' );
 
 if ( is_array( $attachments ) && count( $attachments ) ) : ?>
-    <div class="tutor-course-attachments tutor-row">
-        <?php foreach ($attachments as $attachment) : ?>
-            <div class="tutor-col-md-6 tutor-mt-16">
-                <div class="tutor-course-attachment tutor-card tutor-card-sm">
-                    <div class="tutor-card-body">
-                        <div class="tutor-row">
-                            <div class="tutor-col tutor-overflow-hidden">
-                                <div class="tutor-fs-6 tutor-fw-medium tutor-color-black tutor-text-ellipsis tutor-mb-4"><?php echo esc_html( $attachment->name ); ?></div>
-                                <div class="tutor-fs-7 tutor-color-muted"><?php _e('Size', 'tutor'); ?>: <?php echo esc_html( $attachment->size ); ?></div>
-                            </div>
+	<div class="tutor-course-attachments tutor-row">
+		<?php foreach ( $attachments as $attachment ) : ?>
+			<div class="tutor-col-md-6 tutor-mt-16">
+				<div class="tutor-course-attachment tutor-card tutor-card-sm">
+					<div class="tutor-card-body">
+						<div class="tutor-row">
+							<div class="tutor-col tutor-overflow-hidden">
+								<div class="tutor-fs-6 tutor-fw-medium tutor-color-black tutor-text-ellipsis tutor-mb-4"><?php echo esc_html( $attachment->name ); ?></div>
+								<div class="tutor-fs-7 tutor-color-muted"><?php esc_html_e( 'Size', 'tutor' ); ?>: <?php echo esc_html( $attachment->size ); ?></div>
+							</div>
 
-                            <div class="tutor-col-auto">
-                                <a href="<?php echo esc_url( $attachment->url ); ?>" class="tutor-iconic-btn tutor-iconic-btn-secondary tutor-stretched-link" <?php echo ($open_mode_view ? $open_mode_view : ' download="'.$attachment->name.'" ' ); ?>>
-                                    <span class="tutor-icon-download" area-hidden="true"></span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        <?php endforeach; ?>
-    </div>
+							<div class="tutor-col-auto">
+								<a href="<?php echo esc_url( $attachment->url ); ?>" class="tutor-iconic-btn tutor-iconic-btn-secondary tutor-stretched-link" <?php echo esc_attr( $open_mode_view ? $open_mode_view : ' download="' . $attachment->name . '" ' ); ?>>
+									<span class="tutor-icon-download" area-hidden="true"></span>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		<?php endforeach; ?>
+	</div>
 <?php else :
-    tutor_utils()->tutor_empty_state(__('No Attachment Found', 'tutor'));
+	tutor_utils()->tutor_empty_state( __( 'No Attachment Found', 'tutor' ) );
 endif;
 
 do_action( 'tutor_global/after/attachments' ); ?>
