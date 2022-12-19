7e057f428ea953780e3cc65adf8bf4648242713c
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2022-12-19 16:09:36 +0600

    wpcs fix dashboard/logged-in.php

--- templates/dashboard/logged-in.php
+++ templates/dashboard/logged-in.php
@@ -1,9 +1,14 @@
 <?php
 /**
- * @package TutorLMS/Templates
- * @version 1.4.3
+ * Already logged in template
+ *
+ * @package Tutor\Templates
+ * @subpackage Dashboard
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 1.4.3
  */
 
 ?>
 
-<h2><?php esc_html_e('You are already logged in', 'tutor') ?></h2>
+<h2><?php esc_html_e( 'You are already logged in', 'tutor' ); ?></h2>
