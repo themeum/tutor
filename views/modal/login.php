3a0cc2f9cd7dbc1577502dbe43ce54a98ab2d59a
shewa12 <shewa12kpi@gmail.com>
2022-12-20 11:46:38 +0600

    Doc Comment: Doc comment updated on views/modal/login.php file

--- views/modal/login.php
+++ views/modal/login.php
@@ -2,11 +2,10 @@
 /**
  * Login View
  *
- * @since 1.0.0
- * @author themeum
+ * @package Tutor\Views
+ * @author Themeum <support@themeum.com>
  * @link https://themeum.com
- *
- * @package TutorLMS/Templates
+ * @since 1.0.0
  */
 
 $lost_pass = apply_filters( 'tutor_lostpassword_url', wp_lostpassword_url() );
@@ -24,7 +23,7 @@ $lost_pass = apply_filters( 'tutor_lostpassword_url', wp_lostpassword_url() );
 					<?php do_action( 'tutor_before_login_form' ); ?>
 					<div class="tutor-fs-4 tutor-fw-medium tutor-color-black tutor-mb-32"><?php esc_html_e( 'Hi, Welcome back!', 'tutor' ); ?></div>
 					<?php
-						// load form template
+						// load form template.
 						$login_form = trailingslashit( tutor()->path ) . 'templates/login-form.php';
 						tutor_load_template_from_custom_path(
 							$login_form,
