b4c154b46a604ec7b96f7729806f97e55215ec5a
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2022-12-20 12:16:59 +0600

    wpcs fix - shortcode/tutor-instructor.php

--- templates/shortcode/tutor-instructor.php
+++ templates/shortcode/tutor-instructor.php
@@ -1,3 +1,15 @@
+<?php
+/**
+ * Tutor instructor
+ *
+ * @package Tutor\Templates
+ * @subpackage Shortcode
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 2.0.0
+ */
+
+?>
 <div class="tutor-pagination-wrapper-replaceable tutor-instructor-list-wrapper">
 	<?php if ( count( $instructors ) ) : ?>
 		<div class="tutor-instructor-list">
@@ -5,10 +17,13 @@
 				<?php foreach ( $instructors as $instructor ) : ?>
 					<?php
 						$instructor->course_count = tutor_utils()->get_course_count_by_instructor( $instructor->ID );
-						$instructor->ratings = tutor_utils()->get_instructor_ratings( $instructor->ID );
-						tutor_load_template( 'instructor.' . $layout, array(
-							'instructor' => $instructor
-						));
+						$instructor->ratings      = tutor_utils()->get_instructor_ratings( $instructor->ID );
+						tutor_load_template(
+							'instructor.' . $layout,
+							array(
+								'instructor' => $instructor,
+							)
+						);
 					?>
 				<?php endforeach; ?>
 			</div>
@@ -19,21 +34,24 @@
 
 	<?php
 		// @todo: convert to pagination
-		if ($current_page > 1 || $instructors_count>$limit) {
-			$pagination_data = array(
-				'total_items'  => $instructors_count,
-				'per_page'    => $limit,
-				'paged'       => $current_page,
-				'ajax'		  => array_merge($filter, array(
+	if ( $current_page > 1 || $instructors_count > $limit ) {
+		$pagination_data = array(
+			'total_items' => $instructors_count,
+			'per_page'    => $limit,
+			'paged'       => $current_page,
+			'ajax'        => array_merge(
+				$filter,
+				array(
 					'loading_container' => '.tutor-instructor-list-wrapper',
-					'action' => 'load_filtered_instructor',
-				))
-			);
+					'action'            => 'load_filtered_instructor',
+				)
+			),
+		);
 
-			tutor_load_template_from_custom_path(
-				tutor()->path . 'templates/dashboard/elements/pagination.php',
-				$pagination_data
-			);
-		}
+		tutor_load_template_from_custom_path(
+			tutor()->path . 'templates/dashboard/elements/pagination.php',
+			$pagination_data
+		);
+	}
 	?>
 </div>
