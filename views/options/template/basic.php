4b0a97e25b31747e703e58cc4b68908dfa83328d
shewa12 <shewa12kpi@gmail.com>
2022-12-21 11:38:00 +0600

    Doc Comment: Doc comment updated, WPCS warnings ignored

--- views/options/template/basic.php
+++ views/options/template/basic.php
@@ -1,13 +1,27 @@
-<?php echo $this->view_template( 'common/reset-button-template.php', $section ); ?>
+<?php
+/**
+ * Settings option view
+ *
+ * @package Tutor\Views
+ * @subpackage Tutor\Settings
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 2.0.0
+ */
+
+//phpcs:ignore -- contain safe data
+echo $this->view_template( 'common/reset-button-template.php', $section ); ?>
 
 <?php
 foreach ( $section['blocks'] as $blocks ) :
 	if ( empty( $blocks['label'] ) ) :
 		?>
-		<div class="tutor-option-single-item tutor-mb-32"><?php echo $this->blocks( $blocks ); ?> </div>
+		<div class="tutor-option-single-item tutor-mb-32">
+			<?php echo $this->blocks( $blocks ); //phpcs:ignore ?>
+		</div>
 	<?php else : ?>
 		<?php
-			echo $this->blocks( $blocks );
+			echo $this->blocks( $blocks ); //phpcs:ignore
 		?>
 	<?php endif; ?>
 <?php endforeach; ?>
