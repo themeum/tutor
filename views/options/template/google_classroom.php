d393d4d7b0e770bf524318dff35dba8aec71a834
shewa12 <shewa12kpi@gmail.com>
2022-12-21 11:46:55 +0600

    WPCS: WPCS warnings fixed & doc comment updated on google_classroom.php file

--- views/options/template/google_classroom.php
+++ views/options/template/google_classroom.php
@@ -1,7 +1,21 @@
+<?php
+/**
+ * Color picker template for settings.
+ *
+ * @package Tutor\Views
+ * @subpackage Tutor\Settings
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 2.0.0
+ */
+
+?>
 <div class="tutor-option-main-title">
-    <div class="tutor-fs-4 tutor-fw-medium tutor-color-black"><?php echo $section->label ?></div>
+	<div class="tutor-fs-4 tutor-fw-medium tutor-color-black">
+		<?php echo esc_html( $section->label ); ?>
+	</div>
 	<button class="reset-btn reset_to_default" data-reset="<?php echo esc_attr( $section['slug'] ); ?>">
 		<i class="btn-icon tutor-icon-refresh"></i>
-		<?php echo esc_attr( 'Reset to Default', 'tutor' ); ?>
+		<?php esc_html_e( 'Reset to Default', 'tutor' ); ?>
 	</button>
 </div>
