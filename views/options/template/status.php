e2d6fef93765cd44981841ebe5acb1ce0ff2780e
shewa12 <shewa12kpi@gmail.com>
2022-12-21 16:01:01 +0600

    WPCS: WPCS warnings fixed on status.php file

--- views/options/template/status.php
+++ views/options/template/status.php
@@ -1,11 +1,26 @@
+<?php
+/**
+ * Status settings
+ *
+ * @package Tutor\Views
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 2.0.0
+ */
+
+?>
 <div class="tutor-option-main-title">
-	<div class="tutor-fs-4 tutor-fw-medium tutor-color-black"><?php _e('Status','tutor'); ?></div>
+	<div class="tutor-fs-4 tutor-fw-medium tutor-color-black"><?php esc_html_e( 'Status', 'tutor' ); ?></div>
 </div>
 
-<?php foreach ( $section['blocks'] as $blocks ) :
-	if ( empty( $blocks['label'] ) ) : ?>
-		<div class="tutor-option-single-item tutor-mb-32"><?php echo $this->blocks( $blocks ); ?> </div>
+<?php
+foreach ( $section['blocks'] as $blocks ) :
+	if ( empty( $blocks['label'] ) ) :
+		?>
+		<div class="tutor-option-single-item tutor-mb-32">
+			<?php echo $this->blocks( $blocks ); //phpcs:ignore --contain safe data ?>
+		</div>
 	<?php else : ?>
-		<?php echo $this->blocks( $blocks ); ?>
+		<?php echo $this->blocks( $blocks ); //phpcs:ignore --contain safe data ?>
 	<?php endif; ?>
 <?php endforeach; ?>
