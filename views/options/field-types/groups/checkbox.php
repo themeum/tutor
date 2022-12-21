ea764240a79a67f2271e0585bda04be194215a49
shewa12 <shewa12kpi@gmail.com>
2022-12-21 17:11:38 +0600

    Doc Comment: Doc comment updated on checkbox.php view file

--- views/options/field-types/groups/checkbox.php
+++ views/options/field-types/groups/checkbox.php
@@ -2,9 +2,13 @@
 /**
  * Checkbox of group for tutor settings.
  *
- * @package Tutor LMS
- * @since 2.0
+ * @package Tutor\Views
+ * @subpackage Tutor\Settings
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 2.0.0
  */
+
 ?>
 <label>
 	<input type="checkbox" name="<?php echo esc_attr( $input_name ); ?>" value="1" <?php echo wp_kses_post( checked( $input_value, '1' ) ); ?> >
