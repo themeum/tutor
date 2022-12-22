ff8d00d761531b42af606a0a433ad35d307af351
shewa12 <shewa12kpi@gmail.com>
2022-12-22 12:01:53 +0600

    WPCS: WPCS warnings, doc comments fixed from view/metabox/course/field-types dir

--- views/metabox/course/field-types/groups/select.php
+++ views/metabox/course/field-types/groups/select.php
@@ -1,8 +1,18 @@
+<?php
+/**
+ * Select field
+ *
+ * @package Tutor\Views
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 2.0.0
+ */
 
+?>
 <select name="<?php echo esc_attr( $input_name ); ?>">
 	<?php
-	if ( ! isset( $group_field['select_options'] ) || $group_field['select_options'] !== false ) {
-		echo '<option value="-1">' . __( 'Select Option', 'tutor' ) . '</option>';
+	if ( ! isset( $group_field['select_options'] ) || false !== $group_field['select_options'] ) {
+		echo '<option value="-1">' . esc_html__( 'Select Option', 'tutor' ) . '</option>';
 	}
 	if ( ! empty( $group_field['options'] ) ) {
 		foreach ( $group_field['options'] as $optionKey => $option ) {
--- views/metabox/course/field-types/groups/text.php
+++ views/metabox/course/field-types/groups/text.php
@@ -1,7 +1,19 @@
+<?php
+/**
+ * Text field
+ *
+ * @package Tutor\Views
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 2.0.0
+ */
 
+?>
 <input type="text" name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $input_value ); ?>" >
 <?php
-if ($label){
-	echo "<p>{$label}</p>";
+if ( $label ) {
+	?>
+	<p><?php echo esc_html( $label ); ?></p>
+	<?php
 }
 ?>
