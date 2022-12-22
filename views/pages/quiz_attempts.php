368d24681db3044f4ed8cd13227dd2c7203a1f03
shewa12 <shewa12kpi@gmail.com>
2022-12-22 14:25:58 +0600

    Doc Comment: Doc comment updated quiz_attempts.php file

--- views/pages/quiz_attempts.php
+++ views/pages/quiz_attempts.php
@@ -2,7 +2,11 @@
 /**
  * Quiz Attempts List Template.
  *
- * @package Quiz Attempts List
+ * @package Tutor\Views
+ * @subpackage Tutor\QuizAttempts
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 2.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -14,11 +18,11 @@ use TUTOR\Input;
 use Tutor\Models\QuizModel;
 
 if ( is_numeric( Input::get( 'view_quiz_attempt_id' ) ) ) {
-    include tutor()->path."views/pages/view_attempt.php";
-    return;
+	include tutor()->path . 'views/pages/view_attempt.php';
+	return;
 }
 
-$quiz_attempts = new TUTOR\Quiz_Attempts_List(false);
+$quiz_attempts = new TUTOR\Quiz_Attempts_List( false );
 
 /**
  * Short able params
@@ -26,7 +30,7 @@ $quiz_attempts = new TUTOR\Quiz_Attempts_List(false);
 $user_id   = Input::get( 'user_id', '' );
 $course_id = Input::get( 'course-id', '' );
 $order     = Input::get( 'order', 'DESC' );
-$date      = Input::has('date') ? tutor_get_formated_date( 'Y-m-d', Input::get( 'date' ) ) : '';
+$date      = Input::has( 'date' ) ? tutor_get_formated_date( 'Y-m-d', Input::get( 'date' ) ) : '';
 $search    = Input::get( 'search', '' );
 
 /**
@@ -41,15 +45,15 @@ $paged    = Input::get( 'paged', 1, Input::TYPE_INT );
 $per_page = tutor_utils()->get_option( 'pagination_per_page' );
 $offset   = ( $per_page * $paged ) - $per_page;
 
-$quiz_attempts_list = QuizModel::get_quiz_attempts($offset, $per_page, $search, $course_id, $date, $order, $active_tab, false, true );
-$total				= QuizModel::get_quiz_attempts($offset, $per_page, $search, $course_id, $date, $order, $active_tab, true, true );
+$quiz_attempts_list = QuizModel::get_quiz_attempts( $offset, $per_page, $search, $course_id, $date, $order, $active_tab, false, true );
+$total              = QuizModel::get_quiz_attempts( $offset, $per_page, $search, $course_id, $date, $order, $active_tab, true, true );
 
 /**
  * Navbar data to make nav menu
  */
 $navbar_data = array(
 	'page_title' => $quiz_attempts->page_title,
-	'tabs'       => $quiz_attempts->tabs_key_value(  $user_id, $date, $search, $course_id ),
+	'tabs'       => $quiz_attempts->tabs_key_value( $user_id, $date, $search, $course_id ),
 	'active'     => $active_tab,
 );
 
@@ -76,26 +80,29 @@ $filters = array(
 
 	<div class="tutor-admin-body">
 		<?php
-			tutor_load_template_from_custom_path(tutor()->path . '/views/quiz/attempt-table.php', array(
-				'attempt_list' => $quiz_attempts_list,
-				'context' => 'backend-dashboard-students-attempts'
-			));
-		?>
+			tutor_load_template_from_custom_path(
+				tutor()->path . '/views/quiz/attempt-table.php',
+				array(
+					'attempt_list' => $quiz_attempts_list,
+					'context'      => 'backend-dashboard-students-attempts',
+				)
+			);
+			?>
 
 		<div class="tutor-admin-page-pagination-wrapper tutor-mt-32">
 			<?php
 				/**
 				 * Prepare pagination data & load template
 				 */
-				if($total > $per_page) {
-					$pagination_data     = array(
-						'total_items' => $total,
-						'per_page'    => $per_page,
-						'paged'       => $paged,
-					);
-					$pagination_template = tutor()->path . 'views/elements/pagination.php';
-					tutor_load_template_from_custom_path( $pagination_template, $pagination_data );
-				}
+			if ( $total > $per_page ) {
+				$pagination_data     = array(
+					'total_items' => $total,
+					'per_page'    => $per_page,
+					'paged'       => $paged,
+				);
+				$pagination_template = tutor()->path . 'views/elements/pagination.php';
+				tutor_load_template_from_custom_path( $pagination_template, $pagination_data );
+			}
 			?>
 		</div>
 	</div>
