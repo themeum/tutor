349c5539874d268ff620588ff11f2decd627cbbb
shewa12 <shewa12kpi@gmail.com>
2022-12-26 22:20:16 +0600

    WPCS: WPCS warnings & doc comment fixed on view/quiz dir

--- views/quiz/header-context/backend-dashboard-students-attempts.php
+++ views/quiz/header-context/backend-dashboard-students-attempts.php
@@ -1,21 +1,30 @@
 <?php
-    if(empty($back_url)) {
-        return;
-    }
+/**
+ * Student attempt page
+ *
+ * @package Tutor\Views
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 2.0.0
+ */
+
+if ( empty( $back_url ) ) {
+	return;
+}
 ?>
 
 <header class="tutor-wp-dashboard-header tutor-justify-between tutor-align-center tutor-px-32 tutor-py-20 tutor-mb-24 tutor-pt-16 tutor-pb-16">
-    <div class="tutor-mb-24">
-        <a class="tutor-btn tutor-btn-ghost" href="<?php echo $back_url; ?>">
-            <span class="tutor-icon-previous" area-hidden="true"></span>
-            <?php _e('Back', 'tutor'); ?>
-        </a>
-    </div>
-    
-    <div class="tutor-fs-7 tutor-color-secondary">
-        <?php echo __('Course', 'tutor').': '.$course_title; ?>
-    </div>
-    <div class="header-title tutor-fs-5 tutor-fw-medium tutor-color-black tutor-mt-12">
-        <?php echo __('Quiz', 'tutor') . ': ' . $quiz_title; ?>
-    </div>
+	<div class="tutor-mb-24">
+		<a class="tutor-btn tutor-btn-ghost" href="<?php echo esc_url( $back_url ); ?>">
+			<span class="tutor-icon-previous" area-hidden="true"></span>
+			<?php esc_html_e( 'Back', 'tutor' ); ?>
+		</a>
+	</div>
+
+	<div class="tutor-fs-7 tutor-color-secondary">
+		<?php echo esc_html__( 'Course', 'tutor' ) . ': ' . esc_html( $course_title ); ?>
+	</div>
+	<div class="header-title tutor-fs-5 tutor-fw-medium tutor-color-black tutor-mt-12">
+		<?php echo esc_html__( 'Quiz', 'tutor' ) . ': ' . esc_html( $quiz_title ); ?>
+	</div>
 </header>
--- views/quiz/header-context/course-single-previous-attempts.php
+++ views/quiz/header-context/course-single-previous-attempts.php
@@ -1,48 +1,57 @@
 <?php
-    $passing_grade = tutor_utils()->get_quiz_option(get_the_ID(), 'passing_grade', 0);
+/**
+ * Single attempt page
+ *
+ * @package Tutor\Views
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 2.0.0
+ */
+
+$passing_grade = tutor_utils()->get_quiz_option( get_the_ID(), 'passing_grade', 0 );
 ?>
-<?php if(!empty($back_url)): ?>
-    <div class="tutor-mb-24">
-        <a class="tutor-btn tutor-btn-ghost" href="<?php echo esc_url( $back_url ); ?>">
-            <span class="tutor-icon-previous tutor-mr-8" area-hidden="true"></span>
-            <?php _e('Back', 'tutor'); ?>
-        </a>
-    </div>
+<?php if ( ! empty( $back_url ) ) : ?>
+	<div class="tutor-mb-24">
+		<a class="tutor-btn tutor-btn-ghost" href="<?php echo esc_url( $back_url ); ?>">
+			<span class="tutor-icon-previous tutor-mr-8" area-hidden="true"></span>
+			<?php esc_html_e( 'Back', 'tutor' ); ?>
+		</a>
+	</div>
 <?php endif; ?>
 
 <div class="tutor-fs-7 tutor-color-secondary">
-    <?php _e('Quiz', 'tutor'); ?>
+	<?php esc_html_e( 'Quiz', 'tutor' ); ?>
 </div>
 
 <div class="header-title tutor-fs-5 tutor-fw-medium tutor-color-black tutor-mt-12 tutor-mb-32">
-    <?php echo $quiz_title; ?>
+	<?php echo esc_html( $quiz_title ); ?>
 </div>
 <div class="tutor-d-flex tutor-justify-between tutor-py-20 tutor-my-20 tutor-border-top tutor-border-bottom">
-    <div class="tutor-d-flex">
-        <?php _e('Questions', 'tutor'); ?>: 
-        <span class="tutor-color-black">
-            <?php echo $question_count; ?>
-        </span>
-    </div>
-    <div class="tutor-d-flex-flex">
-        <?php _e('Quiz Time', 'tutor'); ?>: 
-        <span class="tutor-color-black">
-            <?php echo $quiz_time; ?>
-        </span>
-    </div>
-    <div class="tutor-d-flex">
-        <?php _e('Total Marks', 'tutor'); ?>: 
-        <span class="tutor-color-black">
-            <?php echo $earned_marks . '/' . $total_marks; ?>
-        </span>
-    </div>
-    <div class="tutor-d-flex">
-        <?php _e('Passing Marks', 'tutor'); ?>: 
-        <span class="tutor-color-black">
-            <?php
-                $pass_marks = ($total_marks * $passing_grade) / 100;
-                echo number_format_i18n($pass_marks, 2);
+	<div class="tutor-d-flex">
+		<?php esc_html_e( 'Questions', 'tutor' ); ?>: 
+		<span class="tutor-color-black">
+			<?php echo esc_html( $question_count ); ?>
+		</span>
+	</div>
+	<div class="tutor-d-flex-flex">
+		<?php esc_html_e( 'Quiz Time', 'tutor' ); ?>: 
+		<span class="tutor-color-black">
+			<?php echo esc_html( $quiz_time ); ?>
+		</span>
+	</div>
+	<div class="tutor-d-flex">
+		<?php esc_html_e( 'Total Marks', 'tutor' ); ?>: 
+		<span class="tutor-color-black">
+			<?php echo esc_html( $earned_marks . '/' . $total_marks ); ?>
+		</span>
+	</div>
+	<div class="tutor-d-flex">
+		<?php esc_html_e( 'Passing Marks', 'tutor' ); ?>: 
+		<span class="tutor-color-black">
+			<?php
+				$pass_marks = ( $total_marks * $passing_grade ) / 100;
+				echo esc_html( number_format_i18n( $pass_marks, 2 ) );
 			?>
-        </span>
-    </div>
+		</span>
+	</div>
 </div>
