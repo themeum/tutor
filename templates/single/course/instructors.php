a7d89faf668408d9c343d2c319d0e51bb5b43011
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2022-12-26 13:08:54 +0600

    wpcs fix single/course/instructors.php

--- templates/single/course/instructors.php
+++ templates/single/course/instructors.php
@@ -2,13 +2,11 @@
 /**
  * Template for displaying course instructors/ instructor
  *
- * @since v.1.0.0
- *
- * @author Themeum
- * @url https://themeum.com
- *
- * @package TutorLMS/Templates
- * @version 1.4.3
+ * @package Tutor\Templates
+ * @subpackage Single\Course
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 1.0.0
  */
 
 use TUTOR\Instructors_List;
@@ -20,23 +18,28 @@ $instructors = tutor_utils()->get_instructors_by_course();
 if ( $instructors && count( $instructors ) ) : ?>
 <div class="tutor-course-details-instructors">
 	<h3 class="tutor-fs-6 tutor-fw-medium tutor-color-black tutor-mb-16">
-		<?php echo _e( 'A course by', 'tutor' ); ?>
+		<?php echo esc_html( 'A course by', 'tutor' ); ?>
 	</h3>
 
 	<?php foreach ( $instructors as $key => $instructor ) : ?>
-		<div class="tutor-d-flex tutor-align-center<?php echo ( $key != count( $instructors ) - 1 ) ? ' tutor-mb-24' : ''; ?>">
+		<div class="tutor-d-flex tutor-align-center<?php echo ( count( $instructors ) - 1 ) != $key ? ' tutor-mb-24' : ''; ?>">
 			<div class="tutor-d-flex tutor-mr-16">
-				<?php echo tutor_utils()->get_tutor_avatar( $instructor->ID, 'md' ); ?>
+				<?php
+				echo wp_kses(
+					tutor_utils()->get_tutor_avatar( $instructor->ID, 'md' ),
+					tutor_utils()->allowed_avatar_tags()
+				);
+				?>
 			</div>
 
 			<div>
-				<a class="tutor-fs-6 tutor-fw-bold tutor-color-black" href="<?php echo tutor_utils()->profile_url( $instructor->ID, true ); ?>">
-					<?php echo $instructor->display_name; ?>
+				<a class="tutor-fs-6 tutor-fw-bold tutor-color-black" href="<?php echo esc_url( tutor_utils()->profile_url( $instructor->ID, true ) ); ?>">
+					<?php echo esc_html( $instructor->display_name ); ?>
 				</a>
 
 				<?php if ( ! empty( $instructor->tutor_profile_job_title ) ) : ?>
 					<div class="tutor-instructor-designation tutor-fs-7 tutor-color-muted">
-						<?php echo $instructor->tutor_profile_job_title; ?>
+						<?php echo esc_html( $instructor->tutor_profile_job_title ); ?>
 					</div>
 				<?php endif; ?>
 			</div>
