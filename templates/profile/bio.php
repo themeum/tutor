c7d74fffe124183a85de191e5a51238d790d729c
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2022-12-28 12:55:46 +0600

    public profile bio allow limited tags

--- templates/profile/bio.php
+++ templates/profile/bio.php
@@ -14,10 +14,27 @@ $get_user  = tutor_utils()->get_user_by_login( $user_name );
 $user_id   = $get_user->ID;
 
 
-$profile_bio = get_user_meta( $user_id, '_tutor_profile_bio', true );
+$profile_bio  = get_user_meta( $user_id, '_tutor_profile_bio', true );
+$allowed_tags = array(
+	'p'      => array(),
+	'a'      => array(
+		'href'   => 1,
+		'target' => 1,
+		'rel'    => 1,
+	),
+	'span'   => array( 'style' => 1 ),
+	'br'     => array(),
+	'em'     => array(),
+	'strong' => array(),
+	'u'      => array(),
+	'ul'     => array(),
+	'ol'     => array(),
+	'li'     => array(),
+);
+
 if ( $profile_bio ) {
 	?>
-	<?php echo wpautop( $profile_bio );//phpcs:ignore ?>
+	<?php echo wp_kses( wpautop( $profile_bio ), $allowed_tags ); ?>
 	<?php
 } else {
 	esc_html_e( 'Bio data is empty', 'tutor' );
