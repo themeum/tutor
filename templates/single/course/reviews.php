f65d69f28c7b1343891c8e7ba1f59646d4bb1a88
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2023-02-01 17:06:01 +0600

    is_enrollment and course_rating use from global without re-query

--- templates/single/course/reviews.php
+++ templates/single/course/reviews.php
@@ -16,18 +16,17 @@ if ( $disable ) {
 	return;
 }
 
+global $is_enrolled, $course_rating;
+
 $per_page     = tutor_utils()->get_option( 'pagination_per_page', 10 );
 $current_page = max( 1, Input::post( 'current_page', 0, Input::TYPE_INT ) );
 $offset       = ( $current_page - 1 ) * $per_page;
 
 $current_user_id = get_current_user_id();
 $course_id       = Input::post( 'course_id', get_the_ID(), Input::TYPE_INT );
-$is_enrolled     = tutor_utils()->is_enrolled( $course_id, $current_user_id );
-
-$reviews       = tutor_utils()->get_course_reviews( $course_id, $offset, $per_page, false, array( 'approved' ), $current_user_id );
-$reviews_total = tutor_utils()->get_course_reviews( $course_id, null, null, true, array( 'approved' ), $current_user_id );
-$rating        = tutor_utils()->get_course_rating( $course_id );
-$my_rating     = tutor_utils()->get_reviews_by_user( 0, 0, 150, false, $course_id, array( 'approved', 'hold' ) );
+$reviews         = tutor_utils()->get_course_reviews( $course_id, $offset, $per_page, false, array( 'approved' ), $current_user_id );
+$reviews_total   = tutor_utils()->get_course_reviews( $course_id, null, null, true, array( 'approved' ), $current_user_id );
+$my_rating       = tutor_utils()->get_reviews_by_user( 0, 0, 150, false, $course_id, array( 'approved', 'hold' ) );
 
 if ( Input::has( 'course_id' ) ) {
 	// It's load more.
@@ -54,12 +53,12 @@ do_action( 'tutor_course/single/enrolled/before/reviews' );
 				<div class="tutor-row tutor-gx-xl-5 tutor-align-center">
 					<div class="tutor-col-lg-auto tutor-text-center tutor-mb-16 tutor-mb-lg-0">
 						<div class="tutor-review-summary-average-rating tutor-mb-20">
-							<?php echo number_format( $rating->rating_avg, 1 ); ?>
+							<?php echo number_format( $course_rating->rating_avg, 1 ); ?>
 						</div>
 
 						<div>
 							<div class="tutor-d-inline-block">
-								<?php tutor_utils()->star_rating_generator_v2( $rating->rating_avg, null, false, '', 'lg' ); ?>
+								<?php tutor_utils()->star_rating_generator_v2( $course_rating->rating_avg, null, false, '', 'lg' ); ?>
 							</div>
 						</div>
 
@@ -72,8 +71,8 @@ do_action( 'tutor_course/single/enrolled/before/reviews' );
 
 					<div class="tutor-col-lg">
 						<div class="tutor-review-summary-ratings">
-							<?php foreach ( $rating->count_by_value as $key => $value ) : ?>
-								<?php $rating_count_percent = ( $value > 0 ) ? ( $value * 100 ) / $rating->rating_count : 0; ?>
+							<?php foreach ( $course_rating->count_by_value as $key => $value ) : ?>
+								<?php $rating_count_percent = ( $value > 0 ) ? ( $value * 100 ) / $course_rating->rating_count : 0; ?>
 								<div class="tutor-row tutor-align-center tutor-review-summary-rating">
 									<div class="tutor-col-auto">
 										<div class="tutor-ratings">
