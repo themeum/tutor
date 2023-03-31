43a966ed4b9fbef1307766952c1acc37a8193d4f
shewa12 <shewa12kpi@gmail.com>
2023-03-31 10:15:32 +0600

    Unit Test: FlashMessageTest added

---
+++ tests/phpunit/FlashMessageTest.php
@@ -0,0 +1,78 @@
+<?php
+namespace TutorTest;
+
+use Tutor\Cache\FlashMessage;
+
+/**
+ * FlashMessage test
+ */
+class FlashMessageTest extends \WP_UnitTestCase {
+
+	private static $flash_msg;
+
+    private $key = 'tutor_flash_message';
+
+	public static function set_up_before_class() {
+		self::$flash_msg = new FlashMessage();
+	}
+
+	public function test_data_feeding_with_construct() {
+		$flash_msg = new FlashMessage( 'success', FlashMessage::DANGER );
+
+		$expected = array(
+			'alert'   => FlashMessage::DANGER,
+			'message' => 'success',
+		);
+
+		$flash_msg->set_cache();
+
+		$actual    = $flash_msg->get_cache();
+		$flash_msg = $this->assertSame( $expected, $actual );
+	}
+
+	public function test_get_cache() {
+		$expected = array(
+			'alert'   => 'danger',
+			'message' => 'success',
+		);
+
+		self::$flash_msg->data = $expected;
+
+		self::$flash_msg->set_cache();
+
+		$actual = self::$flash_msg->get_cache();
+
+		$this->assertSame( $expected, $actual );
+	}
+
+	public function test_set_cache() {
+		$expected = array(
+			'alert'   => 'danger',
+			'message' => 'success',
+		);
+
+		self::$flash_msg->data = $expected;
+
+		self::$flash_msg->set_cache();
+
+		$actual = get_transient( $this->key );
+
+		$this->assertSame( $expected, $actual );
+	}
+
+	public function test_delete_cache() {
+		$data = array(
+			'alert'   => 'danger',
+			'message' => 'success',
+		);
+
+		self::$flash_msg->data = $data;
+
+		self::$flash_msg->set_cache();
+
+		self::$flash_msg->delete_cache();
+
+		$this->assertFalse( get_transient( $this->key ) );
+	}
+
+}
