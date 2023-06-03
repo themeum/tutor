0663b398d2e024cfbe9591c0f3dae4f349a5e01d
shewa12 <shewa12kpi@gmail.com>
2023-06-03 21:42:32 +0600

    New: Rest API token list page added

---
+++ restapi/RestAuth.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Manage Rest API Authentication
+ *
+ * Token create, invoke etc
+ *
+ * @package Tutor
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 2.2.1
+ */
+
+namespace TUTOR;
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/**
+ * Rest API authentication
+ *
+ * @since 2.2.1
+ */
+class RestAuth {
+
+}
