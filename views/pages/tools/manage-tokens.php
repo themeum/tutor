d2a537450d87f47f9831002e2a49c26b2725cc16
shewa12 <shewa12kpi@gmail.com>
2023-06-04 21:22:45 +0600

    Update: getting data using custom query since get_user_meta not return umeta_id

--- views/pages/tools/manage-tokens.php
+++ views/pages/tools/manage-tokens.php
@@ -8,20 +8,33 @@
  * @since 2.0.0
  */
 
+use Tutor\Helpers\QueryHelper;
 use TUTOR\RestAuth;
 
+global $wpdb;
+
 $user_id = get_current_user_id();
-$tokens = get_user_meta( $user_id, RestAuth::KEYS_USER_META_KEY );
+
+// Getting user meta using custom query since get_user_meta .
+//not return umeta_id .
+$tokens  = QueryHelper::get_all(
+	$wpdb->usermeta,
+	array(
+		'user_id'  => $user_id,
+		'meta_key' => RestAuth::KEYS_USER_META_KEY,
+	),
+	'umeta_id'
+);
 
 $permissions = RestAuth::available_permissions();
-$current_user = get_userdata( get_current_user_id() );
+$user        = get_userdata( get_current_user_id() );
 
 ?>
 
 <div class="tutor-rest-api-keys-wrapper">
-    <button class="tutor-btn tutor-btn-outline-primary tutor-btn-md tutor-mb-12" data-tutor-modal-target="tutor-add-new-api-keys">
-        + <?php esc_html_e( 'Add New', 'tutor' ); ?>
-    </button>
+	<button class="tutor-btn tutor-btn-outline-primary tutor-btn-md tutor-mb-12" data-tutor-modal-target="tutor-add-new-api-keys">
+		+ <?php esc_html_e( 'Add New', 'tutor' ); ?>
+	</button>
 
 	<table class="tutor-table tutor-pages-table">
 		<thead>
@@ -36,8 +49,8 @@ $current_user = get_userdata( get_current_user_id() );
 		<tbody>
 			<?php
 			foreach ( $tokens as $token ) {
-                $token = json_decode( $token );
-                echo RestAuth::prepare_response( 1, $token->key, $token->secret, $token->permission ); //phpcs:ignore
+				$api = json_decode( $token->meta_value );
+                echo RestAuth::prepare_response( $token->umeta_id, $api->key, $api->secret, $api->permission ); //phpcs:ignore
 			}
 			?>
 		</tbody>
@@ -46,59 +59,59 @@ $current_user = get_userdata( get_current_user_id() );
 
 <!-- add new token modal  -->
 <div id="tutor-add-new-api-keys" class="tutor-modal tutor-modal-scrollable">
-    <div class="tutor-modal-overlay"></div>
-    <div class="tutor-modal-window">
-        <form id="tutor-generate-api-keys" class="tutor-modal-content" autocomplete="off" method="post">
-            <div class="tutor-modal-header">
-                <div class="tutor-modal-title">
-                    <?php esc_html_e( 'Generate API Key, Secret', 'tutor' ); ?>
-                </div>
-                <button class="tutor-iconic-btn tutor-modal-close" data-tutor-modal-close>
-                    <span class="tutor-icon-times" area-hidden="true"></span>
-                </button>
-            </div>
+	<div class="tutor-modal-overlay"></div>
+	<div class="tutor-modal-window">
+		<form id="tutor-generate-api-keys" class="tutor-modal-content" autocomplete="off" method="post">
+			<div class="tutor-modal-header">
+				<div class="tutor-modal-title">
+					<?php esc_html_e( 'Generate API Key, Secret', 'tutor' ); ?>
+				</div>
+				<button class="tutor-iconic-btn tutor-modal-close" data-tutor-modal-close>
+					<span class="tutor-icon-times" area-hidden="true"></span>
+				</button>
+			</div>
 
-            <div class="tutor-modal-body">
-                <?php tutor_nonce_field(); ?>
-                <input type="hidden" name="action" value="tutor_generate_api_keys">
-                <div class="tutor-row">
-                    <div class="tutor-col">
-                        <label class="tutor-form-label">
-                            <?php esc_html_e( 'User', 'tutor' ); ?>
-                        </label>
-                        <div class="tutor-mb-16">
-                            <input type="text" class="tutor-form-control" value="<?php echo esc_html( tutor_utils()->display_name( $current_user->ID )  ); ?>" disabled>
-                        </div>
-                    </div>
-                </div>
-                <div class="tutor-row">
-                    <div class="tutor-col">
-                        <label class="tutor-form-label">
-                            <?php esc_html_e( 'Permission', 'tutor' ); ?>
-                        </label>
-                        <div class="tutor-mb-16">
-                            <select name="permission" class="tutor-form-select">
-                               <?php foreach ( $permissions as $permission ) : ?>
-                                <option value="<?php echo esc_attr( $permission['value'] ); ?>">
-                                    <?php echo esc_html( $permission['label'] ); ?>
-                                </option>
-                                <?php endforeach; ?>
-                            </select>
-                        </div>
-                    </div>
-                </div>
+			<div class="tutor-modal-body">
+				<?php tutor_nonce_field(); ?>
+				<input type="hidden" name="action" value="tutor_generate_api_keys">
+				<div class="tutor-row">
+					<div class="tutor-col">
+						<label class="tutor-form-label">
+							<?php esc_html_e( 'User', 'tutor' ); ?>
+						</label>
+						<div class="tutor-mb-16">
+							<input type="text" class="tutor-form-control" value="<?php echo esc_html( tutor_utils()->display_name( $user->ID ) ); ?>" disabled>
+						</div>
+					</div>
+				</div>
+				<div class="tutor-row">
+					<div class="tutor-col">
+						<label class="tutor-form-label">
+							<?php esc_html_e( 'Permission', 'tutor' ); ?>
+						</label>
+						<div class="tutor-mb-16">
+							<select name="permission" class="tutor-form-select">
+							   <?php foreach ( $permissions as $permission ) : ?>
+								<option value="<?php echo esc_attr( $permission['value'] ); ?>">
+									<?php echo esc_html( $permission['label'] ); ?>
+								</option>
+								<?php endforeach; ?>
+							</select>
+						</div>
+					</div>
+				</div>
 
-            </div>
+			</div>
 
-            <div class="tutor-modal-footer">
-                <button class="tutor-btn tutor-btn-outline-primary" data-tutor-modal-close>
-                    <?php esc_html_e( 'Cancel', 'tutor' ); ?>
-                </button>
+			<div class="tutor-modal-footer">
+				<button class="tutor-btn tutor-btn-outline-primary" data-tutor-modal-close>
+					<?php esc_html_e( 'Cancel', 'tutor' ); ?>
+				</button>
 
-                <button type="submit" class="tutor-btn tutor-btn-primary">
-                    <?php esc_html_e( 'Generate', 'tutor' ); ?>
-                </button>
-            </div>
-        </form>
-    </div>
+				<button type="submit" class="tutor-btn tutor-btn-primary">
+					<?php esc_html_e( 'Generate', 'tutor' ); ?>
+				</button>
+			</div>
+		</form>
+	</div>
 </div>
