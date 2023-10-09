8ae582ac2c56421529048fdaccb050d02a592be7
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2023-10-09 15:56:26 +0600

    course bundle utm link updated

--- views/pages/course-bundle.php
+++ views/pages/course-bundle.php
@@ -18,7 +18,9 @@
 		<div class="tutor-course-bundle-footer-content">
 			<h2><?php esc_html_e( 'Boost your course sale with bundle!', 'tutor' ); ?></h2>
 			<p class="tutor-mt-12 tutor-color-muted"><?php esc_html_e( 'Create course bundles to sell multiple courses together. Offer a course package for a complete eLearning experience for your students and boost sales!', 'tutor' ); ?></p>
-			<a class="tutor-course-bundle-btn" target="_blank" href="https://www.themeum.com/tutor-lms/pricing/?utm_source=get_pro&utm_medium=wordpress_dashboard&utm_campaign=course_bundle"> <span class="tutor-icon-crown"></span> <?php esc_html_e( 'Get Tutor LMS Pro', 'tutor' ); ?></a>
+			<a class="tutor-course-bundle-btn" target="_blank" href="https://www.themeum.com/tutor-lms/pricing?utm_source=get_pro&utm_medium=wordpress_dashboard&utm_campaign=course_bundle#comparison">
+				<?php esc_html_e( 'Compare Free vs Pro', 'tutor' ); ?>
+			</a>
 		</div>
 	</div>
 </div>
