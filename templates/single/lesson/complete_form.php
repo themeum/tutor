dab256d82b2daaa1042b6354cf20b97e731003ee
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2023-12-02 08:38:39 +0600

    Fix - redirect problem

--- templates/single/lesson/complete_form.php
+++ templates/single/lesson/complete_form.php
@@ -20,10 +20,7 @@ if ( ! $is_completed_lesson ) {
 	?>
 <div class="tutor-topbar-complete-btn tutor-mr-20">
 	<form method="post">
-		<?php
-			wp_nonce_field( tutor()->nonce_action, tutor()->nonce, false );
-			tutor_utils()->referer_field();
-		?>
+		<?php wp_nonce_field( tutor()->nonce_action, tutor()->nonce, false ); ?>
 		<input type="hidden" value="<?php echo esc_attr( get_the_ID() ); ?>" name="lesson_id" />
 		<input type="hidden" value="tutor_complete_lesson" name="tutor_action" />
 		<button type="submit" class="tutor-topbar-mark-btn tutor-btn tutor-btn-primary tutor-ws-nowrap"
