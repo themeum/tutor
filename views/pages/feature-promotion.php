ae0023d320a68e10c18f28237fb8a28f9fd20ab0
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2023-12-06 17:07:57 +0600

    Button text changed

--- views/pages/feature-promotion.php
+++ views/pages/feature-promotion.php
@@ -19,7 +19,7 @@
 			<h2><?php esc_html_e( 'Built-in email notification and email marketing tool!', 'tutor' ); ?></h2>
 			<p class="tutor-mt-12 tutor-color-muted"><?php esc_html_e( 'Send automated email notifications to students, instructors, and admins based on their actions and events on your eLearning website. Customize email templates, send manual emails, and do much more.', 'tutor' ); ?></p>
 			<a class="tutor-course-bundle-btn" target="_blank" href="https://www.themeum.com/tutor-lms/pricing/?utm_source=get_pro&utm_medium=wordpress_dashboard&utm_campaign=email_addon#comparison">
-				<?php esc_html_e( 'Compare Free vs Pro', 'tutor' ); ?>
+				<?php esc_html_e( 'Upgrade to Pro', 'tutor' ); ?>
 			</a>
 		</div>
 	</div>
