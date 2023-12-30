d1647c590f7e702f754634302c90d3e956720758
Sazedul Haque <sazedulhaque5@gmail.com>
2023-12-30 11:56:25 +0600

    Frontend announcements and quiz attempts test added

---
+++ cypress/e2e/frontend/dashboard-announcements.cy.ts
@@ -0,0 +1,32 @@
+import { frontendUrls } from "../../config/page-urls"
+
+describe("Tutor Dashboard Admin or Instructor Announcements", () => {
+    beforeEach(() => {
+        cy.visit(`${Cypress.env("base_url")}/${frontendUrls.dashboard.DASHBOARD}`)
+        cy.loginAsAdmin() // Or Instructor
+        cy.url().should("include", frontendUrls.dashboard.DASHBOARD)
+    })
+
+    it ("should create a new announcement", () => {
+        cy.visit(`${Cypress.env("base_url")}/${frontendUrls.dashboard.ANNOUNCEMENTS}`)
+        cy.get("button[data-tutor-modal-target=tutor_announcement_new]").click()
+
+        cy.get("#tutor_announcement_new input[name=tutor_announcement_title]").type("Important Announcement - Upcoming Student Assembly")
+        cy.get("#tutor_announcement_new textarea[name=tutor_announcement_summary]").type("I trust this message finds you well. As we prepare for the commencement of a dynamic new semester, we have pivotal information to share in our upcoming Student Assembly.")
+        cy.get("#tutor_announcement_new button").contains("Publish").click()
+    })
+
+    it ("should view and delete an announcement", () => {
+        cy.visit(`${Cypress.env("base_url")}/${frontendUrls.dashboard.ANNOUNCEMENTS}`)
+
+        cy.get("body").then(($body) => {
+            if ($body.text().includes("No Data Available in this Section")) {
+                cy.log("No data found")
+            } else {
+                cy.get("button.tutor-announcement-details").eq(0).click()
+                cy.get(".tutor-modal.tutor-is-active button.tutor-modal-btn-delete").click()
+                cy.get(".tutor-modal.tutor-is-active button").contains("Yes, Delete This").click()
+            }
+        })
+    })
+})
---
+++ cypress/e2e/frontend/dashboard-quiz-attempts.cy.ts
@@ -0,0 +1,25 @@
+import { frontendUrls } from "../../config/page-urls"
+
+describe("Tutor Dashboard Quiz Attempts", () => {
+    beforeEach(() => {
+        cy.visit(`${Cypress.env("base_url")}/${frontendUrls.dashboard.QUIZ_ATTEMPTS}`)
+        cy.loginAsAdmin() // Or Instructor
+        cy.url().should("include", frontendUrls.dashboard.DASHBOARD)
+    })
+
+    it ("should review a quiz", () => {
+        cy.visit(`${Cypress.env("base_url")}/${frontendUrls.dashboard.QUIZ_ATTEMPTS}`)
+
+        cy.get("body").then(($body) => {
+            if ($body.text().includes("No Data Available in this Section")) {
+                cy.log("No data found")
+            } else {
+                cy.get(".tutor-table-quiz-attempts a").contains("Review").eq(0).click()
+                cy.window().scrollTo('bottom', { duration: 500, easing: 'linear' })
+                cy.setTinyMceContent(".tutor-instructor-feedback-wrap", "Nice work! You got it right. If not, don't worry—just a small tweak needed. Keep it up!")
+                cy.get(".quiz-attempt-answers-wrap button.tutor-instructor-feedback").click()
+                cy.get(".tutor-quiz-attempt-details-wrapper a").contains("Back").click()
+            }
+        })
+    })
+})
