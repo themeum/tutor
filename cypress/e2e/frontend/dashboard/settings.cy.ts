d223f03f6a0b41fddff7ffc07097b3c04c599cd5
Sazedul Haque <sazedulhaque5@gmail.com>
2024-01-03 10:06:39 +0600

    Dashboard settings test added

---
+++ cypress/e2e/frontend/dashboard/settings.cy.ts
@@ -0,0 +1,55 @@
+import { frontendUrls } from "../../../config/page-urls"
+
+describe("Tutor Dashboard Settings", () => {
+    beforeEach(() => {
+        cy.visit(`${Cypress.env("base_url")}/${frontendUrls.dashboard.SETTINGS}`)
+        cy.loginAsAdmin() // Or Instructor
+        cy.url().should("include", frontendUrls.dashboard.SETTINGS)
+    })
+
+    it ("should update profile", () => {
+        cy.getByInputName("first_name").clear().type("Claire")
+        cy.getByInputName("last_name").clear().type("Hewitt")
+        cy.getByInputName("phone_number").clear().type("9823458204")
+        cy.getByInputName("tutor_profile_job_title").clear().type("UX/UI Designer")
+        cy.setTinyMceContent("#wp-tutor_profile_bio-editor-container", "Passionate UX designer with a flair for creating intuitive and visually appealing digital experiences. Adept at blending creativity with user-centric design principles to deliver impactful solutions.")
+        cy.get("button").contains("Update Profile").click()
+        cy.wait(300)
+    })
+
+    // it ("should reset password", () => {
+    //     cy.get("a.tutor-nav-link").contains("Password").click()
+
+    //     cy.getByInputName("previous_password").type(Cypress.env("admin_password"))
+    //     cy.getByInputName("new_password").type(Cypress.env("admin_password"))
+    //     cy.getByInputName("confirm_new_password").type(Cypress.env("admin_password"))
+    //     cy.get("button").contains("Reset Password").click()
+    //     cy.wait(300)
+    //     cy.loginAsAdmin()
+    // })
+
+    // it ("should reset password", () => {
+    //     cy.get("body").then(($body) => {
+    //         if ($body.text().includes("Withdraw")) {
+    //             cy.get("a.tutor-nav-link").contains("Withdraw").click()
+                
+    //             cy.getByInputName("previous_password").clear().type("John Due")
+
+    //             cy.get("button").contains("Save Withdrawal Account").click()
+    //             cy.wait(300)
+    //         }
+    //     })
+    // })
+
+    it ("should update social profiles", () => {
+        cy.get("a.tutor-nav-link").contains("Social Profile").click()
+
+        cy.getByInputName("_tutor_profile_facebook").clear().type("https://facebook.com/username")
+        cy.getByInputName("_tutor_profile_twitter").clear().type("https://twitter.com/username")
+        cy.getByInputName("_tutor_profile_linkedin").clear().type("https://linkedin.com/username")
+        cy.getByInputName("_tutor_profile_website").clear().type("https://example.com/")
+        cy.getByInputName("_tutor_profile_github").clear().type("https://github.com/username")
+        cy.get("button").contains("Update Profile").click()
+        cy.wait(3000)
+    })
+})
