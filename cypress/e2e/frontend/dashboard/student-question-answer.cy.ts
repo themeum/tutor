2abdbc83965dc7afcbccf336d3199e8cd5f89a30
Sazedul Haque <sazedulhaque5@gmail.com>
2024-01-04 13:49:21 +0600

    Student question and answer test added

---
+++ cypress/e2e/frontend/dashboard/student-question-answer.cy.ts
@@ -0,0 +1,67 @@
+import { frontendUrls } from "../../../config/page-urls"
+
+describe("Tutor Dashboard Student Question and Answers", () => {
+    beforeEach(() => {
+        cy.visit(`${Cypress.env("base_url")}/${frontendUrls.dashboard.QUESTION_AND_ANSWER}`)
+        cy.loginAsStudent()
+        cy.url().should("include", frontendUrls.dashboard.QUESTION_AND_ANSWER)
+    })
+
+    it("should visit and reply a question", () => {
+        cy.intercept("POST", `${Cypress.env("base_url")}/wp-admin/admin-ajax.php`).as("ajaxRequest");
+
+        cy.get("body").then(($body) => {
+            if ($body.text().includes("No Data Available in this Section")) {
+                cy.log("No data found")
+            } else {
+                cy.get(".frontend-dashboard-qna-table-student tbody tr").eq(0).find("a").contains("Reply").click()
+                cy.setTinyMceContent(".tutor-qna-reply-editor", "Hello there!")
+                cy.get("button").contains("Reply").click()
+
+                cy.wait('@ajaxRequest').then((interception) => {
+                    cy.log('response', interception.response.body.success)
+                    expect(interception.response.body.success).to.equal(true);
+                });
+
+                cy.get("a").contains("Back").click()
+            }
+        })
+    })
+
+    it("should mark a question as read or unread", () => {
+        cy.intercept("POST", `${Cypress.env("base_url")}/wp-admin/admin-ajax.php`).as("ajaxRequest");
+
+        cy.get("body").then(($body) => {
+            if ($body.text().includes("No Data Available in this Section")) {
+                cy.log("No data found")
+            } else {
+                cy.get(".frontend-dashboard-qna-table-student tbody tr").eq(0).find(".tutor-icon-kebab-menu").parent().click()
+                cy.get(".tutor-dropdown-parent.is-open .tutor-dropdown-item").eq(0).click()
+                
+                cy.wait('@ajaxRequest').then((interception) => {
+                    cy.log('response', interception.response.body.success)
+                    expect(interception.response.body.success).to.equal(true);
+                });
+            }
+        })
+    })
+
+    it("should delete a question", () => {
+        cy.intercept("POST", `${Cypress.env("base_url")}/wp-admin/admin-ajax.php`).as("ajaxRequest");
+
+        cy.get("body").then(($body) => {
+            if ($body.text().includes("No Data Available in this Section")) {
+                cy.log("No data found")
+            } else {
+                cy.get(".frontend-dashboard-qna-table-student tbody tr").eq(0).find(".tutor-icon-kebab-menu").parent().click()
+                cy.get(".tutor-dropdown-parent.is-open .tutor-dropdown-item").eq(1).click()
+                cy.get(".tutor-modal.tutor-is-active").find("button").contains("Yes, Delete This").click()
+                
+                cy.wait('@ajaxRequest').then((interception) => {
+                    cy.log('response', interception.response.body.success)
+                    expect(interception.response.body.success).to.equal(true);
+                });
+            }
+        })
+    })
+})
