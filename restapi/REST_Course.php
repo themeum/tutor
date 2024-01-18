27d0531483380f455ae92fa5531895f995b2ea20
shewa12 <shewa12kpi@gmail.com>
2024-01-18 16:45:59 +0600

    Course Read API: Passing course author & ratings along with other course data
    Course Rating is now using tutor_utils->get_course_rating method

--- restapi/REST_Course.php
+++ restapi/REST_Course.php
@@ -1,8 +1,12 @@
 <?php
-/*
-@REST API for courses
-@author : themeum
-*/
+/**
+ * Manage Course API
+ *
+ * @package Tutor\RestAPI
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 1.7.1
+ */
 
 namespace TUTOR;
 
@@ -13,26 +17,62 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Rest_Course class
+ */
 class REST_Course {
 
 	use REST_Response;
 
+
+	/**
+	 * The post type associated with the course handler.
+	 *
+	 * @since 1.7.1
+	 *
+	 * @var string $post_type The post type for courses.
+	 */
 	private $post_type;
 
+	/**
+	 * The taxonomy for course categories.
+	 *
+	 * @since 1.7.1
+	 *
+	 * @var string $course_cat_tax The taxonomy for course categories.
+	 */
 	private $course_cat_tax = 'course-category';
 
+	/**
+	 * The taxonomy for course tags.
+	 *
+	 * @since 1.7.1
+	 *
+	 * @var string $course_tag_tax The taxonomy for course tags.
+	 */
 	private $course_tag_tax = 'course-tag';
 
+	/**
+	 * Constructor for the Tutor_Course_Handler class.
+	 *
+	 * Initializes the post type property.
+	 *
+	 * @since 1.7.1
+	 */
 	public function __construct() {
 		$this->post_type = tutor()->course_post_type;
 	}
 
-	/*
-	*require rest request
-	*return course info
-	*pagination enable
-	*category,tags terms included
-	*/
+	/**
+	 * Course read API handler
+	 *
+	 * Get course list along with pagination, categories, tags
+	 * author details, reviews
+	 *
+	 * @param WP_REST_Request $request request data.
+	 *
+	 * @return WP_REST_Response
+	 */
 	public function course( WP_REST_Request $request ) {
 		$order   = sanitize_text_field( $request->get_param( 'order' ) );
 		$orderby = sanitize_text_field( $request->get_param( 'orderby' ) );
@@ -47,11 +87,13 @@ class REST_Course {
 			'orderby'        => $orderby ? $orderby : 'title',
 		);
 
+		$args = apply_filters( 'tutor_rest_course_query_args', $args );
+
 		$query = new WP_Query( $args );
 
-		// if post found
+		// if post found.
 		if ( count( $query->posts ) > 0 ) {
-			// unset filter properpty
+			// unset filter property.
 			array_map(
 				function( $post ) {
 					unset( $post->filter );
@@ -70,12 +112,17 @@ class REST_Course {
 
 				$tag = wp_get_post_terms( $post->ID, $this->course_tag_tax );
 
+				$author = get_userdata( $post->post_author );
+				is_a( $author, 'WP_User' ) ? $post->post_author = $author->data : new \stdClass();
+
+				$post->ratings = tutor_utils()->get_course_rating( $post->ID );
+
 				$post->course_category = $category;
 
 				$post->course_tag = $tag;
 
+				$post = apply_filters( 'tutor_rest_course_single_post', $post );
 				array_push( $data['posts'], $post );
-
 			}
 
 			$response = array(
@@ -96,11 +143,16 @@ class REST_Course {
 		return self::send( $response );
 	}
 
-	/*
-	*require rest request
-	*return post meta items
-	*/
-	function course_detail( WP_REST_Request $request ) {
+	/**
+	 * Course Details API handler
+	 *
+	 * @since 1.7.1
+	 *
+	 * @param WP_REST_Request $request request params.
+	 *
+	 * @return WP_REST_Response
+	 */
+	public function course_detail( WP_REST_Request $request ) {
 		$post_id = $request->get_param( 'id' );
 
 		$detail = array(
@@ -143,14 +195,20 @@ class REST_Course {
 		return self::send( $response );
 	}
 
-	/*
-	*return post type terms
-	*/
+	/**
+	 * Get Course by Terms API handler
+	 *
+	 * @since 1.7.1
+	 *
+	 * @param WP_REST_Request $request request params.
+	 *
+	 * @return WP_REST_Response
+	 */
 	public function course_by_terms( WP_REST_Request $request ) {
 		$post_fields  = $request->get_params();
 		$validate_err = $this->validate_terms( $post_fields );
 
-		// check array or not
+		// check array or not.
 		if ( count( $validate_err ) > 0 ) {
 			$response = array(
 				'status_code' => 'validation_error',
@@ -161,7 +219,7 @@ class REST_Course {
 			return self::send( $response );
 		}
 
-		// sanitize terms
+		// sanitize terms.
 		$categories = sanitize_term( $request['categories'], $this->course_cat_tax, $context = 'db' );
 
 		$tags = sanitize_term( $request['tags'], $this->course_tag_tax, $context = 'db' );
@@ -187,7 +245,7 @@ class REST_Course {
 		$query = new WP_Query( $args );
 
 		if ( count( $query->posts ) > 0 ) {
-			// unset filter proterty
+			// unset filter property.
 			array_map(
 				function( $post ) {
 					unset( $post->filter );
@@ -212,10 +270,15 @@ class REST_Course {
 		return self::send( $response );
 	}
 
-	/*
-	*categories array validation
-	*tags array validation
-	*/
+	/**
+	 * Validate terms
+	 *
+	 * @since 1.7.1
+	 *
+	 * @param array $post post array.
+	 *
+	 * @return array validation errors.
+	 */
 	public function validate_terms( array $post ) {
 		$categories = $post['categories'];
 		$tags       = $post['tags'];
@@ -233,6 +296,15 @@ class REST_Course {
 		return $error;
 	}
 
+	/**
+	 * Course sort by price API handler
+	 *
+	 * @since 1.7.1
+	 *
+	 * @param WP_REST_Request $request request params.
+	 *
+	 * @return WP_REST_Response
+	 */
 	public function course_sort_by_price( WP_REST_Request $request ) {
 		$order = $request->get_param( 'order' );
 		$paged = $request->get_param( 'page' );
@@ -258,11 +330,12 @@ class REST_Course {
 				),
 			),
 		);
+		$args = apply_filters( 'tutor_rest_course_sort_by_price_args', $args );
 
 		$query = new WP_Query( $args );
 
 		if ( count( $query->posts ) > 0 ) {
-			// unset filter property
+			// unset filter property.
 			array_map(
 				function( $post ) {
 					unset( $post->filter );
