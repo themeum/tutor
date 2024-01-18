0c3af52f769666e8f72894653182ec95abcc7a94
shewa12 <shewa12kpi@gmail.com>
2024-01-18 15:20:28 +0600

    WPCS: Spacing and indentations fixed

--- restapi/REST_Author.php
+++ restapi/REST_Author.php
@@ -1,9 +1,11 @@
 <?php
 namespace TUTOR;
+
 use WP_REST_Request;
 
-if(!defined( 'ABSPATH' ))
-exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 class REST_Author {
 
@@ -13,38 +15,39 @@ class REST_Author {
 
 	/*
 	*require user id
-	*return json object with user detail 
+	*return json object with user detail
 	*/
-	public function author_detail(WP_REST_Request $request) {
-		$this->user_id = $request->get_param('id');
+	public function author_detail( WP_REST_Request $request ) {
+		$this->user_id = $request->get_param( 'id' );
 		global $wpdb;
-		$table = $wpdb->prefix."users";
-		//author obj
+		$table = $wpdb->prefix . 'users';
+		// author obj
 		$author = $wpdb->get_row(
 			$wpdb->prepare(
-			"SELECT user_email, user_registered, display_name FROM $table WHERE ID = %d", 
-			$this->user_id
-		));
-
-		if($author) {
-			//get author course id
-			$author->courses = get_user_meta($this->user_id,'_tutor_instructor_course_id',false);
-
-			$response = array(
-				'status_code'=> 'success',
-				'message'=> __('Author details retrieved successfully','tutor'),
-				'data'=> $author
-			);
-
-			return self::send($response);
-		}
-		
-		$response = array(
-			'status_code'=> 'invalid_id',
-			'message'=> __('Author not found','tutor'),
-			'data'=> []
+				"SELECT user_email, user_registered, display_name FROM $table WHERE ID = %d",
+				$this->user_id
+			)
 		);
 
-		return self::send($response);		
+		if ( $author ) {
+			// get author course id
+			$author->courses = get_user_meta( $this->user_id, '_tutor_instructor_course_id', false );
+
+			$response = array(
+				'status_code' => 'success',
+				'message'     => __( 'Author details retrieved successfully', 'tutor' ),
+				'data'        => $author,
+			);
+
+			return self::send( $response );
+		}
+
+		$response = array(
+			'status_code' => 'invalid_id',
+			'message'     => __( 'Author not found', 'tutor' ),
+			'data'        => array(),
+		);
+
+		return self::send( $response );
 	}
 }
--- restapi/REST_Response.php
+++ restapi/REST_Response.php
@@ -5,17 +5,19 @@
 */
 
 namespace TUTOR;
+
 use WP_REST_Response;
 
-if( ! defined('ABSPATH')) 
-exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 trait REST_Response {
 	/*
-		@send WP_REST_Response with 
+		@send WP_REST_Response with
 		code, message along with data
 	*/
-	public static function send(array $response) {
-		return new WP_REST_Response($response);
-	} 
+	public static function send( array $response ) {
+		return new WP_REST_Response( $response );
+	}
 }
