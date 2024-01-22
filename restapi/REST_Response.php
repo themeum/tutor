8ae9e4cafaf44284ffcb8847d69bd04c25a90047
shewa12 <shewa12kpi@gmail.com>
2024-01-22 10:44:02 +0600

    Filter Hook: Filter hook provided for extending REST Response

--- restapi/REST_Response.php
+++ restapi/REST_Response.php
@@ -1,8 +1,12 @@
 <?php
-/*
-@Reliable for give rest response
-@author : themeum
-*/
+/**
+ * Ensure REST response
+ *
+ * @package Tutor\RestAPI
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 1.7.1
+ */
 
 namespace TUTOR;
 
@@ -13,11 +17,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 trait REST_Response {
-	/*
-		@send WP_REST_Response with
-		code, message along with data
-	*/
+
+	/**
+	 * Send response
+	 *
+	 * @since 1.7.1
+	 *
+	 * @param array $response The response data.
+	 *
+	 * @return WP_REST_Response
+	 */
 	public static function send( array $response ) {
-		return new WP_REST_Response( $response );
+		$response = new WP_REST_Response( $response );
+		return apply_filters( 'tutor_rest_response', $response );
 	}
 }
