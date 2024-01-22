ba2e8e75894e629dd0f11928e86f662e4f6f1470
shewa12 <shewa12kpi@gmail.com>
2024-01-22 12:42:37 +0600

    WPCS: WPCS erros fixed

--- restapi/REST_Topic.php
+++ restapi/REST_Topic.php
@@ -1,28 +1,54 @@
 <?php
-/*
-@REST API for course topics
-@author : themeum
-*/
+/**
+ * REST API for course topics.
+ *
+ * @package Tutor\RestAPI
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 1.7.1
+ */
 
 namespace TUTOR;
 
 use WP_REST_Request;
-use WP_Query;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Class REST_Topic
+ *
+ * @package Tutor
+ *
+ * @since 1.7.1
+ */
 class REST_Topic {
 	use REST_Response;
 
+	/**
+	 * Post parent ID.
+	 *
+	 * @var int $post_parent The ID of the post parent.
+	 */
 	private $post_parent;
+
+	/**
+	 * Post type.
+	 *
+	 * @var string $post_type The post type for topics.
+	 */
 	private $post_type = 'topics';
 
-	/*
-	*require rest request
-	*return topic by course id
-	*/
+	/**
+	 * Retrieve topics by course ID via REST API.
+	 *
+	 * @param WP_REST_Request $request The REST request object.
+	 *
+	 * @since 1.7.1
+	 *
+	 * @return mixed
+	 */
 	public function course_topic( WP_REST_Request $request ) {
 		$this->post_parent = $request->get_param( 'id' );
 
