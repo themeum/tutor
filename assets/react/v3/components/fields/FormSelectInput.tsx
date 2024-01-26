5867a76c099798fdb03159357d8febc43de6cc5c
Sazedul Haque <sazedulhaque5@gmail.com>
2024-01-26 10:35:45 +0600

    Removed files from previous project

--- assets/react/v3/components/fields/FormSelectInput.tsx
+++ assets/react/v3/components/fields/FormSelectInput.tsx
@@ -1,21 +1,27 @@
-import Button, { ButtonVariant } from '@Atoms/Button';
-import Radio from '@Atoms/Radio';
-import SVGIcon from '@Atoms/SVGIcon';
-import { borderRadius, colorPalate, fontSize, shadow, spacing, zIndex } from '@Config/styles';
-import { typography } from '@Config/typography';
-import { css } from '@emotion/react';
-import { Portal, usePortalPopover } from '@Hooks/usePortalPopover';
-import { useTranslation } from '@Hooks/useTranslation';
-import { FormControllerProps } from '@Utils/form';
-import { styleUtils } from '@Utils/style-utils';
-import { Option } from '@Utils/types';
-import { noop } from '@Utils/util';
-import { useCombobox } from 'downshift';
-import { ReactNode, useMemo, useState } from 'react';
+import Button, { ButtonVariant } from "@Atoms/Button";
+import SVGIcon from "@Atoms/SVGIcon";
+import {
+  borderRadius,
+  colorPalateTutor,
+  fontSize,
+  lineHeight,
+  shadow,
+  spacing,
+  zIndex,
+} from "@Config/styles";
+import { typography } from "@Config/typography";
+import { css } from "@emotion/react";
+import { Portal, usePortalPopover } from "@Hooks/usePortalPopover";
+import { FormControllerProps } from "@Utils/form";
+import { styleUtils } from "@Utils/style-utils";
+import { Option } from "@Utils/types";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
-import FormFieldWrapper from './FormFieldWrapper';
+import FormFieldWrapper from "./FormFieldWrapper";
+import { noop } from "@Utils/util";
+import { __ } from "@wordpress/i18n";
 
-interface FormSelectInputProps<T> extends FormControllerProps<T | null> {
+type FormSelectInputProps<T> = {
   label?: string;
   options: Option<T>[];
   placeholder?: string;
@@ -27,13 +33,15 @@ interface FormSelectInputProps<T> extends FormControllerProps<T | null> {
   isHidden?: boolean;
   showRadio?: boolean;
   isInlineLabel?: boolean;
-  clearable?: boolean;
-  icon?: ReactNode;
-  iconPosition?: 'left' | 'right';
   hideCaret?: boolean;
   listLabel?: string;
   removeBorder?: boolean;
-}
+  isClearable?: boolean;
+  responsive?: boolean;
+  showArrowUpDown?: boolean;
+  helpText?: string;
+  removeOptionsMinWidth?: boolean;
+} & FormControllerProps<T | null>;
 
 const FormSelectInput = <T,>({
   options,
@@ -41,86 +49,54 @@ const FormSelectInput = <T,>({
   fieldState,
   onChange = noop,
   label,
-  placeholder = '',
+  placeholder = "",
   disabled,
   readOnly,
   loading,
-  isSearchable,
-  isHidden,
-  showRadio,
+  isSearchable = false,
   isInlineLabel,
-  clearable,
-  icon,
-  iconPosition = 'left',
   hideCaret,
   listLabel,
-  removeBorder = false,
+  isClearable = true,
+  showArrowUpDown = false,
+  helpText,
+  removeOptionsMinWidth = false,
 }: FormSelectInputProps<T>) => {
-  const t = useTranslation();
-  const getInitialValue = () => {
-    return options.find((item) => String(item.value) === String(field.value))?.label || '';
-  };
+  const getInitialValue = useCallback(() => {
+    return options.find((item) => item.value === field.value)?.label || "";
+  }, [options, field.value]);
 
   const [inputValue, setInputValue] = useState(getInitialValue);
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
 
   const selections = useMemo(() => {
     if (isSearchable) {
-      return options.filter(({ label }) => label.toLowerCase().startsWith(searchText.toLowerCase()));
+      return options.filter(({ label }) =>
+        label.toLowerCase().startsWith(searchText.toLowerCase())
+      );
     }
 
     return options;
   }, [searchText, isSearchable, options]);
 
-  const {
-    isOpen,
-    openMenu,
-    closeMenu,
-    getInputProps,
-    getMenuProps,
-    getItemProps,
-    getComboboxProps,
-    highlightedIndex,
-    selectedItem,
-  } = useCombobox({
-    items: selections,
-    selectedItem: options.find((option) => String(option.value) === String(field.value)) || null,
-    itemToString: (item) => item?.label || '',
-    onInputValueChange: (changes) => {
-      setInputValue(changes.inputValue || '');
-      setSearchText(changes.inputValue || '');
-    },
-    onSelectedItemChange: (item) => {
-      if (item.selectedItem) {
-        field.onChange(item.selectedItem.value);
-        setInputValue(item.selectedItem.label);
-        onChange(item.selectedItem);
-      }
-    },
-    stateReducer: (_, actionAndChanges) => {
-      const { type, changes } = actionAndChanges;
-      switch (type) {
-        case useCombobox.stateChangeTypes.InputKeyDownEnter:
-        case useCombobox.stateChangeTypes.InputBlur:
-          return {
-            ...changes,
-            isOpen: true,
-          };
-        case useCombobox.stateChangeTypes.ItemClick:
-          return {
-            ...changes,
-            ...(changes.selectedItem ? { inputValue: changes.selectedItem.label } : { inputValue: '' }),
-          };
-        default:
-          return changes;
-      }
-    },
-  });
-
-  const { triggerRef, triggerWidth, position, popoverRef } = usePortalPopover<HTMLDivElement, HTMLDivElement>({
+  const { triggerRef, triggerWidth, position, popoverRef } = usePortalPopover<
+    HTMLDivElement,
+    HTMLDivElement
+  >({
     isOpen,
     isDropdown: true,
   });
+
+  useEffect(() => {
+    setInputValue(getInitialValue);
+  }, [field.value, getInitialValue]);
+
+  useEffect(() => {
+    if (isOpen) {
+      setInputValue(getInitialValue);
+    }
+  }, [getInitialValue, isOpen]);
 
   return (
     <FormFieldWrapper
@@ -130,100 +106,124 @@ const FormSelectInput = <T,>({
       disabled={disabled || options.length === 0}
       readOnly={readOnly}
       loading={loading}
-      isHidden={isHidden}
       isInlineLabel={isInlineLabel}
-      removeBorder={removeBorder}
+      helpText={helpText}
     >
       {(inputProps) => {
         const { css: inputCss, ...restInputProps } = inputProps;
-        const hasIcon = !!icon;
+
         return (
-          <div css={styles.mainWrapper} {...getComboboxProps()}>
+          <div css={styles.mainWrapper}>
             <div css={styles.inputWrapper} ref={triggerRef}>
-              {icon && iconPosition === 'left' && <span css={styles.icon(iconPosition)}>{icon}</span>}
               <input
                 {...restInputProps}
-                {...getInputProps({
-                  onClick: () => {
-                    if (!readOnly && !isOpen) {
-                      setSearchText('');
-                      openMenu();
-                    }
-                  },
-                  value: inputValue || '',
-                })}
-                css={[inputCss, styles.input(hasIcon, iconPosition)]}
+                onClick={() => setIsOpen((previousState) => !previousState)}
+                css={[inputCss, styles.input]}
                 autoComplete="off"
                 readOnly={readOnly || !isSearchable}
                 placeholder={placeholder}
+                value={inputValue}
+                onChange={(event) => {
+                  setInputValue(event.target.value);
+                  setSearchText(event.target.value);
+                }}
               />
 
               {!hideCaret && (
                 <button
                   type="button"
-                  css={styleUtils.resetButton}
+                  css={styles.caretButton}
                   onClick={() => {
-                    isOpen ? closeMenu() : openMenu();
+                    setIsOpen((previousState) => !previousState);
                   }}
-                  disabled={readOnly}
+                  disabled={readOnly || options.length === 0}
                 >
-                  <SVGIcon name="arrowDown" width={12} height={8} style={styles.toggleIcon(isOpen)} />
+                  {showArrowUpDown ? (
+                    <SVGIcon
+                      name="chevronDown"
+                      width={20}
+                      height={20}
+                      style={styles.arrowUpDown}
+                    />
+                  ) : (
+                    <SVGIcon
+                      name="chevronDown"
+                      width={20}
+                      height={20}
+                      style={styles.toggleIcon({ isOpen })}
+                    />
+                  )}
                 </button>
               )}
             </div>
 
-            <div {...getMenuProps()}>
-              <Portal isOpen={isOpen} onClickOutside={closeMenu}>
-                <div
-                  css={[styles.optionsWrapper, { left: position.left, top: position.top, maxWidth: triggerWidth }]}
-                  ref={popoverRef}
-                >
-                  <ul css={[styles.options]}>
-                    {!!listLabel && <li css={styles.listLabel}>{listLabel}</li>}
-                    {selections.map((option, index) => (
-                      <li
-                        key={index}
-                        {...getItemProps({ index, item: option })}
-                        css={styles.optionItem({
-                          isHovered: highlightedIndex === index,
-                          isSelected: selectedItem?.value === option.value,
-                        })}
+            <Portal isOpen={isOpen} onClickOutside={() => setIsOpen(false)}>
+              <div
+                css={[
+                  styles.optionsWrapper,
+                  {
+                    left: position.left,
+                    top: position.top,
+                    maxWidth: triggerWidth,
+                  },
+                ]}
+                ref={popoverRef}
+              >
+                <ul css={[styles.options(removeOptionsMinWidth)]}>
+                  {!!listLabel && <li css={styles.listLabel}>{listLabel}</li>}
+                  {selections.map((option) => (
+                    <li
+                      key={String(option.value)}
+                      css={styles.optionItem({
+                        isSelected: option.value === field.value,
+                      })}
+                    >
+                      <button
+                        css={styles.label}
+                        onClick={() => {
+                          field.onChange(option.value);
+                          setSearchText("");
+                          onChange(option);
+                          setIsOpen(false);
+                        }}
                       >
-                        <p css={styles.label}>
-                          {showRadio ? (
-                            <Radio label={option.label} checked={option.value === field.value} readOnly />
-                          ) : (
-                            option.label
-                          )}
-                        </p>
-                      </li>
-                    ))}
+                        {option.label}
+                      </button>
+                    </li>
+                  ))}
 
-                    {clearable && (
-                      <div css={styles.clearButton(hasIcon)}>
-                        <Button
-                          variant={ButtonVariant.plain}
-                          disabled={inputValue === ''}
-                          onClick={() => {
-                            field.onChange(null);
-                            setInputValue('');
-                            setSearchText('');
-                          }}
-                        >
-                          {t('COM_SPPAGEBUILDER_STORE_FORM_SELECT_CLEAR')}
-                        </Button>
-                      </div>
-                    )}
-                  </ul>
-                </div>
-              </Portal>
-            </div>
+                  {isClearable && (
+                    <div
+                      css={styles.clearButton({
+                        isDisabled: inputValue === "",
+                      })}
+                    >
+                      <Button
+                        variant={ButtonVariant.text}
+                        disabled={inputValue === ""}
+                        icon={<SVGIcon name="delete" />}
+                        onClick={() => {
+                          field.onChange(null);
+                          setInputValue("");
+                          setSearchText("");
+                          setIsOpen(false);
+                        }}
+                      >
+                        {__('Clear', 'tutor')}
+                      </Button>
+                    </div>
+                  )}
+                </ul>
+              </div>
+            </Portal>
           </div>
         );
       }}
     </FormFieldWrapper>
   );
 };
+
+export default FormSelectInput;
 
 const styles = {
   mainWrapper: css`
@@ -236,136 +236,128 @@ const styles = {
     align-items: center;
     position: relative;
   `,
-  input: (hasIcon: boolean, position: 'left' | 'right') => css`
+  input: css`
     ${typography.body()};
     width: 100%;
     cursor: pointer;
+    padding-right: ${spacing[32]};
+    ${styleUtils.textEllipsis};
 
-    ${hasIcon &&
-    position === 'left' &&
-    css`
-      padding-left: ${spacing[40]};
-    `}
-
-    ${hasIcon &&
-    position === 'right' &&
-    css`
-      padding-right: ${spacing[40]};
-    `}
+    :focus {
+      outline: none;
+      box-shadow: ${shadow.focus};
+    }
   `,
-  icon: (position: 'left' | 'right') => css`
-    position: absolute;
-    top: ${spacing[8]};
-    height: 20px;
-    width: 20px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: ${colorPalate.icon.default};
-
-    ${position === 'left' &&
-    css`
-      left: ${spacing[16]};
-    `}
-
-    ${position === 'right' &&
-    css`
-      right: ${spacing[16]};
-    `}
-  `,
-
   listLabel: css`
     ${typography.body()};
-    color: ${colorPalate.text.neutral};
+    color: ${colorPalateTutor.text.subdued};
     min-height: 40px;
     display: flex;
     align-items: center;
     padding-left: ${spacing[16]};
   `,
-  clearButton: (hasOptionIcon: boolean) => css`
-    padding: ${spacing[8]} ${spacing[12]};
+  clearButton: ({ isDisabled = false }: { isDisabled: boolean }) => css`
+    padding: ${spacing[4]} ${spacing[8]};
+    border-top: 1px solid ${colorPalateTutor.stroke.default};
 
     & > button {
       padding: 0;
-    }
+      width: 100%;
+      font-size: ${fontSize[12]};
 
-    ${hasOptionIcon &&
-    css`
-      padding-left: ${spacing[32]};
-    `}
-  `,
-  optionIcon: css`
-    font-size: ${fontSize[24]};
+      > span {
+        justify-content: center;
+      }
+
+      ${!isDisabled &&
+        css`
+          color: ${colorPalateTutor.text.title};
+
+          &:hover {
+            text-decoration: underline;
+          }
+        `}
+    }
   `,
   optionsWrapper: css`
     position: absolute;
     width: 100%;
   `,
-  options: css`
+  options: (removeOptionsMinWidth: boolean) => css`
     z-index: ${zIndex.dropdown};
-    background-color: ${colorPalate.basic.white};
+    background-color: ${colorPalateTutor.background.white};
     list-style-type: none;
     box-shadow: ${shadow.popover};
     padding: ${spacing[4]} 0;
     margin: 0;
     max-height: 500px;
-    overflow-y: auto;
     border-radius: ${borderRadius[6]};
+    ${styleUtils.overflowYAuto};
 
-    ::-webkit-scrollbar {
-      background-color: ${colorPalate.basic.white};
-      width: 10px;
-    }
-
-    ::-webkit-scrollbar-thumb {
-      background-color: ${colorPalate.basic.secondary};
-      border-radius: ${borderRadius[6]};
-    }
+    ${!removeOptionsMinWidth &&
+      css`
+        min-width: 200px;
+      `}
   `,
-  optionItem: ({ isHovered, isSelected }: { isHovered: boolean; isSelected: boolean }) => css`
+  optionItem: ({ isSelected = false }: { isSelected: boolean }) => css`
     ${typography.body()};
-    min-height: 40px;
+    min-height: 36px;
+    height: 100%;
+    width: 100%;
     display: flex;
     align-items: center;
     transition: background-color 0.3s ease-in-out;
     cursor: pointer;
 
-    ${isHovered &&
-    css`
-      background-color: ${colorPalate.surface.hover};
-    `}
+    &:hover {
+      background-color: ${colorPalateTutor.background.hover};
+    }
 
     ${isSelected &&
-    css`
-      background-color: ${colorPalate.surface.selected.default};
-      position: relative;
-      &::before {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 3px;
-        height: 100%;
-        background-color: ${colorPalate.actions.primary.default};
-        border-radius: 0 ${borderRadius[6]} ${borderRadius[6]} 0;
-      }
-    `}
+      css`
+        background-color: ${colorPalateTutor.background.active};
+        position: relative;
+
+        &::before {
+          content: "";
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 3px;
+          height: 100%;
+          background-color: ${colorPalateTutor.action.primary.default};
+          border-radius: 0 ${borderRadius[6]} ${borderRadius[6]} 0;
+        }
+      `}
   `,
   label: css`
-    margin: ${spacing[4]} ${spacing[12]};
+    ${styleUtils.resetButton};
+    width: 100%;
+    height: 100%;
     display: flex;
     gap: ${spacing[8]};
+    margin: 0 ${spacing[12]};
+    padding: ${spacing[6]} 0;
+    text-align: left;
+    line-height: ${lineHeight[24]};
+    word-break: break-all;
+    cursor: pointer;
   `,
-  toggleIcon: (isOpen: boolean) => css`
-    position: absolute;
-    top: 14px;
-    right: ${spacing[12]};
+  toggleIcon: ({ isOpen = false }: { isOpen: boolean }) => css`
+    color: ${colorPalateTutor.icon.default};
     transition: transform 0.3s ease-in-out;
 
     ${isOpen &&
-    css`
-      transform: rotate(180deg);
-    `}
+      css`
+        transform: rotate(180deg);
+      `}
+  `,
+  arrowUpDown: css`
+    color: ${colorPalateTutor.icon.default};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: ${spacing[2]};
   `,
   optionsContainer: css`
     position: absolute;
@@ -375,6 +367,14 @@ const styles = {
     max-width: calc(100% - 32px);
     max-height: calc(100% - 32px);
   `,
+  caretButton: css`
+    ${styleUtils.resetButton};
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: ${spacing[8]};
+    margin: auto 0;
+    display: flex;
+    align-items: center;
+  `,
 };
-
-export default FormSelectInput;
