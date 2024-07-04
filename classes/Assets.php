1244724686d88274ffdd5acb31164728d7c872d8
shewa12 <shewa12kpi@gmail.com>
2024-07-04 10:27:46 +0600

    Order & Coupon Scripts: Order details & coupon js scripts enqueued

--- classes/Assets.php
+++ classes/Assets.php
@@ -160,6 +160,8 @@ class Assets {
 	 * Enqueue scripts for admin
 	 *
 	 * @since 1.0.0
+	 * @since 3.0.0 Order details & coupon scripts added.
+	 *
 	 * @return void
 	 */
 	public function admin_scripts() {
@@ -178,6 +180,10 @@ class Assets {
 
 		wp_enqueue_script( 'tutor-select2', tutor()->url . 'assets/packages/select2/select2.full.min.js', array( 'jquery' ), TUTOR_VERSION, true );
 		wp_enqueue_script( 'tutor-admin', tutor()->url . 'assets/js/tutor-admin.min.js', array( 'jquery', 'tutor-script', 'wp-color-picker', 'wp-i18n', 'wp-data' ), TUTOR_VERSION, true );
+
+		// Tutor order detail & coupon scripts.
+		wp_enqueue_script( 'tutor-order-details', tutor()->url . 'assets/js/tutor-order-details.min.js', array( 'jquery' ), TUTOR_VERSION, true );
+		wp_enqueue_script( 'tutor-coupon', tutor()->url . 'assets/js/tutor-coupon.min.js', array( 'jquery' ), TUTOR_VERSION, true );
 	}
 
 	/**
