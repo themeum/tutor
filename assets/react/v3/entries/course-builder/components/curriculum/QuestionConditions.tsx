5087b6db40498cfde6797f5219d11154dbdf19b6
Fahim Faisal <fahim.faisal.abir@gmail.com>
2024-07-15 18:08:16 +0600

    feat: Update activeQuestionType form field in QuestionConditions
    The code changes update the `activeQuestionType` form field in the `QuestionConditions` component to use the correct path for accessing the question type value. This ensures that the correct value is displayed and submitted when interacting with the form.

--- assets/react/v3/entries/course-builder/components/curriculum/QuestionConditions.tsx
+++ assets/react/v3/entries/course-builder/components/curriculum/QuestionConditions.tsx
@@ -61,7 +61,7 @@ const QuestionConditions = () => {
   const { activeQuestionIndex, activeQuestionId } = useQuizModalContext();
   const form = useFormContext<QuizForm>();
 
-  const activeQuestionType = form.watch(`questions.${activeQuestionIndex}.question_type`);
+  const activeQuestionType = form.watch(`questions.${activeQuestionIndex}.question_settings.question_type`);
 
   if (!activeQuestionId) {
     return (
@@ -76,7 +76,9 @@ const QuestionConditions = () => {
       <div css={styles.questionTypeWrapper}>
         <Controller
           control={form.control}
-          name={`questions.${activeQuestionIndex}.question_type` as 'questions.0.question_type'}
+          name={
+            `questions.${activeQuestionIndex}.question_settings.question_type` as 'questions.0.question_settings.question_type'
+          }
           render={(controllerProps) => (
             <FormSelectInput {...controllerProps} label={__('Question Type', 'tutor')} options={questionTypeOptions} />
           )}
