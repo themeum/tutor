01ce7fd4fd00a35a03a0622c8aedc53ff4f0d752
shewa12 <shewa12kpi@gmail.com>
2024-09-07 22:57:09 +0600

    Order update email templates

---
+++ templates/email/order_updated_email_to_students.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * E-mail template for refunded order
+ *
+ * @package TutorPro
+ * @subpackage Templates\Email
+ *
+ * @since 2.5.0
+ */
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+	<?php require TUTOR_EMAIL()->path . 'views/email_styles.php'; ?>
+</head>
+
+<body>
+	<div class="tutor-email-body">
+		<div class="tutor-email-wrapper">
+			<?php require TUTOR_PRO()->path . 'templates/email/email_header.php'; ?>
+			<div class="tutor-email-content">
+				<div class="tutor-mr-160">
+					<h6 data-source="email-heading" class="tutor-email-heading">{email_heading}</h6>
+				</div>
+				<br>
+				<div class="email-user-content" data-source="email-additional-message">{email_message}</div>
+				<br>
+
+				<div class="tutor-email-buttons">
+				<a href="{site_order_url}" class="tutor-email-button"><?php esc_html_e( 'View Details', 'tutor-pro' ); ?></a>
+				</div>
+			</div>
+			<?php require TUTOR()->path . 'templates/email/email_footer.php'; ?>
+		</div>
+	</div>
+</body>
+</html>
