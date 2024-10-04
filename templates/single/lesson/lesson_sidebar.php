d99d9cf8fd148ca5111ff07336c65b97d53d9d51
Anindra Das Bivas <bivasdas911@gmail.com>
2024-10-04 17:26:40 +0600

    phpcs fix

--- templates/single/lesson/lesson_sidebar.php
+++ templates/single/lesson/lesson_sidebar.php
@@ -118,7 +118,7 @@ if ( $topics->have_posts() ) {
 						$is_h5p_quiz = isset( $quiz_option['quiz_type'] ) && 'tutor_h5p_quiz' === $quiz_option['quiz_type'];
 						?>
 						<?php if ( $is_h5p_quiz ) : ?>
-						<?php if ( tutor()->has_pro && \TutorPro\H5P\H5P::is_enabled() ) : ?>
+							<?php if ( tutor()->has_pro && \TutorPro\H5P\H5P::is_enabled() ) : ?>
 						<div class="tutor-course-topic-item tutor-course-topic-item-quiz<?php echo ( get_the_ID() == $current_post->ID ) ? ' is-active' : ''; ?>" data-quiz-id="<?php echo esc_attr( $quiz->ID ); ?>">
 							<a href="<?php echo $show_permalink ? esc_url( get_permalink( $quiz->ID ) ) : '#'; ?>" data-quiz-id="<?php echo esc_attr( $quiz->ID ); ?>">
 								<div class="tutor-d-flex tutor-mr-32">
@@ -177,8 +177,8 @@ if ( $topics->have_posts() ) {
 								</div>
 							</a>
 						</div>
-						<?php endif; ?>
-						<?php else: ?>
+							<?php endif; ?>
+						<?php else : ?>
 						<div class="tutor-course-topic-item tutor-course-topic-item-quiz<?php echo ( get_the_ID() == $current_post->ID ) ? ' is-active' : ''; ?>" data-quiz-id="<?php echo esc_attr( $quiz->ID ); ?>">
 							<a href="<?php echo $show_permalink ? esc_url( get_permalink( $quiz->ID ) ) : '#'; ?>" data-quiz-id="<?php echo esc_attr( $quiz->ID ); ?>">
 								<div class="tutor-d-flex tutor-mr-32">
