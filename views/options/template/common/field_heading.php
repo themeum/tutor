d69f19127c6fee14e54a0b161f23e30155da31b0
shewa12 <shewa12kpi@gmail.com>
2024-10-07 18:46:11 +0600

    Mergin removed if desc field not available

--- views/options/template/common/field_heading.php
+++ views/options/template/common/field_heading.php
@@ -11,6 +11,6 @@
 
 ?>
 <div class="tutor-option-field-label">
-	<?php isset( $field['label'] ) ? printf( '<div class="tutor-fs-6 tutor-fw-medium tutor-mb-8" tutor-option-name>%s</div>', esc_attr( $field['label'] ) ) : null; ?>
-	<?php isset( $field['desc'] ) ? printf( '<div class="tutor-fs-7 tutor-color-muted">%s</div>', wp_kses_post( $field['desc'] ) ) : null; ?>
+	<?php isset( $field['label'] ) ? printf( '<div class="tutor-fs-6 tutor-fw-medium" tutor-option-name>%s</div>', esc_attr( $field['label'] ) ) : null; ?>
+	<?php ( isset( $field['desc'] ) && ! empty( $field['desc'] ) ) ? printf( '<div class="tutor-fs-7 tutor-color-muted tutor-mt-8">%s</div>', wp_kses_post( $field['desc'] ) ) : null; ?>
 </div>
