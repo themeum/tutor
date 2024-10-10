3f97b71000e73f9ab223ef7840218cc3a2649f28
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2024-10-10 16:47:26 +0600

    update checkout page for tax calculation

--- templates/ecommerce/checkout-details.php
+++ templates/ecommerce/checkout-details.php
@@ -8,7 +8,9 @@
  * @since 3.0.0
  */
 
+use Tutor\Ecommerce\CartController;
 use Tutor\Ecommerce\OptionKeys;
+use Tutor\Ecommerce\Tax;
 use TUTOR\Input;
 use Tutor\Models\CouponModel;
 use Tutor\Models\OrderModel;
@@ -19,6 +21,14 @@ $coupon_model = new CouponModel();
 
 $plan_info = apply_filters( 'tutor_checkout_plan_info', $plan_info, $plan_id );
 
+$cart_controller = new CartController();
+$get_cart        = $cart_controller->get_cart_items();
+$courses         = $get_cart['courses'];
+$total_count     = $courses['total_count'];
+$course_list     = $courses['results'];
+$subtotal        = 0;
+$grand_total     = 0;
+
 /**
  * Course/Bundle ids to apply coupon
  */
@@ -26,6 +36,18 @@ $object_ids = array();
 $order_type = OrderModel::TYPE_SINGLE_ORDER;
 
 $is_coupon_applicable = tutor_utils()->get_option( OptionKeys::IS_COUPON_APPLICABLE );
+
+$coupon_code            = Input::sanitize_request_data( 'coupon_code', null );
+$has_manual_coupon_code = ! empty( $coupon_code );
+
+$is_tax_included_in_price = Tax::is_tax_included_in_price();
+$tax_rate                 = Tax::get_user_tax_rate( get_current_user_id() );
+$country                  = Input::sanitize_request_data( 'country', '' );
+$state                    = Input::sanitize_request_data( 'state', '' );
+
+if ( ! empty( $country ) ) {
+	$tax_rate = Tax::get_country_state_tax_rate( $country, $state );
+}
 ?>
 
 <div class="tutor-checkout-details">
@@ -41,14 +63,17 @@ $is_coupon_applicable = tutor_utils()->get_option( OptionKeys::IS_COUPON_APPLICA
 			<div class="tutor-checkout-courses">
 				<?php
 				if ( isset( $plan_info->plan_name, $plan_info->regular_price ) ) :
-					$order_type       = OrderModel::TYPE_SUBSCRIPTION;
-					$automatic_coupon = $coupon_model->apply_automatic_coupon_discount( $plan_info->id, $order_type );
-					$regular_price    = $plan_info->regular_price;
-					$sale_price       = $plan_info->in_sale_price ? $plan_info->sale_price : 0;
-					$enrollment_fee   = floatval( $plan_info->enrollment_fee );
+					$order_type     = OrderModel::TYPE_SUBSCRIPTION;
+					$applied_coupon = empty( $coupon_code )
+										? $coupon_model->apply_automatic_coupon_discount( $plan_info->id, $order_type )
+										: $coupon_model->apply_coupon_discount( $plan_info->id, $coupon_code, $order_type );
 
-					if ( $automatic_coupon->is_applied ) {
-						foreach ( $automatic_coupon->items as $item ) {
+					$regular_price  = $plan_info->regular_price;
+					$sale_price     = $plan_info->in_sale_price ? $plan_info->sale_price : 0;
+					$enrollment_fee = floatval( $plan_info->enrollment_fee );
+
+					if ( $applied_coupon->is_applied ) {
+						foreach ( $applied_coupon->items as $item ) {
 							if ( $item->item_id === $plan_info->course_id ) {
 								$regular_price = $item->regular_price;
 								$sale_price    = $item->discount_price;
@@ -68,10 +93,10 @@ $is_coupon_applicable = tutor_utils()->get_option( OptionKeys::IS_COUPON_APPLICA
 							<h6 class="tutor-checkout-course-title">
 								<?php echo esc_html( $plan_info->plan_name ); ?>
 							</h6>
-							<?php if ( $automatic_coupon->is_applied ) : ?>
+							<?php if ( $applied_coupon->is_applied ) : ?>
 							<div class="tutor-checkout-coupon-badge">
 								<i class="tutor-icon-tag" area-hidden="true"></i>
-								<span><?php echo esc_html( $automatic_coupon->coupon_title ); ?></span>
+								<span><?php echo esc_html( $applied_coupon->coupon_title ); ?></span>
 							</div>
 							<?php endif; ?>
 						</div>
@@ -103,8 +128,10 @@ $is_coupon_applicable = tutor_utils()->get_option( OptionKeys::IS_COUPON_APPLICA
 				<?php else : ?>
 					<?php
 					if ( is_array( $course_list ) && count( $course_list ) ) :
-						$course_ids       = array_column( $course_list, 'ID' );
-						$automatic_coupon = ( new CouponModel() )->apply_automatic_coupon_discount( $course_ids );
+						$course_ids     = array_column( $course_list, 'ID' );
+						$applied_coupon = empty( $coupon_code )
+										? $coupon_model->apply_automatic_coupon_discount( $course_ids, $order_type )
+										: $coupon_model->apply_coupon_discount( $course_ids, $coupon_code, $order_type );
 						?>
 						<?php
 						foreach ( $course_list as $key => $course ) :
@@ -113,8 +140,8 @@ $is_coupon_applicable = tutor_utils()->get_option( OptionKeys::IS_COUPON_APPLICA
 							$regular_price        = $course_price->regular_price;
 							$sale_price           = $course_price->sale_price;
 
-							if ( $automatic_coupon->is_applied ) {
-								foreach ( $automatic_coupon->items as $item ) {
+							if ( $applied_coupon->is_applied ) {
+								foreach ( $applied_coupon->items as $item ) {
 									if ( $item->item_id === $course->ID && $item->is_applied ) {
 										$has_automatic_coupon = $item->is_applied;
 										$regular_price        = $item->regular_price;
@@ -124,7 +151,8 @@ $is_coupon_applicable = tutor_utils()->get_option( OptionKeys::IS_COUPON_APPLICA
 								}
 							}
 
-							$subtotal += $sale_price ? $sale_price : $regular_price;
+							$subtotal    += $regular_price;
+							$grand_total += $sale_price ? $sale_price : $regular_price;
 
 							array_push( $object_ids, $course->ID );
 							?>
@@ -148,7 +176,7 @@ $is_coupon_applicable = tutor_utils()->get_option( OptionKeys::IS_COUPON_APPLICA
 										</h6>
 										<div class="tutor-checkout-coupon-badge <?php echo esc_attr( $has_automatic_coupon ? '' : 'tutor-d-none' ); ?>">
 											<i class="tutor-icon-tag" area-hidden="true"></i>
-											<span><?php echo esc_html( $has_automatic_coupon ? $automatic_coupon->coupon_title : '' ); ?></span>
+											<span><?php echo esc_html( $has_automatic_coupon ? $applied_coupon->coupon_title : '' ); ?></span>
 										</div>
 									</div>
 									<div class="tutor-text-right">
@@ -171,6 +199,8 @@ $is_coupon_applicable = tutor_utils()->get_option( OptionKeys::IS_COUPON_APPLICA
 			</div>
 		</div>
 
+		<?php $tax_amount = Tax::calculate_tax( $grand_total, $tax_rate ); ?>
+
 		<div class="tutor-checkout-detail-item tutor-checkout-summary">
 			<div class="tutor-checkout-summary-item">
 				<div class="tutor-fw-medium"><?php esc_html_e( 'Subtotal', 'tutor' ); ?></div>
@@ -178,40 +208,74 @@ $is_coupon_applicable = tutor_utils()->get_option( OptionKeys::IS_COUPON_APPLICA
 					<?php echo tutor_get_formatted_price( $subtotal ); //phpcs:ignore?>
 				</div>
 			</div>
-			<?php if ( $is_coupon_applicable && ( ! isset( $automatic_coupon ) || ! $automatic_coupon->is_applied ) ) : ?>
+			<?php if ( ! $applied_coupon->is_applied ) : ?>
 			<div class="tutor-checkout-summary-item tutor-have-a-coupon">
 				<div><?php esc_html_e( 'Have a coupon?', 'tutor' ); ?></div>
-				<button type="button" id="tutor-toggle-coupon-form" class="tutor-btn tutor-btn-link">
+				<button type="button" id="tutor-toggle-coupon-button" class="tutor-btn tutor-btn-link">
 					<?php esc_html_e( 'Click here', 'tutor' ); ?>
 				</button>
 			</div>
 			<div class="tutor-apply-coupon-form tutor-d-none">
-				<input type="text" name="coupon_code" placeholder="<?php esc_html_e( 'Add coupon code', 'tutor' ); ?>">
-				<button type="button" class="tutor-btn tutor-btn-secondary" data-object-ids="<?php echo esc_attr( implode( ',', $object_ids ) ); ?>"><?php esc_html_e( 'Apply', 'tutor' ); ?></button>
+				<input type="text" name="coupon_code"
+				<?php if ( 'manual' === $applied_coupon->coupon_type && $applied_coupon->is_applied ) : ?>
+						value="<?php echo esc_attr( $coupon_code ); ?>"
+				<?php endif; ?>
+				placeholder="<?php esc_html_e( 'Add coupon code', 'tutor' ); ?>">
+				<button type="button" id="tutor-apply-coupon-button" class="tutor-btn tutor-btn-secondary" data-object-ids="<?php echo esc_attr( implode( ',', $object_ids ) ); ?>"><?php esc_html_e( 'Apply', 'tutor' ); ?></button>
 			</div>
-			<div class="tutor-checkout-summary-item tutor-checkout-coupon-wrapper tutor-d-none">
+			<?php endif; ?>
+
+			<div class="tutor-checkout-summary-item tutor-checkout-coupon-wrapper <?php echo esc_attr( $applied_coupon->deducted_price ? '' : 'tutor-d-none' ); ?>">
 				<div class="tutor-checkout-coupon-badge tutor-has-delete-button">
 					<i class="tutor-icon-tag" area-hidden="true"></i>
-					<span></span>
+					<span><?php echo esc_html( $applied_coupon->coupon_title ); ?></span>
+
+					<?php if ( 'manual' === $applied_coupon->coupon_type && $applied_coupon->is_applied ) : ?>
 					<button type="button" id="tutor-checkout-remove-coupon">
 						<i class="tutor-icon-times" area-hidden="true"></i>
 					</button>
+					<?php endif; ?>
 				</div>
-				<div class="tutor-fw-bold tutor-discount-amount"></div>
+				<div class="tutor-fw-bold tutor-discount-amount">-<?php echo esc_html( tutor_get_formatted_price( $applied_coupon->deducted_price ) ); ?></div>
+			</div>
+
+			<?php
+			if ( Tax::is_tax_configured() && ! $is_tax_included_in_price ) :
+				?>
+			<div class="tutor-checkout-summary-item">
+				<div><?php esc_html_e( 'Tax', 'tutor' ); ?></div>
+				<div class="tutor-checkout-tax-amount"><?php echo tutor_get_formatted_price( $tax_amount ); //phpcs:ignore?></div>
 			</div>
 			<?php endif; ?>
-			<!-- <div class="tutor-checkout-summary-item">
-				<div><?php esc_html_e( 'Tax', 'tutor' ); ?></div>
-				<div><?php echo tutor_get_formatted_price( $tax_amount ); //phpcs:ignore?></div>
-			</div> -->
 		</div>
 
 		<div class="tutor-checkout-detail-item">
 			<div class="tutor-checkout-summary-item">
 				<div class="tutor-fw-medium"><?php esc_html_e( 'Grand Total', 'tutor' ); ?></div>
-				<div class="tutor-fw-bold tutor-checkout-grand-total"><?php echo tutor_get_formatted_price( $subtotal + $tax_amount ); //phpcs:ignore?></div>
+				<div class="tutor-fw-bold tutor-checkout-grand-total">
+					<?php
+						$grand_total = $grand_total + ( $is_tax_included_in_price ? 0 : $tax_amount );
+						echo tutor_get_formatted_price( $grand_total ); //phpcs:ignore
+					?>
+				</div>
+			</div>
+			<div class="tutor-checkout-summary-item">
+				<div></div>
+			<?php
+			if ( Tax::is_tax_configured() && $is_tax_included_in_price ) :
+				?>
+					<div class="tutor-fs-7 tutor-color-muted">
+						<?php
+						/* translators: %s: tax amount */
+						echo esc_html( sprintf( __( '(Incl. Tax %s)', 'tutor' ), tutor_get_formatted_price( $tax_amount ) ) );
+						?>
+					</div>
+					<?php endif ?>
 			</div>
 
+			<?php if ( 'manual' === $applied_coupon->coupon_type && $applied_coupon->is_applied ) : ?>
+				<input type="hidden" name="coupon_code" value="<?php echo esc_attr( $coupon_code ); ?>">
+			<?php endif; ?>
 			<input type="hidden" name="object_ids" value="<?php echo esc_attr( implode( ',', $object_ids ) ); ?>">
 			<input type="hidden" name="order_type" value="<?php echo esc_attr( $order_type ); ?>">
 		</div>
