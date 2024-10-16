34810336e2e6ddc972e9467b78b20c1cb07d31ce
Sazedul Haque <sazedulhaque5@gmail.com>
2024-10-16 15:59:52 +0600

    Hide tax and payment options initially

--- ecommerce/Settings.php
+++ ecommerce/Settings.php
@@ -223,6 +223,7 @@ class Settings {
 					'label'      => '',
 					'slug'       => 'options',
 					'block_type' => 'uniform',
+					'class'      => 'tutor-d-none',
 					'fields'     => array(
 						array(
 							'key'   => 'payment_settings',
@@ -248,6 +249,7 @@ class Settings {
 					'label'      => '',
 					'slug'       => 'options',
 					'block_type' => 'uniform',
+					'class'      => 'tutor-d-none',
 					'fields'     => array(
 						array(
 							'key'   => 'ecommerce_tax',
