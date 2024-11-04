07e5ee4a5853b481b299b2c9597b454ac32ba4f2
Sazedul Haque <sazedulhaque5@gmail.com>
2024-11-04 15:35:50 +0600

    course_permalink_base added to course_archive_page_url

--- classes/Utils.php
+++ classes/Utils.php
@@ -437,7 +437,7 @@ class Utils {
 	 */
 	public function course_archive_page_url() {
 		$course_post_type = tutor()->course_post_type;
-		$course_page_url  = trailingslashit( home_url() ) . $course_post_type;
+		$course_page_url  = home_url( $this->get_option( 'course_permalink_base', $course_post_type ) );
 
 		$course_archive_page = $this->get_option( 'course_archive_page' );
 		if ( $course_archive_page && '-1' !== $course_archive_page ) {
@@ -5575,15 +5575,6 @@ class Utils {
 		$page_id = (int) $this->get_option( 'tutor_dashboard_page_id' );
 		$page_id = apply_filters( 'tutor_dashboard_page_id', $page_id );
 		return apply_filters( 'tutor_dashboard_url', trailingslashit( get_the_permalink( $page_id ) ) . $sub_url );
-	}
-
-	/**
-	 * Get course archive URL
-	 *
-	 * @since 3.0.0
-	 */
-	public function get_course_archive_url() {
-		return home_url( $this->get_option( 'course_permalink_base', 'courses' ) );
 	}
 
 	/**
