a6303d882a390b68b2a83c64fbdddea676b25d17
Sazedul Haque <sazedulhaque5@gmail.com>
2024-11-04 11:51:51 +0600

    HashRouter added to coupon details

--- assets/react/v3/entries/coupon-details/components/App.tsx
+++ assets/react/v3/entries/coupon-details/components/App.tsx
@@ -5,6 +5,8 @@ import { createGlobalCss } from '@Utils/style-utils';
 import { Global } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
+import { QueryParamProvider } from 'use-query-params';
+import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
 import Main from './layout/Main';
 
 function App() {
@@ -22,18 +24,20 @@ function App() {
             networkMode: 'always',
           },
         },
-      })
+      }),
   );
   return (
     <RTLProvider>
-      <QueryClientProvider client={queryClient}>
-        <ToastProvider position="bottom-center">
-          <ModalProvider>
-            <Global styles={createGlobalCss()} />
-            <Main />
-          </ModalProvider>
-        </ToastProvider>
-      </QueryClientProvider>
+      <QueryParamProvider adapter={ReactRouter6Adapter}>
+        <QueryClientProvider client={queryClient}>
+          <ToastProvider position="bottom-center">
+            <ModalProvider>
+              <Global styles={createGlobalCss()} />
+              <Main />
+            </ModalProvider>
+          </ToastProvider>
+        </QueryClientProvider>
+      </QueryParamProvider>
     </RTLProvider>
   );
 }
