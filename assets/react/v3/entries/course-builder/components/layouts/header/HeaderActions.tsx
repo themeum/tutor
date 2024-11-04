1c74b7a4911ca75a9768c124c5daeda23179eda6
Fahim Faisal <fahim.faisal.abir@gmail.com>
2024-11-04 16:14:57 +0600

    🐛 fix(HeaderActions): add inline padding to button for improved layout

--- assets/react/v3/entries/course-builder/components/layouts/header/HeaderActions.tsx
+++ assets/react/v3/entries/course-builder/components/layouts/header/HeaderActions.tsx
@@ -343,6 +343,7 @@ const HeaderActions = () => {
           icon={<SVGIcon name="upload" width={24} height={24} />}
           loading={localPostStatus === 'draft' && updateCourseMutation.isPending}
           iconPosition="left"
+          buttonCss={css`padding-inline: ${spacing[16]};`}
           onClick={form.handleSubmit((data) =>
             handleSubmit(
               {
