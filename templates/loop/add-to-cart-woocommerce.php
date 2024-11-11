3002517cf4c738864a15e1aae4214eb4177abd49
Sazedul Haque <sazedulhaque5@gmail.com>
2024-11-11 10:25:19 +0600

    Add to cart buttons style fixed

--- templates/loop/add-to-cart-woocommerce.php
+++ templates/loop/add-to-cart-woocommerce.php
@@ -42,7 +42,7 @@ $defaults = array(
 		' ',
 		array_filter(
 			array(
-				'tutor-btn tutor-btn-outline-primary tutor-btn-md tutor-btn-block tutor-nowrap-ellipsis ',
+				'tutor-btn tutor-btn-outline-primary tutor-btn-md tutor-btn-block ',
 				'product_type_' . $product->get_type(),
 				$product->is_purchasable() && $product->is_in_stock() ? 'add_to_cart_button ' : '',
 				$ajax_add_to_cart_class,
