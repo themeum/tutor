ddd0b58593a7f950bbd8df330c4bf56b1c4c3846
Fahim Faisal <fahim.faisal.abir@gmail.com>
2024-11-18 10:39:52 +0600

    Remove absolute positioning from addH5PContentButton in LessonModal

--- assets/react/v3/entries/course-builder/components/modals/LessonModal.tsx
+++ assets/react/v3/entries/course-builder/components/modals/LessonModal.tsx
@@ -565,9 +565,6 @@ const styles = {
     left: 110px;
   `,
   addH5PContentButton: css`
-    position: absolute;
-    top: 36px;
-    left: 110px;
     display: inline-block;
     text-decoration: none;
     font-size: 13px;
