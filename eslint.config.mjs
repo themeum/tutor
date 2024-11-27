4529aa099b44d1fd15533290558576ef4fab5add
Fahim Faisal <fahim.faisal.abir@gmail.com>
2024-11-27 13:19:22 +0600

    Add Prettier and ESLint configuration files; remove Biome configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintPluginPrettierRecommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      'react/no-unknown-property': ['error', { ignore: ['css'] }],
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+        },
+      ],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
