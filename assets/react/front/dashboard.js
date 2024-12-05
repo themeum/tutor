e1fe3eb7a2eb9afebf3d41e8db0b58ef8dd2d1b8
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2024-12-05 17:25:48 +0600

    moved to pro

--- assets/react/front/dashboard.js
+++ assets/react/front/dashboard.js
@@ -3,5 +3,4 @@ import './dashboard/withdrawal';
 import './dashboard/settings/profile';
 import './dashboard/settings/passowrd-reset';
 import './dashboard/settings/billing';
-import './dashboard/settings/notification-preference';
 import './dashboard/index';
