508b03998636b639d16b6da88d7f2284ceb4bbc1
Fahim Faisal <fahim.faisal.abir@gmail.com>
2024-12-10 15:13:16 +0600

    fix: improve condition for displaying curriculum topics and ensure topic ID comparison is type-safe

--- assets/react/v3/entries/course-builder/pages/Curriculum.tsx
+++ assets/react/v3/entries/course-builder/pages/Curriculum.tsx
@@ -379,7 +379,11 @@ const Curriculum = () => {
 
         <div css={styles.content}>
           <Show
-            when={!courseCurriculumQuery.isLoading && topics.length > 0}
+            when={
+              !courseCurriculumQuery.isLoading &&
+              courseCurriculumQuery.data &&
+              (courseCurriculumQuery.data.length > 0 || topics.length > 0)
+            }
             fallback={
               <EmptyState
                 emptyStateImage={curriculumEmptyState}
