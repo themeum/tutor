1c0faef4bf2d3b8f4c8a99cbf5941e87f6c787ea
shewa12 <shewa12kpi@gmail.com>
2025-01-20 15:48:58 +0600

    What's new page content updated

--- classes/Admin.php
+++ classes/Admin.php
@@ -134,7 +134,7 @@ class Admin {
 		add_submenu_page( 'tutor', __( 'Courses', 'tutor' ), __( 'Courses', 'tutor' ), 'manage_tutor_instructor', 'tutor', array( $this, 'tutor_course_list' ) );
 
 		if ( '3.0.0' !== get_option( 'tutor-new-feature' ) ) {
-			add_submenu_page( 'tutor', __( 'What\'s New in 3.0', 'tutor' ), sprintf( '<span class="tutor-new-feature tutor-text-orange">%s</span>', __( 'What\'s New in 3.0', 'tutor' ) ), 'manage_tutor', 'tutor-new-feature', array( $this, 'feature_promotion_page' ) );
+			add_submenu_page( 'tutor', __( 'What\'s New', 'tutor' ), sprintf( '<span class="tutor-new-feature tutor-text-orange">%s</span>', __( 'What\'s New', 'tutor' ) ), 'manage_tutor', 'tutor-new-feature', array( $this, 'feature_promotion_page' ) );
 		}
 
 		// Ecommerce menu @since 3.0.0.
