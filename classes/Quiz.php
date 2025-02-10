0b68c2a7e11955770328f495052ad853e0329352
Sazedul Haque <sazedulhaque5@gmail.com>
2025-02-10 13:45:23 +0600

    Array typecast removed

--- classes/Quiz.php
+++ classes/Quiz.php
@@ -1133,7 +1133,7 @@ class Quiz {
 
 		$data = QuizModel::get_quiz_details( $quiz_id );
 
-		$data = apply_filters( 'tutor_quiz_details_response', (array) $data, $quiz_id );
+		$data = apply_filters( 'tutor_quiz_details_response', $data, $quiz_id );
 
 		$this->json_response(
 			__( 'Quiz data fetched successfully', 'tutor' ),
