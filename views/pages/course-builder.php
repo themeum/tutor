e3047bfb0177b8402013cc11c1c080b6e7f1e1c4
Sazedul Haque <sazedulhaque5@gmail.com>
2025-02-20 10:52:02 +0600

    Admin bar added to course builder

--- views/pages/course-builder.php
+++ views/pages/course-builder.php
@@ -17,23 +17,33 @@ if ( ! defined( 'ABSPATH' ) ) {
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="UTF-8">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 	<title><?php esc_html_e( 'Tutor Course Builder', 'tutor' ); ?></title>
-	<?php wp_print_styles(); ?>
+
+	<?php wp_head(); ?>
 
 	<style>
-		.mce-notification-error {
+		#adminmenumain,
+		#wpfooter {
+			display: none !important;
+		}
+		#wpcontent {
+			margin: 0 !important;
+		}
+		#wpbody-content {
+			padding-bottom: 0px !important;
+			float: none;
+		}
+		.notice {
 			display: none;
 		}
 	</style>
 </head>
-<body data-tutor-context="<?php echo is_admin() ? 'backend' : 'frontend'; ?>">
+<body <?php body_class(); ?>>
 	<div id="tutor-course-builder"></div>
-	<?php
-		do_action( 'tutor_course_builder_footer' );
-		wp_print_footer_scripts();
-	?>
+	<?php do_action( 'tutor_course_builder_footer' ); ?>
+	<?php wp_footer(); ?>
 </body>
 </html>
-
