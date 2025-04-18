f76d293c5ad9e96d7376b7e294ec8c96a3f5ea2a
Anindra Das Bivas <bivasdas911@gmail.com>
2025-04-18 17:45:57 +0600

    Fix: Assignment submission and deadline based on content drip settings

--- templates/single/assignment/content.php
+++ templates/single/assignment/content.php
@@ -101,6 +101,21 @@ $upload_basedir = trailingslashit( $upload_dir['basedir'] ?? '' );
 				$total_mark        = tutor_utils()->get_assignment_option( get_the_ID(), 'total_mark' );
 				$pass_mark         = tutor_utils()->get_assignment_option( get_the_ID(), 'pass_mark' );
 				$file_upload_limit = tutor_utils()->get_assignment_option( get_the_ID(), 'upload_file_size_limit' );
+				$drip_type         = get_tutor_course_settings( $course_id, 'content_drip_type', 'unlock_by_date' );
+
+				if ( 'specific_days' === $drip_type ) {
+					$days         = (int) get_item_content_drip_settings( $post_id, 'after_xdays_of_enroll' );
+					$days_in_time = 60 * 60 * 24 * $days;
+					$enrollment_time += $days_in_time;
+				}
+
+				if ( $drip_type === 'unlock_by_date' ) {
+					$unlock_timestamp = strtotime( get_item_content_drip_settings( $post_id, 'unlock_date' ) );
+
+					if ( $unlock_timestamp ) {
+						$enrollment_time = $unlock_timestamp < $enrollment_time ? $enrollment_time : $unlock_timestamp;
+					}
+				}
 
 				global $post;
 				$assignment_created_time = strtotime( $post->post_date_gmt );
