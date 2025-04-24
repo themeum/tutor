95ce06ff9dd8ebd4462954b3531331baa0650acd
shewa12 <shewa12kpi@gmail.com>
2025-04-24 10:44:01 +0600

    Fix: Translation error on Course_Widget class init

--- classes/Course_Widget.php
+++ classes/Course_Widget.php
@@ -32,8 +32,8 @@ class Course_Widget extends \WP_Widget {
 	public function __construct() {
 		parent::__construct(
 			'tutor_course_widget', // Base ID.
-			'Tutor Course',
-			array( 'description' => 'Display courses wherever widget support is available.' )
+			_x( 'Tutor Course', 'widget title', 'tutor' ),
+			array( 'description' => _x( 'Display courses wherever widget support is available.', 'widget description', 'tutor' ) )
 		);
 	}
 
