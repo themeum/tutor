d903e46902fe10711d9cb23262b9d8e7e7c2aaa2
Fahim Faisal <fahim.faisal.abir@gmail.com>
2025-05-06 22:32:19 +0600

    feat: enhance import/export functionality with export checkboxes and modal

--- views/options/template/import_export.php
+++ views/options/template/import_export.php
@@ -10,6 +10,136 @@
  */
 
 ?>
+
+<!-- An array for export checkbox with name and label, cna be nested and will use it for rendering in modal -->
+<?php
+$default_checkbox_value = '0';
+
+$export_checkboxes = array(
+	'courses'         => array(
+		'label'          => __( 'Courses', 'tutor' ),
+		'count'          => tutor_utils()->get_courses_count(),
+		'name'           => 'courses',
+		'value'          => $default_checkbox_value,
+		'keep_user_data' => true,
+		'children'       => array(
+			'lessons'     => array(
+				'label' => __( 'Lessons', 'tutor' ),
+				'count' => tutor_utils()->get_lessons_count(),
+				'name'  => 'lessons',
+				'value' => $default_checkbox_value,
+			),
+			'quizzes'     => array(
+				'label' => __( 'Quizzes', 'tutor' ),
+				'count' => tutor_utils()->get_quizzes_count(),
+				'name'  => 'quizzes',
+				'value' => $default_checkbox_value,
+			),
+			'assignments' => array(
+				'label' => __( 'Assignments', 'tutor' ),
+				'count' => tutor_utils()->get_assignments_count(),
+				'name'  => 'assignments',
+				'value' => $default_checkbox_value,
+			),
+			'resources'   => array(
+				'label' => __( 'Resources', 'tutor' ),
+				'count' => tutor_utils()->get_resources_count(),
+				'name'  => 'resources',
+				'value' => $default_checkbox_value,
+			),
+		),
+	),
+	'bundles'         => array(
+		'label'          => __( 'Bundles', 'tutor' ),
+		'count'          => tutor_utils()->get_bundles_count(),
+		'name'           => 'bundles',
+		'value'          => $default_checkbox_value,
+		'keep_user_data' => true,
+	),
+	'users'           => array(
+		'label' => __( 'Users List', 'tutor' ),
+		'count' => tutor_utils()->get_users_count(),
+		'name'  => 'users',
+		'value' => $default_checkbox_value,
+	),
+	'global_settings' => array(
+		'label' => __( 'Global Settings', 'tutor' ),
+		'name'  => 'global_settings',
+		'value' => $default_checkbox_value,
+	),
+);
+
+/**
+ * Render export checkboxes
+ *
+ * @param array $checkboxes checkboxes.
+ * @return void
+ */
+function render_export_checkboxes( $checkboxes ) {
+	?>
+	<div class="item-wrapper setting-checkboxes tutor-mx-20 tutor-py-0">
+		<?php
+		foreach ( $checkboxes as $key => $checkbox ) {
+			$label = $checkbox['label'];
+			$count = $checkbox['count'];
+			$name  = $checkbox['name'];
+			?>
+			<div class="tutor-option-field-row">
+				<div class="tutor-px-20">
+					<label class="tutor-form-check">
+					<input type="checkbox" class="tutor-form-check-input" name="export_<?php echo esc_attr( $name ); ?>" value="1"> <?php 
+							if ( isset($count) && (is_numeric($count) || (!empty($count) && is_string($count)))) {
+								// translators: %s: label, %d: count
+								echo sprintf( __( '%s (%d)', 'tutor' ), esc_html( $label ), esc_html( $count ) );
+							} else {
+								echo esc_html( $label );
+							}
+						?>
+					</label>
+					<?php
+					if ( isset( $checkbox['children'] ) && is_array( $checkbox['children'] ) ) {
+						?>
+						<div class="item-wrapper tutor-mt-8 tutor-mb-0">
+							<div class="tutor-option-field-row tutor-px-16 tutor-grid-3">
+								<?php
+								foreach ( $checkbox['children'] as $child_key => $child_checkbox ) {
+									$child_label = $child_checkbox['label'];
+									$child_count = $child_checkbox['count'];
+									$child_name  = $child_checkbox['name'];
+									?>
+									<label class="tutor-form-check">
+										<input type="checkbox" class="tutor-form-check-input" name="export_<?php echo esc_attr( $name ); ?>_<?php echo esc_attr( $child_name ); ?>" value="1" <?php echo checked( 1, isset( $_POST[ 'export_' . esc_attr( $name ) . '_' . esc_attr( $child_name ) ] ) ? $_POST[ 'export_' . esc_attr( $name ) . '_' . esc_attr( $child_name ) ] : 0, true ); ?>><?php echo sprintf( __( '%s (%d)', 'tutor' ), esc_html( $child_label ), esc_html( $child_count ) ); ?>
+									</label>
+									<?php
+								}
+								?>
+							</div>
+
+							<?php
+							if ( isset( $checkbox['keep_user_data'] ) && $checkbox['keep_user_data'] ) {
+								?>
+								<div class="tutor-option-field-row" style="background-color: rgba(var(--tutor-color-primary-rgb), 0.03);">
+									<label class="tutor-form-check">
+										<input type="checkbox" class="tutor-form-check-input" name="export_<?php echo esc_attr( $name ); ?>_keep_user_data" value="1"> <?php esc_html_e( 'Keep User Data', 'tutor' ); ?>
+									</label>
+								</div>
+								<?php
+							}
+							?>
+						</div>
+						<?php
+					}
+					?>
+				</div>
+			</div>
+			<?php
+		}
+		?>
+	</div>
+	<?php
+}
+?>
+
 <div class="tutor-option-main-title">
 	<div class="tutor-fs-4 tutor-fw-medium tutor-color-black">
 		<?php esc_html_e( 'Import/Export', 'tutor' ); ?>
@@ -26,18 +156,18 @@
 				<div class="drag-drop-zone">
 					<span class="tutor-icon-upload tutor-fs-1 tutor-color-primary"></span>
 					<div>
-					<label for="drag-drop-input" class="tutor-btn tutor-btn-secondary tutor-btn-sm tutor-mt-8">
-						<input type="file" name="drag-drop-input" id="drag-drop-input" class="tutor-d-none">
-						<span><?php esc_html_e( 'Choose a file', 'tutor' ); ?></span>
-					</label>
+						<label for="drag-drop-input" class="tutor-btn tutor-btn-secondary tutor-btn-sm tutor-mt-8">
+							<input type="file" name="drag-drop-input" id="drag-drop-input" class="tutor-d-none">
+							<span><?php esc_html_e( 'Choose a file', 'tutor' ); ?></span>
+						</label>
 					</div>
 					<div class="subtitle">
 						<span>
 							<?php
-							printf(
+							echo sprintf(
 								/* translators: %s: file type */
-								esc_html__( 'Supported format: %s', 'tutor' ),
-								".JSON"
+								__( 'Supported format: %s', 'tutor' ),
+								'.JSON'
 							);
 							?>
 						</span>
@@ -62,8 +192,8 @@
 				</div>
 			</div>
 			<div class="tutor-option-field-input">
-				<button class="tutor-btn tutor-btn-primary tutor-btn-sm" id="tutor_export_settings">
-					<i class="btn-icon tutor-icon-export tutor-mr-4"></i>	
+				<button class="tutor-btn tutor-btn-primary tutor-btn-sm" data-tutor-modal-target="tutor-export-data-modal">
+					<i class="btn-icon tutor-icon-export tutor-mr-4"></i>
 					<?php esc_html_e( 'Initiate Export', 'tutor' ); ?>
 				</button>
 			</div>
@@ -71,85 +201,167 @@
 	</div>
 </div>
 
-<div class="tutor-option-single-item tutor-mb-32 item-variation-table settings-history">
+<div class="tutor-option-single-item tutor-mb-32 item-variation-table">
 	<div class="tutor-option-group-title tutor-mb-16">
-		<div class="tutor-fs-6 tutor-color-muted"><?php esc_attr_e( 'Settings History', 'tutor' ); ?></div>
+		<div class="tutor-fs-6 tutor-color-muted"><?php esc_attr_e( 'History', 'tutor' ); ?></div>
 	</div>
-	<div class="item-wrapper history_data">
-		<div class="tutor-option-field-row">
-			<div class="tutor-option-field-label">
-				<?php esc_attr_e( 'Date', 'tutor' ); ?>
-			</div>
-		</div>
-		<?php
-		$tutor_options = get_option( 'tutor_settings_log', array() );
-		if ( $tutor_options ) :
-			?>
-			<?php
-			foreach ( $tutor_options as $key => $option_data ) :
-				$datetype_class = 'saved' === $option_data['datatype'] ? ' label-primary' : ' label-default';
-				?>
-				<div class="tutor-option-field-row">
-					<div class="tutor-option-field-label">
-						<div class="tutor-fs-7 tutor-fw-medium">
-							<?php echo esc_html( $option_data['history_date'] ); ?>
-							<span class="tutor-badge-label tutor-ml-16<?php echo esc_attr( $datetype_class ); ?>"> <?php echo esc_html( ucwords( $option_data['datatype'] ) ); ?></span>
+	<div class="tutor-table-responsive">
+		<table class="tutor-table table-import-export-history">
+			<thead>
+				<tr>
+					<th>
+						<?php esc_attr_e( 'Title', 'tutor' ); ?>
+					</th>
+					<th>
+						<?php esc_attr_e( 'Type', 'tutor' ); ?>
+					</th>
+					<th>
+						<?php esc_attr_e( 'Author', 'tutor' ); ?>
+					</th>
+					<th>
+						<?php esc_attr_e( 'Date', 'tutor' ); ?>
+					</th>
+					<th />
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+				$tutor_import_export_history = 
+				// Generate mock data for testing
+				array(
+					'1' => array(
+						'title'      => 'Courses',
+						'author'     => 'John Doe',
+						'date'       => '2023-10-01 12:00am',
+						'type'       => 'export',
+						'is_setting' => false,
+					),
+					'2' => array(
+						'title'      => 'Lessons',
+						'author'     => 'Jane Smith',
+						'date'       => '2023-10-02 1:00pm',
+						'type'       => 'import',
+						'is_setting' => false,
+					),
+					'3' => array(
+						'title'      => 'Settings',
+						'author'     => 'Alice Johnson',
+						'date'       => '2023-10-03 2:00pm',
+						'type'       => 'export',
+						'is_setting' => true,
+					),
+				);
+				if ( $tutor_import_export_history ) :
+					foreach ( $tutor_import_export_history as $key => $option_data ) :
+						$datetype_class = 'saved' === $option_data['datatype'] ? ' label-primary' : ' label-default';
+						?>
+						<tr>
+							<td>
+								<div class="tutor-fs-7 tutor-fw-medium">
+									<?php echo esc_html( $option_data['title'] ); ?>
+								</div>
+							</td>
+							<td>
+								<?php
+								$type_icon  = 'import' === $option_data['type'] ? 'import' : 'export';
+								$type_class = 'import' === $option_data['type'] ? 'tutor-color-primary' : 'tutor-color-secondary';
+								$type       = 'import' === $option_data['type'] ? esc_html__( 'Imported', 'tutor' ) : esc_html__( 'Exported', 'tutor' );
+								?>
+								<div class="tutor-fs-7 tutor-fw-medium tutor-text-c <?php echo esc_attr( $type_class ); ?>">
+									<i class="tutor-icon-<?php echo esc_attr( $option_data['type'] ); ?> tutor-mr-4 <?php echo esc_attr( $type_icon ); ?>" area-hidden="true"></i>
+									<?php echo esc_html( $type ); ?>
+								</div>
+							</td>
+							<td>
+								<div class="tutor-fs-7 tutor-fw-medium">
+									<?php echo esc_html( $option_data['author'] ); ?>
+								</div>
+							</td>
+							<td>
+								<div class="tutor-fs-7 tutor-fw-normal">
+									<?php echo esc_html( $option_data['date'] ); ?>
+								</div>
+							</td>
+							<td>
+								<div class="tutor-dropdown-parent tutor-ml-16">
+									<button type="button" class="tutor-iconic-btn" action-tutor-dropdown="toggle">
+										<span class="tutor-icon-kebab-menu" area-hidden="true"></span>
+									</button>
+									<ul class="tutor-dropdown tutor-dropdown-dark tutor-text-left">
+										<?php
+										if ( $option_data['is_setting'] ) {
+											?>
+											<li>
+												<a href="javascript:;" class="tutor-dropdown-item apply_settings" data-tutor-modal-target="tutor-modal-bulk-action" data-btntext="<?php esc_attr_e( 'Yes, Restore Settings', 'tutor' ); ?>" data-heading="<?php esc_attr_e( 'Restore Previous Settings?', 'tutor' ); ?>" data-message="<?php esc_attr_e( 'WARNING! This will overwrite all existing settings, please proceed with caution.', 'tutor' ); ?>" data-id="<?php echo esc_attr( $key ); ?>">
+													<span class="tutor-mr-8" area-hidden="true">✓</span>
+													<span><?php esc_html_e( 'Apply Settings', 'tutor' ); ?></span>
+												</a>
+											</li>
+											<?php
+										}
+										?>
+										<li>
+											<a href="javascript:;" class="tutor-dropdown-item export_single_settings" data-id="<?php echo esc_attr( $key ); ?>">
+												<span class="tutor-icon-archive tutor-mr-8" area-hidden="true"></span>
+												<span><?php esc_html_e( 'Download', 'tutor' ); ?></span>
+											</a>
+										</li>
+										<li>
+											<a href="javascript:;" class="tutor-dropdown-item delete_single_settings" data-tutor-modal-target="tutor-modal-bulk-action" data-btntext="<?php esc_attr_e( 'Yes, Delete Settings', 'tutor' ); ?>" data-heading="<?php esc_attr_e( 'Delete This Settings?', 'tutor' ); ?>" data-message="<?php esc_attr_e( 'WARNING! This will remove the settings history data from your system, please proceed with caution.', 'tutor' ); ?>" data-id="<?php echo esc_attr( $key ); ?>">
+												<span class="icon tutor-icon-trash-can-bold tutor-mr-8" area-hidden="true"></span>
+												<span><?php esc_html_e( 'Delete', 'tutor' ); ?></span>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</td>
+						</tr>
+					<?php endforeach; ?>
+				<?php else : ?>
+					<div class="tutor-option-field-row">
+						<div class="tutor-option-field-label">
+							<div class="tutor-fs-7 tutor-fw-medium"><?php esc_html_e( 'No settings data found.', 'tutor' ); ?></div>
 						</div>
 					</div>
-					<div class="tutor-option-field-input">
-						<button class="tutor-btn tutor-btn-outline-primary tutor-btn-sm apply_settings" data-tutor-modal-target="tutor-modal-bulk-action" data-btntext="<?php esc_attr_e( 'Yes, Restore Settings', 'tutor' ); ?>" data-heading="<?php esc_attr_e( 'Restore Previous Settings?', 'tutor' ); ?>" data-message="<?php esc_attr_e( 'WARNING! This will overwrite all existing settings, please proceed with caution.', 'tutor' ); ?>" data-id="<?php echo esc_attr( $key ); ?>"><?php esc_html_e( 'Apply', 'tutor' ); ?></button>
-						<div class="tutor-dropdown-parent tutor-ml-16">
-							<button type="button" class="tutor-iconic-btn" action-tutor-dropdown="toggle">
-								<span class="tutor-icon-kebab-menu" area-hidden="true"></span>
-							</button>
-							<ul class="tutor-dropdown tutor-dropdown-dark tutor-text-left">
-								<li>
-									<a href="javascript:;" class="tutor-dropdown-item export_single_settings" data-id="<?php echo esc_attr( $key ); ?>">
-										<span class="tutor-icon-archive tutor-mr-8" area-hidden="true"></span>
-										<span><?php esc_html_e( 'Download', 'tutor' ); ?></span>
-									</a>
-								</li>
-								<li>
-									<a href="javascript:;" class="tutor-dropdown-item delete_single_settings" data-tutor-modal-target="tutor-modal-bulk-action" data-btntext="<?php esc_attr_e( 'Yes, Delete Settings', 'tutor' ); ?>" data-heading="<?php esc_attr_e( 'Delete This Settings?', 'tutor' ); ?>" data-message="<?php esc_attr_e( 'WARNING! This will remove the settings history data from your system, please proceed with caution.', 'tutor' ); ?>" data-id="<?php echo esc_attr( $key ); ?>">
-										<span class="icon tutor-icon-trash-can-bold tutor-mr-8" area-hidden="true"></span>
-										<span><?php esc_html_e( 'Delete', 'tutor' ); ?></span>
-									</a>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			<?php endforeach; ?>
-		<?php else : ?>
-			<div class="tutor-option-field-row">
-				<div class="tutor-option-field-label">
-					<div class="tutor-fs-7 tutor-fw-medium"><?php esc_html_e( 'No settings data found.', 'tutor' ); ?></div>
-				</div>
-			</div>
-		<?php endif; ?>
+				<?php endif; ?>
+			</tbody>
+		</table>
 	</div>
 </div>
 
-<div class="tutor-option-single-item tutor-mb-32">
-	<div class="tutor-option-group-title tutor-mb-16">
-		<div class="tutor-fs-6 tutor-color-muted"><?php esc_html_e( 'Reset Settings', 'tutor' ); ?></div>
-	</div>
-	<div class="item-wrapper">
-		<div class="tutor-option-field-row">
-			<div class="tutor-option-field-label">
-				<div class="tutor-fs-6 tutor-color-black"><?php esc_html_e( 'Reset Everything to Default', 'tutor' ); ?></div>
-				<div class="tutor-fs-7 tutor-fw-medium tutor-color-secondary">
-					<span class="tutor-fw-medium"><?php esc_html_e( 'It will revert all settings to initial setup.', 'tutor' ); ?></span>
+<!-- Export Data Modal -->
+<div id="tutor-export-data-modal" class="tutor-modal tutor-modal-scrollable">
+	<div class="tutor-modal-overlay"></div>
+	<div class="tutor-modal-window">
+		<form id="tutor-export-data-form" class="tutor-modal-content" autocomplete="off" method="post">
+			<div class="tutor-modal-header">
+				<div class="tutor-modal-title">
+					<span class="tutor-color-primary tutor-fw-medium"><?php esc_html_e( 'Exporter', 'tutor' ); ?></span>
+
+					<button class="tutor-btn tutor-btn-primary tutor-btn-sm" data-tutor-modal-target="tutor-export-data-modal">
+						<i class="tutor-icon-export tutor-mr-4" area-hidden="true"></i>
+						<?php esc_html_e( 'Export', 'tutor' ); ?>
+					</button>
+				</div>
+				<button class="tutor-iconic-btn tutor-modal-close tutor-flex-shrink-0" data-tutor-modal-close>
+					<span class="tutor-icon-times" area-hidden="true"></span>
+				</button>
+			</div>
+
+			<div class="tutor-modal-body">
+				<?php tutor_nonce_field(); ?>
+				<input type="hidden" name="action" value="tutor_export_settings">
+				<input type="hidden" name="export_type" value="">
+				<input type="hidden" name="export_id" value="">
+				<div class="tutor-option-single-item tutor-mb-32">
+					<div class="item-wrapper">
+						<div class="tutor-fs-7 tutor-color-black tutor-mb-16 tutor-mx-20">
+							<?php esc_html_e( 'What do you want to export?', 'tutor' ); ?>
+						</div>
+						<?php echo render_export_checkboxes( $export_checkboxes ); ?>
+					</div>
 				</div>
 			</div>
-			<div class="tutor-option-field-input">
-				<button class="tutor-btn tutor-btn-outline-primary tutor-btn-sm tutor-reset-all" 
-						data-tutor-modal-target="tutor-modal-bulk-action" 
-						data-btntext="<?php esc_attr_e( 'Yes, Reset Settings', 'tutor' ); ?>" 
-						data-heading="<?php esc_attr_e( 'Reset All Settings?', 'tutor' ); ?>" 
-						data-message="<?php esc_attr_e( 'WARNING! This will reset all settings to default, please proceed with caution.', 'tutor' ); ?>" 
-						id="tutor_reset_options"><?php esc_html_e( 'Reset All Settings', 'tutor' ); ?></button>
-			</div>
-		</div>
+		</form>
 	</div>
 </div>
