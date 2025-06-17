143db4fb41b5afeb4bc73cf84faaa1afe3cbe986
Fahim Faisal <fahim.faisal.abir@gmail.com>
2025-06-17 11:41:41 +0600

    fix: enforce no-console rule as an error in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,7 +26,7 @@ export default [
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
       'react/display-name': 'off',
-      'no-console': 'warn',
+      'no-console': 'error',
       '@typescript-eslint/consistent-type-imports': [
         'error',
         {
