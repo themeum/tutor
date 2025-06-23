30a854c7f8c547ef37cdc8f9f371b6619917663a
Fahim Faisal <fahim.faisal.abir@gmail.com>
2025-06-23 13:31:40 +0600

    fix: refactor imports and remove unused QuizDataStatus and QuizQuestion interfaces

--- assets/react/v3/entries/course-builder/services/quiz.ts
+++ assets/react/v3/entries/course-builder/services/quiz.ts
@@ -18,64 +18,15 @@ import type { ErrorResponse } from '@TutorShared/utils/form';
 import { convertToErrorMessage, isAddonEnabled, normalizeLineEndings } from '@TutorShared/utils/util';
 
 import type { ContentDripType } from '@CourseBuilderServices/course';
-import { type ID, type TopicContentType, type TutorMutationResponse } from '@TutorShared/utils/types';
-
-export const QuizDataStatus = {
-  NEW: 'new',
-  UPDATE: 'update',
-  NO_CHANGE: 'no_change',
-} as const;
-
-export type QuizDataStatus = (typeof QuizDataStatus)[keyof typeof QuizDataStatus];
-
-export type QuizQuestionType =
-  | 'true_false'
-  | 'single_choice'
-  | 'multiple_choice'
-  | 'open_ended'
-  | 'fill_in_the_blank'
-  | 'short_answer'
-  | 'matching'
-  | 'image_matching'
-  | 'image_answering'
-  | 'ordering'
-  | 'h5p';
-
-export interface QuizQuestionOption {
-  _data_status: QuizDataStatus;
-  is_saved: boolean;
-  answer_id: ID;
-  belongs_question_id: ID;
-  belongs_question_type: QuizQuestionType;
-  answer_title: string;
-  is_correct: '0' | '1';
-  image_id?: ID;
-  image_url?: string;
-  answer_two_gap_match: string;
-  answer_view_format: string;
-  answer_order: number;
-}
-
-export interface QuizQuestion {
-  _data_status: QuizDataStatus;
-  question_id: ID;
-  question_title: string;
-  question_description: string;
-  question_mark: number;
-  answer_explanation: string;
-  question_order: number;
-  question_type: QuizQuestionType;
-  question_settings: {
-    question_type: QuizQuestionType;
-    answer_required: boolean;
-    randomize_question: boolean;
-    question_mark: number;
-    show_question_mark: boolean;
-    has_multiple_correct_answer: boolean;
-    is_image_matching: boolean;
-  };
-  question_answers: QuizQuestionOption[];
-}
+import {
+  QuizDataStatus,
+  type ID,
+  type QuizQuestion,
+  type QuizQuestionOption,
+  type QuizQuestionType,
+  type TopicContentType,
+  type TutorMutationResponse,
+} from '@TutorShared/utils/types';
 
 interface ImportQuizPayload {
   topic_id: ID;
@@ -610,25 +561,6 @@ export const useGetQuizDetailsQuery = (quizId: ID) => {
     queryFn: () => getQuizDetails(quizId).then((response) => response.data),
     enabled: !!quizId,
   });
-};
-
-export const calculateQuizDataStatus = (dataStatus: QuizDataStatus, currentStatus: QuizDataStatus) => {
-  if (currentStatus === dataStatus) {
-    return null;
-  }
-
-  if (dataStatus === QuizDataStatus.NEW) {
-    return QuizDataStatus.NEW;
-  }
-
-  if (
-    (dataStatus === QuizDataStatus.UPDATE || dataStatus === QuizDataStatus.NO_CHANGE) &&
-    currentStatus === QuizDataStatus.UPDATE
-  ) {
-    return QuizDataStatus.UPDATE;
-  }
-
-  return QuizDataStatus.NO_CHANGE;
 };
 
 const getH5PQuizContentById = (id: ID) => {
