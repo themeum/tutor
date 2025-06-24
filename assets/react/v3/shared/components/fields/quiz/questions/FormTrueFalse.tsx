fd9cbd1cf10a129bdf6d54c5dc0c9fa53c059c04
Fahim Faisal <fahim.faisal.abir@gmail.com>
2025-06-24 11:19:11 +0600

    fix: update FormTrueFalse component to use inputValue for answer_id and is_correct

--- assets/react/v3/shared/components/fields/quiz/questions/FormTrueFalse.tsx
+++ assets/react/v3/shared/components/fields/quiz/questions/FormTrueFalse.tsx
@@ -28,7 +28,7 @@ const FormTrueFalse = ({ field, onCheckCorrectAnswer, isOverlay = false, questio
   };
 
   const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
-    id: field.value.answer_id || 0,
+    id: inputValue.answer_id || 0,
     animateLayoutChanges,
   });
 
@@ -41,15 +41,15 @@ const FormTrueFalse = ({ field, onCheckCorrectAnswer, isOverlay = false, questio
   return (
     <div
       {...attributes}
-      css={styles.option({ isSelected: !!Number(field.value.is_correct), isOverlay })}
+      css={styles.option({ isSelected: !!Number(inputValue.is_correct), isOverlay })}
       tabIndex={-1}
       ref={setNodeRef}
       style={style}
     >
       <button data-check-button type="button" css={styleUtils.optionCheckButton} onClick={onCheckCorrectAnswer}>
-        <SVGIcon name={Number(field.value.is_correct) ? 'checkFilled' : 'check'} height={32} width={32} />
+        <SVGIcon name={Number(inputValue.is_correct) ? 'checkFilled' : 'check'} height={32} width={32} />
       </button>
-      <div css={styles.optionLabel({ isSelected: !!Number(field.value.is_correct), isDragging, isOverlay })}>
+      <div css={styles.optionLabel({ isSelected: !!Number(inputValue.is_correct), isDragging, isOverlay })}>
         <span>{inputValue.answer_title}</span>
 
         <button
