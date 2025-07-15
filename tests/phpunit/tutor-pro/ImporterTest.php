ad33b927f49127d41238e3a8eb277aee0009e74f
Anindra Das Bivas <bivasdas911@gmail.com>
2025-07-15 11:31:22 +0600

    phpcs fix

--- tests/phpunit/tutor-pro/ImporterTest.php
+++ tests/phpunit/tutor-pro/ImporterTest.php
@@ -9,12 +9,29 @@
 
 namespace TutorTest;
 
+/**
+ * Importer Test Class
+ */
 class ImporterTest extends \PHPUnit\Framework\TestCase {
 
+	/**
+	 * Importer instance
+	 *
+	 * @since 3.6.0
+	 *
+	 * @var object
+	 */
 	private $importer;
 
 	const TUTOR_TEST_PRO_DIRECTORY = '';
 
+	/**
+	 * Setup function that runs before the test case runs.
+	 *
+	 * @since 3.6.0
+	 *
+	 * @return void
+	 */
 	protected function setUp(): void {
 		global $current_user;
 		parent::setUp();
@@ -27,38 +44,51 @@ class ImporterTest extends \PHPUnit\Framework\TestCase {
 		if ( file_exists( $importer_path ) ) {
 			require_once $importer_path;
 		} else {
-			echo $importer_path . ' ' . 'does not exists.';
+			echo esc_html( $importer_path . ' does not exists.' );
 		}
 
 		if ( file_exists( $course_importer_path ) ) {
 			require_once $course_importer_path;
 		} else {
-			echo $course_importer_path . ' ' . 'does not exists.';
+			echo esc_html( $course_importer_path . ' does not exists.' );
 		}
 
 		if ( file_exists( $assignment_importer_path ) ) {
 			require_once $assignment_importer_path;
 		} else {
-			echo $assignment_importer_path . ' ' . 'does not exists.';
+			echo esc_html( $assignment_importer_path . ' does not exists.' );
 		}
 
 		if ( file_exists( $quiz_importer_path ) ) {
 			require_once $quiz_importer_path;
 		} else {
-			echo $quiz_importer_path . ' ' . 'does not exists.';
+			echo esc_html( $quiz_importer_path . ' does not exists.' );
 		}
 
 		$this->importer = new \TutorPro\Tools\Importer();
 		wp_set_current_user( 1 );
 	}
 
+	/**
+	 * Teardown function that runs after the test has ran.
+	 *
+	 * @since 3.6.0
+	 *
+	 * @return void
+	 */
 	protected function tearDown(): void {
 		unset( $this->importer );
 
 		parent::tearDown();
 	}
 
-
+	/**
+	 * Test importing json content with importer.
+	 *
+	 * @since 3.6.0
+	 *
+	 * @return void
+	 */
 	public function test_import_json_content() {
 		$json_data = file_get_contents( '' ); // file path.
 
