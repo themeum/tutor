3c6f4958cd6c477a6e4a269f0113906ebcbffbe6
Sazedul Haque <sazedulhaque5@gmail.com>
2025-07-17 11:45:29 +0600

    Refactor Q&A tab parameter from 'tab' to 'data'
    Replaces usage of the 'tab' query parameter with 'data' across Q_And_A, Question_Answers_List, and related templates. This change ensures consistent parameter naming for filtering and navigation in the Q&A dashboard and views.

--- classes/Q_And_A.php
+++ classes/Q_And_A.php
@@ -393,7 +393,7 @@ class Q_And_A {
 					'key'   => 'all' === $tab ? '' : $tab,
 					'title' => tutor_utils()->translate_dynamic_text( $tab ),
 					'value' => $stats[ $tab ],
-					'url'   => add_query_arg( array( 'tab' => $tab ), remove_query_arg( 'tab' ) ),
+					'url'   => add_query_arg( array( 'data' => $tab ), remove_query_arg( 'data' ) ),
 				);
 			},
 			array_keys( $stats )
