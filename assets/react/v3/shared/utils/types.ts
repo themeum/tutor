5c1db531510cac4dcc806bb4522a2d2afb932097
Fahim Faisal <fahim.faisal.abir@gmail.com>
2025-07-31 16:19:33 +0600

    feat: add QuizQuestionPayload interface for quiz question data structure

--- assets/react/v3/shared/utils/types.ts
+++ assets/react/v3/shared/utils/types.ts
@@ -333,6 +333,28 @@ export interface QuizQuestion {
   question_answers: QuizQuestionOption[];
 }
 
+export interface QuizQuestionPayload {
+  _data_status: QuizDataStatus;
+  is_cb_question?: boolean;
+  question_id: ID;
+  question_title: string;
+  question_description: string;
+  question_mark: number;
+  answer_explanation: string;
+  question_order: number;
+  question_type: QuizQuestionType;
+  question_settings: {
+    question_type: QuizQuestionType;
+    answer_required: '0' | '1';
+    randomize_question: '0' | '1';
+    question_mark: number;
+    show_question_mark: '0' | '1';
+    has_multiple_correct_answer: '0' | '1';
+    is_image_matching: '0' | '1';
+  };
+  question_answers: QuizQuestionOption[];
+}
+
 export type QuizValidationErrorType = 'question' | 'quiz' | 'correct_option' | 'add_option' | 'save_option';
 
 export type CollectionContentType = 'cb-question' | 'cb-lesson' | 'cb-assignment';
