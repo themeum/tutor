6c005d327e830e603f2734d1e23f93ecbd1a3c18
Fahim Faisal <fahim.faisal.abir@gmail.com>
2025-08-05 17:23:08 +0600

    fix: update storybook command to prevent automatic opening and clean up dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from 'storybook-react-rsbuild';
 
 const config: StorybookConfig = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-docs', '@storybook/addon-onboarding', '@storybook/addon-a11y', '@storybook/addon-themes'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-a11y', '@storybook/addon-themes'],
   framework: {
     name: 'storybook-react-rsbuild',
     options: {},
