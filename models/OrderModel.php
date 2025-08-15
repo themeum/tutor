4daaf75ca657c86aad4e108f24ac114d175bb86f
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2025-08-15 18:03:03 +0600

    fix: wrong tax re-calculation and payment fail

--- models/OrderModel.php
+++ models/OrderModel.php
@@ -202,23 +202,39 @@ class OrderModel {
 	 * Get recalculated order tax data.
 	 *
 	 * @since 3.4.0
+	 * @since 3.8.0 tax re-calculation based on total_price_before_tax column value.
 	 *
 	 * @param int|object $order the order id or object.
 	 *
 	 * @return array
 	 */
 	public function get_recalculated_order_tax_data( $order ) {
-		$order       = self::get_order( $order );
-		$total_price = $order->total_price;
-		$tax_rate    = Tax::get_user_tax_rate( $order->user_id );
-		$order_data  = array();
-		if ( $tax_rate ) {
+		$order      = self::get_order( $order );
+		$order_data = array(
+			'tax_type'   => null,
+			'tax_rate'   => null,
+			'tax_amount' => null,
+		);
+
+		if ( ! Tax::should_calculate_tax() ) {
+			if ( $order->total_price_before_tax ) {
+				$order_data['total_price'] = $order->total_price_before_tax;
+				$order_data['net_payment'] = $order->total_price_before_tax;
+			}
+
+			return $order_data;
+		}
+
+		$total_price_before_tax = $order->total_price_before_tax;
+		$tax_rate               = Tax::get_user_tax_rate( $order->user_id );
+
+		if ( $total_price_before_tax && $tax_rate ) {
 			$order_data['tax_type']   = Tax::get_tax_type();
 			$order_data['tax_rate']   = $tax_rate;
-			$order_data['tax_amount'] = Tax::calculate_tax( $total_price, $tax_rate );
+			$order_data['tax_amount'] = Tax::calculate_tax( $total_price_before_tax, $tax_rate );
 
 			if ( ! Tax::is_tax_included_in_price() ) {
-				$total_price              += $order_data['tax_amount'];
+				$total_price               = $total_price_before_tax + $order_data['tax_amount'];
 				$order_data['total_price'] = $total_price;
 				$order_data['net_payment'] = $total_price;
 			}
