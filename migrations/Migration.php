c0dc6ec6d62612de98c7233d4ae509e491be0770
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2025-08-21 19:08:02 +0600

    new: add Migration class for handling quiz attempt migrations

---
+++ migrations/Migration.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Migration
+ *
+ * @package Tutor
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 3.8.0
+ */
+
+namespace Tutor\Migrations;
+
+/**
+ * Class Migration
+ */
+class Migration {
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		QuizAttemptMigrator::instance()->schedule();
+	}
+}
