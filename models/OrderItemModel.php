494ee4fbf365bb4c2e3b9f7f5da4d2cfe652f72c
shewa12 <shewa12kpi@gmail.com>
2025-08-21 12:52:58 +0600

    OrderItemModel & OrderItemMetaModel classes added

--- models/OrderItemModel.php
+++ models/OrderItemModel.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Order Item Meta Model
- * Handles CRUD operations for order item meta data.
+ * Order Item Model
+ * Handles CRUD operations for order item data.
  *
  * @package Tutor\Models
  * @author Themeum <support@themeum.com>
@@ -11,146 +11,25 @@
 
 namespace Tutor\Ecommerce\Models;
 
-use Tutor\Helpers\QueryHelper;
+use Tutor\Models\BaseModel;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
 /**
- * Class OrderItemModel
+ * OrderItemModel class
+ * Handles CRUD operations for order item data.
+ *
+ * @since 3.8.0
  */
-class OrderItemModel {
+class OrderItemModel extends BaseModel {
 
 	/**
-	 * Table name.
+	 * Table name without prefix
+	 *
+	 * @since 3.8.0
 	 *
 	 * @var string
 	 */
-	private $table;
-
-	/**
-	 * Constructor.
-	 */
-	public function __construct() {
-		global $wpdb;
-		$this->table = $wpdb->prefix . 'tutor_order_itemmeta';
-	}
-
-	/**
-	 * Add meta for an order item.
-	 *
-	 * @since 3.8.0
-	 *
-	 * @param int    $item_id Item ID.
-	 * @param string $meta_key Meta key.
-	 * @param mixed  $meta_value Meta value.
-	 *
-	 * @return int Inserted row ID on success, 0 on failure.
-	 */
-	public function add_meta( $item_id, $meta_key, $meta_value ):int {
-		$meta_id = QueryHelper::insert(
-			$this->table,
-			array(
-				'item_id'    => $item_id,
-				'meta_key'   => $meta_key,
-				'meta_value' => maybe_serialize( $meta_value ),
-			),
-		);
-
-		return (int) $meta_id;
-	}
-
-	/**
-	 * Get meta by item_id and optional meta_key.
-	 *
-	 * @since 3.8.0
-	 *
-	 * @param int         $item_id Item ID.
-	 * @param string|null $meta_key Meta key (optional).
-	 * @param bool        $single Get a single value or all.
-	 *
-	 * @return array|false Meta results or false if none.
-	 */
-	public function get_meta( int $item_id, $meta_key = null, $single = true ) {
-		$where = array(
-			'item_id' => $item_id,
-		);
-		if ( $meta_key ) {
-			if ( $meta_key ) {
-				$where['meta_key'] = sanitize_key( $meta_key );
-			}
-		}
-
-		if ( $single ) {
-			$meta = QueryHelper::get_row(
-				$this->table,
-				$where,
-				'id'
-			);
-			if ( $meta ) {
-				$meta->meta_value = maybe_unserialize( $meta->meta_value );
-			}
-		} else {
-			$meta = QueryHelper::get_all(
-				$this->table,
-				$where,
-				'id'
-			);
-
-			if ( tutor_utils()->count( $meta ) ) {
-				foreach ( $meta as $row ) {
-					$row->meta_value = maybe_unserialize( $row->meta_value );
-				}
-			}
-		}
-
-		return $meta;
-	}
-
-	/**
-	 * Update meta for an order item.
-	 *
-	 * @since 3.8.0
-	 *
-	 * @param int    $item_id Item ID.
-	 * @param string $meta_key Meta key.
-	 * @param mixed  $meta_value Meta value.
-	 *
-	 * @return bool True on success, false on failure.
-	 */
-	public function update_meta( int $item_id, string $meta_key, $meta_value ): bool {
-		return QueryHelper::update(
-			$this->table,
-			array(
-				'meta_value'     => maybe_serialize( $meta_value ),
-			),
-			array(
-				'item_id'  => $item_id,
-				'meta_key' => $meta_key,
-			),
-		);
-	}
-
-	/**
-	 * Delete meta by item_id and optional meta_key.
-	 *
-	 * @since 3.8.0
-	 *
-	 * @param int         $item_id Item ID.
-	 * @param string|null $meta_key Meta key (optional).
-	 *
-	 * @return bool True on success, false on failure.
-	 */
-	public function delete_meta( $item_id, $meta_key = null ): bool {
-		$where = array(
-			'item_id' => $item_id,
-		);
-
-		if ( $meta_key ) {
-			$where['meta_key'] = $meta_key;
-		}
-
-		return QueryHelper::delete( $this->table, $where );
-	}
+	protected $table_name = 'tutor_order_items';
 }
