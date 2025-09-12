eea962946d7408aeccf946a3c28fe85558861a85
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2025-09-12 15:23:45 +0600

    feat: Add certificate selection and preview components
    Introduces Certificate, CertificateCard, and CertificateEmptyState components for managing certificate templates and custom certificates. Also adds CertificatePreviewModal for previewing and selecting certificates within a modal dialog.

---
+++ assets/react/v3/shared/components/certificate/CertificateEmptyState.tsx
@@ -0,0 +1,126 @@
+import { css } from '@emotion/react';
+import { __ } from '@wordpress/i18n';
+
+import Button from '@TutorShared/atoms/Button';
+import SVGIcon from '@TutorShared/atoms/SVGIcon';
+
+import config, { tutorConfig } from '@TutorShared/config/config';
+import { borderRadius, colorTokens, spacing } from '@TutorShared/config/styles';
+import { typography } from '@TutorShared/config/typography';
+import { styleUtils } from '@TutorShared/utils/style-utils';
+
+import certificate2x from '@SharedImages/pro-placeholders/certificates-2x.webp';
+import certificate from '@SharedImages/pro-placeholders/certificates.webp';
+
+const isTutorPro = !!tutorConfig.tutor_pro_url;
+
+const CertificateEmptyState = () => {
+  if (isTutorPro) {
+    return null;
+  }
+
+  return (
+    <div css={styles.emptyState}>
+      <img
+        css={styles.placeholderImage}
+        src={certificate}
+        srcSet={`${certificate} 1x, ${certificate2x} 2x`}
+        alt={__('Pro Placeholder', 'tutor')}
+      />
+
+      <div css={styles.featureAndActionWrapper}>
+        <h5 css={styles.title}>{__('Award Students with Custom Certificates', 'tutor')}</h5>
+        <div css={styles.featuresWithTitle}>
+          <div>
+            {
+              // prettier-ignore
+              __( 'Celebrate success with personalized certificates. Recognize student achievements with unique designs that inspire and motivate students.', 'tutor')
+            }
+          </div>
+
+          <div css={styles.features}>
+            <div css={styles.feature}>
+              <SVGIcon name="materialCheck" width={20} height={20} style={styles.checkIcon} />
+              <span>
+                {
+                  // prettier-ignore
+                  __('Design personalized certificates that highlight their accomplishments and boost their confidence.', 'tutor')
+                }
+              </span>
+            </div>
+            <div css={styles.feature}>
+              <SVGIcon name="materialCheck" width={20} height={20} style={styles.checkIcon} />
+              <span>
+                {__('Inspire them with a touch of credibility and recognition tailored just for them.', 'tutor')}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div css={styles.actionsButton}>
+        <Button
+          variant={'primary'}
+          icon={<SVGIcon name={'crown'} width={24} height={24} />}
+          onClick={() => {
+            window.open(config.TUTOR_PRICING_PAGE, '_blank', 'noopener');
+          }}
+        >
+          {__('Get Tutor LMS Pro', 'tutor')}
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default CertificateEmptyState;
+
+const styles = {
+  emptyState: css`
+    padding-bottom: ${spacing[12]};
+    ${styleUtils.display.flex('column')}
+    gap: ${spacing[20]};
+  `,
+  placeholderImage: ({ notFound }: { notFound?: boolean }) => css`
+    max-width: 100%;
+    width: 100%;
+    height: ${notFound ? '189px' : '312px;'};
+    object-fit: cover;
+    object-position: center;
+    border-radius: ${borderRadius[6]};
+  `,
+  featureAndActionWrapper: css`
+    ${styleUtils.display.flex('column')}
+    align-items: center;
+    gap: ${spacing[12]};
+  `,
+  title: css`
+    ${typography.heading5('medium')}
+    color: ${colorTokens.text.primary};
+  `,
+  featuresWithTitle: css`
+    ${styleUtils.display.flex('column')}
+    max-width: 500px;
+    width: 100%;
+    gap: ${spacing[8]};
+    ${typography.body('regular')};
+  `,
+  features: css`
+    ${styleUtils.display.flex('column')}
+    gap: ${spacing[8]};
+  `,
+  feature: css`
+    ${styleUtils.display.flex()}
+    gap: ${spacing[12]};
+    color: ${colorTokens.text.title};
+    text-wrap: pretty;
+  `,
+  checkIcon: css`
+    flex-shrink: 0;
+    color: ${colorTokens.text.success};
+  `,
+  actionsButton: css`
+    ${styleUtils.flexCenter()}
+    margin-top: ${spacing[4]};
+  `,
+};
