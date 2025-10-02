8375117d3b320be2012454758533c60c0afee0a5
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2025-10-03 00:10:32 +0600

    feat: Add rules for text domain in shared folder

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -54,6 +54,13 @@ export default [
     files: ['./assets/react/v3/shared/**/*.{ts,tsx,js,jsx}'],
     rules: {
       '@wordpress/i18n-text-domain': 'off',
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector: 'CallExpression[callee.name="__"] > Literal.arguments:nth-child(2)',
+          message: 'In shared folder, use __TUTOR_TEXT_DOMAIN__ instead of hardcoded string literal for text domain.',
+        },
+      ],
     },
   },
 ];
