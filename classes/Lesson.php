ca6746eebfc1470ee5c537e35e5a11d07abbaef9
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2025-10-16 12:59:20 +0600

    feat: Remove `lesson-note` comment type from lesson specific comments

--- classes/Lesson.php
+++ classes/Lesson.php
@@ -166,7 +166,6 @@ class Lesson extends Tutor_Base {
 		} else {
 			delete_post_meta( $post_ID, '_tutor_attachments' );
 		}
-
 	}
 
 	/**
@@ -581,6 +580,14 @@ class Lesson extends Tutor_Base {
 	 * @return mixed  based on arguments
 	 */
 	public static function get_comments( array $args ) {
+		$args['meta_query'] = array(
+			array(
+				'key'     => 'comment_type',
+				'value'   => 'lesson_note',
+				'compare' => '!=', // Exclude 'lesson_note'.
+			),
+		);
+
 		$comments = get_comments( $args );
 		return $comments;
 	}
