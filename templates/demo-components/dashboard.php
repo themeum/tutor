7c5c5178e4c7c9f132caa8b0b9a8495f21074a35
nur-alam <nuralam862@gmail.com>
2025-11-03 11:28:22 +0600

    Add badge component styles and mixins; update dashboard to display badges

--- templates/demo-components/dashboard.php
+++ templates/demo-components/dashboard.php
@@ -1,1 +1,28 @@
 <h2>Dashboard</h2>
+
+<div class="tutor-bg-white tutor-py-6 tutor-px-8">
+    <h2>Badges</h2>
+    <div class="tutor-rounded-lg tutor-shadow-sm" style="display: flex; gap:10px;">
+        <div class="tutor-badge tutor-badge-primary">
+            Primary
+        </div>
+        <div class="tutor-badge tutor-badge-pending">
+            Pending
+        </div> 
+        <div class="tutor-badge tutor-badge-completed">
+            Completed
+        </div>
+        <div class="tutor-badge tutor-badge-cancelled">
+            Cancelled
+        </div> 
+        <div class="tutor-badge tutor-badge-secondary">
+            Secondary
+        </div> 
+        <div class="tutor-badge tutor-badge-secondary tutor-badge-circle">
+            <span class="tutor-text-secondary">Points:</span> 20
+        </div>
+        <div class="tutor-badge tutor-badge-exception tutor-badge-circle">
+            Bundle
+        </div>
+    </div>
+</div>
