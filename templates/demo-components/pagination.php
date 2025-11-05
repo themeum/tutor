bd632b3fe8bd303d0adcf77beadc15fd356ea77a
nur-alam <nuralam862@gmail.com>
2025-11-05 12:21:42 +0600

    remove bem style class naming

--- templates/demo-components/pagination.php
+++ templates/demo-components/pagination.php
@@ -13,28 +13,28 @@ use TUTOR\Icon;
 	<h2>Pagination</h2>
 	<div class="tutor-rounded-lg tutor-shadow-sm">
 		<nav class="tutor-pagination" role="navigation" aria-label="Pagination Navigation">
-			<span class="tutor-pagination__info" aria-live="polite">
-				Page <span class="tutor-pagination__current">3</span> of <span class="tutor-pagination__total">12</span>
+			<span class="tutor-pagination-info" aria-live="polite">
+				Page <span class="tutor-pagination-current">3</span> of <span class="tutor-pagination-total">12</span>
 			</span>
 
-			<ul class="tutor-pagination__list">
+			<ul class="tutor-pagination-list">
 				<li>
-					<a class="tutor-pagination__item tutor-pagination__item--prev" aria-label="Previous page" aria-disabled="true">
+					<a class="tutor-pagination-item tutor-pagination-item-prev" aria-label="Previous page" aria-disabled="true">
 						<?php tutor_utils()->render_svg_icon( Icon::CHEVRON_LEFT, 14, 14 ); ?>
 					</a>
 				</li>
 
-				<li><a class="tutor-pagination__item">1</a></li>
+				<li><a class="tutor-pagination-item">1</a></li>
 				<li>
-					<a class="tutor-pagination__item tutor-pagination__item--active" aria-current="page">2</a>
+					<a class="tutor-pagination-item tutor-pagination-item-active" aria-current="page">2</a>
 				</li>
-				<li><a class="tutor-pagination__item">3</a></li>
-				<li><span class="tutor-pagination__ellipsis" aria-hidden="true">…</span></li>
-				<li><a class="tutor-pagination__item">6</a></li>
-				<li><a class="tutor-pagination__item">7</a></li>
+				<li><a class="tutor-pagination-item">3</a></li>
+				<li><span class="tutor-pagination-ellipsis" aria-hidden="true">…</span></li>
+				<li><a class="tutor-pagination-item">6</a></li>
+				<li><a class="tutor-pagination-item">7</a></li>
 
 				<li>
-					<a class="tutor-pagination__item tutor-pagination__item--next" aria-label="Next page">
+					<a class="tutor-pagination-item tutor-pagination-item-next" aria-label="Next page">
 						<?php tutor_utils()->render_svg_icon( Icon::CHEVRON_RIGHT, 14, 14 ); ?>
 					</a>
 				</li>
