4326685ddfc5b52af01ffd32b7928bff7272b808
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2025-11-07 17:32:30 +0600

    feat: Register form component and service in registry
    Added formMeta to the components and formServiceMeta to the services in TutorComponentRegistry registration. This enables usage of the form component and its related service throughout the application.

--- assets/core/ts/index.ts
+++ assets/core/ts/index.ts
@@ -6,19 +6,32 @@ import { TutorComponentRegistry } from '@Core/ComponentRegistry';
 import { accordionMeta } from '@Core/components/accordion';
 import { buttonMeta } from '@Core/components/button';
 import { fileUploaderMeta } from '@Core/components/file-uploader';
+import { formMeta } from '@Core/components/form';
 import { iconMeta } from '@Core/components/icon';
 import { modalMeta } from '@Core/components/modal';
 import { popoverMeta } from '@Core/components/popover';
 import { staticsMeta } from '@Core/components/statics';
 import { tabsMeta } from '@Core/components/tabs';
+
+import { formServiceMeta } from '@Core/services/Form';
 import { modalServiceMeta } from '@Core/services/Modal';
 
 Alpine.plugin(focus);
 
 const initializePlugin = () => {
   TutorComponentRegistry.registerAll({
-    components: [buttonMeta, fileUploaderMeta, tabsMeta, iconMeta, modalMeta, popoverMeta, staticsMeta, accordionMeta],
-    services: [modalServiceMeta],
+    components: [
+      buttonMeta,
+      fileUploaderMeta,
+      tabsMeta,
+      iconMeta,
+      modalMeta,
+      popoverMeta,
+      staticsMeta,
+      accordionMeta,
+      formMeta,
+    ],
+    services: [formServiceMeta, modalServiceMeta],
   });
 
   TutorComponentRegistry.initWithAlpine(Alpine);
