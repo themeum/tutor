ea17cbc7915d7bc9c5be62c32c979b8018f4630e
Sadman Soumique <saadman.ofc@gmail.com>
2025-11-07 16:10:03 +0600

    Add Live Session Card Component
    Introduced a new Live Session Card component with associated SCSS styles. Updated the component index to include the new card and integrated it into the dashboard home template for enhanced user experience.

--- templates/demo-components/dashboard/pages/home.php
+++ templates/demo-components/dashboard/pages/home.php
@@ -9,6 +9,12 @@
  */
 
 ?>
-<div class="tutor-text-h3 tutor-color-black tutor-p-8">
-	<?php esc_html_e( 'Welcome to TutorLMS Home', 'tutor' ); ?>
+<div class="tutor-p-8">
+	<div class="tutor-text-h3 tutor-color-black tutor-mb-6">
+		<?php esc_html_e( 'Welcome to TutorLMS Home', 'tutor' ); ?>
+	</div>
+	
+	<div class="tutor-mb-4">
+		<?php tutor_load_template( 'core-components.live-session-card' ); ?>
+	</div>
 </div>
