e43667535f184872eadb4ca6965afc4f6993cc51
nur-alam <nuralam862@gmail.com>
2025-11-14 13:33:18 +0600

    style(profile-header): Refactor profile header responsive layout and styling
    - Update profile header SCSS to improve mobile responsiveness
    - Modify profile header template to add mobile-friendly layout
    - Adjust button positioning and visibility for small screens
    - Add close button for mobile view
    - Update file paths and template loading for account settings
    - Improve overall header structure and styling

--- templates/demo-components/dashboard/components/profile-header.php
+++ templates/demo-components/dashboard/components/profile-header.php
@@ -12,16 +12,23 @@ use TUTOR\Icon;
 ?>
 <div class="tutor-profile-header">
 	<div class="tutor-dashboard-container tutor-flex tutor-items-center tutor-justify-between">
-		<div class="tutor-flex tutor-items-center">
-			<button type="button" class="tutor-profile-settings-back-btn tutor-btn tutor-btn-ghost tutor-btn-x-small">
-				<span x-data="tutorIcon({ name: '<?php echo esc_html( Icon::ARROW_LEFT ); ?>', width: 20, height: 20})"></span>
+		<div class="tutor-profile-header-left tutor-flex tutor-items-center">
+			<button class="tutor-btn tutor-btn-ghost tutor-btn-x-small tutor-btn-icon">
+				<?php tutor_utils()->render_svg_icon( Icon::LEFT ); ?>
 			</button>
 			<h4 class="tutor-text-h4 tutor-font-semibold tutor-ml-4">Settings</h4>
 			<span class="tutor-badge tutor-badge-secondary tutor-badge-circle tutor-ml-5 tutor-sm-hidden">Unsaved changes</span>
 		</div>
-		<div class="tutor-flex tutor-gap-4">
-			<button type="button" class="tutor-btn tutor-btn-ghost tutor-btn-x-small">Discard</button>
-			<button type="button" class="tutor-btn tutor-btn-primary tutor-btn-x-small">Save</button>
+		<div class="tutor-profile-header-right tutor-flex tutor-gap-4">
+			<div class="tutor-sm-hidden">
+				<button type="button" class="tutor-btn tutor-btn-ghost tutor-btn-x-small">Discard</button>
+				<button type="button" class="tutor-btn tutor-btn-primary tutor-btn-x-small">Save</button>
+			</div>
+			<div class="tutor-profile-header-close">
+				<button class="tutor-btn tutor-btn-ghost tutor-btn-x-small tutor-btn-icon">
+					<?php tutor_utils()->render_svg_icon( Icon::CROSS ); ?>
+				</button>
+			</div>
 		</div>
 	</div>
 </div>
