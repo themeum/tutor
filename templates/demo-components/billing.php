9f4bee7d5e401e6752d14942c8d42b25e8f5edef
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2025-11-19 00:54:25 +0600

    feat: Add subscription details page

--- templates/demo-components/billing.php
+++ templates/demo-components/billing.php
@@ -10,6 +10,31 @@
 
 use TUTOR\Icon;
 
+/**
+ * Check if the current page is the subscription details page.
+ *
+ * @return bool
+ * @since 4.0.0
+ */
+function is_subscription_details_page() {
+	// Using filter_input which doesn't add slashes.
+	$subpage         = filter_input( INPUT_GET, 'subpage', FILTER_SANITIZE_SPECIAL_CHARS );
+	$tab             = filter_input( INPUT_GET, 'tab', FILTER_SANITIZE_SPECIAL_CHARS );
+	$subscription_id = filter_input( INPUT_GET, 'subscription_id', FILTER_VALIDATE_INT );
+
+	return 'billing' === $subpage &&
+			'subscriptions' === $tab &&
+			! empty( $subscription_id );
+}
+
+$back_url = add_query_arg(
+	array(
+		'subpage' => 'billing',
+		'tab'     => 'subscriptions',
+	),
+	remove_query_arg( 'subscription_id' )
+);
+
 $billing_tabs = array(
 	array(
 		'id'    => 'orders',
@@ -21,7 +46,7 @@ $billing_tabs = array(
 		'label' => __( 'Subscriptions', 'tutor' ),
 		'icon'  => Icon::SUBSCRIPTION,
 	),
-)
+);
 
 ?>
 
@@ -58,41 +83,93 @@ $billing_tabs = array(
 					}
 				})'
 			>
-				<div x-ref="tablist" class="tutor-billing-tabs tutor-tabs-nav" role="tablist" aria-orientation="horizontal">
-					<template x-for="tab in tabs" :key="tab.id">
-						<button
-						type="button"
-						role="tab"
-						:class='getTabClass(tab)'
-						x-bind:aria-selected="isActive(tab.id)"
-						:disabled="tab.disabled ? true : false"
-						@click="selectTab(tab.id)"
-						>
-							<span x-data="TutorCore.icon({ name: tab.icon, width: 24, height: 24})"></span>
-							<span x-text="tab.label"></span>
-						</button>
-					</template>
-				</div>
+				<?php if ( ! is_subscription_details_page() ) : ?>
+					<div x-ref="tablist" class="tutor-billing-tabs tutor-tabs-nav" role="tablist" aria-orientation="horizontal">
+						<template x-for="tab in tabs" :key="tab.id">
+							<button
+							type="button"
+							role="tab"
+							:class='getTabClass(tab)'
+							x-bind:aria-selected="isActive(tab.id)"
+							:disabled="tab.disabled ? true : false"
+							@click="selectTab(tab.id)"
+							>
+								<span x-data="TutorCore.icon({ name: tab.icon, width: 24, height: 24})"></span>
+								<span x-text="tab.label"></span>
+							</button>
+						</template>
+					</div>
 
-				<!-- Filters -->
-				<div class="tutor-billing-filters">
-					<div>All</div>
-					<div>Newest first</div>
-				</div>
-	
-				<!-- Tabs Content -->
-				<div class="tutor-billing-tab-content tutor-tabs-content">
-					<template x-if="activeTab === 'orders'">
-						<div class="tutor-tab-panel" role="tabpanel">
-							<?php tutor_load_template( 'demo-components.dashboard.components.order-history' ); ?>
+					<!-- Filters -->
+					<div class="tutor-billing-filters">
+						<div x-data="tutorPopover({ placement: 'bottom-start', offset: 4 })">
+							<button x-ref="trigger" @click="toggle()" class="tutor-btn tutor-btn-link tutor-btn-x-small tutor-p-none tutor-gap-2">
+								<?php esc_html_e( 'All', 'tutor' ); ?>
+								<?php tutor_utils()->render_svg_icon( Icon::CHEVRON_DOWN ); ?>
+							</button>
+
+							<div x-ref="content" x-cloak x-show="open" @click.outside="handleClickOutside()" class="tutor-popover tutor-py-4" style="width: 120px;">
+								<ul>
+									<li class="tutor-m-none">
+										<a href="#" class="tutor-px-5 tutor-py-4 tutor-flex tutor-w-full">
+											<?php esc_html_e( 'All', 'tutor' ); ?>
+										</a>
+									</li>
+									<li class="tutor-m-none">
+										<a href="#" class="tutor-px-5 tutor-py-4 tutor-flex tutor-w-full">
+											<?php esc_html_e( 'Pending', 'tutor' ); ?>
+										</a>
+									</li>
+									<li class="tutor-m-none">
+										<a href="#" class="tutor-px-5 tutor-py-4 tutor-flex tutor-w-full">
+											<?php esc_html_e( 'Active', 'tutor' ); ?>
+										</a>
+									</li>
+									<li class="tutor-m-none">
+										<a href="#" class="tutor-px-5 tutor-py-4 tutor-flex tutor-w-full">
+											<?php esc_html_e( 'On Hold', 'tutor' ); ?>
+										</a>
+									</li>
+									<li class="tutor-m-none">
+										<a href="#" class="tutor-px-5 tutor-py-4 tutor-flex tutor-w-full">
+											<?php esc_html_e( 'Expired', 'tutor' ); ?>
+										</a>
+									</li>
+									<li class="tutor-m-none">
+										<a href="#" class="tutor-px-5 tutor-py-4 tutor-flex tutor-w-full">
+											<?php esc_html_e( 'Cancelled', 'tutor' ); ?>
+										</a>
+									</li>
+								</ul>
+							</div>
 						</div>
-					</template>
-					<template x-if="activeTab === 'subscriptions'">
-						<div class="tutor-tab-panel" role="tabpanel">
-							<?php tutor_load_template( 'demo-components.dashboard.components.subscription-history' ); ?>
-						</div>
-					</template>
-				</div>
+						<div>Newest first</div>
+					</div>
+
+					<!-- Tabs Content -->
+					<div class="tutor-billing-tab-content tutor-tabs-content">
+						<template x-if="activeTab === 'orders'">
+							<div class="tutor-tab-panel" role="tabpanel">
+								<?php tutor_load_template( 'demo-components.dashboard.components.order-history' ); ?>
+							</div>
+						</template>
+						<template x-if="activeTab === 'subscriptions'">
+							<div class="tutor-tab-panel" role="tabpanel">
+								<?php tutor_load_template( 'demo-components.dashboard.components.subscription-history' ); ?>
+							</div>
+						</template>
+					</div>
+				<?php else : ?>
+					<div class="tutor-billing-tabs">
+						<!-- Go to the subscription page -->
+						<a href="<?php echo esc_url( $back_url ); ?>" class="tutor-btn tutor-btn-secondary tutor-gap-2">
+							<?php tutor_utils()->render_svg_icon( Icon::LEFT ); ?>
+							<?php esc_html_e( 'Back', 'tutor' ); ?>
+						</a>
+					</div>
+
+					<?php tutor_load_template( 'demo-components.dashboard.components.billing.subscription-details' ); ?>
+				<?php endif; ?>
 			</div>
 		</div>
 
