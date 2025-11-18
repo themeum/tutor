b3aa01a211a3ceaaaa29325fcb8e087e73b93401
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2025-11-18 10:39:04 +0600

    feat: Add subscription history component

---
+++ templates/demo-components/dashboard/components/subscription-history.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Dashboard Order History
+ *
+ * @package Tutor\Templates
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 4.0.0
+ */
+
+use TUTOR\Icon;
+
+?>
+
+<div class="tutor-order-history">
+	Subscription History
+</div>
