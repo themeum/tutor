56e4cfa0330a277191068a869872364f42e54de1
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2025-11-20 09:51:26 +0600

    chore: Update filters doc block

--- templates/demo-components/dashboard/components/billing/filters.php
+++ templates/demo-components/dashboard/components/billing/filters.php
@@ -1,9 +1,11 @@
 <?php
 /**
- * Filters
+ * Billing Filters
  *
- * @package TutorPress\Templates\Demo_Components\Dashboard\Components\Billing
- * @since 1.0.0
+ * @package Tutor\Templates
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 4.0.0
  */
 
 use TUTOR\Icon;
