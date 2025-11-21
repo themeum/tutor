0b2bad12d34f5cb7efb07417ad7d63a8aa447fbf
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2025-11-21 10:27:08 +0600

    feat: Load templates in quiz page

---
+++ templates/demo-components/quiz.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Tutor quiz.
+ *
+ * @package Tutor\Templates
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 4.0.0
+ */
+
+?>
+
+<div class="tutor-quiz">
+	<?php tutor_load_template( 'demo-components.learning-area.components.quiz.progress-bar' ); ?>
+	
+	<div class="tutor-quiz-questions">
+		<?php tutor_load_template( 'demo-components.learning-area.components.quiz.questions.true-false' ); ?>
+	</div>
+</div>
