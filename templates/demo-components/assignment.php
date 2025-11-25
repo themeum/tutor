91fee7458c6171195220bcb719f2fdd4667cf86a
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2025-11-25 10:45:57 +0600

    feat: introduce assignment demo component and register it as a learning area page

---
+++ templates/demo-components/assignment.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Tutor Assignment.
+ *
+ * @package Tutor\Templates
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 4.0.0
+ */
+
+?>
+
+<div class="tutor-assignment">
+	Tutor Assignment
+</div>
