dcc53ea1faef451a5650a37e97fd6df011cb8ae2
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2025-11-28 11:36:17 +0600

    feat: add responsive list view for assignment attempts table on small screens.

--- templates/demo-components/learning-area/components/assignment/attempts-table.php
+++ templates/demo-components/learning-area/components/assignment/attempts-table.php
@@ -37,7 +37,7 @@ $attempt_details_url = function ( $attempt ) {
 };
 ?>
 
-<div class="tutor-table-wrapper">
+<div class="tutor-table-wrapper tutor-sm-hidden">
 	<table class="tutor-table">
 		<thead>
 			<tr>
@@ -64,4 +64,48 @@ $attempt_details_url = function ( $attempt ) {
 			<?php endforeach; ?>
 		</tbody>
 	</table>
+</div>
+
+<!-- Below sm breakpoint, show table as a list -->
+<div class="tutor-assignment-attempts-list tutor-hidden tutor-sm-block">
+	<?php foreach ( $attempts as $attempt ) : ?>
+		<div class="tutor-assignment-attempts-list-item">
+			<div class="tutor-flex tutor-items-center tutor-justify-between">
+				<a href="<?php echo esc_url( $attempt_details_url( $attempt ) ); ?>" class="tutor-small tutor-text-brand tutor-font-medium">
+					<?php echo esc_html( date_i18n( 'D, ' . get_option( 'date_format' ) . ', ' . get_option( 'time_format' ), strtotime( $attempt['attempt_date'] ) ) ); ?>
+				</a>
+
+				<?php echo wp_kses_post( $render_status( $attempt['status'] ) ); ?>
+			</div>
+
+			<div class="tutor-assignment-attempts-list-card">
+				<div class="tutor-flex tutor-flex-column tutor-gap-3">
+					<span class="tutor-small tutor-text-subdued">
+						<?php esc_html_e( 'Total Marks', 'tutor' ); ?>
+					</span>
+					<span class="tutor-p1 tutor-font-medium tutor-text-secondary">
+						<?php echo esc_html( $attempt['total_marks'] ); ?>
+					</span>
+				</div>
+
+				<div class="tutor-flex tutor-flex-column tutor-gap-3">
+					<span class="tutor-small tutor-text-subdued">
+						<?php esc_html_e( 'Pass Marks', 'tutor' ); ?>
+					</span>
+					<span class="tutor-p1 tutor-font-medium tutor-text-secondary">
+						<?php echo esc_html( $attempt['pass_marks'] ); ?>
+					</span>
+				</div>
+
+				<div class="tutor-flex tutor-flex-column tutor-gap-3">
+					<span class="tutor-small tutor-text-subdued">
+						<?php esc_html_e( 'Earned Marks', 'tutor' ); ?>
+					</span>
+					<span class="tutor-p1 tutor-font-medium tutor-text-secondary">
+						<?php echo esc_html( $attempt['earned_marks'] ? $attempt['earned_marks'] : '-' ); ?>
+					</span>
+				</div>
+			</div>
+		</div>
+	<?php endforeach; ?>
 </div>
