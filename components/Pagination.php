5d257a232ec09fa9bb12aad83a30ef8797a8db5e
Anindra Das Bivas <bivasdas911@gmail.com>
2025-12-02 18:01:54 +0600

    Added Pagination Component Class

---
+++ components/Pagination.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Pagination Component Class.
+ *
+ * @package Tutor\Components
+ * @author Themeum
+ * @link https://themeum.com
+ * @since 4.0.0
+ */
+
+namespace Tutor\Components;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class Pagination
+ *
+ * Responsible for rendering pagination component.
+ *
+ * @since 4.0.0
+ */
+class Pagination extends BaseComponent {
+
+	public function render(): string {
+		throw new \Exception( 'Not implemented' );
+	}
+}
