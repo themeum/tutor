8e2d7372a8835b4680610abbcaa03dfbf328676a
shewa12 <shewa12kpi@gmail.com>
2025-12-04 13:01:00 +0600

    Size & Varitants component added

---
+++ components/Constants/Variant.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Button Variant constants
+ *
+ * @package Tutor\Components
+ * @author Themeum
+ * @link https://themeum.com
+ * @since 4.0.0
+ */
+
+/**
+ * Class for containing variant constants
+ *
+ * @since 4.0.0
+ */
+abstract class Variant {
+
+	/**
+	 * Semantic Variants
+	 *
+	 * @since 4.0.0
+	 *
+	 * @var string
+	 */
+	public const PRIMARY      = 'primary';
+	public const PRIMARY_SOFT = 'primary-soft';
+
+	public const DESTRUCTIVE      = 'destructive';
+	public const DESTRUCTIVE_SOFT = 'destructive-soft';
+
+	public const SECONDARY = 'secondary';
+	public const OUTLINE   = 'outline';
+	public const GHOST     = 'ghost';
+	public const LINK      = 'link';
+
+	// Badge-only variants.
+	public const PENDING   = 'pending';
+	public const COMPLETED = 'completed';
+	public const CANCELLED = 'cancelled';
+	public const EXCEPTION = 'exception';
+
+}
