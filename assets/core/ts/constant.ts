e8fb0139e0b3f59c152d53666adc6685e9c1ce87
Sazedul Haque <sazedulhaque5@gmail.com>
2025-12-04 17:27:16 +0600

    Payload support added to the modal

--- assets/core/ts/constant.ts
+++ assets/core/ts/constant.ts
@@ -1,5 +1,6 @@
 export const TUTOR_CUSTOM_EVENTS = {
   TAB_CHANGE: 'tutor-tab-change',
   MODAL_OPEN: 'tutor-modal-open',
+  MODAL_UPDATE: 'tutor-modal-update',
   MODAL_CLOSE: 'tutor-modal-close',
 };
