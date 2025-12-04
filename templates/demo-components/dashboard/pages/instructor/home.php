2332760555341b5fc762a40da3d41f25ce27ccb5
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2025-12-04 12:58:12 +0600

    feat: Add new instructor home template

---
+++ templates/demo-components/dashboard/pages/instructor/home.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Dashboard page: Home for Instructor.
+ *
+ * @package Tutor\Templates
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 4.0.0
+ */
+
+use TUTOR\Icon;
+
+// Define stat card variations with sample data matching the design.
+$stat_cards = array(
+	array(
+		'variation' => 'success',
+		'title'     => esc_html__( 'Total Earnings', 'tutor' ),
+		'icon'      => Icon::EARNING,
+		'value'     => '$740.00',
+		'change'    => '+2',
+		'data'      => array( 0, 5, 8 ),
+	),
+	array(
+		'variation' => 'brand',
+		'title'     => esc_html__( 'Total Courses', 'tutor' ),
+		'icon'      => Icon::COURSES,
+		'value'     => '12',
+		'change'    => '+2',
+		'data'      => array( 0, 8, 5 ),
+	),
+	array(
+		'variation' => 'exception5',
+		'title'     => esc_html__( 'Total Students', 'tutor' ),
+		'icon'      => Icon::PASSED,
+		'value'     => '3000',
+		'change'    => '+2',
+		'data'      => array( 0, 8, 5 ),
+	),
+	array(
+		'variation' => 'exception4',
+		'title'     => esc_html__( 'Avg. Rating', 'tutor' ),
+		'icon'      => Icon::STAR,
+		'value'     => '4.2',
+		'change'    => '+2',
+		'data'      => array( 4.5, 4.2, 3 ),
+	),
+);
+
+?>
+
+<div class="tutor-flex tutor-flex-column tutor-gap-6 tutor-mt-7">
+	<!-- Filters -->
+	<div class="tutor-flex tutor-justify-between tutor-align-center">
+		<button class="tutor-btn tutor-btn-outline tutor-btn-small tutor-flex-center tutor-gap-2">
+			<?php tutor_utils()->render_svg_icon( Icon::CALENDAR ); ?>
+			<?php esc_html_e( 'All Time', 'tutor' ); ?>
+		</button>
+
+		<button class="tutor-btn tutor-btn-outline tutor-btn-small tutor-btn-icon">
+			<?php tutor_utils()->render_svg_icon( Icon::FILTER_2 ); ?>
+		</button>
+	</div>
+
+	<!-- Stat cards -->
+	<div class="tutor-flex tutor-gap-4">
+		<?php foreach ( $stat_cards as $card ) : ?>
+			<div class="tutor-flex-1">
+			<?php
+			tutor_load_template(
+				'demo-components.dashboard.components.stat-card',
+				array(
+					'variation'  => isset( $card['variation'] ) ? $card['variation'] : 'enrolled',
+					'card_title' => isset( $card['title'] ) ? $card['title'] : '',
+					'icon'       => isset( $card['icon'] ) ? $card['icon'] : '',
+					'value'      => isset( $card['value'] ) ? $card['value'] : '',
+					'change'     => isset( $card['change'] ) ? $card['change'] : '',
+					'data'       => isset( $card['data'] ) ? $card['data'] : array( 0, 0, 0 ),
+					'show_graph' => true,
+				)
+			);
+			?>
+			</div>
+		<?php endforeach; ?>
+	</div>
+</div>
