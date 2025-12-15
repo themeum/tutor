f46302ea8b73f2a867ddb7e3adb2a99f28c7a460
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2025-12-15 15:14:52 +0600

    feat: Add detailed recent activity display with user avatars and course details, and update associated styling.

--- templates/demo-components/dashboard/pages/instructor/home.php
+++ templates/demo-components/dashboard/pages/instructor/home.php
@@ -177,16 +177,94 @@ $upcoming_tasks = array(
 
 $recent_activity = array(
 	array(
-		'name' => 'Complete Web Development Bootcamp',
-		'url'  => '#',
+		'course_name' => 'Complete Web Development Bootcamp',
+		'course_url'  => '#',
+		'date'        => '2022-01-01 10:00 AM',
+		'meta'        => 'enrolled in',
+		'user'        => array(
+			'name'   => 'John Doe',
+			'avatar' => 'https://i.pravatar.cc/300?u=a04258a2462d826712d',
+		),
 	),
 	array(
-		'name' => 'Complete Web Development Bootcamp',
-		'url'  => '#',
+		'course_name' => 'Complete Web Development Bootcamp',
+		'course_url'  => '#',
+		'date'        => '2022-01-01 10:00 AM',
+		'meta'        => 'enrolled in',
+		'user'        => array(
+			'name'   => 'John Doe',
+			'avatar' => 'https://i.pravatar.cc/300?u=a04258a2462d826712d',
+		),
 	),
 	array(
-		'name' => 'Complete Web Development Bootcamp',
-		'url'  => '#',
+		'course_name' => 'Complete Web Development Bootcamp',
+		'course_url'  => '#',
+		'date'        => '2022-01-01 10:00 AM',
+		'meta'        => 'enrolled in',
+		'user'        => array(
+			'name'   => 'John Doe',
+			'avatar' => 'https://i.pravatar.cc/300?u=a04258a2462d826712d',
+		),
+	),
+	array(
+		'course_name' => 'Complete Web Development Bootcamp',
+		'course_url'  => '#',
+		'date'        => '2022-01-01 10:00 AM',
+		'meta'        => 'enrolled in',
+		'user'        => array(
+			'name'   => 'John Doe',
+			'avatar' => 'https://i.pravatar.cc/300?u=a04258a2462d826712d',
+		),
+	),
+	array(
+		'course_name' => 'Complete Web Development Bootcamp',
+		'course_url'  => '#',
+		'date'        => '2022-01-01 10:00 AM',
+		'meta'        => 'enrolled in',
+		'user'        => array(
+			'name'   => 'John Doe',
+			'avatar' => 'https://i.pravatar.cc/300?u=a04258a2462d826712d',
+		),
+	),
+	array(
+		'course_name' => 'Complete Web Development Bootcamp',
+		'course_url'  => '#',
+		'date'        => '2022-01-01 10:00 AM',
+		'meta'        => 'enrolled in',
+		'user'        => array(
+			'name'   => 'John Doe',
+			'avatar' => 'https://i.pravatar.cc/300?u=a04258a2462d826712d',
+		),
+	),
+	array(
+		'course_name' => 'Complete Web Development Bootcamp',
+		'course_url'  => '#',
+		'date'        => '2022-01-01 10:00 AM',
+		'meta'        => 'enrolled in',
+		'user'        => array(
+			'name'   => 'John Doe',
+			'avatar' => 'https://i.pravatar.cc/300?u=a04258a2462d826712d',
+		),
+	),
+	array(
+		'course_name' => 'Complete Web Development Bootcamp',
+		'course_url'  => '#',
+		'date'        => '2022-01-01 10:00 AM',
+		'meta'        => 'enrolled in',
+		'user'        => array(
+			'name'   => 'John Doe',
+			'avatar' => 'https://i.pravatar.cc/300?u=a04258a2462d826712d',
+		),
+	),
+	array(
+		'course_name' => 'Complete Web Development Bootcamp',
+		'course_url'  => '#',
+		'date'        => '2022-01-01 10:00 AM',
+		'meta'        => 'enrolled in',
+		'user'        => array(
+			'name'   => 'John Doe',
+			'avatar' => 'https://i.pravatar.cc/300?u=a04258a2462d826712d',
+		),
 	),
 );
 
@@ -444,18 +522,27 @@ $get_icon_by_post_type = function ( $post_type ) {
 
 			<div class="tutor-dashboard-home-card-body">
 				<?php foreach ( $recent_activity as $item ) : ?>
-					<div class="tutor-dashboard-home-task">
-						<div class="tutor-flex tutor-items-center tutor-gap-4">
-							<div class="tutor-dashboard-home-task-icon">
-								<?php tutor_utils()->render_svg_icon( Icon::CLOCK ); ?>
+					<div class="tutor-dashboard-home-card-item tutor-dashboard-home-activity">
+						<div class="tutor-flex tutor-items-center tutor-gap-4 tutor-w-full">
+							<!-- Avatar -->
+							<div class="tutor-dashboard-home-card-avatar">
+								<?php Avatar::make()->src( $item['user']['avatar'] )->initials( $item['user']['name'] )->size( Size::SIZE_40 )->render(); ?>
 							</div>
-							<div class="tutor-flex tutor-flex-column">
-								<div class="tutor-p3">
-									<?php echo esc_html( $item['name'] ); ?>
+							<div class="tutor-flex-1 tutor-flex tutor-flex-column">
+								<div class="tutor-flex tutor-justify-between tutor-items-center tutor-tiny">
+									<div>
+										<?php echo esc_html( $item['user']['name'] ); ?>
+										<span class="tutor-text-subdued">
+											<?php echo esc_html( $item['meta'] ); ?>
+										</span>
+									</div>
+									<div class="tutor-tiny-2 tutor-text-subdued">
+										<?php echo esc_html( human_time_diff( strtotime( $item['date'] ) ) ); ?>
+									</div>
 								</div>
-								<div class="tutor-text-subdued">
-									<?php echo esc_html( $item['date'] ); ?>
-								</div>
+								<a class="tutor-tiny tutor-font-medium" href="<?php echo esc_url( $item['course_url'] ); ?>">
+									<?php echo esc_html( $item['course_name'] ); ?>
+								</a>
 							</div>
 						</div>
 					</div>
