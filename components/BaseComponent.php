6552028dfdec4252800c08034237caa1051f0c01
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2025-12-24 12:20:45 +0600

    update: made components classes autocomplete friendly

--- components/BaseComponent.php
+++ components/BaseComponent.php
@@ -24,7 +24,7 @@ defined( 'ABSPATH' ) || exit;
  *
  * @since 4.0.0
  */
-abstract class BaseComponent implements ComponentInterface {
+abstract class BaseComponent {
 
 	/**
 	 * Keep the component as string
@@ -36,13 +36,13 @@ abstract class BaseComponent implements ComponentInterface {
 	protected $component_string = '';
 
 	/**
-	 * Create a new Button instance.
+	 * Create a new component instance.
 	 *
 	 * @since 4.0.0
 	 *
-	 * @return ComponentInterface
+	 * @return static
 	 */
-	public static function make(): ComponentInterface {
+	public static function make() {
 		return new static();
 	}
 
@@ -56,6 +56,7 @@ abstract class BaseComponent implements ComponentInterface {
 	 * ]
 	 *
 	 * @since 4.0.0
+	 *
 	 * @var array
 	 */
 	protected $attributes = array();
@@ -69,6 +70,7 @@ abstract class BaseComponent implements ComponentInterface {
 	 * @since 4.0.0
 	 *
 	 * @param array $attrs Key–value pairs of HTML attributes.
+	 *
 	 * @return self
 	 */
 	public function attrs( array $attrs ): self {
@@ -116,6 +118,18 @@ abstract class BaseComponent implements ComponentInterface {
 	}
 
 	/**
+	 * Get the component output as an HTML string.
+	 *
+	 * Note: Child classes must implement this method and are responsible
+	 * for preparing and properly sanitizing the component’s HTML output.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @return string The component HTML output.
+	 */
+	abstract public function get(): string;
+
+	/**
 	 * Render the component
 	 *
 	 * @since 4.0.0
@@ -123,7 +137,8 @@ abstract class BaseComponent implements ComponentInterface {
 	 * @return void
 	 */
 	public function render(): void {
-		echo $this->get(); // phpcs:ignore.
+		// phpcs:ignore -- Sanitization is performed within each child class’s `get` method implementation.
+		echo $this->get();
 	}
 
 }
