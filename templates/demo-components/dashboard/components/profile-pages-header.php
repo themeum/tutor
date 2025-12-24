8851da45597700138a9fb5f95aace916bbcd8068
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2025-12-24 12:17:27 +0600

    feat: Make profile header sticky, standardize dashboard navigation permalinks, and refactor the given reviews template.

--- templates/demo-components/dashboard/components/profile-pages-header.php
+++ templates/demo-components/dashboard/components/profile-pages-header.php
@@ -10,21 +10,23 @@
 
 use TUTOR\Icon;
 
-$page_title = $page_title ?? __( 'Profile', 'tutor' );
+$page_title    = $page_title ?? __( 'Profile', 'tutor' );
+$previous_url  = wp_get_referer();
+$dashboard_url = tutor_utils()->get_tutor_dashboard_page_permalink();
 
 ?>
 <div class="tutor-profile-header">
 	<div class="tutor-profile-container">
 		<div class="tutor-flex tutor-items-center tutor-justify-between">
-			<button class="tutor-btn tutor-btn-ghost tutor-btn-x-small tutor-btn-icon">
+			<a href="<?php echo esc_url( $previous_url ); ?>" class="tutor-btn tutor-btn-ghost tutor-btn-x-small tutor-btn-icon">
 				<?php tutor_utils()->render_svg_icon( Icon::LEFT ); ?>
-			</button>
+			</a>
 			<h4 class="tutor-profile-header-title">
 				<?php echo esc_html( $page_title ); ?>
 			</h4>
-			<button class="tutor-btn tutor-btn-ghost tutor-btn-x-small tutor-btn-icon">
+			<a href="<?php echo esc_url( $dashboard_url ); ?>" class="tutor-btn tutor-btn-ghost tutor-btn-x-small tutor-btn-icon">
 				<?php tutor_utils()->render_svg_icon( Icon::CROSS ); ?>
-			</button>
+			</a>
 		</div>
 	</div>
 </div>
