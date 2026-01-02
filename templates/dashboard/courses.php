177f09c8372cbdf0833e2fff327ae724cc074951
nur-alam <nuralam862@gmail.com>
2026-01-02 12:00:26 +0600

    Refactor student dashboard course handling and improve social profile method indentation

--- templates/dashboard/courses.php
+++ templates/dashboard/courses.php
@@ -17,11 +17,12 @@ use Tutor\Components\Pagination;
 use Tutor\Models\CourseModel;
 use Tutor\Components\EmptyState;
 use Tutor\Components\Nav;
+use TUTOR\Student;
 
 // Pagination.
-$per_page = tutor_utils()->get_option( 'pagination_per_page', 10 );
-$paged    = max( 1, Input::get( 'current_page', 1, Input::TYPE_INT ) );
-$offset   = ( $per_page * $paged ) - $per_page;
+$courses_per_page = tutor_utils()->get_option( 'pagination_per_page', 10 );
+$current_page     = max( 1, Input::get( 'current_page', 1, Input::TYPE_INT ) );
+$offset           = ( $courses_per_page * $current_page ) - $courses_per_page;
 
 $post_type_query = Input::get( 'type', '' );
 
@@ -44,9 +45,9 @@ if ( ! isset( $active_tab, $page_tabs[ $active_tab ] ) ) {
 
 // Get Paginated course list.
 $courses_list_array = array(
-	'courses'                   => CourseModel::get_enrolled_courses_by_user( get_current_user_id(), array( 'private', 'publish' ), $offset, $per_page ),
-	'courses/active-courses'    => CourseModel::get_active_courses_by_user( null, $offset, $per_page ),
-	'courses/completed-courses' => CourseModel::get_completed_courses_by_user( null, $offset, $per_page ),
+	'courses'                   => CourseModel::get_enrolled_courses_by_user( get_current_user_id(), array( 'private', 'publish' ), $offset, $courses_per_page ),
+	'courses/active-courses'    => CourseModel::get_active_courses_by_user( null, $offset, $courses_per_page ),
+	'courses/completed-courses' => CourseModel::get_completed_courses_by_user( null, $offset, $courses_per_page ),
 );
 
 // Get Full course list.
@@ -61,36 +62,7 @@ $enrolled_course_count  = $full_course_list_array['courses'] ? $full_course_list
 $active_course_count    = $full_course_list_array['courses/active-courses'] ? $full_course_list_array['courses/active-courses']->found_posts : 0;
 $completed_course_count = $full_course_list_array['courses/completed-courses'] ? $full_course_list_array['courses/completed-courses']->found_posts : 0;
 
-$courses_tab = array(
-	array(
-		'type'    => 'dropdown',
-		'icon'    => Icon::ENROLLED,
-		'active'  => ( ( 'courses' === $active_tab ) || ( 'courses/active-courses' === $active_tab ) || ( 'courses/completed-courses' === $active_tab ) ) ? true : false,
-		'options' => array(
-			array(
-				'label'  => __( 'Enrolled', 'tutor' ),
-				'icon'   => Icon::ENROLLED,
-				'url'    => esc_url( add_query_arg( $post_type_args, tutor_utils()->get_tutor_dashboard_page_permalink( 'courses' ) ) ),
-				'active' => 'courses' === $active_tab ? true : false,
-				'count'  => $enrolled_course_count,
-			),
-			array(
-				'label'  => __( 'Active', 'tutor' ),
-				'icon'   => Icon::PLAY_LINE,
-				'url'    => esc_url( add_query_arg( $post_type_args, tutor_utils()->get_tutor_dashboard_page_permalink( 'courses/active-courses' ) ) ),
-				'active' => 'courses/active-courses' === $active_tab ? true : false,
-				'count'  => $active_course_count,
-			),
-			array(
-				'label'  => __( 'Complete', 'tutor' ),
-				'icon'   => Icon::COMPLETED_CIRCLE,
-				'url'    => esc_url( add_query_arg( $post_type_args, tutor_utils()->get_tutor_dashboard_page_permalink( 'courses/completed-courses' ) ) ),
-				'active' => 'courses/completed-courses' === $active_tab ? true : false,
-				'count'  => $completed_course_count,
-			),
-		),
-	),
-);
+$courses_tab = ( new Student() )->get_courses_tab( $active_tab, $post_type_args, $enrolled_course_count, $active_course_count, $completed_course_count );
 
 // Prepare course list based on page tab.
 $courses_list           = $courses_list_array[ $active_tab ];
@@ -119,14 +91,13 @@ $paginated_courses_list = $full_course_list_array[ $active_tab ];
 			endif;
 			?>
 		</div>
-
-		<?php if ( $courses_list->found_posts ?? 0 > $per_page ) : ?>
+		<?php if ( ! empty( $courses_list->found_posts ) && $courses_list->found_posts > $courses_per_page ) : ?>
 		<div class="tutor-p-6 tutor-border-t">
 			<?php
 				Pagination::make()
-				->current( $paged )
+				->current( $current_page )
 				->total( $courses_list->found_posts )
-				->limit( $per_page )
+				->limit( $courses_per_page )
 				->render();
 			?>
 		</div>
