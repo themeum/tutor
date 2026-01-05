9f6c8b3f3e9f5f264cb33dcc0ecdfadff53a8912
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2026-01-05 13:13:36 +0600

    feat: conditionally display "Unsaved changes" badge based on Alpine.js form dirty state

--- templates/dashboard/settings/profile-header.php
+++ templates/dashboard/settings/profile-header.php
@@ -14,8 +14,12 @@ use TUTOR\Icon;
 <div class="tutor-profile-header">
 	<div class="tutor-dashboard-container tutor-flex tutor-items-center tutor-justify-between">
 		<div class="tutor-profile-header-left tutor-flex tutor-items-center"
-			x-data="{ windowWidth: window.innerWidth }"
+			x-data="{ 
+				windowWidth: window.innerWidth,
+				isDirty: {}
+			}"
 			@resize.window="windowWidth = window.innerWidth"
+			@tutor-form-state-change.document="if ($event.detail.id === activeTab + '-form') isDirty[$event.detail.id] = $event.detail.isDirty"
 		>
 			<button @click="window.history.back()" class="tutor-btn tutor-btn-ghost tutor-btn-x-small tutor-btn-icon">
 				<?php tutor_utils()->render_svg_icon( Icon::LEFT ); ?>
@@ -24,7 +28,13 @@ use TUTOR\Icon;
 				class="tutor-profile-header-title tutor-text-h4 tutor-font-semibold tutor-ml-4"
 				x-text="windowWidth <= 576 ? (activeTab === 'none' ? '<?php esc_html_e( 'Settings', 'tutor' ); ?>' : tabs.find(tab =>tab.id == activeTab).label) : '<?php esc_html_e( 'Settings', 'tutor' ); ?>'"
 			></h4>
-			<span class="tutor-badge tutor-badge-secondary tutor-badge-circle tutor-ml-5 tutor-sm-hidden">Unsaved changes</span>
+			<span 
+				class="tutor-badge tutor-badge-secondary tutor-badge-circle tutor-ml-5 tutor-sm-hidden"
+				x-show="activeTab !== 'none' && isDirty[activeTab + '-form']"
+				x-cloak
+			>
+				<?php esc_html_e( 'Unsaved changes', 'tutor' ); ?>
+			</span>
 		</div>
 		<div class="tutor-profile-header-right tutor-flex tutor-gap-4">
 			<div class="tutor-sm-hidden">
