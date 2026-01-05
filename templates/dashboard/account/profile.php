741d9a9e1069b2a9cf12e242a66425d164b0e297
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2026-01-05 11:28:57 +0600

    new: account sub pages added

---
+++ templates/dashboard/account/profile.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Profile Template for Account
+ *
+ * @package Tutor\Templates
+ * @subpackage Dashboard
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 4.0.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+
