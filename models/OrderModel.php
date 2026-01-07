027a9b893688934d36bb75817acdd99ba74f6f09
sanjana4khan <sanjanakhan772@gmail.com>
2026-01-07 16:50:55 +0600

    statements filter

--- models/OrderModel.php
+++ models/OrderModel.php
@@ -1902,17 +1902,20 @@ class OrderModel {
 	 * Retrieves statements for a specific user.
 	 *
 	 * @since 3.5.0
+	 * @since 4.0.0 Added $order_by and $order option.
 	 *
-	 * @param string $post_type_in_clause SQL clause to filter the course post types.
-	 * @param string $course_query SQL query string to further filter the courses .
-	 * @param string $date_query SQL query string to filter by date range.
-	 * @param int    $user_id The user ID for which the statements are being retrieved.
-	 * @param int    $offset The offset for pagination.
-	 * @param int    $limit The number of rows to return.
+	 * @param string $post_type_in_clause Prepared SQL IN clause containing allowed course post types.
+	 * @param string $course_query        Optional SQL fragment to filter by course ID.
+	 * @param string $date_query          Optional SQL fragment to filter by statement date.
+	 * @param int    $user_id             User (instructor) ID.
+	 * @param int    $offset              Number of records to skip (pagination offset).
+	 * @param int    $limit               Maximum number of records to return.
+	 * @param string $order_by            Column name to order results by.
+	 * @param string $order               Sort direction. Accepts 'ASC' or 'DESC'.
 	 *
 	 * @return array
 	 */
-	public function get_statements( $post_type_in_clause, $course_query, $date_query, $user_id, $offset, $limit ): array {
+	public function get_statements( $post_type_in_clause, $course_query, $date_query, $user_id, $offset, $limit, $order_by, $order ): array {
 		global $wpdb;
 
 		//phpcs:disable
@@ -1936,9 +1939,8 @@ class OrderModel {
 				WHERE statements.user_id = %d
 				{$course_query}
 				{$date_query}
-				ORDER BY statements.created_at DESC
-				LIMIT %d, %d
-			",
+				ORDER BY {$order_by} {$order}
+				LIMIT %d, %d",
 				$user_id,
 				$offset,
 				$limit
@@ -1953,8 +1955,7 @@ class OrderModel {
 					AND course.post_type IN ({$post_type_in_clause})
 				WHERE statements.user_id = %d
 				{$course_query}
-				{$date_query}
-			",
+				{$date_query}",
 				$user_id
 			)
 		);
