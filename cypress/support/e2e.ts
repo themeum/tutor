03e42af72389ed9884140c73ac472f3b6235480f
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2026-01-07 23:35:51 +0600

    feat: Set global Tutor text domain for Cypress and add eslint disable comments.

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,6 +1,10 @@
 import './commands';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(window as any).__TUTOR_TEXT_DOMAIN__ = 'tutor';
+
 Cypress.on('uncaught:exception', (err) => {
+  // eslint-disable-next-line no-console
   console.error(err);
   return false;
 });
