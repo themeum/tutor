f723969fcea8095387bda231308868f4bbe68244
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2026-01-09 12:13:43 +0600

    refactor: Simplify initials processing in Avatar component
    Refactored the initials() method to directly use the sanitized and uppercased input, removing logic that extracted initials from a display name. This change assumes the input is already the desired initials.

--- components/Avatar.php
+++ components/Avatar.php
@@ -157,21 +157,10 @@ class Avatar extends BaseComponent {
 	 *
 	 * @since 4.0.0
 	 *
-	 * @param string $initials User display name.
+	 * @param string $initials User initials.
 	 * @return $this
 	 */
 	public function initials( string $initials ): self {
-		$initials = implode(
-			'',
-			array_map(
-				function ( $word ) {
-					return strtoupper( substr( $word, 0, 1 ) );
-				},
-				explode( ' ', sanitize_text_field( $initials ) )
-			)
-		);
-
-		$this->initials = substr( $initials, 0, 2 );
 		$this->initials = strtoupper( sanitize_text_field( $initials ) );
 		return $this;
 	}
