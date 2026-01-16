567f5d9bd9bc744702d825063013112a77773ff2
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2026-01-16 10:39:47 +0600

    new: component helper and update

--- helpers/ComponentHelper.php
+++ helpers/ComponentHelper.php
@@ -11,6 +11,10 @@
 namespace Tutor\Helpers;
 
 use Tutor\Components\Badge;
+use Tutor\Components\Constants\Variant;
+use Tutor\Ecommerce\Ecommerce;
+use Tutor\Ecommerce\Settings;
+use Tutor\Models\OrderModel;
 
 /**
  * Class ComponentHelper
@@ -20,7 +24,7 @@ use Tutor\Components\Badge;
 class ComponentHelper {
 
 	/**
-	 * Order status badge
+	 * Render order status badge
 	 *
 	 * @since 4.0.0
 	 *
@@ -28,7 +32,7 @@ class ComponentHelper {
 	 *
 	 * @return void
 	 */
-	public static function order_status_badge( $status ) : void {
+	public static function render_order_status_badge( $status ) : void {
 		$badge_class = 'secondary';
 		switch ( $status ) {
 			case 'processing':
@@ -51,6 +55,57 @@ class ComponentHelper {
 		$label = tutor_utils()->translate_dynamic_text( $status );
 
 		Badge::make()->attr( 'class', 'tutor-badge-' . $badge_class )->circle()->label( $label )->render();
+	}
 
+	/**
+	 * Render payment status badge
+	 *
+	 * @since 4.0.0
+	 *
+	 * @param string $status order status.
+	 *
+	 * @return void
+	 */
+	public static function render_payment_status_badge( $status ) : void {
+		$badge_class = 'secondary';
+		switch ( $status ) {
+			case OrderModel::PAYMENT_REFUNDED:
+			case OrderModel::PAYMENT_PARTIALLY_REFUNDED:
+				$badge_class = 'cancelled';
+				break;
+			case OrderModel::PAYMENT_UNPAID:
+				$badge_class = 'pending';
+				break;
+			case OrderModel::PAYMENT_PAID:
+				$badge_class = 'completed';
+				break;
+			case OrderModel::PAYMENT_FAILED:
+				$badge_class = 'secondary';
+				break;
+
+		}
+
+		$label = tutor_utils()->translate_dynamic_text( $status );
+
+		Badge::make()->attr( 'class', 'tutor-badge-' . $badge_class )->circle()->label( $label )->render();
+	}
+
+	/**
+	 * Render payment gateway badge.
+	 *
+	 * @param string $payment_method payment method name.
+	 *
+	 * @return void
+	 */
+	public static function render_payment_method_badge( $payment_method ) {
+		$gateway_config = Settings::get_payment_gateway_settings( $payment_method ?? '' );
+		$icon_url       = $gateway_config['icon'] ?? '';
+		$label          = Ecommerce::get_payment_method_label( $payment_method ?? '' );
+
+		Badge::make()
+			->variant( Variant::SECONDARY )
+			->icon( $icon_url, 12, 12 )
+			->label( $label )
+			->render();
 	}
 }
