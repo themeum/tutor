dc6632992496a767ffde7389d8a9bf1af226fadb
sanjana4khan <sanjanakhan772@gmail.com>
2026-02-03 11:00:36 +0600

    statistics show/hide for user profile

--- templates/dashboard/account/profile.php
+++ templates/dashboard/account/profile.php
@@ -13,6 +13,8 @@ defined( 'ABSPATH' ) || exit;
 
 use TUTOR\User;
 
+$show_statistics = User::is_instructor() ? true : false;
+
 ?>
 
 <div class="tutor-profile-wrapper">
@@ -20,7 +22,7 @@ use TUTOR\User;
 
 	<div class="tutor-profile-container">
 		<div class="tutor-flex tutor-flex-column tutor-gap-5 tutor-mt-9">
-			<?php tutor_load_template( 'user-profile', array( 'show_statistics' => true ) ); ?>
+			<?php tutor_load_template( 'user-profile', array( 'show_statistics' => $show_statistics ) ); ?>
 		</div>
 	</div>
 </div>	
