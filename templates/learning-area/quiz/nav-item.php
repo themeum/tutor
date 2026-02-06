1a57ef4b0c837346f38601f8160f4f1ac3b6c89d
shewa12 <shewa12kpi@gmail.com>
2026-02-06 15:58:51 +0600

    Quiz content info added

--- templates/learning-area/quiz/nav-item.php
+++ templates/learning-area/quiz/nav-item.php
@@ -23,16 +23,22 @@ if ( ! $quiz && ! is_a( $quiz, 'WP_Post' ) ) {
 }
 
 $is_completed = tutor_utils()->is_completed_lesson( $quiz->ID );
-$lesson_title = $quiz->post_title;
+$quiz_title   = $quiz->post_title;
 
 $active_class = $tutor_current_content_id === $quiz->ID ? 'active' : '';
+$content_type = __( 'Quiz', 'tutor' );
+
+$quiz_title_html = '<div>
+	<div>' . esc_html( $quiz_title ) . '</div>
+	<div class="tutor-tiny tutor-text-subdued">' . esc_html( $content_type ) . '</div>
+</div>';
 ?>
 
 <div class="<?php echo esc_html( sprintf( 'tutor-learning-nav-item %s', $active_class ) ); ?>">
 	<?php if ( $is_completed ) : ?>
 	<a href="<?php echo esc_url( $can_access ? get_permalink( $quiz->ID ) : '#' ); ?>">
 		<?php tutor_utils()->render_svg_icon( Icon::COMPLETED_COLORIZE, 20, 20 ); ?>
-		<div><?php echo esc_html( $lesson_title ); ?></div>
+		<?php echo $quiz_title_html; //phpcs:ignore -- already-escaped ?>
 	</a>
 	<?php else : ?>
 	<a href="<?php echo esc_url( $can_access ? get_permalink( $quiz->ID ) : '#' ); ?>">
@@ -47,7 +53,7 @@ $active_class = $tutor_current_content_id === $quiz->ID ? 'active' : '';
 				<div x-html="render()"></div>
 			</div>
 		</div>
-		<div><?php echo esc_html( $lesson_title ); ?></div>
+		<?php echo $quiz_title_html; //phpcs:ignore -- already-escaped ?>
 	</a>
 	<?php endif; ?>
 </div>
