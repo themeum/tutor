ad69be9726832ed44c18ec4f73c0baf231247c93
shewa12 <shewa12kpi@gmail.com>
2026-02-06 14:05:53 +0600

    Lesson type info added on the learning-area sidebar

--- templates/learning-area/lesson/nav-item.php
+++ templates/learning-area/lesson/nav-item.php
@@ -24,13 +24,27 @@ $is_completed = tutor_utils()->is_completed_lesson( $lesson->ID );
 $lesson_title = $lesson->post_title;
 
 $active_class = $tutor_current_content_id === $lesson->ID ? 'active' : '';
+
+// Check if lesson has video.
+$video_info  = tutor_utils()->get_video_info( $lesson->ID );
+$lesson_type = __( 'Reading', 'tutor' );
+
+if ( $video_info && ! empty( $video_info->playtime ) ) {
+	$duration    = tutor_utils()->get_optimized_duration( $video_info->playtime );
+	$lesson_type = sprintf( __( 'Video - %s mins', 'tutor' ), $duration );
+}
+
+$lesson_title_html = '<div>
+	<div>' . esc_html( $lesson_title ) . '</div>
+	<div class="tutor-tiny tutor-text-subdued">' . esc_html( $lesson_type ) . '</div>
+</div>';
 ?>
 
 <div class="<?php echo esc_html( sprintf( 'tutor-learning-nav-item %s', $active_class ) ); ?>">
 	<?php if ( $is_completed ) : ?>
 	<a href="<?php echo esc_url( $can_access ? get_permalink( $lesson->ID ) : '#' ); ?>">
 		<?php tutor_utils()->render_svg_icon( Icon::COMPLETED_COLORIZE, 20, 20 ); ?>
-		<div><?php echo esc_html( $lesson_title ); ?></div>
+		<?php echo $lesson_title_html; // phpcs:ignore -- already escaped ?>
 	</a>
 	<?php else : ?>
 	<a href="<?php echo esc_url( $can_access ? get_permalink( $lesson->ID ) : '#' ); ?>">
@@ -45,7 +59,7 @@ $active_class = $tutor_current_content_id === $lesson->ID ? 'active' : '';
 				<div x-html="render()"></div>
 			</div>
 		</div>
-		<div><?php echo esc_html( $lesson_title ); ?></div>
+		<?php echo $lesson_title_html; // phpcs:ignore -- already escaped ?>
 	</a>
 	<?php endif; ?>
 </div>
