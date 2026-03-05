6a2ff38afcf61ccfd897f03238316f307615941f
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2026-03-05 15:22:08 +0600

    refactor(quiz-questions): remove attempt-review correctness logic

--- templates/learning-area/quiz/questions/true-false.php
+++ templates/learning-area/quiz/questions/true-false.php
@@ -11,9 +11,6 @@
 defined( 'ABSPATH' ) || exit;
 
 use TUTOR\Icon;
-use Tutor\Quiz;
-
-global $tutor_is_started_quiz;
 
 $field_name     = $question_field_name_base ?? '';
 $register_rules = '';
@@ -22,57 +19,17 @@ if ( $answer_is_required ) {
 }
 $register_attr = "register('{$field_name}'{$register_rules})";
 
-/*
- * Check if current answer is correct
- *
- * @param array $answer Answer data.
- *
- * @return string Correct, Incorrect or Empty string.
- */
-$is_correct = function ( $answer ) {
-	if ( ! array_key_exists( 'is_correct', $answer ) ) {
-		return '';
-	}
-
-	$value = $answer['is_correct'];
-
-	// values that should return an empty string.
-	$empty_values = array( null, '' );
-
-	if ( in_array( $value, $empty_values, true ) ) {
-		return '';
-	}
-
-	// map boolean values to their labels.
-	$map = array(
-		true  => 'correct',
-		false => 'incorrect',
-	);
-
-	return $map[ $value ] ?? '';
-};
-
-$show_correct_answers = Quiz::show_correct_answers( $tutor_is_started_quiz->attempt_status );
-
 ?>
 
 <div class="tutor-quiz-question-options">
 	<?php foreach ( $question['question_answers'] as $answer ) : ?>
-		<label 
-			class="tutor-quiz-question-option" 
-			<?php if ( $show_correct_answers ) : ?>
-				data-option="<?php echo esc_attr( $is_correct( $answer ) ); ?>"
-			<?php endif; ?>
-		>
+		<label class="tutor-quiz-question-option">
 			<input
 				class="tutor-hidden"
 				type="radio"
 				name="<?php echo esc_attr( $field_name ); ?>"
 				value="<?php echo esc_attr( $answer['answer_id'] ); ?>"
 				x-bind="<?php echo esc_attr( $register_attr ); ?>"
-				<?php if ( $show_correct_answers ) : ?>
-					checked
-				<?php endif; ?>
 			>
 			<?php tutor_utils()->render_svg_icon( $answer['is_correct'] ? Icon::CHECK_2 : Icon::CROSS, 20, 20 ); ?>
 			<?php echo esc_html( $answer['answer_title'] ); ?>
