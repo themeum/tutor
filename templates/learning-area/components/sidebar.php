cf036e3691962bbf76015f3b6e5372c76504dcec
Sazedul Haque <sazedulhaque5@gmail.com>
2026-03-06 15:45:28 +0600

    Leaning area sidebar topic style fixed

--- templates/learning-area/components/sidebar.php
+++ templates/learning-area/components/sidebar.php
@@ -10,6 +10,9 @@
 
 defined( 'ABSPATH' ) || exit;
 
+use Tutor\Components\Constants\Size;
+use Tutor\Components\Progress;
+use Tutor\Components\Tooltip;
 use TUTOR\Icon;
 use TUTOR\Input;
 use TUTOR\Template;
@@ -20,6 +23,8 @@ $tutor_course_id,
 $tutor_course_list_url,
 $tutor_current_content_id,
 $tutor_is_enrolled,
+$tutor_is_public_course,
+$tutor_is_course_instructor,
 $tutor_current_post;
 
 $is_preview       = get_post_meta( $tutor_current_post->ID, '_is_preview', true );
@@ -76,25 +81,39 @@ $active_menu = Input::get( 'subpage', '' );
 					);
 					?>
 
-					<div x-data="{ expanded: true }" class="tutor-learning-nav-topic active">
+					<div x-data="{ expanded: <?php echo esc_attr( $is_topic_active ? 'true' : 'false' ); ?> }" class="tutor-learning-nav-topic <?php echo esc_attr( $is_topic_active ? 'active' : '' ); ?>">
 						<div role="button" @click="expanded = !expanded" class="tutor-learning-nav-header">
 							<div class="tutor-learning-nav-header-progress">
-								<div x-data="tutorStatics({ 
-									value: <?php echo esc_attr( $total_contents['percentage'] ?? 0 ); ?>,
-									size: 'tiny',
-									type: 'progress',
-									showLabel: false,
-									background: 'var(--tutor-actions-gray-empty)',
-									strokeColor: 'var(--tutor-border-hover)' })">
-									<div x-html="render()"></div>
-								</div>
-								<!-- <div class="tutor-learning-nav-header-progress-inner"></div> -->
+								<?php
+								Progress::make()
+									->value( $total_contents['percentage'] ?? 0 )
+									->type( 'circle' )
+									->size( Size::X_SMALL )
+									->show_label( false )
+									->background( 'var(--tutor-actions-gray-empty)' )
+									->stroke_color( 'var(--tutor-border-hover)' )
+									->render();
+								?>
 							</div>
-							<div class="tutor-learning-nav-header-title">
+							<div class="tutor-learning-nav-header-title" title="<?php the_title(); ?>">
 								<?php the_title(); ?>
 							</div>
-							<div class="tutor-learning-nav-header-arrow" :class="{ 'is-expanded': expanded }">
-								<?php tutor_utils()->render_svg_icon( Icon::CHEVRON_UP_2, 20, 20 ); ?>
+							<div class="tutor-learning-nav-header-actions">
+								<?php if ( ! empty( $topic_summery ) ) : ?>
+								<div class="tutor-learning-nav-header-summary">
+									<?php
+									Tooltip::make()
+										->content( $topic_summery )
+										->trigger_element( tutor_utils()->get_svg_icon( Icon::INFO_OCTAGON, 16, 16, array( 'class' => 'tutor-icon-secondary' ) ) )
+										->size( Size::LARGE )
+										->arrow( Tooltip::ARROW_CENTER )
+										->render();
+									?>
+								</div>
+								<?php endif; ?>
+								<div class="tutor-learning-nav-header-arrow" :class="{ 'is-expanded': expanded }">
+									<?php tutor_utils()->render_svg_icon( Icon::CHEVRON_UP_2, 20, 20 ); ?>
+								</div>
 							</div>
 						</div>
 						<div x-show="expanded" x-collapse x-cloak class="tutor-learning-nav-body">
@@ -107,7 +126,7 @@ $active_menu = Input::get( 'subpage', '' );
 
 								$topic_item = get_post();
 
-								$can_access = ! $is_preview || $tutor_is_enrolled || get_post_meta( $post->ID, '_is_preview', true ) || $is_public_course || $is_instructor_of_this_course;
+								$can_access = ! $is_preview || $tutor_is_enrolled || get_post_meta( $topic_item->ID, '_is_preview', true ) || $tutor_is_public_course || $tutor_is_course_instructor;
 								$can_access = apply_filters( 'tutor_course/single/content/show_permalink', $can_access, get_the_ID() );
 								$can_access = null === $can_access ? true : $can_access;
 
@@ -126,7 +145,7 @@ $active_menu = Input::get( 'subpage', '' );
 			?>
 		</div>
 	</div>
-	<div class="tutor-learning-sidebar-pages">
+	<!-- <div class="tutor-learning-sidebar-pages">
 		<div class="tutor-learning-pages">
 			<?php
 			foreach ( $menu_items as $key => $item ) {
@@ -149,5 +168,5 @@ $active_menu = Input::get( 'subpage', '' );
 			}
 			?>
 		</div>
-	</div>
+	</div> -->
 </div>
