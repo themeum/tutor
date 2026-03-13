e35535fe5fb340475bec4c1ebe08a344ca6c3b16
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2026-03-13 12:39:23 +0600

    fix(modal): emit closed event

--- assets/core/ts/constant.ts
+++ assets/core/ts/constant.ts
@@ -3,6 +3,7 @@ export const TUTOR_CUSTOM_EVENTS = {
   MODAL_OPEN: 'tutor-modal-open',
   MODAL_UPDATE: 'tutor-modal-update',
   MODAL_CLOSE: 'tutor-modal-close',
+  MODAL_CLOSED: 'tutor-modal-closed',
   TOAST_SHOW: 'tutor-toast-show',
   TOAST_CLEAR: 'tutor-toast-clear',
   FORM_REGISTER: 'tutor-form-register',
