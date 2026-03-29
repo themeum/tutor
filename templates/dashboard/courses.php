890f2ee9bbf0b99d3d169427c0f422f0be62074f
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2026-03-29 13:10:47 +0600

    feat: update kids dashboard settings styles

--- templates/dashboard/courses.php
+++ templates/dashboard/courses.php
@@ -72,7 +72,7 @@ $courses_list = $courses_list_array[ $active_tab ];
 <div class="tutor-dashboard-courses-wrapper">
 
 	<!-- Courses nav  -->
-	<div class="tutor-surface-l1 tutor-border tutor-rounded-2xl">
+	<div class="tutor-dashboard-courses-card tutor-surface-l1 tutor-border tutor-rounded-2xl">
 		<div class="tutor-p-6 tutor-border-b">
 			<?php Nav::make()->items( $courses_tab )->render(); ?>
 		</div>
