38f78b5f58606eda9d4eeef7e85ad6b965624c16
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2026-04-06 15:24:42 +0600

    feat(quiz): add themed retry and abandon illustrations

--- templates/learning-area/quiz/attempt.php
+++ templates/learning-area/quiz/attempt.php
@@ -353,8 +353,9 @@ $default_values = array(
 	<?php
 		ConfirmationModal::make()
 			->id( $modal_id )
-			->title( __( 'Abandon Quiz?', 'tutor' ) )
-			->message( __( 'Do you want to abandon this quiz? The quiz will be submitted partially up to this question if you leave this page.', 'tutor' ) )
+			->icon( UrlHelper::themed_asset( 'images/illustrations/quiz-abandon.webp' ) )
+			->title( __( 'Leave this Quiz?', 'tutor' ) )
+			->message( __( 'If you leave now, your quiz will be submitted with the answers completed so far.', 'tutor' ) )
 			->confirm_text( __( 'Yes, Leave Quiz', 'tutor' ) )
 			->confirm_handler( "handleAbandonConfirm(); TutorCore.modal.closeModal('$modal_id')" )
 			->mutation_state( 'abandonQuizMutation' )
