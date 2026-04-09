c576eba831a5e784cb2ee484db3670f87e19dbc7
sanjana4khan <sanjanakhan772@gmail.com>
2026-04-09 15:08:58 +0600

    code refactor

--- ecommerce/CartController.php
+++ ecommerce/CartController.php
@@ -109,8 +109,10 @@ class CartController {
 		$selling_option = Course::get_selling_option( $course_id );
 		$btn_class      = apply_filters( 'tutor_enroll_required_login_class', ! is_user_logged_in() ? 'tutor-open-login-modal' : '' );
 
-		if ( tutor_utils()->is_addon_enabled( 'subscription' ) && Course::SELLING_OPTION_ONE_TIME !== $selling_option ) {
-			return $add_to_cart_btn;
+		if ( tutor_utils()->is_addon_enabled( 'subscription' )
+			&& Course::SELLING_OPTION_ONE_TIME !== $selling_option
+			&& in_array( $selling_option, Course::get_selling_options(), true ) ) {
+				return $add_to_cart_btn;
 		}
 
 		if ( Settings::is_buy_now_enabled() ) {
