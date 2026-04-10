fb724ab38fe2732eba0fe47d18fba9ab044b16c1
shewa <shewa12kpi@gmail.com>
2026-04-10 16:42:16 +0600

    Missing quiz status (pass/fail) icon indication added

--- templates/learning-area/quiz/nav-item.php
+++ templates/learning-area/quiz/nav-item.php
@@ -13,6 +13,7 @@ defined( 'ABSPATH' ) || exit;
 
 use TUTOR\Icon;
 use Tutor\Components\SvgIcon;
+use Tutor\Models\QuizModel;
 
 global $tutor_current_content_id;
 
@@ -23,8 +24,7 @@ if ( ! $quiz && ! is_a( $quiz, 'WP_Post' ) ) {
 	return;
 }
 
-$is_completed = tutor_utils()->is_completed_lesson( $quiz->ID );
-$quiz_title   = $quiz->post_title;
+$quiz_title = $quiz->post_title;
 
 $active_class   = $tutor_current_content_id === $quiz->ID ? 'active' : '';
 $disabled_class = $can_access ? '' : 'disabled';
@@ -32,8 +32,23 @@ $disabled_class = $can_access ? '' : 'disabled';
 $icon_name = Icon::QUIZ_2;
 if ( ! $can_access ) {
 	$icon_name = Icon::LOCK_STROKE_2;
-} elseif ( $is_completed ) {
-	$icon_name = Icon::COMPLETED_COLORIZE;
+} else {
+	$last_attempt  = ( new QuizModel() )->get_first_or_last_attempt( $quiz->ID );
+	$attempt_ended = is_object( $last_attempt ) && QuizModel::ATTEMPT_STARTED !== $last_attempt->attempt_status;
+	$result_class  = '';
+
+	$quiz_result = QuizModel::get_quiz_result( $quiz->ID );
+	if ( $attempt_ended && QuizModel::ATTEMPT_STARTED !== $last_attempt->attempt_status ) {
+		if ( 'fail' === $quiz_result ) {
+			$icon_name    = Icon::CROSS_CIRCLE_LINE;
+			$result_class = 'fail';
+		} elseif ( 'pending' === $quiz_result ) {
+			$icon_name    = Icon::INFO_2;
+			$result_class = 'pending';
+		} elseif ( 'pass' === $quiz_result ) {
+			$icon_name = Icon::COMPLETED_COLORIZE;
+		}
+	}
 }
 ?>
 
@@ -43,7 +58,7 @@ if ( ! $can_access ) {
 	class="<?php echo esc_html( sprintf( 'tutor-learning-nav-item %s %s', $active_class, $disabled_class ) ); ?>"
 	<?php echo ! $can_access ? 'aria-disabled="true"' : ''; ?>
 >
-	<?php SvgIcon::make()->name( $icon_name )->size( 20 )->render(); ?>
+	<?php SvgIcon::make()->name( $icon_name )->attr( 'class', $result_class )->size( 20 )->render(); ?>
 	<div class="tutor-overflow-hidden">
 		<div class="tutor-truncate"><?php echo esc_html( $quiz_title ); ?></div>
 		<div class="tutor-tiny-2 tutor-text-subdued"><?php esc_html_e( 'Quiz', 'tutor' ); ?></div>
