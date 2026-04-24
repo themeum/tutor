9d0c5990755c4b9beb61ebb746c6e656888c2c3f
shewa <shewa12kpi@gmail.com>
2026-04-24 17:25:55 +0600

    Update version on consent update

--- GDPR/Controllers/LegalConsent.php
+++ GDPR/Controllers/LegalConsent.php
@@ -350,7 +350,7 @@ class LegalConsent extends BaseController {
 
 		$data = $this->prepare_legal_consent_data( $request, false );
 		if ( is_wp_error( $data ) ) {
-			$this->response_bad_request( __( 'Validation error', 'tutor' ) , $data->errors, 400 );
+			$this->response_bad_request( __( 'Validation error', 'tutor' ), $data->errors, 400 );
 		}
 
 		if ( isset( $data['consent_map'] ) && ! tutor_is_json( $data['consent_map'] ) ) {
@@ -363,6 +363,7 @@ class LegalConsent extends BaseController {
 
 		$wpdb->query( 'START TRANSACTION' ); // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery
 
+		$data['version']        = (int) $existing->version + 1;
 		$data['updated_at_utc'] = current_time( 'mysql', true );
 		$updated                = $this->model->update( $id, $data );
 		if ( ! $updated ) {
