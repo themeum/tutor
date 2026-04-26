b66bb9bf8ff88cd09a9b319866d7d3f431cad8d0
arshidkv12 <arshidkv12@gmail.com>
2026-04-26 12:51:53 +0530

    Fix nullable Throwable parameter for PHP 8.4 compatibility

--- ecommerce/PaymentGateways/Paypal/src/Exceptions/FilesystemException.php
+++ ecommerce/PaymentGateways/Paypal/src/Exceptions/FilesystemException.php
@@ -7,7 +7,7 @@ use Throwable;
 
 class FilesystemException extends RuntimeException implements Throwable
 {
-	public function __construct($message = "", $code = 0, Throwable $previous = null)
+	public function __construct($message = "", $code = 0, ?Throwable $previous = null)
 	{
 		parent::__construct(
 			$message,
