2daab92975adac0709f64b31cb34caa9ff4362d7
Anindra Das Bivas <bivasdas911@gmail.com>
2026-05-08 13:04:14 +0600

    revert phpcs

--- classes/Course.php
+++ classes/Course.php
@@ -1832,7 +1832,7 @@ class Course extends Tutor_Base {
 
 		$i = 0;
 		foreach ( $sort_order as $topic ) {
-			++$i;
+			$i++;
 			$wpdb->update(
 				$wpdb->posts,
 				array( 'menu_order' => $i ),
@@ -2939,10 +2939,10 @@ class Course extends Tutor_Base {
 					}
 				}
 				if ( ! $has_passed ) {
-					++$required_assignment_pass;
+					$required_assignment_pass++;
 				}
 			} else {
-				++$required_assignment_pass;
+				$required_assignment_pass++;
 			}
 		}
 
@@ -2958,11 +2958,11 @@ class Course extends Tutor_Base {
 					$earned_percentage = QuizModel::calculate_attempt_earned_percentage( $attempt );
 
 					if ( $earned_percentage < $passing_grade ) {
-						++$required_quiz_pass;
+						$required_quiz_pass++;
 						$is_quiz_pass = false;
 					}
 				} else {
-					++$required_quiz_pass;
+					$required_quiz_pass++;
 					$is_quiz_pass = false;
 				}
 			}
