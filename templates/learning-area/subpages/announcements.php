2041f296910154b6ca4c15b9e03b63959d57efa0
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2026-05-11 20:59:33 +0600

    fix(learning-area): improve responsive content layouts

--- templates/learning-area/subpages/announcements.php
+++ templates/learning-area/subpages/announcements.php
@@ -57,7 +57,7 @@ $total_announcements = $the_query->found_posts;
 						<div class="tutor-p2 tutor-mb-7 tutor-whitespace-pre-line">
 							<?php echo wp_kses_post( $announcement->post_content ); ?>
 						</div>
-						<div class="tutor-flex tutor-items-center tutor-justify-between">
+						<div class="tutor-flex tutor-items-center tutor-justify-between tutor-gap-3">
 							<div class="tutor-flex tutor-items-center tutor-gap-3">
 								<?php
 								$author_id   = (int) $announcement->post_author;
@@ -66,10 +66,10 @@ $total_announcements = $the_query->found_posts;
 								<?php Avatar::make()->user( $author_id )->size( Size::SIZE_24 )->render(); ?>
 								<div class="tutor-small tutor-flex tutor-items-center tutor-gap-2">
 									<span class="tutor-text-subdued"><?php esc_html_e( 'By:', 'tutor' ); ?></span>
-									<span class="tutor-author-name"><?php echo esc_html( $author_name ); ?></span>
+									<span class="tutor-author-name tutor-line-clamp-1"><?php echo esc_html( $author_name ); ?></span>
 								</div>
 							</div>
-							<div class="tutor-tiny tutor-text-secondary tutor-flex tutor-items-center tutor-gap-3">
+							<div class="tutor-tiny tutor-text-secondary tutor-flex tutor-items-center tutor-gap-3 tutor-flex-shrink-0">
 								<?php SvgIcon::make()->name( Icon::ANNOUNCEMENT )->render(); ?>
 								<?php echo esc_html( tutor_i18n_get_formated_date( $announcement->post_date ) ); ?>
 							</div>
