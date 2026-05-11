0880df6dca9594a3d084a0fb9fa8d8df8c286ffb
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2026-05-11 17:59:28 +0600

    feat: Show content title on small screen device

--- templates/learning-area/components/header.php
+++ templates/learning-area/components/header.php
@@ -20,12 +20,17 @@ use TUTOR\Course;
 global $tutor_course_id,
 $tutor_course,
 $current_user_id,
+$tutor_current_post,
 $tutor_course_progress,
 $tutor_can_complete_course,
 $tutor_can_retake_course,
 $course_complete_modal_id,
 $course_retake_modal_id;
+
+$course_title  = $tutor_course->post_title;
+$content_title = $tutor_current_post->post_title ?? '';
 ?>
+
 <div class="tutor-learning-header">
 	<div class="tutor-learning-header-inner">
 		<div class="tutor-learning-header-content">
@@ -33,18 +38,22 @@ $course_retake_modal_id;
 				<?php SvgIcon::make()->name( Icon::LEFT )->size( 20 )->render(); ?>
 			</a>
 			<h5 class="tutor-learning-header-title tutor-my-none">
-				<?php echo esc_html( $tutor_course->post_title ); ?>
+				<span class="tutor-md-hidden">
+					<?php echo esc_html( $course_title ); ?>
+				</span>
+				<span class="tutor-hidden tutor-md-inline">
+					<?php echo esc_html( $content_title ? $content_title : $course_title ); ?>
+				</span>
 			</h5>
 
 			<div class="tutor-flex tutor-gap-2 tutor-items-center tutor-ml-auto tutor-pr-4 tutor-whitespace-nowrap tutor-md-hidden">
-				<?php
-				if ( $tutor_can_complete_course ) {
-					Course::render_course_complete_btn( $course_complete_modal_id, $tutor_course_id, $tutor_course_progress, Size::SMALL );
-				}
-				if ( $tutor_can_retake_course ) {
-					Course::render_course_retake_btn( $course_retake_modal_id, Size::SMALL );
-				}
-				?>
+				<?php if ( $tutor_can_complete_course ) : ?>
+					<?php Course::render_course_complete_btn( $course_complete_modal_id, $tutor_course_id, $tutor_course_progress, Size::SMALL ); ?>
+				<?php endif; ?>
+
+				<?php if ( $tutor_can_retake_course ) : ?>
+					<?php Course::render_course_retake_btn( $course_retake_modal_id, Size::SMALL ); ?>
+				<?php endif; ?>
 			</div>
 		</div>
 		<button class="tutor-learning-header-toggle-mobile" @click.stop="sidebarOpen = !sidebarOpen">
