c8445a2c2cb8882320b13388debde60f45434edc
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2026-05-12 12:40:04 +0600

    refactor: sanitize order filter input

--- templates/dashboard/account/billing.php
+++ templates/dashboard/account/billing.php
@@ -14,13 +14,14 @@ defined( 'ABSPATH' ) || exit;
 use Tutor\Components\Constants\Size;
 use Tutor\Components\Nav;
 use TUTOR\Dashboard;
+use Tutor\Helpers\QueryHelper;
 use TUTOR\Icon;
 use TUTOR\Input;
 
 $user_id       = get_current_user_id();
 $current_tab   = Input::get( 'tab', 'order-history' );
 $replies       = Input::get( 'replies', 0, Input::TYPE_INT );
-$order_filter  = Input::get( 'order', 'DESC' );
+$order_filter  = QueryHelper::get_valid_sort_order( Input::get( 'order', 'DESC' ) );
 $current_page  = max( 1, Input::get( 'current_page', 1, Input::TYPE_INT ) );
 $item_per_page = tutor_utils()->get_option( 'pagination_per_page', 10 );
 $offset        = ( $current_page - 1 ) * $item_per_page;
