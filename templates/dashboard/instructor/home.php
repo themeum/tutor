9ef0fb37011643f2062de24f0fb78f040e000d90
sanjana4khan <sanjanakhan772@gmail.com>
2026-05-22 17:17:27 +0600

    code simplify

--- templates/dashboard/instructor/home.php
+++ templates/dashboard/instructor/home.php
@@ -159,22 +159,33 @@ $stat_cards = array(
  * -------------------------
  */
 if ( $is_pro_reports ) {
-	$labels            = wp_list_pluck( $earnings['earnings'], 'label_name' );
-	$graph_earnings    = array_map( 'intval', wp_list_pluck( $earnings['earnings'], 'total' ) );
-	$enrollments       = Analytics::get_total_students_by_user( $user->ID, '', $start_date, $end_date );
-	$graph_enrollments = array_map( 'intval', wp_list_pluck( $enrollments['enrollments'], 'total' ) );
-	$enrolled_dates    = array_map( fn( $item ) => wp_date( 'M d', strtotime( $item->date_format ) ), $enrollments['enrollments'] );
-	$earnings_dates    = array_map( fn( $item ) => wp_date( 'M d', strtotime( $item->date_format ) ), $earnings['earnings'] );
-
+	$enrollments = Analytics::get_total_students_by_user( $user->ID, '', $start_date, $end_date );
 
 	$overview_chart_data = array(
-		'earnings'      => array_merge( array( 0 ), $graph_earnings, array( 0 ) ),
-		'enrolled'      => array_merge( array( 0 ), $graph_enrollments, array( 0 ) ),
-		'labels'        => array_merge( array( '' ), $labels, array( '' ) ),
+		'earnings'      => array( 0 ),
+		'enrolled'      => array( 0 ),
+		'labels'        => array( '' ),
 		'currency'      => tutor_utils()->get_monetization_currency_config(),
-		'enrolled_date' => $enrolled_dates,
-		'earning_date'  => $earnings_dates,
+		'enrolled_date' => array(),
+		'earning_date'  => array(),
 	);
+
+	foreach ( $earnings['earnings'] as $item ) {
+		$overview_chart_data['earnings'][]     = (float) ( $item->total ?? 0 );
+		$overview_chart_data['labels'][]       = $item->label_name ?? '';
+		$overview_chart_data['earning_date'][] = ! empty( $item->date_format )
+													? wp_date( 'M d', strtotime( $item->date_format ) ) : '';
+	}
+
+	foreach ( $enrollments['enrollments'] as $item ) {
+		$overview_chart_data['enrolled'][]      = (float) ( $item->total ?? 0 );
+		$overview_chart_data['enrolled_date'][] = ! empty( $item->date_format )
+													? wp_date( 'M d', strtotime( $item->date_format ) ) : '';
+	}
+
+	$overview_chart_data['earnings'][] = 0;
+	$overview_chart_data['enrolled'][] = 0;
+	$overview_chart_data['labels'][]   = '';
 }
 
 /**
