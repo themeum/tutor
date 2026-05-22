112f62e14c6a78f8a76202ef2343c1de9652013e
Anindra Das Bivas <bivasdas911@gmail.com>
2026-05-22 12:23:53 +0600

    updated attachment view mode check logic

--- templates/learning-area/lesson/exercise-files.php
+++ templates/learning-area/lesson/exercise-files.php
@@ -16,7 +16,7 @@ use Tutor\Components\SvgIcon;
 global $tutor_current_content_id;
 
 $attachments    = tutor_utils()->get_attachments( $tutor_current_content_id );
-$open_mode_view = apply_filters( 'tutor_pro_attachment_open_mode', null ) == 'view' ? ' target="_blank" ' : null;
+$open_mode_view = 'view' === apply_filters( 'tutor_pro_attachment_open_mode', null );
 
 ?>
 
@@ -39,7 +39,7 @@ $open_mode_view = apply_filters( 'tutor_pro_attachment_open_mode', null ) == 'vi
 							</span>
 						</div>
 						<div class="tutor-attachment-card-actions">
-							<a href="<?php echo esc_url( $attachment->url ); ?>" class="tutor-btn tutor-btn-ghost tutor-btn-x-small tutor-btn-icon" rel="noopener" <?php echo $open_mode_view ? esc_attr( $open_mode_view ) : esc_attr( 'download' ); ?>>
+							<a href="<?php echo esc_url( $attachment->url ); ?>" class="tutor-btn tutor-btn-ghost tutor-btn-x-small tutor-btn-icon" rel="noopener" <?php echo esc_attr( $open_mode_view ? $open_mode_view : 'download' ); ?>>
 								<?php SvgIcon::make()->name( $open_mode_view ? Icon::LINK_EXTERNAL : Icon::DOWNLOAD_2 )->size( 20 )->render(); ?>
 							</a>
 						</div>
