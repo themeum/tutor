7aba2ef40db13f8fcb48f8d1bbae977eaf4b81fc
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2026-06-25 13:25:55 +0600

    fix: security issue

--- classes/Utils.php
+++ classes/Utils.php
@@ -8069,11 +8069,7 @@ class Utils {
 	public function has_enrolled_content_access( $content, $object_id = 0, $user_id = 0 ) {
 		$user_id   = $this->get_user_id( $user_id );
 		$object_id = $this->get_post_id( $object_id );
-
-		$course_id = Input::get( 'course', 0, Input::TYPE_INT );
-		if ( ! $course_id ) {
-			$course_id = $this->get_course_id_by( $content, $object_id );
-		}
+		$course_id = $this->get_course_id_by( $content, $object_id );
 
 		do_action( 'tutor_before_enrolment_check', $course_id, $user_id );
 
