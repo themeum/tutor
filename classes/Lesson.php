a2c2c917257fb528d83a931b11fb39d6b73b4d56
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2026-07-01 15:45:45 +0600

    refactor: replace direct enrollment check with has_enrolled_content_access in lesson comment handler

--- classes/Lesson.php
+++ classes/Lesson.php
@@ -148,9 +148,9 @@ class Lesson extends Tutor_Base {
 		}
 
 		if ( 'tutor_create_lesson_comment' === Input::post( 'action' ) && strlen( $comment ) > 0 ) {
-			$course_id = tutor_utils()->get_course_id_by( 'lesson', $lesson_id );
-			if ( ! tutor_utils()->is_enrolled( $course_id ) ) {
-				wp_send_json_error( __( 'You must be enrolled to comment', 'tutor' ) );
+			if ( ! tutor_utils()->has_enrolled_content_access( 'lesson', $lesson_id ) ) {
+				wp_send_json_error( tutor_utils()->error_message() );
+				return;
 			}
 
 			$comment_data = array(
@@ -729,9 +729,8 @@ class Lesson extends Tutor_Base {
 		}
 
 		$lesson_id = Input::post( 'comment_post_ID', 0, Input::TYPE_INT );
-		$course_id = tutor_utils()->get_course_id_by( 'lesson', $lesson_id );
-		if ( ! tutor_utils()->is_enrolled( $course_id ) ) {
-			wp_send_json_error( __( 'You must be enrolled to comment', 'tutor' ) );
+		if ( ! tutor_utils()->has_enrolled_content_access( 'lesson', $lesson_id ) ) {
+			wp_send_json_error( tutor_utils()->error_message() );
 			return;
 		}
 
