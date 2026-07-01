9a920bc243ecb1340a21430f74293aa50436cc65
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2026-07-01 12:00:46 +0600

    refactor: Use external assets

--- templates/shared/tour.php
+++ templates/shared/tour.php
@@ -12,33 +12,34 @@
 defined( 'ABSPATH' ) || exit;
 
 use Tutor\Components\Modal;
-use Tutor\Helpers\UrlHelper;
+
+$tour_asset_base = 'https://tutor-lms.s3.us-east-1.amazonaws.com/tour';
 
 $slides_data = array(
 	array(
 		'title'      => __( 'Your learning experience just got a whole lot better', 'tutor' ),
-		'imageLarge' => UrlHelper::asset( 'images/tour/tour-1.webp' ),
-		'imageSmall' => UrlHelper::asset( 'images/tour/tour-1-sm.webp' ),
+		'imageLarge' => $tour_asset_base . '/tour-1.webp',
+		'imageSmall' => $tour_asset_base . '/tour-1-sm.webp',
 	),
 	array(
 		'title'      => __( 'Find quiz attempts inside your courses', 'tutor' ),
-		'imageLarge' => UrlHelper::asset( 'images/tour/tour-2.webp' ),
-		'imageSmall' => UrlHelper::asset( 'images/tour/tour-2-sm.webp' ),
+		'imageLarge' => $tour_asset_base . '/tour-2.webp',
+		'imageSmall' => $tour_asset_base . '/tour-2-sm.webp',
 	),
 	array(
 		'title'      => __( 'Access all your lesson & video notes in one place', 'tutor' ),
-		'imageLarge' => UrlHelper::asset( 'images/tour/tour-3.webp' ),
-		'imageSmall' => UrlHelper::asset( 'images/tour/tour-3-sm.webp' ),
+		'imageLarge' => $tour_asset_base . '/tour-3.webp',
+		'imageSmall' => $tour_asset_base . '/tour-3-sm.webp',
 	),
 	array(
 		'title'      => __( 'Everything about your account now lives in one place', 'tutor' ),
-		'imageLarge' => UrlHelper::asset( 'images/tour/tour-4.webp' ),
-		'imageSmall' => UrlHelper::asset( 'images/tour/tour-4-sm.webp' ),
+		'imageLarge' => $tour_asset_base . '/tour-4.webp',
+		'imageSmall' => $tour_asset_base . '/tour-4-sm.webp',
 	),
 	array(
 		'title'      => __( 'Switch between dark and light mode whenever you like', 'tutor' ),
-		'imageLarge' => UrlHelper::asset( 'images/tour/tour-5.webp' ),
-		'imageSmall' => UrlHelper::asset( 'images/tour/tour-5-sm.webp' ),
+		'imageLarge' => $tour_asset_base . '/tour-5.webp',
+		'imageSmall' => $tour_asset_base . '/tour-5-sm.webp',
 	),
 );
 
@@ -49,10 +50,10 @@ $slides_json           = wp_json_encode( $slides_data );
 ?>
 
 <div
-	x-data="tutorTour({ 
-		slidesData: <?php echo esc_attr( $slides_json ); ?>, 
-		modalId: '<?php echo esc_js( $tour_modal_id ); ?>',
-		userId: <?php echo esc_attr( $tour_user_id ); ?> 
+	x-data="tutorTour({
+		slidesData: <?php echo esc_attr( $slides_json ); ?>,
+		modalId: '<?php echo esc_attr( $tour_modal_id ); ?>',
+		userId: <?php echo absint( $tour_user_id ); ?>
 	})"
 >
 	<?php
