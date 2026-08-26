9b52033363b260e5195d6455d0601cb1ddeb25f0
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2026-08-26 12:21:56 +0600

    style(tokens): Update color tokens

--- classes/Assets.php
+++ classes/Assets.php
@@ -519,8 +519,8 @@ class Assets {
 		$admin_colors = array();
 		if ( is_admin() ) {
 			$admin_colors = array(
-				'--tutor-color-primary'       => '#3E64DE',
-				'--tutor-color-primary-hover' => '#3A5CCC',
+				'--tutor-color-primary'       => '#3e64de',
+				'--tutor-color-primary-hover' => '#2b49ca',
 				'--tutor-body-color'          => '#212327',
 				'--tutor-border-color'        => '#CDCFD5',
 				'--tutor-color-gray'          => '#CDCFD5',
@@ -528,8 +528,8 @@ class Assets {
 		}
 
 		$fallback_colors = array(
-			'tutor_primary_color'       => '#3E64DE',
-			'tutor_primary_hover_color' => '#3A5CCC',
+			'tutor_primary_color'       => '#3e64de',
+			'tutor_primary_hover_color' => '#2b49ca',
 			'tutor_text_color'          => '#212327',
 			'tutor_border_color'        => '#E3E5EB',
 			'tutor_gray_color'          => '#CDCFD5',
