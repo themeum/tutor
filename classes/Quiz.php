aaec4497824d1d7a027c87895b3ef0ad4ce9182f
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2026-08-31 16:09:02 +0600

    chore: Use tab instead of space

--- classes/Quiz.php
+++ classes/Quiz.php
@@ -1049,38 +1049,38 @@ class Quiz {
 		wp_redirect( tutor_utils()->input_old( '_wp_http_referer' ) );
 	}
 
-    /**
-     * Get total marks for a quiz
-     *
-     * @since 3.0.0
-     *
-     * @param int $quiz_id quiz id.
-     *
-     * @return int|float
-     */
-    public static function get_quiz_total_marks( $quiz_id ) {
-        global $wpdb;
+	/**
+	 * Get total marks for a quiz
+	 *
+	 * @since 3.0.0
+	 *
+	 * @param int $quiz_id quiz id.
+	 *
+	 * @return int|float
+	 */
+	public static function get_quiz_total_marks( $quiz_id ) {
+		global $wpdb;
 
-        if ( ! $quiz_id ) {
-            return 0;
-        }
+		if ( ! $quiz_id ) {
+			return 0;
+		}
 
-        $max_questions   = (int) tutor_utils()->get_quiz_option( $quiz_id, 'max_questions_for_answer' );
-        $questions_order = tutor_utils()->get_quiz_option( $quiz_id, 'questions_order', 'rand' );
+		$max_questions   = (int) tutor_utils()->get_quiz_option( $quiz_id, 'max_questions_for_answer' );
+		$questions_order = tutor_utils()->get_quiz_option( $quiz_id, 'questions_order', 'rand' );
 
-        $order_by_map = array(
-                'asc'     => 'question_id ASC',
-                'desc'    => 'question_id DESC',
-                'sorting' => 'question_order ASC',
-        );
-        $order_by     = $order_by_map[ $questions_order ] ?? 'question_order ASC';
+		$order_by_map = array(
+			'asc'     => 'question_id ASC',
+			'desc'    => 'question_id DESC',
+			'sorting' => 'question_order ASC',
+		);
+		$order_by     = $order_by_map[ $questions_order ] ?? 'question_order ASC';
 
-        $limit = $max_questions > 0 ? $max_questions : PHP_INT_MAX;
+		$limit = $max_questions > 0 ? $max_questions : PHP_INT_MAX;
 
-        // phpcs:disable
-        $total_marks = $wpdb->get_var(
-                $wpdb->prepare(
-                        "SELECT CASE
+		// phpcs:disable
+		$total_marks = $wpdb->get_var(
+			$wpdb->prepare(
+				"SELECT CASE
         WHEN %s = 'rand' AND %d > 0 AND COUNT(question_id) > %d THEN 0
         ELSE (
           SELECT SUM(questions.question_mark) FROM (
@@ -1094,18 +1094,18 @@ class Quiz {
 				END
 				FROM {$wpdb->prefix}tutor_quiz_questions
 				WHERE quiz_id = %d",
-                        $questions_order,
-                        $max_questions,
-                        $max_questions,
-                        $quiz_id,
-                        $limit,
-                        $quiz_id
-                )
-        );
-        // phpcs:enable
+				$questions_order,
+				$max_questions,
+				$max_questions,
+				$quiz_id,
+				$limit,
+				$quiz_id
+			)
+		);
+		// phpcs:enable
 
-        return (float) $total_marks;
-    }
+		return (float) $total_marks;
+	}
 
 	/**
 	 * Quiz timeout by ajax
