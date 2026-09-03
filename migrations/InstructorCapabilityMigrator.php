0572ef03a8c1dc1b13473c73d602671b4dbb2d72
Anindra Das Bivas <bivasdas911@gmail.com>
2026-09-03 17:58:53 +0600

    added instructor status approval check

--- migrations/InstructorCapabilityMigrator.php
+++ migrations/InstructorCapabilityMigrator.php
@@ -74,8 +74,10 @@ class InstructorCapabilityMigrator extends BatchProcessor implements SingleProce
 		$this->user_model = new UserModel();
 		$users            = $this->user_model->get_users_list(
 			array(
-				'role'   => tutor()->instructor_role,
-				'fields' => 'ID',
+				'role'       => tutor()->instructor_role,
+				'meta_key'   => '_tutor_instructor_status',
+				'meta_value' => 'approved',
+				'fields'     => 'ID',
 			)
 		);
 		return $users->get_total();
@@ -95,9 +97,11 @@ class InstructorCapabilityMigrator extends BatchProcessor implements SingleProce
 		$this->user_model = new UserModel();
 		$users            = $this->user_model->get_users_list(
 			array(
-				'role'   => tutor()->instructor_role,
-				'number' => $limit,
-				'offset' => $offset,
+				'role'       => tutor()->instructor_role,
+				'meta_key'   => '_tutor_instructor_status',
+				'meta_value' => 'approved',
+				'number'     => $limit,
+				'offset'     => $offset,
 			)
 		);
 		return $users->get_results();
